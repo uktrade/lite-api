0245e1fdb4356d87e1176879fba24ec2bc181ef3
Jan Faracik <j.faracik@kainos.com>
2019-02-27 16:51:51 +0000

    Fixed model issues

--- lite_application_service/drafts/models.py
+++ lite_application_service/drafts/models.py
@@ -4,12 +4,14 @@ import uuid
 
 
 class Draft(models.Model):
-    db_table = 'draft'
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    exporter_id = models.TextField(default='')
+    user_id = models.TextField(default='')
     control_code = models.CharField(max_length=30, default='')
     activity = models.TextField(default='')
     destination = models.TextField(default='')
     usage = models.TextField(default='')
     created_at = models.DateTimeField(default=datetime.now, blank=True)
     last_modified_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        db_table = "draft"
