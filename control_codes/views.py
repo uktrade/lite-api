f9cba6ab136e73010410e183e22b2dfcc6615d9d
Jan Faracik <j.faracik@kainos.com>
2019-03-08 14:06:44 +0000

    Fixed control codes endpoint

--- control_codes/views.py
+++ control_codes/views.py
@@ -1,22 +1,19 @@
 from django.http import JsonResponse
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from control_codes.models import ControlCode
 from control_codes.serializers import ControlCodeSerializer
 
 
-@api_view(['GET'])
 def control_codes_list(request):
     if request.method == 'GET':
         snippets = ControlCode.objects.all()
         serializer = ControlCodeSerializer(snippets, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
 
 
-@api_view(['GET'])
 def control_code_detail(request, id):
     if request.method == 'GET':
         snippets = ControlCode.objects.all()
         serializer = ControlCodeSerializer(snippets, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
