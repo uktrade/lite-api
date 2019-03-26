541a46398e854f24bf320e824c6dd7c6afe97d73
Jan Faracik <j.faracik@kainos.com>
2019-03-26 14:16:22 +0000

    Added serializer for creating an org

--- organisations/serializers.py
+++ organisations/serializers.py
@@ -2,13 +2,28 @@ from rest_framework import serializers
 from organisations.models import Organisation
 
 
-class OrganisationSerializer(serializers.ModelSerializer):
+class OrganisationCreateSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
     eori_number = serializers.CharField()
     sic_number = serializers.CharField()
     vat_number = serializers.CharField()
     address = serializers.CharField()
+    admin_user_email = serializers.CharField()
 
+    class Meta:
+        model = Organisation
+        fields = ('id',
+                  'name',
+                  'eori_number',
+                  'sic_number',
+                  'vat_number',
+                  'admin_user_email',
+                  'address',
+                  'created_at',
+                  'last_modified_at')
+
+
+class OrganisationViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organisation
         fields = ('id',
--- organisations/views.py
+++ organisations/views.py
@@ -3,33 +3,22 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 
 from organisations.models import Organisation
-from organisations.serializers import OrganisationSerializer
+from organisations.serializers import OrganisationCreateSerializer, OrganisationViewSerializer
 from users.libraries.CreateFirstAdminUser import CreateFirstAdminUser
 
 
 def organisations_list(request):
     if request.method == "POST":
         data = JSONParser().parse(request)
-        serializer = OrganisationSerializer(data=data)
-        admin_user_email = request.POST.get('admin_user_email', 'placeholder')
-
-        if not admin_user_email:
-            return JsonResponse(data={'status': 'error', 'errors': {'admin_user_email': 'This field cannot be empty.'}},
-                                status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        serializer = OrganisationCreateSerializer(data=data)
 
         if serializer.is_valid():
-            new_organisation = Organisation(name=serializer['name'].value,
-                                            eori_number=serializer['eori_number'].value,
-                                            sic_number=serializer['sic_number'].value,
-                                            vat_number=serializer['vat_number'].value,
-                                            address=serializer['address'].value)
-            new_organisation.save()
+            new_organisation = serializer.save()
 
             # Create an admin for that company
-            CreateFirstAdminUser(admin_user_email, new_organisation)
-            organisation_serializer = OrganisationSerializer(new_organisation)
+            CreateFirstAdminUser(serializer['admin_user_email'].value, new_organisation)
 
-            return JsonResponse(organisation_serializer.data,
+            return JsonResponse(OrganisationViewSerializer(data=data).data,
                                 status=status.HTTP_201_CREATED)
         else:
             return JsonResponse(data={'status': 'error', 'errors': serializer.errors},
@@ -37,6 +26,6 @@ def organisations_list(request):
 
     if request.method == "GET":
         organisations = Organisation.objects.all()
-        serializer = OrganisationSerializer(organisations, many=True)
+        serializer = OrganisationViewSerializer(organisations, many=True)
         return JsonResponse(data={'status': 'success', 'drafts': serializer.data},
                             safe=False)
