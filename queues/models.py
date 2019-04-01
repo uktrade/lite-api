48e8f7303956273ee3895e99af1b00e12738c0e1
Jan Faracik <j.faracik@kainos.com>
2019-04-01 17:19:15 +0100

    Removed unused migrations, renamed applications to cases, added initial queue

--- queues/models.py
+++ queues/models.py
@@ -2,10 +2,10 @@ import uuid
 
 from django.db import models
 
-from applications.models import Application
+from cases.models import Case
 
 
 class Queue(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField(default='Untitled Queue')
-    applications = models.ManyToManyField(Application)
+    cases = models.ManyToManyField(Case)
