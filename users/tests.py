3bbb12093fac9311cbcc86c5f3fc8181d4e1c41b
Jan Faracik <j.faracik@kainos.com>
2019-04-12 15:51:41 +0100

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,7 @@
 import json
 
-from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APITestCase, URLPatternsTestCase, APIClient
+from rest_framework.test import APITestCase, URLPatternsTestCase, APIClient, force_authenticate
 
 from organisations.models import Organisation
 from users.models import User
@@ -12,16 +11,17 @@ from django.urls import path, include
 class TestHelper:
     @staticmethod
     def create_user_and_organisation():
-        new_organisation = Organisation(name="Big Scary Guns ltd",
-                                        eori_number="GB123456789000",
-                                        sic_number="2765",
-                                        vat_number="123456789",
-                                        registration_number="987654321",
-                                        address="London")
+        new_organisation = Organisation(name='Banana Stand ltd',
+                                        eori_number='GB123456789000',
+                                        sic_number='2765',
+                                        vat_number='123456789',
+                                        registration_number='987654321',
+                                        address='London')
 
-        new_user = User(email="trinity@bsg.com",
-                        password="trinity@bsg.com",
+        new_user = User(email='trinity@bsg.com',
+                        username='trinity@bsg.com',
                         organisation=new_organisation)
+        new_user.set_password('password')
 
         new_organisation.save()
         new_user.save()
@@ -42,21 +42,14 @@ class UserTests(APITestCase, URLPatternsTestCase):
 
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.get().email, 'trinity@bsg.com')
-        self.assertEqual(User.objects.get().password, 'trinity@bsg.com')
         self.assertEqual(User.objects.get().organisation, new_organisation)
 
-    def test_super_simple_authentication_success(self):
+    def test_get_signed_in_user(self):
         TestHelper.create_user_and_organisation()
 
-        url = '/users/login/'
-        data = {'email': 'trinity@bsg.com'}
-        response = self.client.get(url, data, format=json)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        url = '/users/me/'
 
-    def test_super_simple_authentication_failure(self):
-        url = '/users/login/'
-        data = {'email': 'emily@bsg.com'}
-        response = self.client.get(url, data, format=json)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        response_data = json.loads(response.content)["errors"]
-        self.assertEqual(response_data, "Can't find user")
+        user = User.objects.get(username='trinity@bsg.com')
+        self.client.force_authenticate(user=user, token=user.auth_token)
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
