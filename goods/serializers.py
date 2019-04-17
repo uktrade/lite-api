72c0cc5e484e022c513de26249aa4a50a4714bf5
Jan Faracik <j.faracik@kainos.com>
2019-04-17 11:41:00 +0100

    Conditional validation

--- goods/serializers.py
+++ goods/serializers.py
@@ -1,3 +1,5 @@
+from typing import Optional, Any
+
 from rest_framework import serializers
 
 from goods.models import Good
@@ -6,6 +8,7 @@ from goods.models import Good
 class GoodSerializer(serializers.ModelSerializer):
     description = serializers.CharField(max_length=280)
     is_good_controlled = serializers.BooleanField()
+    control_code = serializers.CharField(required=False)
     is_good_end_product = serializers.BooleanField()
 
     class Meta:
@@ -17,3 +20,8 @@ class GoodSerializer(serializers.ModelSerializer):
                   'is_good_end_product',
                   'part_number',
                   )
+
+    def __init__(self, instance: Optional[Any] = ..., data: Any = ..., **kwargs: Any):
+        super().__init__(instance, data, **kwargs)
+        if self.fields['is_good_controlled'] is True:
+            self.fields['control_code'].required = True
