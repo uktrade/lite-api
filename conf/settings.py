4c813985738ad673ea9d175ced2ae8dfaf8787c1
Jan Faracik <j.faracik@kainos.com>
2019-04-18 14:16:52 +0100

    Clean up

--- conf/settings.py
+++ conf/settings.py
@@ -82,7 +82,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
--- conf/urls.py
+++ conf/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
 
 from conf.settings import ADMIN_ENABLED
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('applications/', include('applications.urls')),
@@ -11,8 +15,8 @@ urlpatterns = [
     path('organisations/', include('organisations.urls')),
     path('queues/', include('queues.urls')),
     path('users/', include('users.urls')),
-    path('users/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if ADMIN_ENABLED:
