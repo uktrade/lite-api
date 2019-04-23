ac72c095d535635d75be80a333b648fe002b09ad
Jan Faracik <j.faracik@kainos.com>
2019-04-23 11:24:41 +0100

    Removed JWT

--- conf/settings.py
+++ conf/settings.py
@@ -82,7 +82,6 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
--- users/views.py
+++ users/views.py
@@ -1,52 +1,51 @@
-from django.http.response import JsonResponse, Http404
-from rest_framework import permissions
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated
+from django.http.response import JsonResponse, Http404, HttpResponseForbidden
+from rest_framework.parsers import JSONParser
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 from users.models import User
 from users.serializers import ViewUserSerializer
 
 
-@permission_classes((permissions.AllowAny,))
-class UserList(APIView):
+class AuthenticateUser(APIView):
+    permission_classes = (AllowAny,)
     """
-    Get all users
+    Authenticate user
     """
-    def get(self, request):
-        users = User.objects.all()
-        serializer = ViewUserSerializer(users, many=True)
-        return JsonResponse(data={'users': serializer.data},
-                            safe=False)
+    def post(self, request, *args, **kwargs):
+        data = JSONParser().parse(request)
 
+        email = data.get('email')
+        password = data.get('password')
 
-class UserMeDetail(APIView):
-    permission_classes = (IsAuthenticated,)
-    """
-    Get user from token
-    """
-    def get(self, request, *args, **kwargs):
-        user = User.objects.get(email=request.user)
+        user = None
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise Http404
+
+        if not user.check_password(password):
+            return HttpResponseForbidden()
 
         serializer = ViewUserSerializer(user)
         return JsonResponse(data={'user': serializer.data},
                             safe=False)
 
 
-@permission_classes((permissions.AllowAny,))
 class UserDetail(APIView):
+    permission_classes = (AllowAny,)
     """
     Get user from pk
     """
-    def get_object(self, pk):
+    def get_user_by_pk(self, pk):
         try:
-            user = User.objects.get(pk=pk)
-            return user
+            return User.objects.get(pk=pk)
         except User.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
-        user = self.get_object(pk)
+        user = self.get_user_by_pk(pk)
 
         serializer = ViewUserSerializer(user)
         return JsonResponse(data={'user': serializer.data},
