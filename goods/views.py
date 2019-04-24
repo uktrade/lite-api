2474ddc072aea5ff6063c95540404bb6a63d97df
Jan Faracik <j.faracik@kainos.com>
2019-04-24 11:33:15 +0100

    Added organisation to goods, fixed endpoints

--- goods/views.py
+++ goods/views.py
@@ -1,33 +1,29 @@
 from django.http import JsonResponse, Http404
-from rest_framework import permissions, status
-from rest_framework.decorators import permission_classes
+from rest_framework import status
 from rest_framework.parsers import JSONParser
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
+from conf.authentication import PkAuthentication
 from goods.models import Good
 from goods.serializers import GoodSerializer
-from users.models import User
+from organisations.libraries.get_organisation import get_organisation_by_user
 
 
 class GoodList(APIView):
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = (PkAuthentication,)
 
     def get(self, request):
-        user = User.objects.get(email=request.user)
-        organisation = user.organisation
-
+        organisation = get_organisation_by_user(request.user)
         goods = Good.objects.filter(organisation=organisation).order_by('description')
         serializer = GoodSerializer(goods, many=True)
         return JsonResponse(data={'goods': serializer.data},
                             safe=False)
 
     def post(self, request):
-        user = User.objects.get(email=request.user)
-        organisation = user.organisation
+        organisation = get_organisation_by_user(request.user)
 
         data = JSONParser().parse(request)
-        data.organisation = organisation.id
+        data['organisation'] = organisation.id
         serializer = GoodSerializer(data=data)
 
         if serializer.is_valid():
@@ -40,7 +36,7 @@ class GoodList(APIView):
 
 
 class GoodDetail(APIView):
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = (PkAuthentication,)
 
     def get_object(self, pk):
         try:
