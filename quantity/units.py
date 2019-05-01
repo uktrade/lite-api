f0c62065e0bb81f61315c601dfaa974201ba8340
Anirban Lahiri <a.lahiri@kainos.com>
2019-05-01 10:16:19 +0100

    added the primary units

--- quantity/units.py
+++ quantity/units.py
@@ -3,22 +3,13 @@ from collections import namedtuple
 
 Unit = namedtuple('Unit', ['acronym', 'label'])
 
-# Gram(GRM)
-    # Kilogram(KGM)
-    # Number
-    # of
-    # articles(NAR)
-    # Square
-    # metre(MTK)
-    # Metre(MTR)
-    # Litre(LTR)
-    # Cubic
-    # metres(MTQ)
-
 
 class Units(ChoiceEnum):
+    GRM = "Gram"
     KGM = "Kilogram"
     NAR = "Number of articles"
-    
-    # KGM = ('KGM', 'Kilogram')
-    # NAR = ('NAR', 'Number of articles')
+    MTK = "Square metre"
+    MTR = "Metre"
+    LTR = "Litre"
+    MTQ = "Cubic metres"
+
