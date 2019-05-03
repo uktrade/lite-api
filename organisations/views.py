f001792cf6ea6db07785849d1395d3dc59c4c0e6
Jan Faracik <j.faracik@kainos.com>
2019-05-03 16:35:45 +0100

    Fixed transaction alignment issue

--- organisations/views.py
+++ organisations/views.py
@@ -1,16 +1,15 @@
+import reversion
 from django.db import transaction
 from django.http import JsonResponse
-from django.http.response import Http404
 from rest_framework.parsers import JSONParser
-import reversion
 from rest_framework.views import APIView
 
+from organisations.libraries.get_organisation import get_organisation_by_pk
 from organisations.libraries.register_new_business import register_new_business, validate_form_section
 from organisations.models import Organisation
 from organisations.serializers import OrganisationViewSerializer
 
 
-@transaction.atomic
 class OrganisationsList(APIView):
     """
     Get all/create organisations
@@ -21,6 +20,7 @@ class OrganisationsList(APIView):
         return JsonResponse(data={'organisations': view_serializer.data},
                             safe=False)
 
+    @transaction.atomic
     def post(self, request):
         with reversion.create_revision():
             data = JSONParser().parse(request)
@@ -29,15 +29,12 @@ class OrganisationsList(APIView):
 
 class OrganisationsDetail(APIView):
     """
-    Get all/create organisations
+    Get an organisation by its primary key
     """
-    def get(self, request):
-        try:
-            organisation = Organisation.objects.get(pk=pk)
-            view_serializer = OrganisationViewSerializer(organisation)
-            return JsonResponse(data={'organisation': view_serializer.data})
-        except Organisation.DoesNotExist:
-            raise Http404
+    def get(self, request, pk):
+        organisation = get_organisation_by_pk(pk)
+        view_serializer = OrganisationViewSerializer(organisation)
+        return JsonResponse(data={'organisation': view_serializer.data})
 
 
 class Validate(APIView):
