ddda8143bb14b74c08a0ff42926cd8c7578add5d
Jan Faracik <j.faracik@kainos.com>
2019-05-03 18:41:22 +0100

    Removed some libraries

--- organisations/libraries/CreateSite.py
+++
@@ -1,8 +0,0 @@
-from organisations.models import Site
-
-
-class CreateSite:
-    def __init__(self, name, address):
-        new_site = Site(name=name,
-                        address=address)
-        new_site.save()
