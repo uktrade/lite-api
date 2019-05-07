ef059364599182713df644935da33e9628099499
Anirban Lahiri <a.lahiri@kainos.com>
2019-05-07 09:01:56 +0100

    minor url name change to avoid conflict

--- organisations/urls.py
+++ organisations/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.OrganisationsList.as_view(), name='organisations'),
     path('<uuid:pk>/', views.OrganisationsDetail.as_view(), name='organisation'),
     path('sites/', views_site.SiteList.as_view(), name='sites'),
-    path('sites/<uuid:pk>/', views_site.SiteDetail.as_view(), name='sites'),
+    path('sites/<uuid:pk>/', views_site.SiteDetail.as_view(), name='site'),
 ]
