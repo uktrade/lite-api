c8627d13f57d3f8d3ccd4473aa5ca7e86e765112
Anirban Lahiri <a.lahiri@kainos.com>
2019-05-08 12:15:14 +0100

    Added tests for OrgSites

--- organisations/tests/test_site_views.py
+++ organisations/tests/test_site_views.py
@@ -77,3 +77,48 @@ class SiteViewTests(APITestCase, URLPatternsTestCase):
         # There is a dummy address which means there are two real ones after
         # the create additional site and the one dummy one.
         self.assertEqual(Address.objects.all().count(), 3)
+
+
+class OrgSiteViewTests(APITestCase, URLPatternsTestCase):
+
+    urlpatterns = [
+        path('organisations/', include('organisations.urls'))
+    ]
+
+    client = APIClient
+
+    def setUp(self):
+        self.test_helper = OrgAndUserHelper(name='Org1')
+        self.headers = {'HTTP_USER_ID': str(self.test_helper.user.id)}
+
+    def test_site_list(self):
+
+        url = reverse('organisations:sites', kwargs={'org_pk': self.test_helper.organisation.id})
+        response = self.client.get(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data['sites'][0]['name'], 'headquarters')
+
+    def test_site_update(self):
+
+        url = reverse('organisations:site', kwargs={'org_pk': self.test_helper.organisation.id,
+                                                    'site_pk': self.test_helper.organisation.primary_site.id})
+        data = {'name': 'regional site'}
+
+        response = self.client.put(url, data, format='json', **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # more tests desirable
+
+    def test_add_site(self):
+
+        url = reverse('organisations:sites', kwargs={'org_pk': self.test_helper.organisation.id})
+        data = {'name': 'regional site',
+                'address_line_1': 'a street',
+                'city': 'london',
+                'zip': 'E14GH',
+                'state': 'Hertfordshire',
+                'country': 'England'}
+
+        response = self.client.post(url, data, format='json', **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Site.objects.all().count(), 2)
