6f74ce2253b32657d31d08bdf59145bda2b8b8fe
Jan Faracik <j.faracik@kainos.com>
2019-05-17 14:25:29 +0100

    Cleaned up sites tests

--- drafts/tests/tests_adding_sites.py
+++ drafts/tests/tests_adding_sites.py
@@ -1,78 +1,99 @@
 import json
 
-from django.urls import path, include, reverse
+from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient, APITestCase, URLPatternsTestCase
+
+from drafts.models import SitesOnDraft
+from test_helpers.clients import DataTestClient
 from test_helpers.org_and_user_helper import OrgAndUserHelper
 
 
-class SitesOnDraftTests(APITestCase, URLPatternsTestCase):
-
-    urlpatterns = [
-        path('drafts/', include('drafts.urls')),
-        path('applications/', include('applications.urls')),
-        path('organisations/', include('organisations.urls'))
-    ]
-
-    client = APIClient
+class SitesOnDraftTests(DataTestClient):
 
     def setUp(self):
-        self.test_helper = OrgAndUserHelper(name='name')
-        self.headers = {'HTTP_USER_ID': str(self.test_helper.user.id)}
+        super().setUp()
+        self.org = self.test_helper.organisation
+        self.primary_site = self.org.primary_site
+        self.draft = OrgAndUserHelper.complete_draft('Goods test', self.org)
 
-    def test_add_a_site_to_a_draft(self):
-        org = self.test_helper.organisation
-        draft = OrgAndUserHelper.complete_draft('Goods test', org)
-        site, address = OrgAndUserHelper.create_site('site2', org)
+        self.url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
 
+    def test_add_site_to_a_draft(self):
         data = {
-            'sites': [site.id],
+            'sites': [
+                self.primary_site.id
+            ]
         }
 
-        url = reverse('drafts:draft_sites', kwargs={'pk': draft.id})
-        response = self.client.post(url, data, format='json', **self.headers)
+        response = self.client.post(self.url, data, format='json', **self.headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        url = reverse('drafts:draft_sites', kwargs={'pk': draft.id})
-        response = self.client.get(url, **self.headers)
-        response_data = json.loads(response.content)
-        self.assertEqual(len(response_data["sites"]), 1)
+        url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
+        response = self.client.get(url, **self.headers).json()
+        self.assertEqual(len(response["sites"]), 1)
 
-    def test_multiple_sites_to_a_draft(self):
-        org = self.test_helper.organisation
-        primary_site = org.primary_site
-        draft = OrgAndUserHelper.complete_draft('Goods test', org)
-        site2, address = OrgAndUserHelper.create_site('site2', org)
+    def test_add_multiple_sites_to_a_draft(self):
+        site2, address = OrgAndUserHelper.create_site('site2', self.org)
 
         data = {
-            'sites': [primary_site.id, site2.id]
+            'sites': [
+                self.primary_site.id,
+                site2.id
+            ]
         }
 
-        url = reverse('drafts:draft_sites', kwargs={'pk': draft.id})
-        response = self.client.post(url, data, format='json', **self.headers)
+        response = self.client.post(self.url, data, format='json', **self.headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        url = reverse('drafts:draft_sites', kwargs={'pk': draft.id})
+        url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
         response = self.client.get(url, **self.headers)
         response_data = json.loads(response.content)
         self.assertEqual(len(response_data["sites"]), 2)
 
     def test_user_cannot_add_another_organisations_site_to_a_draft(self):
         org2 = OrgAndUserHelper(name='organisation2')
-        draft = OrgAndUserHelper.complete_draft('test', self.test_helper.organisation)
         site_org2 = org2.primary_site
 
         data = {
-            'sites': [site_org2.id],
+            'sites': [
+                site_org2.id
+            ]
         }
 
-        url = reverse('drafts:draft_sites', kwargs={'pk': draft.id})
+        url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
         response = self.client.post(url, data, format='json', **self.headers)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-        url = reverse('drafts:draft_sites', kwargs={'pk': draft.id})
+        url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
         response = self.client.get(url, **self.headers)
         response_data = json.loads(response.content)
         self.assertEqual(len(response_data["sites"]), 0)
 
+    def test_add_a_site_to_a_draft_deletes_existing_sites(self):
+        site2, address = OrgAndUserHelper.create_site('site2', self.org)
 
+        # Add an initial site to the draft
+        existing_site_on_draft = SitesOnDraft(site=site2, draft=self.draft)
+        existing_site_on_draft.save()
+
+        # Ensure it's there
+        url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
+        response = self.client.get(url, **self.headers).json()
+        self.assertEqual(len(response["sites"]), 1)
+
+        # Post a new site to the draft, with the expectation that the existing site is deleted
+        data = {
+            'sites': [
+                self.primary_site.id
+            ]
+        }
+
+        url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
+        response = self.client.post(url, data, format='json', **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check that the new site has been added
+        url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
+        response = self.client.get(url, **self.headers).json()
+        self.assertEqual(len(response['sites']), 1)
+        self.assertNotEqual(response['sites'][0]['id'], existing_site_on_draft.id)
