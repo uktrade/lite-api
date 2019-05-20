534beca668ec66987095a573f4830f62513c20f7
Jan Faracik <j.faracik@kainos.com>
2019-05-20 11:39:02 +0100

    Cleaned up tests

--- drafts/tests/tests_create.py
+++ drafts/tests/tests_create.py
@@ -1,3 +1,6 @@
+import json
+
+from parameterized import parameterized
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -9,7 +12,7 @@ class DraftTests(DataTestClient):
 
     url = reverse('drafts:drafts')
 
-    def test_create_draft_success(self):
+    def test_create_draft_successful(self):
         """
         Ensure we can create a new draft object
         """
@@ -17,48 +20,32 @@ class DraftTests(DataTestClient):
             'name': 'Test',
             'licence_type': 'standard_licence',
             'export_type': 'temporary',
-            'reference_number_on_information_form': '123'
+            'reference_number_on_information_form': '123',
         }
 
         response = self.client.post(self.url, data, format='json', **self.headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Draft.objects.count(), 1)
 
-    def test_create_draft_failure(self):
-        """
-        Ensure we cannot create a new draft object with an invalid POST
-        """
-        data = {
-            'export_type': 'temporary',
-            'reference_number_on_information_form': '123'
-        }
-
-        response = self.client.post(self.url, data, format='json', **self.headers)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(Draft.objects.count(), 0)
-
-    def test_create_draft_failure_2(self):
-        """
-        Ensure we cannot create a new draft object with an invalid POST
-        """
-        data = {
-            'licence_type': 'standard_licence',
-            'reference_number_on_information_form': '123'
-        }
-
-        response = self.client.post(self.url, data, format='json', **self.headers)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(Draft.objects.count(), 0)
-
-    def test_create_draft_failure_3(self):
-        """
-        Ensure we cannot create a new draft object with an invalid POST
-        """
-        data = {
+    @parameterized.expand([
+        [{}],
+        [{
             'licence_type': 'standard_licence',
             'export_type': 'temporary',
-        }
-
-        response = self.client.post(self.url, data, format='json', **self.headers)
+        }],
+        [{
+            'name': 'Test',
+            'export_type': 'temporary',
+        }],
+        [{
+            'name': 'Test',
+            'licence_type': 'standard_licence',
+        }],
+    ])
+    def test_create_draft_failure(self, data):
+        """
+        Ensure we cannot create a new draft object with an invalid POST
+        """
+        response = self.client.post(self.url, data, **self.headers)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Draft.objects.count(), 0)
