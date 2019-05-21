6060908f7c2da2dcc1b66155608fed64969b681a
Christopher Stobart <christophers@Purple.local>
2019-05-21 16:44:10 +0100

    Update tests_adding_endusers.py

--- drafts/tests/tests_adding_endusers.py
+++ drafts/tests/tests_adding_endusers.py
@@ -50,25 +50,25 @@ class EndUserOnDraftTests(DataTestClient):
         url = reverse('drafts:end_users', kwargs={'pk': self.draft.id})
         response = self.client.get(url, **self.headers).json()
         self.assertEqual(len(response["end_users"]), 2)
-    #
-    # def test_user_cannot_add_another_organisations_site_to_a_draft(self):
-    #     org2 = OrgAndUserHelper(name='organisation2')
-    #     site_org2 = org2.primary_site
-    #
-    #     data = {
-    #         'sites': [
-    #             site_org2.id
-    #         ]
-    #     }
-    #
-    #     url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
-    #     response = self.client.post(url, data, format='json', **self.headers)
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    #
-    #     url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
-    #     response = self.client.get(url, **self.headers)
-    #     response_data = json.loads(response.content)
-    #     self.assertEqual(len(response_data["sites"]), 0)
+
+    def test_user_cannot_add_another_organisations_site_to_a_draft(self):
+        org2 = OrgAndUserHelper(name='organisation2').organisation
+        test_end_user = OrgAndUserHelper.create_end_user('test', org2)
+
+        data = {
+            'endusers': [
+                test_end_user.id
+            ]
+        }
+
+        url = reverse('drafts:end_users', kwargs={'pk': self.draft.id})
+        response = self.client.post(url, data, format='json', **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        url = reverse('drafts:end_users', kwargs={'pk': self.draft.id})
+        response = self.client.get(url, **self.headers)
+        response_data = json.loads(response.content)
+        self.assertEqual(len(response_data["end_users"]), 0)
     #
     # def test_add_a_site_to_a_draft_deletes_existing_sites(self):
     #     site2, address = OrgAndUserHelper.create_site('site2', self.org)
