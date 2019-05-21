8f328935deda66da9cfccdb7f0dc1a8d11caa27c
Christopher Stobart <christophers@Purple.local>
2019-05-21 16:49:52 +0100

    Update tests_adding_endusers.py

--- drafts/tests/tests_adding_endusers.py
+++ drafts/tests/tests_adding_endusers.py
@@ -3,7 +3,7 @@ import json
 from django.urls import reverse
 from rest_framework import status
 
-from drafts.models import Draft
+from drafts.models import Draft, EndUserOnDraft
 from test_helpers.clients import DataTestClient
 from test_helpers.org_and_user_helper import OrgAndUserHelper
 
@@ -69,33 +69,33 @@ class EndUserOnDraftTests(DataTestClient):
         response = self.client.get(url, **self.headers)
         response_data = json.loads(response.content)
         self.assertEqual(len(response_data["end_users"]), 0)
-    #
-    # def test_add_a_site_to_a_draft_deletes_existing_sites(self):
-    #     site2, address = OrgAndUserHelper.create_site('site2', self.org)
-    #
-    #     # Add an initial site to the draft
-    #     existing_site_on_draft = SitesOnDraft(site=site2, draft=self.draft)
-    #     existing_site_on_draft.save()
-    #
-    #     # Ensure it's there
-    #     url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
-    #     response = self.client.get(url, **self.headers).json()
-    #     site_id = response["sites"][0]['id']
-    #     self.assertEqual(len(response["sites"]), 1)
-    #
-    #     # Post a new site to the draft, with the expectation that the existing site is deleted
-    #     data = {
-    #         'sites': [
-    #             self.primary_site.id
-    #         ]
-    #     }
-    #
-    #     url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
-    #     response = self.client.post(url, data, format='json', **self.headers)
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #
-    #     # Check that the new site has been added, and the old one deleted
-    #     url = reverse('drafts:draft_sites', kwargs={'pk': self.draft.id})
-    #     response = self.client.get(url, **self.headers).json()
-    #     self.assertEqual(len(response['sites']), 1)
-    #     self.assertNotEqual(response['sites'][0]['id'], site_id)
+
+    def test_add_a_end_user_to_a_draft_deletes_existing_end_users(self):
+        # Add an initial end_user to the draft
+        existing_end_user_on_draft = EndUserOnDraft(end_user=self.end_user, draft=self.draft)
+        existing_end_user_on_draft.save()
+
+        # Ensure it's there
+        url = reverse('drafts:end_users', kwargs={'pk': self.draft.id})
+        response = self.client.get(url, **self.headers).json()
+        end_user_id = response["end_users"][0]['id']
+        self.assertEqual(len(response["end_users"]), 1)
+
+        end_user_2 = OrgAndUserHelper.create_end_user('number 2', self.org)
+
+        # Post a new site to the draft, with the expectation that the existing site is deleted
+        data = {
+            'endusers': [
+                end_user_2.id
+            ]
+        }
+
+        url = reverse('drafts:end_users', kwargs={'pk': self.draft.id})
+        response = self.client.post(url, data, format='json', **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check that the new site has been added, and the old one deleted
+        url = reverse('drafts:end_users', kwargs={'pk': self.draft.id})
+        response = self.client.get(url, **self.headers).json()
+        self.assertEqual(len(response['end_users']), 1)
+        self.assertNotEqual(response['end_users'][0]['id'], end_user_id)
