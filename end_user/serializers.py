76432dc2f2a259af0a63222351197027d2427d63
Anirban Lahiri <a.lahiri@kainos.com>
2019-05-22 11:55:09 +0100

    WIP: Minor cosmetic changes

--- end_user/serializers.py
+++ end_user/serializers.py
@@ -65,31 +65,3 @@ class EndUserUpdateSerializer(OrganisationViewSerializer):
         instance.save()
         return instance
 
-''' 
-class AddressUpdateSerializer(serializers.ModelSerializer):
-    address_line_1 = serializers.CharField()
-    postcode = serializers.CharField(max_length=10)
-    city = serializers.CharField()
-    region = serializers.CharField()
-    country = serializers.CharField()
-
-    class Meta:
-        model = Address
-        fields = ('id',
-                  'address_line_1',
-                  'address_line_2',
-                  'postcode',
-                  'city',
-                  'region',
-                  'country')
-
-    def update(self, instance, validated_data):
-        instance.address_line_1 = validated_data.get('address_line_1', instance.address_line_1)
-        instance.address_line_2 = validated_data.get('address_line_2', instance.address_line_2)
-        instance.postcode = validated_data.get('postcode', instance.postcode)
-        instance.city = validated_data.get('city', instance.city)
-        instance.region = validated_data.get('region', instance.region)
-        instance.country = validated_data.get('country', instance.country)
-        instance.save()
-        return instance
-'''
