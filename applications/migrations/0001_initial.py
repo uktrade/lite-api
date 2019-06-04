09c7a2199d57a6f2a988e1358cba942a2948f6a9
Anirban Lahiri <a.lahiri@kainos.com>
2019-06-04 09:53:20 +0100

    fixed the migration issue created by renaming the fields

--- applications/migrations/0001_initial.py
+++ applications/migrations/0001_initial.py
@@ -68,7 +68,7 @@ class Migration(migrations.Migration):
             name='ApplicationDenialReason',
             fields=[
                 ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('reason_details', models.TextField(blank=True, default=None, max_length=2200, null=True)),
+                ('reasoning', models.TextField(blank=True, default=None, max_length=2200, null=True)),
                 ('application', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='application_denial_reason', to='applications.Application')),
                 ('reasons', models.ManyToManyField(to='denial_reasons.DenialReason')),
             ],
