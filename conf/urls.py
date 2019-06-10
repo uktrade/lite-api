ce9c36d14509f60a0457b4fdaba06e685fd6678d
Luke Sawicki <50659058+lukesa@users.noreply.github.com>
2019-06-10 16:08:10 +0100

    included-docs-app (#45)
    included automatic api docs app

--- conf/urls.py
+++ conf/urls.py
@@ -1,7 +1,25 @@
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
 
 from conf.settings import ADMIN_ENABLED
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Lite-API",
+      default_version='v0.3',
+      description="Service for handling backend calls in LITE.",
+      terms_of_service="https://github.com/uktrade/lite-api/blob/master/LICENSE",
+      contact=openapi.Contact(email="tbd@local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('applications/', include('applications.urls')),
@@ -13,6 +31,7 @@ urlpatterns = [
     path('static/', include('static.urls')),
     path('users/', include('users.urls')),
     path('teams/', include('teams.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
 
 if ADMIN_ENABLED:
