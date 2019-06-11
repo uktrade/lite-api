a610a78ab04ab35e302a5cb78d5857c8f7697a9d
Jan Faracik <j.faracik@kainos.com>
2019-06-11 16:02:33 +0100

    Update tests.py

--- audit/tests.py
+++ audit/tests.py
@@ -1,21 +1,39 @@
 from django.urls import reverse
+from rest_framework import status
 
+from applications.enums import ApplicationStatus
 from test_helpers.clients import DataTestClient
-from test_helpers.org_and_user_helper import OrgAndUserHelper
 
 
 class AuditTests(DataTestClient):
 
-    url = reverse('audit:audit_detail')
+    def setUp(self):
+        super().setUp()
+        self.draft = self.test_helper.create_draft_with_good_end_user_and_site('Example Application',
+                                                                               self.test_helper.organisation)
+        self.application = self.test_helper.submit_draft(self, self.draft)
+        self.url = reverse('audit:audit_detail', kwargs={'type': 'applications', 'pk': self.application.id})
 
-    def test_create_application_case_and_addition_to_queue(self):
+    def test_view_audit_history(self):
         """
-        Test whether we can create a draft first and then submit it as an application
+        Submit an application and check that there is an audit trail
         """
-        draft = OrgAndUserHelper.create_draft_with_good_end_user_and_site(name='test',
-                                                                          org=self.test_helper.organisation)
-        application = OrgAndUserHelper.submit_draft(self, draft)
-
         response = self.client.get(self.url)
+        response_data = response.json()
 
-        print(response.json())
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response_data['changes']), 1)
+
+        # Update the status and and check that the audit trail is updated
+        data = {
+            'status': ApplicationStatus.APPROVED,
+        }
+
+        self.client.put(reverse('applications:application', kwargs={'pk': self.application.id}), data=data)
+
+        # Validate that there are now two objects in changes
+        response = self.client.get(self.url)
+        response_data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response_data['changes']), 2)
