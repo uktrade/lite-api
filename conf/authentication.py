50d19b11c06640525a2f3b089f4cd117dca81963
Christopher Stobart <christophers@Purple.local>
2019-06-11 16:01:33 +0100

    EmailAuthentication

--- conf/authentication.py
+++ conf/authentication.py
@@ -1,6 +1,6 @@
 from rest_framework import authentication, exceptions
 
-from users.models import User
+from users.models import User, UserStatuses
 
 
 class PkAuthentication(authentication.BaseAuthentication):
@@ -11,5 +11,20 @@ class PkAuthentication(authentication.BaseAuthentication):
             user = User.objects.get(pk=pk)
         except User.DoesNotExist:
             raise exceptions.AuthenticationFailed('No such user with that ID')
+
+        if user.status == UserStatuses.deactivated:
+            raise exceptions.AuthenticationFailed('User has been deactivated')
+
+        return user, None
+
+
+class EmailAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        email = request.META.get('HTTP_USER_EMAIL')
+
+        try:
+            user = GovUser.objects.get(email=email)
+        except User.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No such user with that email')
 
         return user, None
