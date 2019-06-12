5de73a1344092f8f37bc1f5a552b5b4f70745e93
Anirban Lahiri <a.lahiri@kainos.com>
2019-06-12 11:18:52 +0100

    WIP: updated the serializer for queues and added the post view

--- queues/serializers.py
+++ queues/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 
 from cases.serializers import CaseSerializer
 from queues.models import Queue
+from teams.serializers import TeamSerializer
 
 
 class QueueSerializer(serializers.ModelSerializer):
     cases = CaseSerializer(many=True, read_only=True)
+    team = TeamSerializer(read_only=True)
 
     class Meta:
         model = Queue
         fields = ('id',
                   'name',
+                  'team',
                   'cases')
--- queues/views.py
+++ queues/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse, Http404
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework.decorators import permission_classes
+from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 
 from queues.models import Queue
@@ -17,6 +18,18 @@ class QueuesList(APIView):
         serializer = QueueSerializer(queues, many=True)
         return JsonResponse(data={'status': 'success', 'queues': serializer.data},
                             safe=False)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = QueueSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data={'queue': serializer.data},
+                                status=status.HTTP_201_CREATED)
+
+        return JsonResponse(data={'errors': serializer.errors},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 @permission_classes((permissions.AllowAny,))
