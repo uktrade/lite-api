6098b27fb3b2fd466473be0fdcb8b8535cf3c64b
Anirban Lahiri <a.lahiri@kainos.com>
2019-06-12 09:49:30 +0100

    WIP: updated the queue model to include reference to Team

--- queues/models.py
+++ queues/models.py
@@ -3,9 +3,12 @@ import uuid
 from django.db import models
 
 from cases.models import Case
+from teams.models import Team
 
 
 class Queue(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField(default='Untitled Queue')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, default=None, null=True)
     cases = models.ManyToManyField(Case)
+
