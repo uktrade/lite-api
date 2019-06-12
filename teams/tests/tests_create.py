29facf213fe4c0776f6b7799a69a05d467770a52
Anirban Lahiri <a.lahiri@kainos.com>
2019-06-12 13:41:53 +0100

    WIP: Fixed some of the tests which broke since the model and migrations were updated

--- teams/tests/tests_create.py
+++ teams/tests/tests_create.py
@@ -17,7 +17,7 @@ class TeamCreateTests(DataTestClient):
         response = self.client.post(self.url, data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Team.objects.get().name, 'new team')
+        self.assertEqual(Team.objects.filter(name='new team').count(), 1)
 
     @parameterized.expand([
         [{
@@ -33,6 +33,7 @@ class TeamCreateTests(DataTestClient):
     def tests_team_name_must_be_unique(self, data):
         Team(name='this is a name').save()
         response = self.client.post(self.url, data)
-        self.assertEqual(Team.objects.all().count(), 1)
+        # Note: there already exists a team (Reception) which is created by default
+        self.assertEqual(Team.objects.all().count(), 2)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
