770386c6e88570ed57b8f6f10d12ada16f5f96c1
Anirban Lahiri <a.lahiri@kainos.com>
2019-06-17 13:57:35 +0100

    Updated the migration such that the default Queue belongs to the default Team

--- queues/models.py
+++ queues/models.py
@@ -9,6 +9,6 @@ from teams.models import Team
 class Queue(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField(default='Untitled Queue')
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, default=None, null=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
     cases = models.ManyToManyField(Case)
 
