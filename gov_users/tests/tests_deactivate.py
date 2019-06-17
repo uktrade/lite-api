49b3a7f3255ffba17222f6645e804da2f844be37
Christopher Stobart <christophers@Purple.local>
2019-06-17 14:20:22 +0100

    Update tests_deactivate.py

--- gov_users/tests/tests_deactivate.py
+++ gov_users/tests/tests_deactivate.py
@@ -22,10 +22,10 @@ class GovUserDeactivateTests(DataTestClient):
         data = {
             'status': 'Deactivated'
         }
-        url = reverse('gov_users:gov_user', kwargs={'pk': user_2.id})
+        url = reverse('gov_users:gov_user', kwargs={'pk': self.valid_user.id})
         response = self.client.put(url, data, format='json', **self.gov_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual((GovUser.objects.get(id=user_2.id).status), GovUserStatuses.DEACTIVATED)
+        self.assertEqual((GovUser.objects.get(id=self.valid_user.id).status), GovUserStatuses.DEACTIVATED)
 
     def test_cannot_deactivate_self(self):
         data = {
@@ -37,18 +37,18 @@ class GovUserDeactivateTests(DataTestClient):
 
     def test_deactivate_and_reactivate_a_user(self):
         url = reverse('gov_users:gov_users')
-        response = self.client.get(url, **{'HTTP_GOV_USER_EMAIL': str(user_2.email)})
+        response = self.client.get(url, **{'HTTP_GOV_USER_EMAIL': str(self.valid_user.email)})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = {
             'status': 'Deactivated'
         }
-        url = reverse('gov_users:gov_user', kwargs={'pk': user_2.id})
+        url = reverse('gov_users:gov_user', kwargs={'pk': self.valid_user.id})
         self.client.put(url, data, format='json', **self.gov_headers)
-        response = self.client.get(reverse('gov_users:gov_users'), **{'HTTP_GOV_USER_EMAIL': str(user_2.email)})
+        response = self.client.get(reverse('gov_users:gov_users'), **{'HTTP_GOV_USER_EMAIL': str(self.valid_user.email)})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         data = {
             'status': 'Active'
         }
         self.client.put(url, data, format='json', **self.gov_headers)
-        response = self.client.get(reverse('gov_users:gov_users'), **{'HTTP_GOV_USER_EMAIL': str(user_2.email)})
+        response = self.client.get(reverse('gov_users:gov_users'), **{'HTTP_GOV_USER_EMAIL': str(self.valid_user.email)})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
