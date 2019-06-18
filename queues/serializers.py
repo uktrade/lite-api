49a8c299814ef6f98a2173750e5afa5312fe1ad0
Anirban Lahiri <a.lahiri@kainos.com>
2019-06-18 08:57:58 +0100

    Added a method field - team_name to the queue serializer

--- queues/serializers.py
+++ queues/serializers.py
@@ -1,3 +1,5 @@
+import uuid
+
 from rest_framework import serializers
 from rest_framework.relations import PrimaryKeyRelatedField
 
@@ -9,16 +11,20 @@ from teams.models import Team
 class QueueSerializer(serializers.ModelSerializer):
     cases = CaseSerializer(many=True, read_only=True, required=False)
     team = PrimaryKeyRelatedField(queryset=Team.objects.all())
+    team_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Queue
         fields = ('id',
                   'name',
                   'team',
+                  'team_name',
                   'cases')
+
+    def get_team_name(self, obj):
+        return obj.team.name
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
-
         instance.save()
         return instance
