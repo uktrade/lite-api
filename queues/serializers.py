17fc352b2269d5ed4f129b10ca13ac0dbbd277b4
Jan Faracik <j.faracik@kainos.com>
2019-06-18 11:50:45 +0100

    Added team to queue serializer

--- queues/serializers.py
+++ queues/serializers.py
@@ -6,9 +6,14 @@ from queues.models import Queue
 
 class QueueSerializer(serializers.ModelSerializer):
     cases = CaseSerializer(many=True, read_only=True)
+    team = serializers.SlugRelatedField(many=False,
+                                        read_only=True,
+                                        slug_field='name'
+                                        )
 
     class Meta:
         model = Queue
         fields = ('id',
                   'name',
-                  'cases')
+                  'cases',
+                  'team')
