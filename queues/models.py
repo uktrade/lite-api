15005fb044b04326dcfeb8ddc9ec29ab52fbbcb7
Luke Sawicki <l.sawicki@kainos.com>
2019-06-18 12:32:29 +0100

    models

--- queues/models.py
+++ queues/models.py
@@ -2,8 +2,6 @@ import uuid
 
 from django.db import models
 
-from cases.models import Case
-
 
 class Queue(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
