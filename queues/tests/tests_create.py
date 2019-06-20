dc28db2c063ea46deae0dfb2c5b388ebdeff64d7
Jan Faracik <j.faracik@kainos.com>
2019-06-20 12:04:02 +0100

    Fix

--- queues/tests/tests_create.py
+++ queues/tests/tests_create.py
@@ -9,7 +9,6 @@ from test_helpers.clients import DataTestClient
 class QueueCreateTests(DataTestClient):
 
     url = reverse('queues:queues')
-    existing_queues_count = Queue.objects.all().count()
 
     def tests_create_queue(self):
         data = {
@@ -19,8 +18,10 @@ class QueueCreateTests(DataTestClient):
         response = self.client.post(self.url, data, **self.gov_headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        existing_queues_count = Queue.objects.all().count()
+
         # Note: A queue "New Cases" is created by default
-        self.assertEqual(Queue.objects.all().count(), self.existing_queues_count + 1)
+        self.assertEqual(Queue.objects.all().count(), existing_queues_count + 1)
 
         queue = Queue.objects.get(pk=response.json().get('queue').get('id'))
 
