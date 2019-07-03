d69fb775003ac8607ff4a525eebbf87e9313b1e9
Jan Faracik <j.faracik@kainos.com>
2019-07-03 14:36:22 +0100

    Cleanup

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -39,43 +39,12 @@ class AddressSerializer(serializers.ModelSerializer):
         return instance
 
 
-class AddressWorkaroundSerializer(serializers.ModelSerializer):
-    address_line_1 = serializers.CharField()
-    postcode = serializers.CharField(max_length=10)
-    city = serializers.CharField()
-    region = serializers.CharField()
-    # TODO: Add country back
-    # country = serializers.PrimaryKeyRelatedField(queryset=Country.objects.all())
-
-    class Meta:
-        model = Address
-        fields = ('id',
-                  'address_line_1',
-                  'address_line_2',
-                  'postcode',
-                  'city',
-                  'region')
-
-    def update(self, instance, validated_data):
-        instance.address_line_1 = validated_data.get('address_line_1',
-                                                     instance.address_line_1)
-        instance.address_line_2 = validated_data.get('address_line_2',
-                                                     instance.address_line_2)
-        instance.postcode = validated_data.get('postcode', instance.postcode)
-        instance.city = validated_data.get('city', instance.city)
-        instance.region = validated_data.get('region', instance.region)
-        # instance.country = validated_data.get('country', instance.country)
-        instance.save()
-        return instance
-
-
 class AddressCountrylessSerializer(serializers.ModelSerializer):
     address_line_1 = serializers.CharField()
     postcode = serializers.CharField(max_length=10)
     city = serializers.CharField()
     region = serializers.CharField()
     # TODO: Add country primary key back
-    # country = serializers.PrimaryKeyRelatedField(queryset=Country.objects.all())
     country = serializers.CharField(allow_blank=False, allow_null=False)
 
     class Meta:
--- addresses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from addresses import views
-
-app_name = "addresses"
-
-urlpatterns = [
-    path('', views.Address.as_view(), name='gov_users'),
-]
