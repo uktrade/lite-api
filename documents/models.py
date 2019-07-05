2fe959e8d957266a4099a71df298f9f6a6988242
Jan Faracik <j.faracik@kainos.com>
2019-07-05 16:44:46 +0100

    Updated

--- documents/models.py
+++ documents/models.py
@@ -2,6 +2,9 @@ import uuid
 
 from django.db import models
 
+from conf.settings import env
+from documents.utils import s3_client
+
 
 class Document(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -12,3 +15,40 @@ class Document(models.Model):
     safe = models.NullBooleanField()
     checksum = models.CharField(max_length=64, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def delete(self, **kwargs):
+        s3_client().delete_object(Bucket=env('AWS_STORAGE_BUCKET_NAME'), Key=self.s3_key)
+        super().delete()
+
+    def scan_for_viruses(self):
+        from documents.av_scan import virus_scan_document
+        virus_scan_document(self.id)
+        self.refresh_from_db()
+        return self
+
+    def prepare_document(self):
+        """
+        Prepares the document for usage. This is run async after the document
+        is already on S3.
+            - perform a virus check
+            - get the checksum/etag
+        """
+        try:
+            # self.update_md5_checksum()
+            self.scan_for_viruses()
+            if self.safe is False:
+                self.delete()
+            return bool(self.checksum) and self.safe
+        except Exception:
+            return False
+
+    # def update_md5_checksum(self):
+    #     """
+    #     Update the md5 checksum on the document record
+    #     """
+    #     self.checksum = self.get_md5_checksum()
+    #     self.save()
+
