13d91d2c6ead6306661dfe1082a7349e5d0c5363
Spencer Barriball <sbarriball@me.com>
2019-07-09 11:11:57 +0100

    fix for circleci pass

--- goods/serializers.py
+++ goods/serializers.py
@@ -48,7 +48,8 @@ class GoodSerializer(serializers.ModelSerializer):
             template = "An exception of type {0} occurred. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             message.capitalize()
-            return None
+            pass
+            # return None
 
     def validate(self, cleaned_data):
         is_controlled_good = cleaned_data.get('is_good_controlled') == GoodControlled.YES
