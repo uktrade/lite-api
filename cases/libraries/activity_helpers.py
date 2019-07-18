ddedbf4cec0ad3881bdc42f46d32049ec4b66fc8
Jan Faracik <j.faracik@kainos.com>
2019-07-18 13:59:27 +0100

    Added user serializer info

--- cases/libraries/activity_helpers.py
+++ cases/libraries/activity_helpers.py
@@ -6,23 +6,21 @@ from reversion.models import Version
 from cases.models import CaseNote
 
 # from users.models import GovUserRevisionMeta
+from users.libraries.get_user import get_user_by_pk
+from users.models import GovUser
+from users.serializers import BaseUserViewSerializer
 
 CHANGE = 'change'
 CASE_NOTE = 'case_note'
 
 
-def _activity_item(activity_type, date, user, data):
+def _activity_item(activity_type, date, user, data, status=None):
     data = {
         'type': activity_type,
         'date': date,
-        'user': {
-            'id': user['id'],
-            'email': user['email'],
-            'first_name': user['first_name'],
-            'last_name': user['last_name'],
-            'group': 'GOV USER',
-        },
-        'data': data
+        'user': user,
+        'data': data,
+        'status': status
     }
     return data
 
@@ -31,15 +29,13 @@ def convert_case_note_to_activity(case_note: CaseNote):
     """
     Converts a case note to a dict suitable for the case activity list
     """
+    user = get_user_by_pk(case_note.user.id)
+
     return _activity_item(CASE_NOTE,
                           case_note.created_at,
-                          {
-                              'id': case_note.user.id,
-                              'email': case_note.user.email,
-                              'first_name': case_note.user.first_name,
-                              'last_name': case_note.user.last_name,
-                          },
-                          case_note.text)
+                          BaseUserViewSerializer(user).data,
+                          case_note.text,
+                          status='Visible to exporter' if case_note.is_visible_to_exporter else None)
 
 
 def convert_audit_to_activity(version: Version):
