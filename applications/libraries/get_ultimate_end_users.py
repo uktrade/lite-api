d0e2f632a86f816692420539499c0e5b07097258
Jan Faracik <j.faracik@kainos.com>
2019-07-30 14:11:52 +0100

    Update get_ultimate_end_users.py

--- applications/libraries/get_ultimate_end_users.py
+++ applications/libraries/get_ultimate_end_users.py
@@ -1,10 +1,2 @@
-from end_user.models import EndUser
-
-
 def get_ultimate_end_user_ids(obj):
-    ultimate_end_users_ids = obj.ultimate_end_users.values_list('id', flat=True)
-    ultimate_end_users = []
-    for id in ultimate_end_users_ids:
-        ultimate_end_users.append(EndUser.objects.get(id=str(id)))
-
-    return ultimate_end_users
+    return obj.ultimate_end_users.all()
