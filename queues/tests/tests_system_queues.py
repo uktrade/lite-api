d77f9f7e36d8a91acf14c6a98d5641025b9dc1a9
Eugene Fahy <e.fahy@kainos.com>
2019-07-31 12:41:17 +0100

    Addressed review comments - improved commenting of API tests

--- queues/tests/tests_system_queues.py
+++ queues/tests/tests_system_queues.py
@@ -19,6 +19,13 @@ class RetrieveAllCases(DataTestClient):
         self.case3.application.save(update_fields=['status'])
 
     def test_get_all_case_assignments(self):
+        """
+        Given Cases assigned to various queues
+        When a user requests the All Cases system queue
+        Then all cases are returned regardless of the queues they are assigned to
+        """
+
+        # Arrange
         case_assignment = CaseAssignment(queue=self.queue1, case=self.case1)
         case_assignment.save()
         case_assignment = CaseAssignment(queue=self.queue2, case=self.case2)
@@ -26,7 +33,10 @@ class RetrieveAllCases(DataTestClient):
 
         url = '/queues/' + ALL_CASES_SYSTEM_QUEUE_ID + '/case-assignments/'
 
+        # Act
         response = self.client.get(url, **self.gov_headers)
+
+        # Assert
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -38,9 +48,19 @@ class RetrieveAllCases(DataTestClient):
         self.assertTrue(str(self.case2.id) in case_id_list)
 
     def test_get_all_queues_including_true_system_queues_param(self):
+        """
+        Given that a number of user defined queues exists
+        When a user requests the queues including system queues
+        Then all user defined queues and system queues are returned
+        """
+
+        # Arrange
         url = '/queues/?include_system_queues=True'
 
+        # Act
         response = self.client.get(url, **self.gov_headers)
+
+        # Assert
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -50,9 +70,20 @@ class RetrieveAllCases(DataTestClient):
         self.assertTrue(OPEN_CASES_SYSTEM_QUEUE_ID in queue_id_list)
 
     def test_get_all_queues_including_false_system_queues_param(self):
+        """
+        Given that a number of user defined queues exists
+        When a user requests queues not including system queues
+        Then all user defined queues are returned
+        And system queues are not returned
+        """
+
+        # Arrange
         url = '/queues/?include_system_queues=False'
 
+        # Act
         response = self.client.get(url, **self.gov_headers)
+
+        # Assert
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -62,20 +93,42 @@ class RetrieveAllCases(DataTestClient):
         self.assertFalse(OPEN_CASES_SYSTEM_QUEUE_ID in queue_id_list)
 
     def test_get_all_queues_without_system_queues_param(self):
+        """
+        Given that a number of user defined queues exists
+        When a user requests queues and does not indicate if system queues should be included
+        Then all user defined queues are returned
+        And system queues are not returned
+        """
+
+        # Arrange
         url = '/queues/'
 
+        # Act
         response = self.client.get(url, **self.gov_headers)
+
+        # Assert
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         queue_id_list = [q['id'] for q in response_data['queues']]
         self.assertFalse(ALL_CASES_SYSTEM_QUEUE_ID in queue_id_list)
+        self.assertFalse(OPEN_CASES_SYSTEM_QUEUE_ID in queue_id_list)
 
     def test_get_all_cases_system_queue(self):
+        """
+        Given that a number of cases exist and are assigned to different user defined queues
+        When a user the all cases system queue
+        Then all cases are returned regardless of which user defined queues they are assigned to
+        """
+
+        # Arrange
         url = '/queues/' + ALL_CASES_SYSTEM_QUEUE_ID + '/'
 
+        # Act
         response = self.client.get(url, **self.gov_headers)
+
+        # Assert
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -84,9 +137,19 @@ class RetrieveAllCases(DataTestClient):
         self.assertEqual(3, len(response_data['queue']['cases']))
 
     def test_get_open_cases_system_queue(self):
+        """
+        Given that a number of cases exist and are assigned to different user defined queues
+        When a user the open cases system queue
+        Then only open cases are returned regardless of which user defined queues they are assigned to
+        """
+
+        # Arrange
         url = '/queues/' + OPEN_CASES_SYSTEM_QUEUE_ID + '/'
 
+        # Act
         response = self.client.get(url, **self.gov_headers)
+
+        # Assert
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
