5e66bb77b2d639c2e248db51f2440017f1fb5858
Jan Faracik <j.faracik@kainos.com>
2019-08-01 11:27:50 +0100

    Clean

--- picklists/serializers.py
+++ picklists/serializers.py
@@ -1,23 +1,28 @@
-from rest_framework import serializers
+from rest_framework.fields import CharField, SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer
+
+from conf.serializers import KeyValueChoiceField
 from content_strings.strings import get_string
 from picklists.enums import PicklistType, PickListStatus
 from picklists.models import PicklistItem
 from teams.models import Team
 
 
-class PicklistSerializer(serializers.ModelSerializer):
+class PicklistSerializer(ModelSerializer):
     team = PrimaryKeyRelatedField(queryset=Team.objects.all())
-    name = serializers.CharField(allow_blank=False,
-                                 error_messages={'blank': get_string('picklist_items.error_messages.blank_name')})
-    text = serializers.CharField(allow_blank=False,
-                                 max_length=5000,
-                                 error_messages={'blank': get_string('picklist_items.error_messages.blank_text')})
-    type = serializers.ChoiceField(choices=PicklistType.choices,
-                                   error_messages={'invalid_choice': get_string('picklist_items.error_messages.blank_type')})
-    status = serializers.ChoiceField(choices=PickListStatus.choices,
-                                     error_messages={'invalid_choice': get_string('picklist_items.error_messages.blank_status')})
-    team_name = serializers.SerializerMethodField()
+    name = CharField(allow_blank=False,
+                     error_messages={'blank': get_string('picklist_items.error_messages.blank_name')})
+    text = CharField(allow_blank=False,
+                     max_length=5000,
+                     error_messages={'blank': get_string('picklist_items.error_messages.blank_text')})
+    type = KeyValueChoiceField(choices=PicklistType.choices,
+                               error_messages={
+                                   'invalid_choice': get_string('picklist_items.error_messages.blank_type')})
+    status = KeyValueChoiceField(choices=PickListStatus.choices,
+                                 error_messages={
+                                     'invalid_choice': get_string('picklist_items.error_messages.blank_status')})
+    team_name = SerializerMethodField()
 
     def get_team_name(self, instance):
         return instance.team.name
@@ -31,4 +36,3 @@ class PicklistSerializer(serializers.ModelSerializer):
                   'type',
                   'status',
                   'team_name')
-
