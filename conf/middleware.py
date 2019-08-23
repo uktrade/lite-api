2618b7d6cf70fe4fde18431e2e0bc1e6b260bcf8
mh-hmcts <mh@hmcts.net>
2019-08-23 16:22:06 +0100

    correlation propagation

--- conf/middleware.py
+++ conf/middleware.py
@@ -1,26 +1,32 @@
 import json
 import logging
 import uuid
-from django.utils.deprecation import MiddlewareMixin
+import time
 
 
-class LoggingMiddleware(MiddlewareMixin):
+class LoggingMiddleware:
+    def __init__(self, get_response=None):
+        self.get_response = get_response
 
-    @staticmethod
-    def process_request(request):
-        request.correlation = uuid.uuid4().hex
+    def __call__(self, request):
+        correlation = None
+        start = time.time()
+        if 'HTTP_X_CORRELATION_ID' in request.META:
+            correlation = request.META['HTTP_X_CORRELATION_ID']
+        request.correlation = correlation or uuid.uuid4().hex
         logging.info(json.dumps({
             "correlation": request.correlation,
             "type": "request",
-            "path": request.path,
             "method": request.method,
+            "path": request.path,
         }))
-
-    @staticmethod
-    def process_response(request, response):
+        response = self.get_response(request)
+        elapsed = time.time() - start
         logging.info(json.dumps({
             "correlation": request.correlation,
             'type': 'response',
             "status": response.status_code,
+            "elapsed": elapsed
         }))
         return response
+
