48796f39989832f88c7e4b3bc00a64e0d4194b1a
mh-hmcts <mh@hmcts.net>
2019-08-23 17:55:34 +0100

    reformat log

--- conf/middleware.py
+++ conf/middleware.py
@@ -14,19 +14,15 @@ class LoggingMiddleware:
         if 'HTTP_X_CORRELATION_ID' in request.META:
             correlation = request.META['HTTP_X_CORRELATION_ID']
         request.correlation = correlation or uuid.uuid4().hex
-        logging.info(json.dumps({
-            "correlation": request.correlation,
-            "type": "request",
-            "method": request.method,
-            "path": request.path,
-        }))
         response = self.get_response(request)
-        elapsed = time.time() - start
-        logging.info(json.dumps({
-            "correlation": request.correlation,
-            'type': 'response',
-            "status": response.status_code,
-            "elapsed": elapsed
-        }))
+        logging.info({
+            "message": "liteolog",
+            "corrID": request.correlation,
+            "type": "http",
+            "method": request.method,
+            "url": request.path,
+            "elapsed_time": time.time() - start
+        })
+
         return response
 
