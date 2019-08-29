4006e99fc5c0c1067b1375b092499d9c63960adc
Jan Faracik <j.faracik@kainos.com>
2019-08-29 16:47:11 +0100

    push

--- organisations/models.py
+++ organisations/models.py
@@ -4,6 +4,7 @@ import reversion
 from django.db import models
 
 from addresses.models import Address
+from conf.exceptions import NotFoundError
 from static.countries.models import Country
 
 
@@ -20,6 +21,15 @@ class Organisation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     last_modified_at = models.DateTimeField(auto_now_add=True, blank=True)
 
+    def has_user(self, user):
+        from users.models import UserOrganisationRelationship
+        try:
+            user_organisation_relationship = UserOrganisationRelationship.objects.get(user=user,
+                                                                                      organisation=self)
+            return user_organisation_relationship
+        except UserOrganisationRelationship.DoesNotExist:
+            raise NotFoundError({'user': 'User does not belong to this organisation'})
+
 
 @reversion.register()
 class Site(models.Model):
@@ -35,4 +45,5 @@ class ExternalLocation(models.Model):
     name = models.TextField(default=None, blank=False)
     address = models.TextField(default=None, blank=False)
     country = models.ForeignKey(Country, blank=False, null=False, on_delete=models.CASCADE)
-    organisation = models.ForeignKey(Organisation, blank=True, null=True, related_name='external_location', on_delete=models.CASCADE)
+    organisation = models.ForeignKey(Organisation, blank=True, null=True, related_name='external_location',
+                                     on_delete=models.CASCADE)
