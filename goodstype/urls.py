dc1fe66a37b1e697e2347798da6d7198b3c80d24
cstobart <c.stobart@kainos.com>
2019-09-03 12:18:35 +0100

    Update urls.py

--- goodstype/urls.py
+++ goodstype/urls.py
@@ -5,8 +5,10 @@ from goodstype import views
 app_name = 'goodstype'
 
 urlpatterns = [
+    # ex: /goodstype/
     path('', views.GoodsTypeList.as_view(), name='goodstypes-list'),
+    # ex: /goodstype/<uuid:pk>/
     path('<uuid:pk>/', views.GoodsTypeDetail.as_view(), name='goodstypes-detail'),
-    # ex: /goods/<uuid:pk>/activity/
+    # ex: /goodstype/<uuid:pk>/activity/
     path('<uuid:pk>/activity/', views.GoodsTypeActivity.as_view(), name='activity'),
 ]
