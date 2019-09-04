26fa18dda11baf420c71e372c440ee4603f9182b
BenAndrew-Kainos <bena@kainos.com>
2019-09-04 17:09:47 +0100

    change end-user enpoint back to not passing end user id

--- drafts/views/draft_end_user_document_views.py
+++ drafts/views/draft_end_user_document_views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 
 from conf.authentication import ExporterAuthentication
 from drafts.libraries.get_document import get_document
+from drafts.libraries.get_draft import get_draft
 from end_user.document.models import EndUserDocument
 from end_user.models import EndUser
 from end_user.serializers import EndUserDocumentSerializer
@@ -14,17 +15,25 @@ from end_user.serializers import EndUserDocumentSerializer
 class EndUserDocuments(APIView):
     authentication_classes = (ExporterAuthentication,)
 
-    def _get_end_user(self, eu_pk):
+    def _get_end_user(self, pk, kwargs):
+        eu_pk = self._get_end_user_id(pk, kwargs)
         end_users = EndUser.objects.filter(id=eu_pk)
         if len(end_users) != 1:
             return None
         return end_users.first()
 
-    def get(self, request, pk, eu_pk):
+    def _get_end_user_id(self, pk, kwargs):
+        if 'eu_pk' in kwargs:
+            return kwargs['eu_pk']
+        else:
+            draft = get_draft(pk)
+            return draft.end_user
+
+    def get(self, request, pk, **kwargs):
         """
         Returns document for the specified end user
         """
-        end_user = self._get_end_user(eu_pk)
+        end_user = self._get_end_user(pk, kwargs)
         if not end_user:
             return JsonResponse(data={'error': 'No such user'},
                                 status=status.HTTP_400_BAD_REQUEST)
@@ -36,11 +45,11 @@ class EndUserDocuments(APIView):
             400: 'JSON parse error'
         })
     @transaction.atomic()
-    def post(self, request, pk, eu_pk):
+    def post(self, request, pk, **kwargs):
         """
         Adds a document to the specified end user
         """
-        end_user = self._get_end_user(eu_pk)
+        end_user = self._get_end_user(pk, kwargs)
         if not end_user:
             return JsonResponse(data={'error': 'No such user'},
                                 status=status.HTTP_400_BAD_REQUEST)
@@ -70,11 +79,11 @@ class EndUserDocuments(APIView):
             400: 'JSON parse error'
         })
     @transaction.atomic()
-    def delete(self, request, pk, eu_pk):
+    def delete(self, request, pk, **kwargs):
         """
         Deletes a document from the specified end user
         """
-        end_user = self._get_end_user(eu_pk)
+        end_user = self._get_end_user(pk, kwargs)
         if not end_user:
             return JsonResponse(data={'error': 'No such user'},
                                 status=status.HTTP_400_BAD_REQUEST)
