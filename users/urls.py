2fbc457e4d3728543cbcda034354c8900c22309a
Eugene Fahy <e.fahy@kainos.com>
2019-09-10 12:33:17 +0100

    fix: LT-1485 Correct notification counts to be specific to user and org (#110)
    fix: LT-1485 Correct notification counts to be specific to user and org

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from rest_framework import routers
 
 from users import views
 
 app_name = 'users'
-
-router = routers.SimpleRouter()
-router.register(r'notifications', views.NotificationViewset,)
-router.register(r'clc_notifications', views.ClcNotificationViewset,)
 
 urlpatterns = [
     # ex: /users/
@@ -18,4 +13,8 @@ urlpatterns = [
     path('<uuid:pk>/', views.UserDetail.as_view(), name='user'),
     # ex: /users/me/
     path('me/', views.UserMeDetail.as_view(), name='me'),
-] + router.urls
+    # ex: /users/notifications/
+    path('notifications/', views.NotificationViewset.as_view(), name='notifications'),
+    # ex: /users/clc_notifications/
+    path('clc_notifications/', views.ClcNotificationViewset.as_view(), name='clc_notifications'),
+]
