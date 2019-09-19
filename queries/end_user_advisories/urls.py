cd4f28860f059e52c6146f02f5d583857c3aeaed
Mark Spiers <marksp@kainos.com>
2019-09-19 10:17:37 +0100

    added url comments

--- queries/end_user_advisories/urls.py
+++ queries/end_user_advisories/urls.py
@@ -5,6 +5,8 @@ from queries.end_user_advisories import views
 app_name = 'end_user_advisories'
 
 urlpatterns = [
+    # ex: /queries/end-user-advisories/
     path('', views.EndUserAdvisoriesList.as_view(), name='end_user_advisories'),
+    # ex: /queries/end-user-advisories/1234567890   - 10 digit int for eua primary key
     path('<int:pk>/', views.EndUserAdvisoriesDetail.as_view(), name='end_user_advisory'),
 ]
