80514a57334d3209cd692a327ac12d89a1c2a79c
BenAndrew-Kainos <53567470+BenAndrew-Kainos@users.noreply.github.com>
2019-09-23 13:38:25 +0100

    LT-1465-optional-advice (#123)
    Made reason for refusal optional unless the case is being rejected and refactored case note posting

---
+++ cases/libraries/post_advice.py
@@ -0,0 +1,48 @@
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
+
+from cases.models import FinalAdvice, TeamAdvice
+from content_strings.strings import get_string
+
+
+def check_if_final_advice_exists(case):
+    if FinalAdvice.objects.filter(case=case):
+        return JsonResponse({'errors': 'Final advice already exists for this case'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+def check_if_team_advice_exists(case, user):
+    if TeamAdvice.objects.filter(case=case, team=user.team):
+        return JsonResponse({'errors': 'Team advice from your team already exists for this case'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+def check_refusal_errors(advice):
+    if advice['type'].lower() == 'refuse' and not advice['text']:
+        return {'text': [ErrorDetail(string=get_string('cases.advice_refusal_error'), code='blank')]}
+    return None
+
+
+def post_advice(request, case, serializer_object, team=False):
+    data = request.data
+
+    # Update the case and user in each piece of advice
+    refusal_error = False
+    for advice in data:
+        advice['case'] = str(case.id)
+        advice['user'] = str(request.user.id)
+        if team:
+            advice['team'] = str(request.user.team.id)
+        if not refusal_error:
+            refusal_error = check_refusal_errors(advice)
+
+    serializer = serializer_object(data=data, many=True)
+
+    if serializer.is_valid() and not refusal_error:
+        serializer.save()
+        return JsonResponse({'advice': serializer.data}, status=status.HTTP_201_CREATED)
+
+    if refusal_error:
+        serializer.errors[0].update(refusal_error)
+    return JsonResponse({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
