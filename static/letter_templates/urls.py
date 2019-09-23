120385200e98cd255b75f472f3b4b3b660814cf7
Jan Faracik <j.faracik@kainos.com>
2019-09-23 16:07:15 +0100

    Add

--- static/letter_templates/urls.py
+++ static/letter_templates/urls.py
@@ -5,5 +5,6 @@ from static.letter_templates import views
 app_name = 'letter_templates'
 
 urlpatterns = [
-    path('', views.LetterTemplatesList.as_view(), name='letter_templates')
+    path('', views.LetterTemplatesList.as_view(), name='letter_templates'),
+    path('<str:pk>/', views.LetterTemplatesDetail.as_view(), name='letter_template')
 ]
