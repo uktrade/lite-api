82a5c773746b3fa02e997377132bd59036abaadd
Jan Faracik <j.faracik@kainos.com>
2019-09-24 11:24:31 +0100

    Create control_list_entries.py

---
+++ static/management/commands/control_list_entries.py
@@ -0,0 +1,12 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    pipenv run ./manage.py seedcontrollistentries
+    """
+
+    def handle(self, *args, **options):
+        pass
+    
+
