908de6187f8dea1b8469cca8fcd27307df375372
cstobart <c.stobart@kainos.com>
2019-09-30 10:21:29 +0100

    Update tests_create_goods_countries_decisions.py

--- cases/tests/tests_create_goods_countries_decisions.py
+++ cases/tests/tests_create_goods_countries_decisions.py
@@ -34,17 +34,17 @@ class CreateGoodsCountriesDecisions(DataTestClient):
 
     def test_make_goods_countries_decisions_success(self):
         data = {'good_countries':
-                [
-                    {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'ZM', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'LR', 'advice_type': 'Refuse', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'AL', 'advice_type': 'No Licence Required', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '566e6af6-90af-4496-8b83-f639a1346b55', 'country': 'BW', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '566e6af6-90af-4496-8b83-f639a1346b55', 'country': 'DE', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '566e6af6-90af-4496-8b83-f639a1346b55', 'country': 'SC', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '27e73af3-6faf-45e6-8877-5138c446c311', 'country': 'DE', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '27e73af3-6faf-45e6-8877-5138c446c311', 'country': 'AI', 'advice_type': 'Refuse', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                    {'good': '27e73af3-6faf-45e6-8877-5138c446c311', 'country': 'BW', 'advice_type': 'Refuse', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
-                ]}
+            [
+                {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'ZM', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'LR', 'advice_type': 'Refuse', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'AL', 'advice_type': 'No Licence Required', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '566e6af6-90af-4496-8b83-f639a1346b55', 'country': 'BW', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '566e6af6-90af-4496-8b83-f639a1346b55', 'country': 'DE', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '566e6af6-90af-4496-8b83-f639a1346b55', 'country': 'SC', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '27e73af3-6faf-45e6-8877-5138c446c311', 'country': 'DE', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '27e73af3-6faf-45e6-8877-5138c446c311', 'country': 'AI', 'advice_type': 'Refuse', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '27e73af3-6faf-45e6-8877-5138c446c311', 'country': 'BW', 'advice_type': 'Refuse', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+            ]}
 
         response = self.client.post(self.goods_countries_url, data, **self.gov_headers)
 
@@ -53,4 +53,13 @@ class CreateGoodsCountriesDecisions(DataTestClient):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.json()['data']['good_countries']), 9)
 
-    def test_goods_countries_
+    def test_goods_countries_multiple_decisions_same_good_country_combination_failure(self):
+        data = {'good_countries':
+            [
+                {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'ZM', 'advice_type': 'Approve', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+                {'good': '58b63f71-943c-4b24-8497-b4480d1e7580', 'country': 'ZM', 'advice_type': 'Refuse', 'case': 'cc051e5a-fb32-428a-bd07-bf2de585e193'},
+            ]
+        }
+
+        response = self.client.post(self.goods_countries_url, data, **self.gov_headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
