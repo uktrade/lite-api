ced45b4eecf3d0bbbc8848846e23979bfed79922
Andy Taylor <a.taylor@kainos.com>
2019-10-03 12:27:56 +0100

    use isInstance

--- applications/libraries/application_helpers.py
+++ applications/libraries/application_helpers.py
@@ -1,10 +1,9 @@
-from applications.enums import ApplicationLicenceType
-from applications.models import BaseApplication
+from applications.models import BaseApplication, StandardApplication
 from applications.serializers import StandardApplicationSerializer, OpenApplicationSerializer
 
 
 def get_serializer_for_application(draft: BaseApplication, many=False):
-    if draft.licence_type == ApplicationLicenceType.STANDARD_LICENCE:
+    if isinstance(draft, StandardApplication):
         return StandardApplicationSerializer(draft, many=many)
     else:
         return OpenApplicationSerializer(draft, many=many)
