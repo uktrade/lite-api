84c324feb524925d9ab086d4775434b3b6038f28
Mark M <none>
2019-10-10 14:57:45 +0100

    Rename methods

--- applications/managers.py
+++ applications/managers.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class BaseApplicationManager(models.Manager):
-    def draft_applications(self, organisation):
+    def draft(self, organisation):
         return self.get_queryset().filter(submitted_at__isnull=True, organisation=organisation)
 
-    def submitted_applications(self, organisation):
+    def submitted(self, organisation):
         return self.get_queryset().filter(submitted_at__isnull=False, organisation=organisation)
