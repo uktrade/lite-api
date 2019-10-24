934e75e278983fa3835755f062d241f7c076b74b
Andy Taylor <a.taylor@kainos.com>
2019-10-24 17:57:08 +0100

    review comment

--- users/libraries/token_to_user.py
+++ users/libraries/token_to_user.py
@@ -3,8 +3,4 @@ from users.libraries.token import Token
 
 def token_to_user_pk(token):
     data = Token.decode_to_json(token)
-    if 'id' in data:
-        pk = data['id']
-        return pk
-
-    return None
+    return data.get('id')
