36560fd213c1f4ac9e06516081285db856c81161
Andy Taylor <a.taylor@kainos.com>
2019-10-25 13:15:19 +0100

    split validation for managing statuses

--- applications/libraries/application_helpers.py
+++ applications/libraries/application_helpers.py
@@ -3,7 +3,7 @@ from typing import Optional
 from applications.models import BaseApplication, StandardApplication
 from applications.serializers import StandardApplicationSerializer, OpenApplicationSerializer
 from static.statuses.enums import CaseStatusEnum
-from users.models import BaseUser, ExporterUser
+from users.models import BaseUser, ExporterUser, GovUser
 
 
 def get_serializer_for_application(application: BaseApplication, many=False):
@@ -24,17 +24,19 @@ def optional_str_to_bool(optional_string: str):
         raise ValueError('You provided ' + optional_string + ', while the allowed values are None, "true" or "false"')
 
 
-def validate_status_can_be_set(original_status: str, new_status: str, user: BaseUser) -> Optional[str]:
-    if isinstance(user, ExporterUser):
-        if original_status != CaseStatusEnum.SUBMITTED and new_status == CaseStatusEnum.APPLICANT_EDITING:
-            return f'Setting application status to "{new_status}" when application status is ' \
-                f'"{original_status}" is not allowed.'
+def validate_status_can_be_set_by_exporter_user(original_status: str, new_status: str) -> Optional[str]:
+    if original_status != CaseStatusEnum.SUBMITTED and new_status == CaseStatusEnum.APPLICANT_EDITING:
+        return f'Setting application status to "{new_status}" when application status is ' \
+            f'"{original_status}" is not allowed.'
 
-        if new_status == CaseStatusEnum.SUBMITTED:
-            return f'Setting application status to "{new_status}" is not allowed.'
-    else:
-        if new_status == CaseStatusEnum.APPLICANT_EDITING:
-            return f'Setting application status to "{new_status}" is not allowed for GovUsers.'
-        elif original_status == CaseStatusEnum.APPLICANT_EDITING:
-            return f'Setting application status when its existing status is ' \
-                f'"{original_status}" is not allowed for GovUsers.'
+    if new_status == CaseStatusEnum.SUBMITTED:
+        return f'Setting application status to "{new_status}" is not allowed.'
+
+
+def validate_status_can_be_set_by_gov_user(original_status: str, new_status: str) -> Optional[str]:
+    if new_status == CaseStatusEnum.APPLICANT_EDITING:
+        return f'Setting application status to "{new_status}" is not allowed for GovUsers.'
+
+    if original_status == CaseStatusEnum.APPLICANT_EDITING:
+        return f'Setting application status when its existing status is ' \
+            f'"{original_status}" is not allowed for GovUsers.'
