0582f415754cb91e6f6c0b4f8395d6492d50aa36
BenAndrew-Kainos <bena@kainos.com>
2019-11-06 14:26:00 +0000

    Add parent SeedCommand class

---
+++ static/management/SeedCommand.py
@@ -0,0 +1,21 @@
+from django.core.management import BaseCommand
+
+
+class SeedCommand(BaseCommand):
+    """
+    help and success message should be overridden
+    with messages relevant to the operation
+    """
+    help = None
+    success = None
+
+    """
+    operation should be overridden in child class
+    with the code required to execute the seed operation
+    """
+    def operation(self, *args, **options):
+        pass
+
+    def handle(self, *args, **options):
+        self.operation(*args, **options)
+        self.stdout.write(self.style.SUCCESS(self.success))
