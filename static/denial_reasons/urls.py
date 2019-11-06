a15d43bfa49f9fe86201295c0b64fd4c58746d2f
BenAndrew-Kainos <bena@kainos.com>
2019-11-06 13:37:36 +0000

    Remove unused denial reasons endpoint

--- static/denial_reasons/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from static.denial_reasons import views
-
-app_name = 'denial-reasons'
-
-urlpatterns = [
-    path('', views.DenialReasonsList.as_view(), name='denial-reasons')
-]
