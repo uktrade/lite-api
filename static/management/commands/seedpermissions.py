e4b8b6bc93968a80a7038b8d1077db349b27d0c4
BenAndrew-Kainos <bena@kainos.com>
2019-11-07 12:44:42 +0000

    Refactor seed permissions

--- static/management/commands/seedpermissions.py
+++ static/management/commands/seedpermissions.py
@@ -8,10 +8,12 @@ class Command(SeedCommand):
     """
     help = 'Seeds permissions'
     success = 'Successfully seeded permissions'
+    permissions = {
+        'MANAGE_TEAM_ADVICE': 'Manage team advice',
+        'MANAGE_FINAL_ADVICE': 'Manage final advice'
+    }
 
     def operation(self, *args, **options):
-        if not Permission.objects.filter(id='MANAGE_TEAM_ADVICE'):
-            Permission.objects.create(id='MANAGE_TEAM_ADVICE', name='Manage team advice')
-
-        if not Permission.objects.filter(id='MANAGE_FINAL_ADVICE'):
-            Permission.objects.create(id='MANAGE_FINAL_ADVICE', name='Manage final advice')
+        for id, name in self.permissions.items():
+            if not Permission.objects.filter(id=id):
+                Permission.objects.create(id=id, name=name)
