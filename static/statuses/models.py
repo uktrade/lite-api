158a2472d5b1b810ec9bd5b9088167c34c77de70
stephanieboyd <boydstep01@gmail.com>
2019-11-07 14:26:02 +0000

    Set is_read_only to nullable

--- static/statuses/models.py
+++ static/statuses/models.py
@@ -9,4 +9,4 @@ class CaseStatus(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     status = models.CharField(choices=CaseStatusEnum.choices, null=False, blank=False, max_length=50)
     priority = models.IntegerField(null=False, blank=False)
-    is_read_only = models.BooleanField(blank=False, null=False)
+    is_read_only = models.BooleanField(blank=False, null=True)
