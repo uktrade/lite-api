91534a937298dc511aca31e2153e5ab7e42f9331
Mark Spiers <marksp@kainos.com>
2019-11-13 11:07:20 +0000

    seed updated

--- static/management/commands/seedsystemflags.py
+++ static/management/commands/seedsystemflags.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
             if not Flag.objects.filter(id=SystemFlags.id[choice[0]],
                                        team=uuid.UUID('00000000-0000-0000-0000-000000000001')).count():
                 if Flag.objects.filter(id=SystemFlags.id[choice[0]]).count():
-                    Flag.objects.filter(id=SystemFlags.id[choice[0]]).update(id=uuid.uuid4())
+                    Flag.objects.filter(id=SystemFlags.id[choice[0]]).delete()
                 Flag.objects.create(id=SystemFlags.id[choice[0]],
                                     name=choice[1], level='Case', status='Active',
                                     team_id=uuid.UUID('00000000-0000-0000-0000-000000000001'))
