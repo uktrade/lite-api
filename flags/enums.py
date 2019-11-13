325a3731e29dfaf730e11403e76bc0b2ab77bc75
Mark Spiers <marksp@kainos.com>
2019-11-13 11:02:36 +0000

    seed

--- flags/enums.py
+++ flags/enums.py
@@ -20,3 +20,18 @@ class FlagStatuses:
         (ACTIVE, 'Active'),
         (DEACTIVATED, 'Deactivated'),
     ]
+
+
+class SystemFlags:
+    REFUSAL_FLAG = 'refusal_advice'
+
+    flags = [
+        (REFUSAL_FLAG, 'Refusal Advice')
+    ]
+
+    id = {
+        REFUSAL_FLAG: '00000000-0000-0000-0000-000000000001'
+    }
+
+
+
---
+++ static/management/commands/seedsystemflags.py
@@ -0,0 +1,30 @@
+import uuid
+
+from django.core.management import BaseCommand
+from django.db import transaction
+
+from flags.models import Flag
+from flags.enums import SystemFlags
+
+success_message = 'System flags seeded successfully!'
+
+
+class Command(BaseCommand):
+    help = 'Creates system flags'
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        """
+        pipenv run ./manage.py seedsystemflags
+        """
+
+        for choice in SystemFlags.flags:
+            if not Flag.objects.filter(id=SystemFlags.id[choice[0]],
+                                       team=uuid.UUID('00000000-0000-0000-0000-000000000001')).count():
+                if Flag.objects.filter(id=SystemFlags.id[choice[0]]).count():
+                    Flag.objects.filter(id=SystemFlags.id[choice[0]]).update(id=uuid.uuid4())
+                Flag.objects.create(id=SystemFlags.id[choice[0]],
+                                    name=choice[1], level='Case', status='Active',
+                                    team_id=uuid.UUID('00000000-0000-0000-0000-000000000001'))
+
+        self.stdout.write(self.style.SUCCESS(success_message))
