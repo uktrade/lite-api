d7a29ae831d96e1f70a408943f498fb1668a16be
Mark Spiers <marksp@kainos.com>
2019-11-13 11:38:28 +0000

    forward error handling for system flags

--- static/management/commands/seedsystemflags.py
+++ static/management/commands/seedsystemflags.py
@@ -3,8 +3,12 @@ import uuid
 from django.core.management import BaseCommand
 from django.db import transaction
 
+from cases.models import Case
 from flags.models import Flag
 from flags.enums import SystemFlags
+from goods.models import Good
+from organisations.models import Organisation
+from static.countries.models import Country
 
 success_message = 'System flags seeded successfully!'
 
@@ -19,11 +23,20 @@ class Command(BaseCommand):
         """
 
         for choice in SystemFlags.flags:
-            if not Flag.objects.filter(id=SystemFlags.id[choice[0]],
+            flag_id = SystemFlags.id[choice[0]]
+            if not Flag.objects.filter(id=flag_id,
                                        team=uuid.UUID('00000000-0000-0000-0000-000000000001')).count():
-                if Flag.objects.filter(id=SystemFlags.id[choice[0]]).count():
-                    Flag.objects.filter(id=SystemFlags.id[choice[0]]).delete()
-                Flag.objects.create(id=SystemFlags.id[choice[0]],
+                if Flag.objects.filter(id=flag_id).count():
+                    flag = Flag.objects.filter(id=flag_id).update(id=uuid.uuid4())
+                    if flag.level == 'Case':
+                        Case.objects.filter(flags__id=flag_id).update(flag.id)
+                    elif flag.level == 'Organisation':
+                        Organisation.objects.filter(flags__id=flag_id).update(flag.id)
+                    elif flag.level == 'Good':
+                        Good.objects.filter(flags__id=flag_id).update(flag.id)
+                    elif flag.level == 'Destination':
+                        Country.objects.filter(flags__id=flag_id).update(flag.id)
+                Flag.objects.create(id=flag_id,
                                     name=choice[1], level='Case', status='Active',
                                     team_id=uuid.UUID('00000000-0000-0000-0000-000000000001'))
 
