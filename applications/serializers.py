9754399052f1c399cccff01955ae3a24a54b99b5
Jan Faracik <j.faracik@kainos.com>
2019-11-13 16:26:29 +0000

    Initial upload

--- applications/serializers.py
+++ applications/serializers.py
@@ -11,12 +11,13 @@ from applications.models import BaseApplication, GoodOnApplication, ApplicationD
 from applications.models import Site, SiteOnApplication
 from cases.models import Case
 from conf.serializers import KeyValueChoiceField
-from content_strings.strings import get_string, get_const_string
+from content_strings.strings import get_string
 from documents.libraries.process_document import process_document
 from goods.models import Good
 from goods.serializers import GoodWithFlagsSerializer, GoodSerializer
 from goodstype.models import GoodsType
 from goodstype.serializers import FullGoodsTypeSerializer
+from lite_content.lite_api import strings
 from organisations.models import ExternalLocation, Organisation
 from organisations.serializers import SiteViewSerializer, ExternalLocationSerializer, OrganisationDetailSerializer
 from parties.serializers import EndUserSerializer, UltimateEndUserSerializer, ConsigneeSerializer, ThirdPartySerializer
@@ -62,7 +63,7 @@ class GoodOnApplicationCreateSerializer(serializers.ModelSerializer):
     quantity = DecimalField(max_digits=256, decimal_places=6,
                             error_messages={'invalid': get_string('goods.error_messages.invalid_qty')})
     value = DecimalField(max_digits=256, decimal_places=2,
-                         error_messages={'invalid': get_const_string('GOOD_ON_APPLICATION_ERROR_MISSING')}),
+                         error_messages={'invalid': strings.GOOD_ON_APPLICATION_ERROR_MISSING}),
     unit = ChoiceField(choices=Units.choices, error_messages={
         'required': get_string('goods.error_messages.required_unit'),
         'invalid_choice': get_string('goods.error_messages.required_unit')})
