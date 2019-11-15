22f257b1bf072d76634733a6dbae891fc0e23f4c
Mark Spiers <marksp@kainos.com>
2019-11-15 11:48:45 +0000

    removed more unused commands

--- cases/tests/tests_create_final_advice.py
+++ cases/tests/tests_create_final_advice.py
@@ -1,5 +1,3 @@
-from django.core.management import call_command
-from django.test import tag
 from django.urls import reverse
 from parameterized import parameterized
 from rest_framework import status
