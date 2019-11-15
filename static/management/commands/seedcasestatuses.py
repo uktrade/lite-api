33aeb5a41537715b869a8886f0222d77c1f2d5b6
stephanieboyd <boydstep01@gmail.com>
2019-11-15 12:02:34 +0000

    Add CaseStatusOnType table

--- static/management/commands/seedcasestatuses.py
+++ static/management/commands/seedcasestatuses.py
@@ -1,11 +1,12 @@
 from static.management.SeedCommand import SeedCommand, SeedCommandTest
-from static.statuses.models import CaseStatus
+from static.statuses.models import CaseStatus, CaseStatusOnType
 
-FILE = 'lite_content/lite-api/case_statuses.csv'
+STATUSES_FILE = 'lite_content/lite-api/case_statuses.csv'
+STATUS_ON_TYPE_FILE = 'lite_content/lite-api/case_status_on_type.csv'
 
 
 class Command(SeedCommand):
-    help = 'Creates case statuses'
+    help = 'Creates case statuses and case statuses on case types.'
     success = 'Successfully seeded case statuses'
     seed_command = 'seedcasestatuses'
 
@@ -13,12 +14,18 @@ class Command(SeedCommand):
         """
         pipenv run ./manage.py seedcasestatuses
         """
-        reader = self.read_csv(FILE)
+        reader = self.read_csv(STATUSES_FILE)
         for row in reader:
             CaseStatus.objects.get_or_create(status=row[0], priority=row[1], is_read_only=row[2])
+
+        status_ids = {status.status: status for status in CaseStatus.objects.all()}
+
+        # Case statuses on case types
+        for row in self.read_csv(STATUS_ON_TYPE_FILE):
+            CaseStatusOnType.objects.get_or_create(type=row[0], status=status_ids[row[1]])
 
 
 class SeedCaseStatusesTests(SeedCommandTest):
     def test_seed_case_statuses(self):
         self.seed_command(Command)
-        self.assertTrue(CaseStatus.objects.count() == len(self.read_csv(FILE)))
+        self.assertTrue(CaseStatus.objects.count() == len(self.read_csv(STATUSES_FILE)))
--- static/statuses/models.py
+++ static/statuses/models.py
@@ -2,9 +2,19 @@ import uuid
 
 from django.db import models
 
+from cases.enums import CaseType
+
 
 class CaseStatus(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     status = models.CharField(null=False, blank=False, max_length=50)
     priority = models.IntegerField(null=False, blank=False)
     is_read_only = models.BooleanField(blank=False, null=True)
+
+
+class CaseStatusOnType(models.Model):
+    class Meta:
+        unique_together = (('type', 'status'),)
+
+    type = models.CharField(choices=CaseType.choices, null=False, max_length=35)
+    status = models.ForeignKey(CaseStatus, on_delete=models.PROTECT, null=False)
