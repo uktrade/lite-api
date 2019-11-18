beb889c5ae61f0f0adee8e1d6d76532505587c00
stephanieboyd <boydstep01@gmail.com>
2019-11-18 11:38:59 +0000

    Remove docstring

--- static/statuses/enums.py
+++ static/statuses/enums.py
@@ -1,11 +1,4 @@
 class CaseStatusEnum:
-    """
-    This enum is used in this application's model and `0001_initial` migration file to create and populate the
-    `statuses_casestatus table`
-    If you want to add a status to that table:
-        add it to this enum, specify the status' priority and if it is read-only below
-        run `./manage.py makemigrations`
-    """
     APPLICANT_EDITING = "applicant_editing"
     FINALISED = "finalised"
     INITIAL_CHECKS = "initial_checks"
