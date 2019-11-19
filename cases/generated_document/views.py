f3c40338154de25ebbfc7d8bdc94273efde6ee2b
BenAndrew-Kainos <bena@kainos.com>
2019-11-19 17:47:45 +0000

    Add create generated document endpoint

--- cases/generated_document/views.py
+++ cases/generated_document/views.py
@@ -1,14 +1,48 @@
+from datetime import datetime
+
 from django.http import JsonResponse
 from rest_framework.views import APIView
 
+from cases.generated_document.helpers import html_to_pdf
 from cases.generated_document.models import GeneratedDocument
 from cases.generated_document.serialzers import GeneratedDocumentSerializer
 from cases.libraries.get_case import get_case
+from conf.authentication import GovAuthentication
+from documents.helpers import DocumentOperation
+from documents.models import Document
+from letter_templates.helpers import generate_preview, paragraphs_to_markdown
+from letter_templates.models import LetterTemplate
 
 
 class GeneratedDocuments(APIView):
-    def get(self, request, pk):
+    authentication_classes = (GovAuthentication,)
+    queryset = GeneratedDocument.objects.all()
+    serializer_class = GeneratedDocumentSerializer
+
+    def post(self, request, pk):
+        # TODO Add validation
         case = get_case(pk)
-        generated_docs = GeneratedDocument.objects.filter(case=case)
-        serializer = GeneratedDocumentSerializer(generated_docs, many=True)
-        return JsonResponse({"documents": serializer.data})
+        template = LetterTemplate.objects.get(id=request.data['template'])
+
+        paragraphs = [paragraph.text for paragraph in template.letter_paragraphs.all()]
+        paragraphs = paragraphs_to_markdown(paragraphs)
+        html = generate_preview(template.layout.filename, paragraphs)
+
+        pdf = html_to_pdf(html)
+        s3_key = DocumentOperation().upload_bytes_file(raw_file=pdf, file_extension=".pdf")
+
+        document = Document.objects.create(
+            name=s3_key,
+            s3_key=s3_key,
+            virus_scanned_at=datetime.now(),
+            safe=True
+        )
+
+        generated_doc = GeneratedDocument.objects.create(
+            document=document,
+            case=case,
+            template=template,
+            name=template.name+str(case.id)
+        )
+
+        return JsonResponse(data={"generated_document": generated_doc})
---
+++ documents/helpers.py
@@ -0,0 +1,26 @@
+import uuid
+
+import boto3
+
+from conf.settings import env
+
+
+class DocumentOperation:
+    @staticmethod
+    def _get_bucket():
+        return boto3.client(
+            "s3", aws_access_key_id=env("AWS_ACCESS_KEY_ID"), aws_secret_access_key=env("AWS_SECRET_ACCESS_KEY"),
+        )
+
+    @staticmethod
+    def _get_bucket_name():
+        return env("AWS_STORAGE_BUCKET_NAME")
+
+    def upload_bytes_file(self, raw_file, file_extension=None, s3_key=None):
+        if not s3_key:
+            s3_key = str(uuid.uuid4()) + file_extension
+        bucket = self._get_bucket()
+        bucket.put_object(Bucket=self._get_bucket_name(),
+                          Key=s3_key,
+                          Body=raw_file)
+        return s3_key
