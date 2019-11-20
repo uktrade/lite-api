34361bdfec9b41358861e1343ff04d1f5cecdd9e
BenAndrew-Kainos <bena@kainos.com>
2019-11-20 10:44:48 +0000

    Update seedgovuser to seed users correctly

--- static/management/commands/seedgovuser.py
+++ static/management/commands/seedgovuser.py
@@ -8,6 +8,7 @@ from users.models import GovUser, Role, Permission
 
 DEFAULT_ID = "00000000-0000-0000-0000-000000000001"
 SUPER_USER_ROLE_ID = "00000000-0000-0000-0000-000000000002"
+SUPER_USER_ROLE_NAME = "Super User"
 TEAM_NAME = "Admin"
 ROLE_NAME = "Default"
 
@@ -22,25 +23,31 @@ class Command(SeedCommand):
     seed_command = "seedgovuser"
 
     def operation(self, *args, **options):
+        # Default team
         team = Team.objects.get_or_create(id=DEFAULT_ID, name=TEAM_NAME)[0]
+
+        # "Default" role
         Role.objects.get_or_create(id=DEFAULT_ID, name=ROLE_NAME)
-        Role.objects.get_or_create(id=SUPER_USER_ROLE_ID, name="Super User")
+
+        # "Super User" role
+        super_user = Role.objects.get_or_create(id=SUPER_USER_ROLE_ID, name=SUPER_USER_ROLE_NAME)[0]
+
+        # Add all permissions to the super user role
         role = Role.objects.get(id=SUPER_USER_ROLE_ID)
         role.permissions.set(
             [
                 Permissions.MANAGE_FINAL_ADVICE,
-                Permissions.MANAGE_TEAM_CONFIRM_OWN_ADVICE,
                 Permissions.MANAGE_TEAM_ADVICE,
                 Permissions.REVIEW_GOODS,
                 Permissions.ADMINISTER_ROLES,
+                Permissions.MANAGE_TEAM_CONFIRM_OWN_ADVICE,
             ]
         )
         role.save()
+
+        # Create all SEED_USERS and give them the super user role
         for email in json.loads(env("SEED_USERS")):
-            GovUser.objects.get_or_create(email=email, team=team)
-        user = GovUser.objects.get(email="test-uat-user@digital.trade.gov.uk")
-        user.role = Role.objects.get(id=SUPER_USER_ROLE_ID)
-        user.save()
+            GovUser.objects.get_or_create(email=email, team=team, role=super_user)
 
 
 class SeedGovUserTests(SeedCommandTest):
