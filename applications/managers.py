aa7356203ab36523068183f3ccb402de6b6fd0b6
Mark Spiers <marksp@kainos.com>
2019-11-25 11:48:04 +0000

    ordering for applications

--- applications/managers.py
+++ applications/managers.py
@@ -3,15 +3,21 @@ from django.db import models
 
 class BaseApplicationManager(models.Manager):
     def drafts(self, organisation):
-        return self.get_queryset().filter(status__isnull=True, organisation=organisation)
+        return self.get_queryset().filter(status__isnull=True, organisation=organisation).order_by("-created_at")
 
     def submitted(self, organisation):
-        return self.get_queryset().filter(status__isnull=False, organisation=organisation)
+        return self.get_queryset().filter(status__isnull=False, organisation=organisation).order_by("-submitted_at")
 
 
 class HmrcQueryManager(models.Manager):
     def drafts(self, hmrc_organisation):
-        return self.get_queryset().filter(status__isnull=True, hmrc_organisation=hmrc_organisation)
+        return (
+            self.get_queryset().filter(status__isnull=True, hmrc_organisation=hmrc_organisation).order_by("-created_at")
+        )
 
     def submitted(self, hmrc_organisation):
-        return self.get_queryset().filter(status__isnull=False, hmrc_organisation=hmrc_organisation)
+        return (
+            self.get_queryset()
+            .filter(status__isnull=False, hmrc_organisation=hmrc_organisation)
+            .order_by("-submitted_at")
+        )
