2b6b798cb312cef080f6bc9f82ec6449ace39324
BenAndrew-Kainos <bena@kainos.com>
2019-11-27 11:26:07 +0000

    Remove generated document url example

--- cases/generated_documents/urls.py
+++ cases/generated_documents/urls.py
@@ -5,6 +5,5 @@ from cases.generated_documents import views
 app_name = "generated_documents"
 
 urlpatterns = [
-    # ex: /cases/<uuid:pk>/generated-documents/
     path("", views.GeneratedDocuments.as_view(), name="generated_documents"),
 ]
