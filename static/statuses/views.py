f529567701035c36aba0eaf9ffd170ff9c77f205
stephanieboyd <boydstep01@gmail.com>
2019-11-27 17:14:23 +0000

    Remove Draft status from static statuses endpoit

--- static/statuses/views.py
+++ static/statuses/views.py
@@ -7,6 +7,7 @@ from static.statuses.serializers import CaseStatusSerializer
 
 class StatusesAsList(APIView):
     def get(self, request):
-        statuses = CaseStatus.objects.all().order_by("priority")
+        # Exclude the 'Draft' system status
+        statuses = CaseStatus.objects.all().order_by("priority").exclude(status='draft')
         serializer = CaseStatusSerializer(statuses, many=True)
         return JsonResponse(data={"statuses": serializer.data})
