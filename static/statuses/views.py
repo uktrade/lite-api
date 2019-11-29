b8f1a3dceca49513bb31cb8c19b1da5b90b16bae
stephanieboyd <boydstep01@gmail.com>
2019-11-29 16:08:34 +0000

    Change status

--- static/statuses/views.py
+++ static/statuses/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework.status import HTTP_200_OK
 
 from static.statuses.models import CaseStatus
 from static.statuses.serializers import CaseStatusSerializer
@@ -11,7 +11,7 @@ class StatusesAsList(APIView):
         # Exclude the 'Draft' system status
         statuses = CaseStatus.objects.all().order_by("priority").exclude(status="draft")
         serializer = CaseStatusSerializer(statuses, many=True)
-        return JsonResponse(data={"statuses": serializer.data}, status=status.HTTP_200_OK)
+        return JsonResponse(data={"statuses": serializer.data}, status=HTTP_200_OK)
 
 
 class StatusProperties(APIView):
@@ -19,5 +19,5 @@ class StatusProperties(APIView):
         """ Return is_read_only and is_terminal properties for a case status. """
         status_properties = CaseStatus.objects.filter(status=status).values_list("is_read_only", "is_terminal")[0]
         return JsonResponse(
-            data={"is_read_only": status_properties[0], "is_terminal": status_properties[1]}, status=status.HTTP_200_OK
+            data={"is_read_only": status_properties[0], "is_terminal": status_properties[1]}, status=HTTP_200_OK
         )
