9bb2c6101e8b1644c64e83a19bebdf75845e7ea1
Jan Faracik <j.faracik@kainos.com>
2019-12-03 15:49:11 +0000

    Update tests_edit_exporter.py

--- users/tests/tests_edit_exporter.py
+++ users/tests/tests_edit_exporter.py
@@ -10,26 +10,6 @@ class UserTests(DataTestClient):
         original_last_name = self.exporter_user.last_name
         original_email = self.exporter_user.email
 
-        data = {"first_name": "hamster", "last_name": "gerbal", "email": "some@thing.com"}
-
-        url = reverse("users:user", kwargs={"pk": self.exporter_user.id})
-        response = self.client.put(url, data, **self.exporter_headers)
-        response_data = response.json()
-
-        self.assertNotEqual(response_data["user"]["first_name"], original_first_name)
-        self.assertNotEqual(response_data["user"]["last_name"], original_last_name)
-        self.assertNotEqual(response_data["user"]["email"], original_email)
-
-        url = reverse("users:authenticate")
-        data = {"email": "some@thing.com", "user_profile": {"first_name": "Matt", "last_name": "Berninger"}}
-        response = self.client.post(url, data, **self.exporter_headers)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_edit_a_user_some_fields(self):
-        original_first_name = self.exporter_user.first_name
-        original_last_name = self.exporter_user.last_name
-        original_email = self.exporter_user.email
-
         data = {"first_name": "hamster", "last_name": "gerbal"}
 
         url = reverse("users:user", kwargs={"pk": self.exporter_user.id})
@@ -37,6 +17,7 @@ class UserTests(DataTestClient):
         response = self.client.put(url, data, **self.exporter_headers)
         response_data = response.json()
 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNotEqual(response_data["user"]["first_name"], original_first_name)
         self.assertNotEqual(response_data["user"]["last_name"], original_last_name)
         self.assertEqual(response_data["user"]["email"], original_email)
