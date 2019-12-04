578ed39388d22e01f913a454655843153a21b262
Jan Faracik <j.faracik@kainos.com>
2019-12-04 14:11:08 +0000

    Remove first and last name from exporter users

--- users/tests/tests_edit_exporter.py
+++ users/tests/tests_edit_exporter.py
@@ -6,18 +6,12 @@ from test_helpers.clients import DataTestClient
 
 class UserTests(DataTestClient):
     def test_edit_a_user(self):
-        original_first_name = self.exporter_user.first_name
-        original_last_name = self.exporter_user.last_name
         original_email = self.exporter_user.email
-
-        data = {"first_name": "hamster", "last_name": "gerbal"}
-
+        data = {"email": "hamster@gmail.com"}
         url = reverse("users:user", kwargs={"pk": self.exporter_user.id})
 
         response = self.client.put(url, data, **self.exporter_headers)
         response_data = response.json()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertNotEqual(response_data["user"]["first_name"], original_first_name)
-        self.assertNotEqual(response_data["user"]["last_name"], original_last_name)
-        self.assertEqual(response_data["user"]["email"], original_email)
+        self.assertEqual(response_data["user"]["email"], data["email"])
