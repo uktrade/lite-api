7310d0e59a637931b1fbe3e130d9890a7e9b9f35
stephanieboyd <54948803+stephanieboyd@users.noreply.github.com>
2019-12-10 16:33:00 +0000

    Lt 1805 application major editing (#224)
    * Allow applicant editing in all editable states

--- applications/tests/tests_application_status.py
+++ applications/tests/tests_application_status.py
@@ -14,87 +14,105 @@ from users.libraries.user_to_token import user_to_token
 class ApplicationManageStatusTests(DataTestClient):
     def setUp(self):
         super().setUp()
-
         self.standard_application = self.create_standard_application(self.organisation)
         self.submit_application(self.standard_application)
         self.url = reverse("applications:manage_status", kwargs={"pk": self.standard_application.id})
 
-    def test_exporter_set_status_to_applicant_editing_when_previously_submitted_success(self):
+    def test_gov_set_application_status_to_applicant_editing_failure(self):
         data = {"status": CaseStatusEnum.APPLICANT_EDITING}
-        previous_submitted_at = self.standard_application.submitted_at
-
-        response = self.client.put(self.url, data=data, **self.exporter_headers)
-        self.standard_application.refresh_from_db()
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(self.standard_application.status, get_case_status_by_status(CaseStatusEnum.APPLICANT_EDITING))
-        self.assertEqual(self.standard_application.submitted_at, previous_submitted_at)
-
-    def test_exporter_set_status_to_applicant_editing_when_not_previously_submitted_failure(self,):
-        self.standard_application.status = get_case_status_by_status(CaseStatusEnum.INITIAL_CHECKS)
-        self.standard_application.save()
-
-        data = {"status": CaseStatusEnum.APPLICANT_EDITING}
-        response = self.client.put(self.url, data=data, **self.exporter_headers)
+        response = self.client.put(self.url, data=data, **self.gov_headers)
 
         self.standard_application.refresh_from_db()
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
-            response.json().get("errors")[0],
-            'Setting application status to "applicant_editing" when application status is '
-            '"initial_checks" is not allowed.',
+            json.loads(response.content).get("errors")[0],
+            'Setting application status to "applicant_editing" is not allowed for GovUsers.',
         )
-        self.assertEqual(
-            self.standard_application.status, get_case_status_by_status(CaseStatusEnum.INITIAL_CHECKS),
-        )
-
-    def test_exporter_set_status_to_submitted_failure(self):
-        self.standard_application.status = get_case_status_by_status(CaseStatusEnum.APPLICANT_EDITING)
-        self.standard_application.save()
-
-        data = {"status": CaseStatusEnum.SUBMITTED}
-        response = self.client.put(self.url, data=data, **self.exporter_headers)
-
-        self.standard_application.refresh_from_db()
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            json.loads(response.content).get("errors")[0], 'Setting application status to "submitted" is not allowed.',
-        )
-        self.assertEqual(
-            self.standard_application.status, get_case_status_by_status(CaseStatusEnum.APPLICANT_EDITING),
-        )
-
-    def test_exporter_set_status_to_invalid_status_failure(self):
-        self.standard_application.status = get_case_status_by_status(CaseStatusEnum.SUBMITTED)
-        self.standard_application.save()
-
-        data = {"status": "Invalid status"}
-        response = self.client.put(self.url, data=data, **self.exporter_headers)
-
-        self.standard_application.refresh_from_db()
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(
             self.standard_application.status, get_case_status_by_status(CaseStatusEnum.SUBMITTED),
         )
 
-    def test_set_application_status_on_application_not_in_users_organisation_failure(self,):
-        self.standard_application.status = get_case_status_by_status(CaseStatusEnum.SUBMITTED)
-        self.standard_application.save()
-
+    def test_set_application_status_on_application_not_in_users_organisation_failure(self):
+        self.submit_application(self.standard_application)
         other_organisation, _ = self.create_organisation_with_exporter_user()
+        data = {"status": "Invalid status"}
         permission_denied_user = UserOrganisationRelationship.objects.get(organisation=other_organisation).user
         permission_denied_user_headers = {
             "HTTP_EXPORTER_USER_TOKEN": user_to_token(permission_denied_user),
             "HTTP_ORGANISATION_ID": other_organisation.id,
         }
 
-        data = {"status": "something_stupid"}
         response = self.client.put(self.url, data=data, **permission_denied_user_headers)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertEqual(
-            self.standard_application.status, get_case_status_by_status(CaseStatusEnum.SUBMITTED),
-        )
+        self.assertEqual(self.standard_application.status, get_case_status_by_status(CaseStatusEnum.SUBMITTED))
+
+    def test_exporter_set_application_status_applicant_editing_when_in_editable_status_success(self):
+        self.submit_application(self.standard_application)
+
+        data = {"status": CaseStatusEnum.APPLICANT_EDITING}
+        response = self.client.put(self.url, data=data, **self.exporter_headers)
+
+        self.standard_application.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.standard_application.status, get_case_status_by_status(CaseStatusEnum.APPLICANT_EDITING))
+
+    def test_exporter_set_application_status_withdrawn_when_application_not_terminal_success(self):
+        self.submit_application(self.standard_application)
+
+        data = {"status": CaseStatusEnum.WITHDRAWN}
+        response = self.client.put(self.url, data=data, **self.exporter_headers)
+
+        self.standard_application.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.standard_application.status, get_case_status_by_status(CaseStatusEnum.WITHDRAWN))
+
+    def test_exporter_set_application_status_withdrawn_when_application_terminal_failure(self):
+        self.standard_application.status = get_case_status_by_status(CaseStatusEnum.FINALISED)
+        self.standard_application.save()
+
+        data = {"status": CaseStatusEnum.WITHDRAWN}
+        response = self.client.put(self.url, data=data, **self.exporter_headers)
+
+        self.standard_application.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(self.standard_application.status, get_case_status_by_status(CaseStatusEnum.FINALISED))
+
+    def test_exporter_set_application_status_applicant_editing_when_in_read_only_status_failure(self):
+        self.standard_application.status = get_case_status_by_status(CaseStatusEnum.UNDER_FINAL_REVIEW)
+        self.standard_application.save()
+
+        data = {"status": CaseStatusEnum.APPLICANT_EDITING}
+        response = self.client.put(self.url, data=data, **self.exporter_headers)
+
+        self.standard_application.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(self.standard_application.status, get_case_status_by_status(CaseStatusEnum.UNDER_FINAL_REVIEW))
+
+    @parameterized.expand(
+        [
+            status
+            for status, value in CaseStatusEnum.choices
+            if status not in [CaseStatusEnum.APPLICANT_EDITING, CaseStatusEnum.FINALISED, CaseStatusEnum.WITHDRAWN]
+        ]
+    )
+    def test_exporter_set_application_status_failure(self, new_status):
+        """ Test failure in setting application status to any status other than 'Applicant Editing' and 'Withdrawn'
+        as an exporter user.
+        """
+        self.submit_application(self.standard_application)
+
+        data = {"status": new_status}
+        response = self.client.put(self.url, data=data, **self.exporter_headers)
+
+        self.standard_application.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(self.standard_application.status, get_case_status_by_status(CaseStatusEnum.SUBMITTED))
 
     def test_gov_set_status_to_applicant_editing_failure(self):
         data = {"status": CaseStatusEnum.APPLICANT_EDITING}
@@ -114,7 +132,7 @@ class ApplicationManageStatusTests(DataTestClient):
         [
             status
             for status, value in CaseStatusEnum.choices
-            if status != CaseStatusEnum.APPLICANT_EDITING and status != CaseStatusEnum.FINALISED
+            if status not in [CaseStatusEnum.APPLICANT_EDITING, CaseStatusEnum.FINALISED]
         ]
     )
     def test_gov_set_status_for_all_except_applicant_editing_and_finalised_success(self, case_status):
