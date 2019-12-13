070e5972412e7826f25a696f9452ee95f320d1be
cstobart <c.stobart@kainos.com>
2019-12-13 16:11:38 +0000

    Update get_destination.py

--- cases/libraries/get_destination.py
+++ cases/libraries/get_destination.py
@@ -16,61 +16,56 @@ def get_destination(pk):
     return destination
 
 
+def get_standard_application_destination_flags(application, flags):
+    [flags.append(flag) for flag in application.end_user.flags.order_by("name") if flag not in flags]
+    [flags.append(flag) for flag in application.consignee.flags.order_by("name") if flag not in flags]
+    ultimate_end_users = [
+        ultimate_end_user_id for ultimate_end_user_id in application.ultimate_end_users.values_list("id", flat=True)
+    ]
+    for ultimate_end_user in ultimate_end_users:
+        [
+            flags.append(flag)
+            for flag in get_destination(str(ultimate_end_user)).flags.order_by("name")
+            if flag not in flags
+        ]
+    third_parties = [third_party_id for third_party_id in application.third_parties.values_list("id", flat=True)]
+    for third_party in third_parties:
+        [flags.append(flag) for flag in get_destination(third_party).flags.order_by("name") if flag not in flags]
+    return flags
+
+
 def get_destination_flags(instance):
     application = get_application(instance.id)
     countries = CountryOnApplication.objects.filter(application=instance).select_related("country")
     countries_flags = list(itertools.chain.from_iterable([c.country.flags.order_by("name") for c in countries]))
-    country_flag_data = FlagSerializer(countries_flags, many=True).data
-    flag_data = country_flag_data
+    flags = countries_flags
     if isinstance(application, StandardApplication):
-        end_user_flags = [flag for flag in application.end_user.flags.order_by("name")]
-        end_user_flag_data = FlagSerializer(end_user_flags, many=True).data
-        flag_data += end_user_flag_data
-        consignee_flags = [flag for flag in application.consignee.flags.order_by("name") if flag not in end_user_flags]
-        consignee_flag_data = FlagSerializer(consignee_flags, many=True).data
-        flag_data += consignee_flag_data
-        ultimate_end_users = [
-            ultimate_end_user_id for ultimate_end_user_id in application.ultimate_end_users.values_list("id", flat=True)
-        ]
-        ultimate_end_users_flags = []
-        for ultimate_end_user in ultimate_end_users:
-            [
-                ultimate_end_users_flags.append(flag)
-                for flag in get_destination(str(ultimate_end_user)).flags.order_by("name")
-                if flag not in (ultimate_end_users_flags or end_user_flags or consignee_flags)
-            ]
-        ultimate_end_user_flag_data = FlagSerializer(ultimate_end_users_flags, many=True).data
-        flag_data += ultimate_end_user_flag_data
-        third_parties = [third_party_id for third_party_id in application.third_parties.values_list("id", flat=True)]
-        third_parties_flags = []
-        for third_party in third_parties:
-            [
-                third_parties_flags.append(flag)
-                for flag in get_destination(third_party).flags.order_by("name")
-                if flag not in (third_parties_flags or ultimate_end_users_flags or end_user_flags or consignee_flags)
-            ]
-        third_party_flag_data = FlagSerializer(third_parties_flags, many=True).data
-        flag_data += third_party_flag_data
+        standard_application_destination_flags = get_standard_application_destination_flags(application, flags)
 
-    return flag_data
+        return flags + standard_application_destination_flags
+    return flags
 
 
 def get_ordered_flags(instance, team):
-    case_flag_data = FlagSerializer(instance.flags.order_by("name"), many=True).data
-    org_flag_data = FlagSerializer(instance.organisation.flags.order_by("name"), many=True).data
+    case_flags = instance.flags.order_by("name")
+    org_flags = instance.organisation.flags.order_by("name")
 
     if instance.type in [CaseTypeEnum.APPLICATION, CaseTypeEnum.HMRC_QUERY]:
         goods = GoodOnApplication.objects.filter(application=instance).select_related("good")
         goods_flags = list(itertools.chain.from_iterable([g.good.flags.order_by("name") for g in goods]))
         good_ids = {}
         goods_flags = [good_ids.setdefault(g, g) for g in goods_flags if g.id not in good_ids]  # dedup
-        good_flag_data = FlagSerializer(goods_flags, many=True).data
-        destination_flag_data = get_destination_flags(instance)
+        destination_flags = get_destination_flags(instance)
     else:
-        good_flag_data = []
-        destination_flag_data = []
+        goods_flags = []
+        destination_flags = []
 
-    flag_data = case_flag_data + org_flag_data + good_flag_data + destination_flag_data
+    flag_data = (
+        FlagSerializer(case_flags, many=True).data
+        + FlagSerializer(org_flags, many=True).data
+        + FlagSerializer(goods_flags, many=True).data
+        + FlagSerializer(destination_flags, many=True).data
+    )
     flag_data = sorted(flag_data, key=lambda x: x["name"])
 
     if not team:
