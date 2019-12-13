5a7eb10c7dc713311b90f60ee597f7f4f65bf5dc
asmund <asmund.bekker@gmail.com>
2019-12-13 14:56:40 +0000

    fix tests

--- audit_trail/tests/test_audit_payload.py
+++ audit_trail/tests/test_audit_payload.py
@@ -10,22 +10,22 @@ class TestPayload(DataTestClient):
             [
                 AuditType.ADD_FLAGS,
                 {'added_flags': 'Flag 1, Flag 2'},
-                'added flags: Flag 1, Flag 2'
+                'added flags: Flag 1, Flag 2.'
             ],
             [
                 AuditType.MOVE_CASE,
                 {'queues': 'Queue 1, Queue 2'},
-                'moved the case to: Queue 1, Queue 2'
+                'moved the case to: Queue 1, Queue 2.'
             ],
             [
                 AuditType.REMOVE_CASE,
                 {'queues': 'Queue 1, Queue 2'},
-                'removed case from queues: Queue 1, Queue 2'
+                'removed case from queues: Queue 1, Queue 2.'
             ],
             [
                 AuditType.UPLOAD_PARTY_DOCUMENT,
                 {'file_name': 'file.png', 'party_type': 'Party', 'party_name': 'Name'},
-                "uploaded the document file.png for Party Name"
+                "uploaded the document file.png for Party Name."
             ]
         ]
     )
