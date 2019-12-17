7bbe2da9fe239dc9cd2f5736fc94e922d7d14c5b
BenAndrew-Kainos <bena@kainos.com>
2019-12-17 14:03:34 +0000

    Add comment for GoodMissingDocumentReasons

--- static/missing_document_reasons/enums.py
+++ static/missing_document_reasons/enums.py
@@ -2,6 +2,14 @@ from lite_content.lite_api.static import GoodMissingDocumentReasonsOptions
 
 
 class GoodMissingDocumentReasons:
+    """
+    Selection of possible reasons why an exporter may not be expected to upload
+    a document for the good through LITE.
+    - NO_DOCUMENT: There is no document available for the good they've added
+    - OFFICIAL_SENSITIVE: The document is official-sensitive or above
+    - COMMERCIALLY_SENSITIVE: The document is commercially sensitive
+    In these cases ECJU be contacted to arrange an alternative
+    """
     NO_DOCUMENT = "NO_DOCUMENT"
     OFFICIAL_SENSITIVE = "OFFICIAL_SENSITIVE"
     COMMERCIALLY_SENSITIVE = "COMMERCIALLY_SENSITIVE"
