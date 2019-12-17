f10a5070898920be5142175ff234985e03e68ea5
Andy Taylor <a.taylor@kainos.com>
2019-12-17 12:46:52 +0000

    fix tests

--- audit_trail/tests/test_audit_trail.py
+++ audit_trail/tests/test_audit_trail.py
@@ -8,25 +8,24 @@ class CasesAuditTrail(DataTestClient):
     # TODO: test schema and creation
     def setUp(self):
         super().setUp()
-        self.draft = self.create_open_application(self.organisation)
+        self.case = self.create_standard_application_case(self.organisation)
 
     def test_audit_not_deleted(self):
         audit_qs = Audit.objects.all()
-        draft = self.create_open_application(self.organisation)
 
         self.assertEqual(audit_qs.count(), 0)
 
-        service.create(actor=self.exporter_user, verb=AuditType.ADD_FLAGS, target=draft)
+        service.create(actor=self.exporter_user, verb=AuditType.ADD_FLAGS, target=self.case)
 
         self.assertEqual(audit_qs.count(), 1)
 
-        draft.delete()
+        self.case.delete()
 
         self.assertEqual(audit_qs.count(), 1)
 
     def test_retrieve_audit_trail(self):
-        service.create(actor=self.exporter_user, verb=AuditType.CREATED_FINAL_ADVICE, target=self.draft)
+        service.create(actor=self.exporter_user, verb=AuditType.CREATED_FINAL_ADVICE, target=self.case)
 
-        audit_trail = service.get_obj_trail(self.draft)
+        audit_trail = service.get_obj_trail(self.case)
 
         self.assertEqual(len(audit_trail), 1)
