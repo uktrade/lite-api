925d82a02dc6ae953217ae1650303d8ff9a1eddb
stephanieboyd <boydstep01@gmail.com>
2019-12-18 12:21:11 +0000

    test build

--- queries/end_user_advisories/tests/test_end_user_advisories_status.py
+++ queries/end_user_advisories/tests/test_end_user_advisories_status.py
@@ -24,6 +24,9 @@ class EndUserAdvisoryStatus(DataTestClient):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(self.query.status.status, CaseStatusEnum.CLOSED)
 
+
+
+
     def test_gov_set_status_when_they_have_permission_to_reopen_closed_cases_success(self):
         data = {"status": CaseStatusEnum.SUBMITTED}
 
