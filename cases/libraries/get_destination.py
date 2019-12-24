42e9509bc3e97218ac75f51e6650412c3b4893c8
Piotr Konopacki <piotr@giffgaff.co.uk>
2019-12-24 14:35:32 +0000

    feat: LT-1634 - Set flag display order (#244)

--- cases/libraries/get_destination.py
+++ cases/libraries/get_destination.py
@@ -67,12 +67,15 @@ def get_ordered_flags(case: Case, team: Team):
         destination_flags = get_destination_flags(case)
 
     flag_data = (
-        FlagSerializer(case_flags, many=True).data
-        + FlagSerializer(org_flags, many=True).data
-        + FlagSerializer(set(goods_flags), many=True).data
-        + FlagSerializer(set(destination_flags), many=True).data
+        sort_flags_by_team_and_name(FlagSerializer(set(goods_flags), many=True).data, team)
+        + sort_flags_by_team_and_name(FlagSerializer(set(destination_flags), many=True).data, team)
+        + sort_flags_by_team_and_name(FlagSerializer(case_flags, many=True).data, team)
+        + sort_flags_by_team_and_name(FlagSerializer(org_flags, many=True).data, team)
     )
+    return flag_data
 
+
+def sort_flags_by_team_and_name(flag_data, team):
     flag_data = sorted(flag_data, key=lambda x: x["name"])
 
     if not team:
