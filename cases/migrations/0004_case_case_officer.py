71526c36c2fdb95b835375a1961d59fdfd15fd40
Mark Spiers <marksp@kainos.com>
2020-01-02 15:35:14 +0000

    linting

--- cases/migrations/0004_case_case_officer.py
+++ cases/migrations/0004_case_case_officer.py
@@ -7,14 +7,14 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
-        ('cases', '0003_auto_20191209_1209'),
+        ("users", "0001_initial"),
+        ("cases", "0003_auto_20191209_1209"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='case',
-            name='case_officer',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.DO_NOTHING, to='users.GovUser'),
+            model_name="case",
+            name="case_officer",
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.DO_NOTHING, to="users.GovUser"),
         ),
     ]
