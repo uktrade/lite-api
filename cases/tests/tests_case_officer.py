a597c7bcc0287be30d32cf5cd62726857c050830
Mark Spiers <marksp@kainos.com>
2020-01-07 11:42:10 +0000

    updated tests

--- cases/tests/tests_case_officer.py
+++ cases/tests/tests_case_officer.py
@@ -2,7 +2,6 @@ from django.urls import reverse
 from rest_framework import status
 
 from test_helpers.clients import DataTestClient
-from users.enums import UserStatuses
 
 
 class CaseOfficerTests(DataTestClient):
@@ -11,11 +10,10 @@ class CaseOfficerTests(DataTestClient):
         self.standard_application = self.create_standard_application(self.organisation)
         self.case = self.submit_application(self.standard_application)
         self.user = self.create_gov_user("new_user@their.email.com", self.team)
+        self.url = reverse("cases:case_officers", kwargs={"pk": self.case.id})
 
     def test_assign_gov_user(self):
-        self.url = reverse("cases:case_officer", kwargs={"pk": self.case.id, "gov_user_pk": self.user.id})
-
-        request = self.client.post(self.url, data={}, **self.gov_headers)
+        request = self.client.put(self.url, data={"gov_user_pk": self.user.id}, **self.gov_headers)
 
         self.assertEqual(request.status_code, status.HTTP_204_NO_CONTENT)
         self.case.refresh_from_db()
@@ -25,7 +23,6 @@ class CaseOfficerTests(DataTestClient):
     def test_unassign_gov_user(self):
         self.case.case_officer = self.user
         self.case.save()
-        self.url = reverse("cases:case_officers", kwargs={"pk": self.case.id})
 
         request = self.client.delete(self.url, data={}, **self.gov_headers)
 
@@ -43,18 +40,3 @@ class CaseOfficerTests(DataTestClient):
         request = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(request.status_code, status.HTTP_200_OK)
-
-    def test_dont_get_deactivated_users(self):
-        self.user.status = UserStatuses.DEACTIVATED
-        self.user.save()
-
-        self.url = reverse("cases:case_officers", kwargs={"pk": self.case.id})
-
-        request = self.client.get(self.url, **self.gov_headers)
-
-        self.assertEqual(request.status_code, status.HTTP_200_OK)
-
-        response_data = request.json()["GovUsers"]
-
-        for user in response_data["users"]:
-            self.assertIsNot(user["id"], self.user.id, "deactivated user should not be returned")
