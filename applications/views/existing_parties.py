7c909628c4833ab7658c5a3962866d497135880c
BenAndrew-Kainos <bena@kainos.com>
2020-01-07 17:03:15 +0000

    Get end user/consignee for all applications

--- applications/views/existing_parties.py
+++ applications/views/existing_parties.py
@@ -1,7 +1,12 @@
+from django.http import JsonResponse
 from rest_framework.views import APIView
 
+from applications.libraries.get_applications import get_case_payload
+from cases.enums import CaseTypeEnum
+from cases.models import Case
 from conf.authentication import ExporterAuthentication
 from conf.decorators import authorised_users
+from parties.serializers import PartySerializer
 from users.models import ExporterUser
 
 
@@ -10,7 +15,15 @@ class ExistingParties(APIView):
 
     @authorised_users(ExporterUser)
     def get(self, request, application):
-        """
-        Get existing parties created by the organisation
-        """
-        pass
+        parties = []
+        cases = Case.objects.filter(
+            organisation=application.organisation, type=CaseTypeEnum.APPLICATION, submitted_at__isnull=False
+        )
+
+        for case in cases:
+            payload = get_case_payload(case)
+            parties.append(payload.end_user)
+            parties.append(payload.consignee)
+
+        parties = PartySerializer(parties, many=True).data
+        return JsonResponse(data={"parties": parties})
