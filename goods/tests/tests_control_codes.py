6d5b8b202658800a6a700c2ae06573c745526d44
Mark Spiers <marksp@kainos.com>
2020-01-09 16:29:15 +0000

    tests

--- goods/tests/tests_control_codes.py
+++ goods/tests/tests_control_codes.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from applications.models import GoodOnApplication
 from conf import constants
 from goods.models import Good
+from goodstype.models import GoodsType
 from picklists.enums import PicklistType, PickListStatus
 from static.statuses.enums import CaseStatusEnum
 from static.statuses.libraries.get_case_status import get_case_status_by_status
@@ -13,7 +14,7 @@ from test_helpers.clients import DataTestClient
 from users.models import Role, GovUser
 
 
-class GoodsVerifiedTests(DataTestClient):
+class GoodsVerifiedTestsStandardApplication(DataTestClient):
     def setUp(self):
         super().setUp()
 
@@ -185,6 +186,183 @@ class GoodsVerifiedTests(DataTestClient):
             "report_summary": self.report_summary.pk,
             "control_code": "ML1a",
             "is_good_controlled": "yes",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class GoodsVerifiedTestsOpenApplication(DataTestClient):
+    def setUp(self):
+        super().setUp()
+
+        self.report_summary = self.create_picklist_item(
+            "Report Summary", self.team, PicklistType.REPORT_SUMMARY, PickListStatus.ACTIVE
+        )
+
+        role = Role(name="review_goods")
+        role.permissions.set([constants.GovPermissions.REVIEW_GOODS.name])
+        role.save()
+        self.gov_user.role = role
+        self.gov_user.save()
+
+        self.application = self.create_open_application(organisation=self.organisation)
+
+        self.good_1 = self.create_goods_type(self.application)
+        self.good_1.flags.set([self.create_flag("New Flag", "Good", self.team)])
+        self.good_2 = self.create_goods_type(self.application)
+
+        self.case = self.submit_application(self.application)
+        self.url = reverse_lazy("goods:control_code", kwargs={"case_pk": self.case.id})
+
+    def test_verify_single_good(self):
+        data = {
+            "objects": self.good_1.pk,
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "ML1a",
+            "is_good_controlled": "True",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        verified_good = GoodsType.objects.get(pk=self.good_1.pk)
+        self.assertEqual(verified_good.control_code, "ML1a")
+
+        # determine that flags have been removed when good verified
+        self.assertEqual(verified_good.flags.count(), 0)
+
+    def test_verify_multiple_goods(self):
+        data = {
+            "objects": [self.good_1.pk, self.good_2.pk],
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "ML1a",
+            "is_good_controlled": "True",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        self.good_1.refresh_from_db()
+        self.assertEqual(self.good_1.control_code, "ML1a")
+
+        self.good_2.refresh_from_db()
+        self.assertEqual(self.good_2.control_code, "ML1a")
+
+    def test_verify_single_good_NLR(self):
+        data = {
+            "objects": self.good_1.pk,
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "ML1a",
+            "is_good_controlled": "False",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        self.good_1.refresh_from_db()
+        self.assertEqual(self.good_1.control_code, "")
+
+        # determine that flags have been removed when good verified
+        self.assertEqual(self.good_1.flags.count(), 0)
+
+    def test_verify_multiple_goods_NLR(self):
+        data = {
+            "objects": [self.good_1.pk, self.good_2.pk],
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "",
+            "is_good_controlled": "False",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        self.good_1.refresh_from_db()
+        self.assertEqual(self.good_1.control_code, "")
+
+        self.good_2.refresh_from_db()
+        self.assertEqual(self.good_2.control_code, "")
+
+    def test_invalid_pk(self):
+        data = {
+            "objects": [self.team.pk, self.good_1.pk],  # first value is invalid
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "",
+            "is_good_controlled": "False",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        self.good_1.refresh_from_db()
+        self.assertEqual(self.good_1.control_code, "")
+
+    def test_invalid_control_code(self):
+        data = {
+            "objects": [self.good_1.pk, self.good_2.pk],
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "invalid",
+            "is_good_controlled": "True",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # since it has an invalid control code, flags should not be removed
+        self.good_1.refresh_from_db()
+        self.assertEqual(self.good_1.flags.count(), 1)
+
+    def test_controlled_good_empty_control_code(self):
+        data = {
+            "objects": [self.good_1.pk, self.good_2.pk],
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "",
+            "is_good_controlled": "True",
+        }
+
+        response = self.client.post(self.url, data, **self.gov_headers)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # since it has an empty control code, flags should not be removed
+        self.good_1.refresh_from_db()
+        self.assertEqual(self.good_1.flags.count(), 1)
+
+    def test_user_cannot_respond_to_good_without_permissions(self):
+        """
+        Tests that the right level of permissions are required
+        """
+        # create a second user to adopt the super user role as it will
+        # overwritten otherwise if we try and remove the role from the first
+        valid_user = GovUser(
+            email="test2@mail.com", first_name="John", last_name="Smith", team=self.team, role=self.super_user_role
+        )
+        valid_user.save()
+
+        self.gov_user.role = self.default_role
+        self.gov_user.save()
+
+        response = self.client.post(self.url, **self.gov_headers)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @parameterized.expand(CaseStatusEnum.terminal_statuses())
+    def test_cannot_set_control_codes_when_application_in_terminal_state(self, terminal_status):
+        self.application.status = get_case_status_by_status(terminal_status)
+        self.application.save()
+
+        data = {
+            "objects": self.good_1.pk,
+            "comment": "I Am Easy to Find",
+            "report_summary": self.report_summary.pk,
+            "control_code": "ML1a",
+            "is_good_controlled": "True",
         }
 
         response = self.client.post(self.url, data, **self.gov_headers)
