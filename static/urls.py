218eda0a3f8fc7b19577d6fdb2bb42d6845f6cdf
BenAndrew-Kainos <bena@kainos.com>
2020-01-16 15:38:16 +0000

    Static CaseTypes endpoint

--- static/urls.py
+++ static/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 app_name = "static"
 
 urlpatterns = [
+    path("case-types/", include("static.case_types.urls")),
     path("control-list-entries/", include("static.control_list_entries.urls")),
     path("countries/", include("static.countries.urls")),
     path("letter-layouts/", include("static.letter_layouts.urls")),
