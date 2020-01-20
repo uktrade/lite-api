9f785f3a665f500b48d988ae019f2f9060d7c0df
BenAndrew-Kainos <bena@kainos.com>
2020-01-20 14:49:19 +0000

    Add good document download endpoint

--- documents/urls.py
+++ documents/urls.py
@@ -6,4 +6,5 @@ app_name = "documents"
 
 urlpatterns = [
     path("<uuid:pk>/", views.DocumentDetail.as_view(), name="document"),
+    path("<uuid:good_pk>/<uuid:file_pk>/download/", views.ExporterGoodDocumentDownload.as_view(), name="download"),
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,10 +1,13 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from rest_framework import status
 from rest_framework.views import APIView
 
-from conf.authentication import GovAuthentication
+from conf.authentication import GovAuthentication, ExporterAuthentication
 from conf.exceptions import NotFoundError
 from documents.models import Document
 from documents.serializers import DocumentViewSerializer
+from documents.helpers import download_document
+from goods.libraries.get_goods import get_good
 
 
 class DocumentDetail(APIView):
@@ -20,3 +23,13 @@ class DocumentDetail(APIView):
             return JsonResponse({"document": serializer.data})
         except Document.DoesNotExist:
             raise NotFoundError({"document": "Document not found"})
+
+
+class ExporterGoodDocumentDownload(APIView):
+    authentication_classes = (ExporterAuthentication,)
+
+    def get(self, request, good_pk, file_pk):
+        good = get_good(good_pk)
+        if good.organisation != request.user.organisation:
+            return HttpResponse(status.HTTP_401_UNAUTHORIZED)
+        return download_document(file_pk)
