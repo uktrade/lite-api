94026b494f2e2134e37db3228f621308bc20176d
Andy Taylor <a.taylor@kainos.com>
2020-01-20 16:08:30 +0000

    trying to pass circle ci

--- static/private_venture_gradings/views.py
+++ static/private_venture_gradings/views.py
@@ -6,4 +6,4 @@ from goods.enums import PvGrading
 
 class PVGradingsList(APIView):
     def get(self, request):
-        return JsonResponse(data={"pv-gradings": PvGrading.choices})
+        return JsonResponse(data={"pv_gradings": [{choice[0]: choice[1]} for choice in PvGrading.choices]})
