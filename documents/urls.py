ab92ea00a45c23aa236f7a9ecffd8a20a2007458
BenAndrew-Kainos <bena@kainos.com>
2020-01-21 13:52:31 +0000

    Move download to seperate standard endpoint

--- documents/urls.py
+++ documents/urls.py
@@ -6,8 +6,11 @@ app_name = "documents"
 
 urlpatterns = [
     path("<uuid:pk>/", views.DocumentDetail.as_view(), name="document"),
+    path("<uuid:pk>/download/", views.DocumentDownload.as_view(), name="download"),
+
+    # Validation for accessing documents
     path(
-        "case/<uuid:case_pk>/<uuid:file_pk>/download/",
+        "case/<uuid:case_pk>/<uuid:file_pk>/",
         views.ExporterCaseDocumentDownload.as_view(),
         name="case_download",
     ),
--- documents/views.py
+++ documents/views.py
@@ -12,6 +12,9 @@ from documents.serializers import DocumentViewSerializer
 
 
 class DocumentDetail(APIView):
+    """
+    Get information about a Document
+    """
     authentication_classes = (GovAuthentication,)
 
     def get(self, request, pk):
@@ -26,6 +29,20 @@ class DocumentDetail(APIView):
             raise NotFoundError({"document": "Document not found"})
 
 
+class DocumentDownload(APIView):
+    authentication_classes = (ExporterAuthentication, GovAuthentication,)
+
+    """
+    Download a document
+    """
+    def get(self, request, pk):
+        try:
+            document = Document.objects.get(id=pk)
+            return download_document_from_s3(s3_key=document.s3_key, original_file_name=document.name)
+        except Document.DoesNotExist:
+            raise NotFoundError({"document": "Document not found"})
+
+
 class ExporterCaseDocumentDownload(APIView):
     authentication_classes = (ExporterAuthentication,)
 
@@ -35,6 +52,6 @@ class ExporterCaseDocumentDownload(APIView):
             return HttpResponse(status.HTTP_401_UNAUTHORIZED)
         try:
             document = CaseDocument.objects.get(id=file_pk, case=case)
-            return download_document_from_s3(s3_key=document.s3_key, original_file_name=document.name)
+            return JsonResponse({"document": document.id})
         except Document.DoesNotExist:
             raise NotFoundError({"document": "Document not found"})
