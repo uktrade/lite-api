f1b587c0e3af897a7efea9e6f0e5b6c1a3539e9f
BenAndrew-Kainos <bena@kainos.com>
2020-01-27 11:31:44 +0000

    Update DeleteApplication to paramertised tests

--- applications/tests/tests_delete.py
+++ applications/tests/tests_delete.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
+from parameterized import parameterized
 
 from applications.models import BaseApplication
 from test_helpers.clients import DataTestClient
@@ -8,65 +9,58 @@ from test_helpers.clients import DataTestClient
 class DeleteApplication(DataTestClient):
     def setUp(self):
         super().setUp()
+        self.applications = {
+            "STANDARD": self.create_standard_application(self.organisation),
+            "HMRC": self.create_hmrc_query(self.organisation)
+        }
+        self.users = {
+            "EXPORTER": self.exporter_headers,
+            "GOV": self.gov_headers,
+            "HMRC": self.hmrc_exporter_headers
+        }
 
-    def test_delete_draft_standard_application_as_exporter_success(self):
-        draft = self.create_standard_application(self.organisation)
+    @parameterized.expand([
+        ("STANDARD", "EXPORTER"),
+        ("HMRC", "HMRC")
+    ])
+    def test_delete_draft_application_as_valid_user_success(self, application_type, user):
+        draft = self.applications[application_type]
+        headers = self.users[user]
         number_of_applications = BaseApplication.objects.all().count()
         url = reverse("applications:application", kwargs={"pk": draft.id})
 
-        response = self.client.delete(url, **self.exporter_headers)
+        response = self.client.delete(url, **headers)
 
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertEqual(number_of_applications - 1, BaseApplication.objects.all().count())
 
-    def test_delete_draft_standard_application_as_gov_user_failure(self):
-        draft = self.create_standard_application(self.organisation)
+    @parameterized.expand([
+        ("STANDARD", "GOV"),
+        ("HMRC", "EXPORTER")
+    ])
+    def test_delete_draft_application_as_invalid_user_failure(self, application_type, user):
+        draft = self.applications[application_type]
+        headers = self.users[user]
         number_of_applications = BaseApplication.objects.all().count()
         url = reverse("applications:application", kwargs={"pk": draft.id})
 
-        response = self.client.delete(url, **self.gov_headers)
+        response = self.client.delete(url, **headers)
 
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
         self.assertEqual(number_of_applications, BaseApplication.objects.all().count())
 
-    def test_delete_submitted_standard_application_as_exporter_failure(self):
-        application = self.create_standard_application(self.organisation)
-        self.submit_application(application)
-        number_of_applications = BaseApplication.objects.all().count()
-        url = reverse("applications:application", kwargs={"pk": application.id})
-
-        response = self.client.delete(url, **self.exporter_headers)
-
-        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
-        self.assertEqual(number_of_applications, BaseApplication.objects.all().count())
-
-    def test_delete_draft_hmrc_query_as_hmrc_exporter_success(self):
-        draft = self.create_hmrc_query(self.organisation)
+    @parameterized.expand([
+        ("STANDARD", "EXPORTER"),
+        ("HMRC", "HMRC")
+    ])
+    def test_delete_submitted_application_failure(self, application_type, user):
+        draft = self.applications[application_type]
+        headers = self.users[user]
+        self.submit_application(draft)
         number_of_applications = BaseApplication.objects.all().count()
         url = reverse("applications:application", kwargs={"pk": draft.id})
 
-        response = self.client.delete(url, **self.hmrc_exporter_headers)
-
-        self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertEqual(number_of_applications - 1, BaseApplication.objects.all().count())
-
-    def test_delete_draft_hmrc_query_as_exporter_failure(self):
-        draft = self.create_hmrc_query(self.organisation)
-        number_of_applications = BaseApplication.objects.all().count()
-        url = reverse("applications:application", kwargs={"pk": draft.id})
-
-        response = self.client.delete(url, **self.exporter_headers)
-
-        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
-        self.assertEqual(number_of_applications, BaseApplication.objects.all().count())
-
-    def test_delete_submitted_hmrc_query_as_hmrc_exporter_failure(self):
-        application = self.create_hmrc_query(self.organisation)
-        self.submit_application(application)
-        number_of_applications = BaseApplication.objects.all().count()
-        url = reverse("applications:application", kwargs={"pk": application.id})
-
-        response = self.client.delete(url, **self.hmrc_exporter_headers)
+        response = self.client.delete(url, **headers)
 
         self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
         self.assertEqual(number_of_applications, BaseApplication.objects.all().count())
