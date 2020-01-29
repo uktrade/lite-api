e93a8e4393cbb6c45b0a5e6b81f39280785c1cb6
Mark Spiers <marksp@kainos.com>
2020-01-29 15:36:47 +0000

    linting

--- queries/goods_query/helpers.py
+++ queries/goods_query/helpers.py
@@ -1,9 +1,6 @@
 from conf.exceptions import NotFoundError
-from flags.enums import SystemFlags
 from lite_content.lite_api import strings
 from queries.goods_query.models import GoodsQuery
-from static.statuses.enums import CaseStatusEnum
-from static.statuses.libraries.get_case_status import get_case_status_by_status
 
 
 def get_goods_query_by_good(good):
