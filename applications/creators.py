a892e8d6d7563e0a4ce9c3e4282738839f4ccc85
Jan Faracik <j.faracik@kainos.com>
2020-01-30 17:27:01 +0000

    Update creators.py

--- applications/creators.py
+++ applications/creators.py
@@ -140,6 +140,7 @@ def validate_application_ready_for_submission(application):
     if (
         not SiteOnApplication.objects.filter(application=application).exists()
         and not ExternalLocationOnApplication.objects.filter(application=application).exists()
+        and not hasattr(application, "is_goods_departed") and not application.is_goods_departed
     ):
         errors["location"] = strings.Applications.Generic.NO_LOCATION_SET
 
