f770e5bf034538bdbd6bab03f52fa95c562d68a4
Piotr Konopacki <p.konopacki@kainos.com>
2020-01-30 11:55:01 +0000

    Black linting

--- organisations/views/users.py
+++ organisations/views/users.py
@@ -118,12 +118,11 @@ class UserDetail(APIView):
                     status=status.HTTP_400_BAD_REQUEST,
                 )
             # Cannot assign a role, you do not have access to
-            roles = filter_roles_by_user_role(
-                request.user, Role.objects.filter(organisation=org_pk), org_pk
-            )
+            roles = filter_roles_by_user_role(request.user, Role.objects.filter(organisation=org_pk), org_pk)
 
-            if data["role"] not in str(Roles.EXPORTER_PRESET_ROLES) and data["role"] not in [str(role.id) for role in
-                                                                                             roles]:
+            if data["role"] not in str(Roles.EXPORTER_PRESET_ROLES) and data["role"] not in [
+                str(role.id) for role in roles
+            ]:
                 raise PermissionDenied()
 
         serializer = UserOrganisationRelationshipSerializer(instance=user_relationship, data=data, partial=True)
