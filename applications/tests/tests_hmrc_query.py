799387b1caa4ac650f864ae6310d3e1132fc2509
Jan Faracik <j.faracik@kainos.com>
2020-01-31 15:54:17 +0000

    Update tests_hmrc_query.py

--- applications/tests/tests_hmrc_query.py
+++ applications/tests/tests_hmrc_query.py
@@ -1,10 +1,11 @@
-from lite_content.lite_api import strings
 from django.urls import reverse
 from rest_framework import status
 
+from applications.models import SiteOnApplication, ExternalLocationOnApplication
 from cases.enums import CaseTypeEnum
 from cases.models import Case
 from goodstype.models import GoodsType
+from lite_content.lite_api import strings
 from parties.models import PartyDocument
 from static.statuses.enums import CaseStatusEnum
 from test_helpers.clients import DataTestClient
@@ -13,18 +14,29 @@ from test_helpers.clients import DataTestClient
 class HmrcQueryTests(DataTestClient):
     def setUp(self):
         super().setUp()
-        self.draft = self.create_hmrc_query(self.organisation)
-        self.url = reverse("applications:application_submit", kwargs={"pk": self.draft.id})
+        self.hmrc_query = self.create_hmrc_query(self.organisation)
+        self.url = reverse("applications:application_submit", kwargs={"pk": self.hmrc_query.id})
 
     def test_submit_hmrc_query_success(self):
         response = self.client.put(self.url, **self.hmrc_exporter_headers)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         case = Case.objects.get()
-        self.assertEqual(case.id, self.draft.id)
+        self.assertEqual(case.id, self.hmrc_query.id)
         self.assertIsNotNone(case.submitted_at)
         self.assertEqual(case.status.status, CaseStatusEnum.SUBMITTED)
         self.assertEqual(case.type, CaseTypeEnum.HMRC_QUERY)
+
+    def test_submit_hmrc_query_with_goods_departed_success(self):
+        SiteOnApplication.objects.get(application=self.hmrc_query).delete()
+        self.hmrc_query.is_goods_departed = True
+        self.hmrc_query.save()
+
+        response = self.client.put(self.url, **self.hmrc_exporter_headers)
+        response_data = response.json()["application"]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_data["is_goods_departed"], True)
 
     def test_submit_hmrc_query_with_invalid_id_failure(self):
         draft_id = "90D6C724-0339-425A-99D2-9D2B8E864EC7"
@@ -35,9 +47,9 @@ class HmrcQueryTests(DataTestClient):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_submit_hmrc_query_without_end_user_failure(self):
-        self.draft.end_user = None
-        self.draft.save()
-        url = reverse("applications:application_submit", kwargs={"pk": self.draft.id})
+        self.hmrc_query.end_user = None
+        self.hmrc_query.save()
+        url = reverse("applications:application_submit", kwargs={"pk": self.hmrc_query.id})
 
         response = self.client.put(url, **self.hmrc_exporter_headers)
 
@@ -46,8 +58,8 @@ class HmrcQueryTests(DataTestClient):
         )
 
     def test_submit_hmrc_query_without_end_user_document_failure(self):
-        PartyDocument.objects.filter(party=self.draft.end_user).delete()
-        url = reverse("applications:application_submit", kwargs={"pk": self.draft.id})
+        PartyDocument.objects.filter(party=self.hmrc_query.end_user).delete()
+        url = reverse("applications:application_submit", kwargs={"pk": self.hmrc_query.id})
 
         response = self.client.put(url, **self.hmrc_exporter_headers)
 
@@ -58,7 +70,7 @@ class HmrcQueryTests(DataTestClient):
         )
 
     def test_submit_hmrc_query_without_goods_type_failure(self):
-        GoodsType.objects.filter(application=self.draft).delete()
+        GoodsType.objects.filter(application=self.hmrc_query).delete()
 
         response = self.client.put(self.url, **self.hmrc_exporter_headers)
 
@@ -89,3 +101,36 @@ class HmrcQueryTests(DataTestClient):
             text=strings.Applications.Standard.END_USER_DOCUMENT_INFECTED,
             status_code=status.HTTP_400_BAD_REQUEST,
         )
+
+    def test_setting_is_goods_departed_success(self):
+        """
+        Ensure that when setting is_goods_departed to True
+        that it deletes all existing sites and locations on that application
+        """
+        data = {"is_goods_departed": True}
+
+        response = self.client.put(
+            reverse("applications:application", kwargs={"pk": self.hmrc_query.id}), data, **self.hmrc_exporter_headers
+        )
+        self.hmrc_query.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(self.hmrc_query.is_goods_departed)
+        self.assertEqual(SiteOnApplication.objects.filter(application=self.hmrc_query).count(), 0)
+        self.assertEqual(ExternalLocationOnApplication.objects.filter(application=self.hmrc_query).count(), 0)
+
+    def test_setting_is_goods_departed_to_false_success(self):
+        """
+        Ensure that when setting is_goods_departed to False that it doesn't
+        delete all existing sites and locations on that application
+        """
+        data = {"is_goods_departed": False}
+
+        response = self.client.put(
+            reverse("applications:application", kwargs={"pk": self.hmrc_query.id}), data, **self.hmrc_exporter_headers
+        )
+        self.hmrc_query.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(self.hmrc_query.is_goods_departed)
+        self.assertEqual(SiteOnApplication.objects.filter(application=self.hmrc_query).count(), 1)
