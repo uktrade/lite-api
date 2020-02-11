f91312ae04692761e6c4b81bf322cbdf246750ff
stephanieboyd <boydstep01@gmail.com>
2020-02-11 12:05:17 +0000

    Black formatting

--- users/migrations/0002_auto_20200211_1101.py
+++ users/migrations/0002_auto_20200211_1101.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='permission',
-            name='id',
+            model_name="permission",
+            name="id",
             field=models.CharField(editable=False, max_length=35, primary_key=True, serialize=False),
         ),
     ]
