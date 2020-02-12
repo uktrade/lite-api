6486b7cb2d673f9e0c489a3eea35fdda8ea1bd6b
Mark Spiers <marksp@kainos.com>
2020-02-12 14:47:46 +0000

    black

--- applications/migrations/0004_auto_20200212_1443.py
+++ applications/migrations/0004_auto_20200212_1443.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('applications', '0003_auto_20200210_1326'),
+        ("applications", "0003_auto_20200210_1326"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='standardapplication',
-            name='reference_number_on_information_form',
+            model_name="standardapplication",
+            name="reference_number_on_information_form",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
