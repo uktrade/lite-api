2acd5dc0d50eddfd9af1d782830bb1356d079121
Andy Taylor <a.taylor@kainos.com>
2020-02-17 14:24:43 +0000

    fix filtering for applications of specific type

--- applications/libraries/get_applications.py
+++ applications/libraries/get_applications.py
@@ -17,26 +17,23 @@ def get_application(pk, organisation_id=None):
     if organisation_id:
         kwargs["organisation_id"] = str(organisation_id)
 
-    application_case_type_sub_type = _get_application_case_type_sub_type(pk)
+    application_type = _get_application_type(pk)
 
     try:
-        if application_case_type_sub_type == CaseTypeSubTypeEnum.STANDARD:
+        if application_type == CaseTypeSubTypeEnum.STANDARD:
             return StandardApplication.objects.get(pk=pk, **kwargs)
-        elif application_case_type_sub_type == CaseTypeSubTypeEnum.OPEN:
+        elif application_type == CaseTypeSubTypeEnum.OPEN:
             return OpenApplication.objects.get(pk=pk, **kwargs)
-        elif application_case_type_sub_type == CaseTypeSubTypeEnum.HMRC:
+        elif application_type == CaseTypeSubTypeEnum.HMRC:
             return HmrcQuery.objects.get(pk=pk)
-        elif application_case_type_sub_type == CaseTypeSubTypeEnum.EXHIBITION:
+        elif application_type == CaseTypeSubTypeEnum.EXHIBITION:
             return ExhibitionClearanceApplication.objects.get(pk=pk)
-        elif application_case_type_sub_type == CaseTypeSubTypeEnum.GIFTING:
+        elif application_type == CaseTypeSubTypeEnum.GIFTING:
             return GiftingClearanceApplication.objects.get(pk=pk)
-        elif application_case_type_sub_type == CaseTypeSubTypeEnum.F680:
+        elif application_type == CaseTypeSubTypeEnum.F680:
             return F680ClearanceApplication.objects.get(pk=pk)
         else:
-            raise NotImplementedError(
-                f"get_application does not support this sub-type for the application's case:"
-                f" {application_case_type_sub_type}"
-            )
+            raise NotImplementedError(f"get_application does not support this application type: {application_type}")
     except (
         StandardApplication.DoesNotExist,
         OpenApplication.DoesNotExist,
@@ -45,8 +42,8 @@ def get_application(pk, organisation_id=None):
         raise Http404
 
 
-def _get_application_case_type_sub_type(pk):
+def _get_application_type(pk):
     try:
-        return BaseApplication.objects.values_list("application_type", flat=True).get(pk=pk)
+        return BaseApplication.objects.values_list("case_type__sub_type", flat=True).get(pk=pk)
     except BaseApplication.DoesNotExist:
         raise Http404
