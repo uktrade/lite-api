ee70f96424102a724520541b2397877f48531145
cstobart <c.stobart@kainos.com>
2020-02-17 15:35:55 +0000

    Update tests_case_notes.py

--- cases/tests/tests_case_notes.py
+++ cases/tests/tests_case_notes.py
@@ -123,11 +123,9 @@ class CaseNotesViewTests(DataTestClient):
     def test_view_pre_submitted_case_notes_post_submit(self):
         case = self.create_draft_standard_application(self.organisation)
         case_note = self.create_case_note(case, "This is cool text", self.exporter_user, True)
-
         self.submit_application(case)
 
         url = reverse("cases:case_notes", kwargs={"pk": case.id})
-
         response = self.client.get(url, **self.gov_headers)
 
         self.assertIn(str(case_note.id), str(response.json()))
