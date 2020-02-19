e7eb7c1f72d05071cfee73c94d45580d8de051fa
BenAndrew-Kainos <bena@kainos.com>
2020-02-19 14:09:51 +0000

    SLA_UPDATE_TASK_TIME

--- cases/sla.py
+++ cases/sla.py
@@ -2,9 +2,9 @@ from datetime import datetime, time
 
 from background_task import background
 
-SLA_UPDATE_TASK_HOUR = 24
+SLA_UPDATE_TASK_TIME = time(24, 0, 0)
 
 
-@background(schedule=datetime.combine(datetime.now(), time(SLA_UPDATE_TASK_HOUR, 0, 0)))
+@background(schedule=datetime.combine(datetime.now(), SLA_UPDATE_TASK_TIME))
 def update_cases_sla():
     print("abc")
