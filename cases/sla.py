c1e36aad7045e1568f122c3d5e0f68c120bcd929
BenAndrew-Kainos <bena@kainos.com>
2020-02-19 16:52:32 +0000

    Filter cases submitted before 6pm

--- cases/sla.py
+++ cases/sla.py
@@ -2,9 +2,13 @@ from datetime import datetime, time
 
 from background_task import background
 
-SLA_UPDATE_TASK_TIME = time(24, 0, 0)
+from cases.models import Case
+
+SLA_UPDATE_TASK_TIME = time(0, 0, 0)
+SLA_UPDATE_CUTOFF_TIME = time(18, 0, 0)
 
 
 @background(schedule=datetime.combine(datetime.now(), SLA_UPDATE_TASK_TIME))
 def update_cases_sla():
-    print("abc")
+    cases = Case.objects.filter(submitted_at__lt=datetime.combine(datetime.now(), SLA_UPDATE_CUTOFF_TIME))
+    return cases
