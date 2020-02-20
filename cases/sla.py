fb851c546c5ebf9df5c2cc156b2aeac9a5d18572
BenAndrew-Kainos <bena@kainos.com>
2020-02-20 09:48:49 +0000

    Fetch bank holidays from GOV api

--- cases/sla.py
+++ cases/sla.py
@@ -1,4 +1,5 @@
 from datetime import datetime, time
+import requests
 
 from background_task import background
 
@@ -6,6 +7,8 @@ from cases.models import Case
 
 SLA_UPDATE_TASK_TIME = time(0, 0, 0)
 SLA_UPDATE_CUTOFF_TIME = time(18, 0, 0)
+BANK_HOLIDAY_API = "https://www.gov.uk/bank-holidays.json"
+BACKUP_FILE_NAME = "bank-holidays.csv"
 
 
 def is_weekend(date):
@@ -13,16 +16,26 @@ def is_weekend(date):
     return date.weekday() > 4
 
 
-def is_bank_holiday(date):
+def get_bank_holidays():
     """
-    Uses the GOV bank holidays API (or a cached version if the service is unavailable).
-    Returns whether today's date is in this list of bank holidays.
+    Uses the GOV bank holidays API (or a local backup if the service is unavailable).
+    Returns whether the list of bank holidays
     """
-    bank_holiday_json = None
-    bank_holidays = [event["date"] for event in bank_holiday_json["england-and-wales"]["events"]]
+    r = requests.get(BANK_HOLIDAY_API)
+    if r.status_code != 200:
+        with open(BACKUP_FILE_NAME, "r") as backup_file:
+            return backup_file.read().split(",")
+    else:
+        data = r.json()["england-and-wales"]["events"]
+        data = [event["date"] for event in data]
+        with open(BACKUP_FILE_NAME, "w") as backup_file:
+            backup_file.write(",".join(data))
+        return data
 
+
+def is_bank_holiday(date):
     formatted_date = date.strftime("%Y-%m-%d")
-    return formatted_date in bank_holidays
+    return formatted_date in get_bank_holidays()
 
 
 @background(schedule=datetime.combine(datetime.now(), SLA_UPDATE_TASK_TIME))
