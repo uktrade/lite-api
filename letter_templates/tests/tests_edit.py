7621a3c9a10839b6904e0655e56a9325c14dbc5e
Andy Taylor <a.taylor@kainos.com>
2020-02-20 14:44:57 +0000

    tests for creating templates

--- letter_templates/tests/tests_edit.py
+++ letter_templates/tests/tests_edit.py
@@ -86,7 +86,6 @@ class LetterTemplateEditTests(DataTestClient):
         response = self.client.put(self.url, data, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
         self.assertEqual(
             response.json()["errors"]["case_types"],
             [strings.LetterTemplates.DECISIONS_NON_APPLICATION_CASE_TYPES_ERROR + ", ".join(case_type_references)],
