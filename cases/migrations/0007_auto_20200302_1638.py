e8535dac962de0b5a7796974c098ea6553ff8780
Chris Iliev <c.iliev@kainos.com>
2020-03-02 16:40:01 +0000

    black on migrations :/

--- cases/migrations/0007_auto_20200302_1638.py
+++ cases/migrations/0007_auto_20200302_1638.py
@@ -6,18 +6,36 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cases', '0006_auto_20200225_1128'),
+        ("cases", "0006_auto_20200225_1128"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='advice',
-            name='collated_pv_grading',
+            model_name="advice",
+            name="collated_pv_grading",
             field=models.TextField(blank=True, default=None, null=True),
         ),
         migrations.AddField(
-            model_name='advice',
-            name='pv_grading',
-            field=models.CharField(choices=[('uk_unclassified', 'UK UNCLASSIFIED'), ('uk_official', 'UK OFFICIAL'), ('uk_official_sensitive', 'UK OFFICAL - SENSITIVE'), ('uk_secret', 'UK SECRET'), ('uk_top_secret', 'UK TOP SECRET'), ('nato_unclassified', 'NATO UNCLASSIFIED'), ('nato_confidential', 'NATO CONFIDENTIAL'), ('nato_restricted', 'NATO RESTRICTED'), ('nato_secret', 'NATO SECRET'), ('occar_unclassified', 'OCCAR UNCLASSIFIED'), ('occar_confidential', 'OCCAR CONFIDENTIAL'), ('occar_restricted', 'OCCAR RESTRICTED'), ('occar_secret', 'OCCAR SECRET')], max_length=30, null=True),
+            model_name="advice",
+            name="pv_grading",
+            field=models.CharField(
+                choices=[
+                    ("uk_unclassified", "UK UNCLASSIFIED"),
+                    ("uk_official", "UK OFFICIAL"),
+                    ("uk_official_sensitive", "UK OFFICAL - SENSITIVE"),
+                    ("uk_secret", "UK SECRET"),
+                    ("uk_top_secret", "UK TOP SECRET"),
+                    ("nato_unclassified", "NATO UNCLASSIFIED"),
+                    ("nato_confidential", "NATO CONFIDENTIAL"),
+                    ("nato_restricted", "NATO RESTRICTED"),
+                    ("nato_secret", "NATO SECRET"),
+                    ("occar_unclassified", "OCCAR UNCLASSIFIED"),
+                    ("occar_confidential", "OCCAR CONFIDENTIAL"),
+                    ("occar_restricted", "OCCAR RESTRICTED"),
+                    ("occar_secret", "OCCAR SECRET"),
+                ],
+                max_length=30,
+                null=True,
+            ),
         ),
     ]
