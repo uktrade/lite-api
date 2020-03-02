1a314e3c1445bc833b74fdc2a59e25e4ffcc993a
Andy Taylor <a.taylor@kainos.com>
2020-03-02 10:18:41 +0000

    filter  DB instead of reading csv

--- static/countries/tests.py
+++ static/countries/tests.py
@@ -3,7 +3,7 @@ import csv
 from rest_framework import status
 from rest_framework.reverse import reverse
 
-from static.management.commands.seedcountries import COUNTRIES_FILE
+from static.countries.models import Country
 from test_helpers.clients import DataTestClient
 
 
@@ -13,26 +13,21 @@ class CountriesTests(DataTestClient):
 
     def setUp(self):
         super().setUp()
-        self.countries = None
-        with open(COUNTRIES_FILE, newline="") as csvfile:
-            reader = csv.DictReader(csvfile)
-            # Convert CSV to a list of the names of the countries
-            self.countries = [list(x.items())[0][1] for x in reader]
 
     def test_get_countries(self):
         response = self.client.get(self.url)
         countries = response.json()["countries"]
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(countries[0]["name"], self.countries[0])
-        self.assertEqual(countries[-1]["name"], self.countries[-1])
+        self.assertTrue(Country.objects.filter(name=countries[0]["name"]).exists())
+        self.assertTrue(Country.objects.filter(name=countries[-1]["name"]).exists())
 
     def test_filter_countries_by_exclude(self):
         response = self.client.get(self.url + "?exclude=GB&exclude=PL")
         countries = response.json()["countries"]
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(countries[0]["name"], self.countries[0])
-        self.assertEqual(countries[-1]["name"], self.countries[-1])
+        self.assertTrue(Country.objects.filter(name=countries[0]["name"]).exists())
+        self.assertTrue(Country.objects.filter(name=countries[-1]["name"]).exists())
         self.assertNotIn("United Kingdom", str(countries))
         self.assertNotIn("Poland", str(countries))
