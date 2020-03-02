40a7cf43b2bc21c61bd23963c689155ec73def3d
Andy Taylor <a.taylor@kainos.com>
2020-03-02 10:15:02 +0000

    refactor some code into methods for external locations

--- applications/views/external_locations.py
+++ applications/views/external_locations.py
@@ -46,19 +46,9 @@ class ApplicationExternalLocations(APIView):
         data = request.data
         location_ids = data.get("external_locations")
 
-        # Validate that have_goods_departed isn't True
-        if getattr(application, "have_goods_departed", False):
-            return JsonResponse(
-                data={"errors": {"external_locations": ["Application has have_goods_departed set to True"]}},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        # Validate that there are actually external locations
-        if not location_ids:
-            return JsonResponse(
-                data={"errors": {"external_locations": ["You have to pick at least one external location"]}},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        errors = self._validate_request(application, location_ids)
+        if errors:
+            return JsonResponse(data={"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
         previous_locations = ExternalLocationOnApplication.objects.filter(application=application)
         previous_location_ids = [
@@ -66,97 +56,62 @@ class ApplicationExternalLocations(APIView):
             for previous_location_id in previous_locations.values_list("external_location__id", flat=True)
         ]
 
-        if (
-            is_case_status_draft(application.status.status)
-            or application.status.status == CaseStatusEnum.APPLICANT_EDITING
-        ):
-            new_locations = [
-                get_location(location_id, request.user.organisation)
-                for location_id in location_ids
-                if location_id not in previous_location_ids
-            ]
-        else:
-            if has_previous_sites(application):
-                return JsonResponse(
-                    data={
-                        "errors": {
-                            "external_locations": [
-                                "Go back and change your answer from ‘Change a site, or delete a good, third party or "
-                                "country’ to ’Change something else’."
-                            ]
-                        }
-                    },
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-            previous_location_countries = list(
-                previous_locations.values_list("external_location__country__id", flat=True)
-            )
-            new_locations = []
-
-            for location_id in location_ids:
-                new_location = get_location(location_id, request.user.organisation)
-
-                if previous_location_countries and new_location.country.id not in previous_location_countries:
-                    return JsonResponse(
-                        data={
-                            "errors": {
-                                "external_locations": [
-                                    "Go back and change your answer from ‘Change a site, or delete a good, third party or "
-                                    "country’ to ’Change something else’."
-                                ]
-                            }
-                        },
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
-
-                elif str(new_location.id) not in previous_location_ids:
-                    new_locations.append(new_location)
+        new_locations, errors = self._get_new_locations(application, request.user.organisation, location_ids,
+                                                        previous_locations, previous_location_ids)
+        if errors:
+            return JsonResponse(data={"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
         # Update activity
         application.activity = self.BROKERING
         application.save()
 
+        external_locations, errors = self._set_locations_and_sites(data.get("method"), previous_location_ids,
+                                                              location_ids, previous_locations, new_locations,
+                                                              application, request.user)
+        if errors:
+            return JsonResponse(data={"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        return JsonResponse(data={"external_locations": external_locations}, status=status.HTTP_201_CREATED)
+
+    def _set_locations_and_sites(self, method, previous_location_ids, location_ids, previous_locations,
+                                 new_locations, application, user):
         # Get locations to be removed
         removed_locations = []
-        if data.get("method") != "append_location":
+        if method != "append_location":
             removed_location_ids = list(set(previous_location_ids) - set(location_ids))
             removed_locations = previous_locations.filter(external_location__id__in=removed_location_ids)
 
         # Append new ExternalLocationOnApplications
-        response_data = []
+        external_locations = []
         for new_location in new_locations:
             serializer = ExternalLocationOnApplicationSerializer(
-                data={"external_location": new_location.id, "application": application.id,}
+                data={"external_location": str(new_location.id), "application": str(application.id)}
             )
 
             if application.case_type.reference == CaseTypeReferenceEnum.SITL:
                 if new_location.country.id in TRANSHIPMENT_BANNED_COUNTRIES:
-                    return JsonResponse(
-                        data={"errors": {"external_locations": [ExternalLocations.Errors.TRANSHIPMENT_GB]}},
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
+                    return None, {"external_locations": [ExternalLocations.Errors.TRANSHIPMENT_GB]}
 
             if serializer.is_valid():
                 serializer.save()
-                response_data.append(serializer.data)
+                external_locations.append(serializer.data)
             else:
-                return JsonResponse(data={"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST,)
+                return None, serializer.errors
 
         # Get sites to be removed if a site is being added
         removed_sites = SiteOnApplication.objects.filter(application=application)
 
         if removed_sites:
             audit_trail_service.create(
-                actor=request.user,
+                actor=user,
                 verb=AuditType.REMOVED_SITES_FROM_APPLICATION,
                 target=application.get_case(),
-                payload={"sites": [site.site.name + " " + site.site.address.country.name for site in removed_sites],},
+                payload={"sites": [site.site.name + " " + site.site.address.country.name for site in removed_sites]},
             )
 
         if removed_locations:
             audit_trail_service.create(
-                actor=request.user,
+                actor=user,
                 verb=AuditType.REMOVED_EXTERNAL_LOCATIONS_FROM_APPLICATION,
                 target=application.get_case(),
                 payload={
@@ -169,18 +124,62 @@ class ApplicationExternalLocations(APIView):
 
         if new_locations:
             audit_trail_service.create(
-                actor=request.user,
+                actor=user,
                 verb=AuditType.ADD_EXTERNAL_LOCATIONS_TO_APPLICATION,
                 target=application.get_case(),
-                payload={"locations": [location.name + " " + location.country.name for location in new_locations],},
+                payload={"locations": [location.name + " " + location.country.name for location in new_locations]},
             )
 
-        if data.get("method") != "append_location":
+        if method != "append_location":
             removed_locations.delete()
 
         removed_sites.delete()
+        return external_locations, None
 
-        return JsonResponse(data={"external_locations": response_data}, status=status.HTTP_201_CREATED)
+    def _validate_request(self, application, location_ids):
+        # Validate that have_goods_departed isn't True
+        if getattr(application, "have_goods_departed", False):
+            return {"external_locations": ["Application has have_goods_departed set to True"]}
+        # Validate that there are actually external locations
+        if not location_ids:
+            return {"external_locations": ["You have to pick at least one external location"]}
+
+    def _get_new_locations(self, application, user_organisation, location_ids, previous_locations,
+                           previous_location_ids):
+        new_locations = []
+
+        if (
+                is_case_status_draft(application.status.status)
+                or application.status.status == CaseStatusEnum.APPLICANT_EDITING
+        ):
+            new_locations = [
+                get_location(location_id, user_organisation)
+                for location_id in location_ids
+                if location_id not in previous_location_ids
+            ]
+        else:
+            if has_previous_sites(application):
+                return None, {"external_locations": [
+                    "Go back and change your answer from ‘Change a site, or delete a good, third party or "
+                    "country’ to ’Change something else’."]
+                }
+
+            previous_location_countries = list(
+                previous_locations.values_list("external_location__country__id", flat=True)
+            )
+
+            for location_id in location_ids:
+                new_location = get_location(location_id, user_organisation)
+
+                if previous_location_countries and new_location.country.id not in previous_location_countries:
+                    return None, {"external_locations": [
+                        "Go back and change your answer from ‘Change a site, or delete a good, "
+                        "third party or country’ to ’Change something else’."]
+                    }
+                elif str(new_location.id) not in previous_location_ids:
+                    new_locations.append(new_location)
+
+        return new_locations, None
 
 
 class ApplicationRemoveExternalLocation(APIView):
@@ -189,7 +188,7 @@ class ApplicationRemoveExternalLocation(APIView):
     @authorised_users(ExporterUser)
     def delete(self, request, application, ext_loc_pk):
         if not is_case_status_draft(application.status.status) and application.status.status in get_case_statuses(
-            read_only=True
+                read_only=True
         ):
             return JsonResponse(
                 data={"error": f"Application status {application.status.status} is read-only."},
@@ -197,14 +196,15 @@ class ApplicationRemoveExternalLocation(APIView):
             )
 
         if (
-            not is_case_status_draft(application.status.status)
-            and application.status.status != CaseStatusEnum.APPLICANT_EDITING
+                not is_case_status_draft(application.status.status)
+                and application.status.status != CaseStatusEnum.APPLICANT_EDITING
         ):
             if ExternalLocationOnApplication.objects.filter(application=application).count() == 1:
                 return JsonResponse(
                     data={
-                        "error": "Go back and change your answer from ‘Change a site, or delete "
-                        "a good, third party or country’ to ’Change something else’."
+                        "errors": {"external_locations": [
+                            "Go back and change your answer from ‘Change a site, or delete a good, "
+                            "third party or country’ to ’Change something else’."]}
                     },
                     status=status.HTTP_400_BAD_REQUEST,
                 )
@@ -228,4 +228,4 @@ class ApplicationRemoveExternalLocation(APIView):
 
         removed_locations.delete()
 
-        return JsonResponse(data={"success": "External location deleted"}, status=status.HTTP_204_NO_CONTENT,)
+        return JsonResponse(data={"success": "External location deleted"}, status=status.HTTP_204_NO_CONTENT)
