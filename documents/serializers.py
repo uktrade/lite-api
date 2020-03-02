8439257080cfaebd35f8f0a65c7bd56aa7a54fa8
BenAndrew-Kainos <bena@kainos.com>
2020-03-02 13:33:19 +0000

    Remove get s3 key SerializerMethodField

--- documents/serializers.py
+++ documents/serializers.py
@@ -7,15 +7,11 @@ from documents.models import Document
 
 
 class DocumentSerializer(serializers.ModelSerializer):
-    s3_key = serializers.SerializerMethodField()
     type = KeyValueChoiceField(choices=CaseDocumentState.choices)
 
     class Meta:
         model = Document
         fields = ("name", "s3_key", "size", "created_at", "description", "type", "safe")
-
-    def get_s3_key(self, instance):
-        return instance.s3_key if instance.safe else "File not ready"
 
     def create(self, validated_data):
         document = super(DocumentSerializer, self).create(validated_data)
