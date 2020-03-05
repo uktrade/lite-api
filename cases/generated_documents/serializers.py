87180e0aebded000fd6ee1049a92145fe3b42832
BenAndrew-Kainos <bena@kainos.com>
2020-03-05 11:56:29 +0000

    Add Gov user details for final adivce

--- cases/generated_documents/serializers.py
+++ cases/generated_documents/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from cases.generated_documents.models import GeneratedCaseDocument
+from gov_users.serializers import GovUserViewSerializer
 
 
 class GeneratedCaseDocumentExporterSerializer(serializers.ModelSerializer):
@@ -23,6 +24,8 @@ class GeneratedCaseDocumentGovSerializer(serializers.ModelSerializer):
 
 
 class GeneratedFinalAdviceDocumentGovSerializer(serializers.ModelSerializer):
+    user = GovUserViewSerializer()
+
     class Meta:
         model = GeneratedCaseDocument
         fields = ("user", "created_at")
