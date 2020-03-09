96ed5657e25f7cc60b7a94c7ca3ddf5ab1297a4d
BenAndrew-Kainos <bena@kainos.com>
2020-03-09 09:59:15 +0000

    Update Licence model

--- applications/models.py
+++ applications/models.py
@@ -254,7 +254,9 @@ class PartyOnApplication(TimestampableModel):
 
 class Licence(TimestampableModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    application = models.ForeignKey(BaseApplication, on_delete=models.CASCADE, null=True, related_name="licence")
+    application = models.ForeignKey(
+        BaseApplication, on_delete=models.CASCADE, null=False, blank=False, related_name="licence"
+    )
     start_date = models.DateField(blank=False, null=False)
-    licence_duration = models.PositiveSmallIntegerField(blank=False, null=False)
+    duration = models.PositiveSmallIntegerField(blank=False, null=False)
     finalised = models.BooleanField(default=False, null=False)
