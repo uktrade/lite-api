965a2a561de8922e965bca9ba61d2c8d02a52012
Andy Taylor <a.taylor@kainos.com>
2020-03-09 11:25:18 +0000

    rework seeding internal users as exporter users

--- static/management/commands/seedexporterusers.py
+++ static/management/commands/seedexporterusers.py
@@ -68,7 +68,7 @@ class Command(SeedCommand):
 
     @classmethod
     def seed_exporter_users(cls):
-        for exporter_user_data in cls._get_users():
+        for exporter_user_data in cls._get_exporter_users():
             default_data = dict(email=exporter_user_data["email"])
 
             exporter_user, created = ExporterUser.objects.get_or_create(
@@ -78,24 +78,30 @@ class Command(SeedCommand):
             if created:
                 cls.print_created_or_updated(ExporterUser, default_data, is_created=True)
 
-            cls._add_exporter_to_organisations(exporter_user, exporter_user_data)
+            cls._add_user_to_organisations(exporter_user, exporter_user_data)
 
     @classmethod
-    def _get_users(cls):
-        admin_users = cls._get_users_from_env("INTERNAL_ADMIN_TEAM_USERS")
-        exporter_users = cls._get_users_from_env("EXPORTER_USERS")
+    def _get_exporter_users(cls):
+        admin_users = cls._parse_users("INTERNAL_ADMIN_TEAM_USERS")
+        exporter_users = cls._parse_users("EXPORTER_USERS")
 
-        # Declare admin users first, as they could be re-defined with an organisation + role in EXPORTER_USERS
-        return [*admin_users, *exporter_users]
+        # Add INTERNAL_ADMIN_TEAM_USERS to exporter_users list if they have not been defined in EXPORTER_USERS
+        admin_user_emails = [admin_user["email"] for admin_user in admin_users]
+        exporter_user_emails = [exporter_user["email"] for exporter_user in exporter_users]
+        for email in admin_user_emails:
+            if email not in exporter_user_emails:
+                exporter_users.append({"email": email})
+
+        return exporter_users
 
     @classmethod
-    def _get_users_from_env(cls, env_variable):
+    def _parse_users(cls, env_variable):
         users = env(env_variable)
         # The JSON representation of the variable is different on environments, so it needs to be parsed first
         parsed_users = users.replace("=>", ":")
 
         try:
-            serialized_users = serialize(parsed_users)
+            parsed_users = serialize(parsed_users)
         except ValueError:
             raise ValueError(
                 f"{env_variable} has incorrect format;"
@@ -103,18 +109,16 @@ class Command(SeedCommand):
                 f"\nbut got: {users}"
             )
 
-        return serialized_users
+        return parsed_users
 
     @classmethod
-    def _add_exporter_to_organisations(cls, exporter_user: ExporterUser, exporter_user_data):
+    def _add_user_to_organisations(cls, exporter_user: ExporterUser, exporter_user_data):
         role_name = exporter_user_data.get("role", Roles.EXPORTER_SUPER_USER_ROLE_NAME)
         organisation_name = exporter_user_data.get("organisation")
 
-        # If organisation was specified, only seed the exporter user to that chosen commercial organisation
+        # If organisation was specified, only seed the user to that chosen commercial organisation
         if organisation_name:
-            organisation = [
-                organisation for organisation in COMMERCIAL_ORGANISATIONS if organisation["name"] == organisation_name
-            ][0]
+            organisation = cls._get_organisation_from_commercial_organisations(organisation_name)
             cls._add_exporter_to_organisation(exporter_user, organisation, role_name)
         else:
             for organisation in COMMERCIAL_ORGANISATIONS:
@@ -122,6 +126,13 @@ class Command(SeedCommand):
 
         for organisation in HMRC_ORGANISATIONS:
             cls._add_exporter_to_organisation(exporter_user, organisation, Roles.EXPORTER_SUPER_USER_ROLE_NAME)
+
+    @classmethod
+    def _get_organisation_from_commercial_organisations(cls, organisation_name):
+        organisations = [
+            organisation for organisation in COMMERCIAL_ORGANISATIONS if organisation["name"] == organisation_name
+        ]
+        return organisations[0] if organisations else None
 
     @classmethod
     def _add_exporter_to_organisation(cls, exporter_user: ExporterUser, organisation, role_name):
