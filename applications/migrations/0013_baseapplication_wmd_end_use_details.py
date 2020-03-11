f423e3a94cdf1dd8ab049c26194facfc643cc442
Andy Taylor <a.taylor@kainos.com>
2020-03-11 08:38:48 +0000

    fix migrations

--- applications/migrations/0013_baseapplication_wmd_end_use_details.py
+++ applications/migrations/0013_baseapplication_wmd_end_use_details.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("applications", "0010_auto_20200304_1344"),
+        ("applications", "0012_auto_20200309_1335"),
     ]
 
     operations = [
