572b786f934904eb07814156c53b7469c86de78b
cstobart <c.stobart@kainos.com>
2020-03-12 11:14:32 +0000

    fix tests

--- flags/tests/tests_create_flagging_rules.py
+++ flags/tests/tests_create_flagging_rules.py
@@ -42,4 +42,4 @@ class FlaggingRulesCreateTest(DataTestClient):
         )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.json["errors"], strings.FlaggingRules.DUPLICATE_RULE)
+        self.assertIn(strings.FlaggingRules.DUPLICATE_RULE, response.json()["errors"]["non_field_errors"])
