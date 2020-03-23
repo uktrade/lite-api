c54e9200aa6947ffc41980b23609efaa037a05e8
BenAndrew-Kainos <bena@kainos.com>
2020-03-23 13:54:03 +0000

    template visible_to_exporter supersedes document visible_to_exporter

--- cases/generated_documents/views.py
+++ cases/generated_documents/views.py
@@ -68,6 +68,10 @@ class GeneratedDocuments(generics.ListAPIView):
         # base the document name on the template name and a portion of the UUID generated for the s3 key
         document_name = f"{s3_key[:len(document.template.name) + 6]}.pdf"
 
+        visible_to_exporter = str_to_bool(request.data.get("visible_to_exporter"))
+        if not document.template.visible_to_exporter:
+            visible_to_exporter = False
+
         try:
             with transaction.atomic():
                 generated_doc = GeneratedCaseDocument.objects.create(
@@ -80,7 +84,7 @@ class GeneratedDocuments(generics.ListAPIView):
                     case=document.case,
                     template=document.template,
                     text=document.text,
-                    visible_to_exporter=str_to_bool(request.data.get("visible_to_exporter")),
+                    visible_to_exporter=visible_to_exporter,
                     advice_type=request.data.get("advice_type"),
                 )
 
