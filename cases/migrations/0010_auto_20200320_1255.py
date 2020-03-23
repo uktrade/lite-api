5d33d362538756f3ca717f3a1446fd1c9fc0c7f2
BenAndrew-Kainos <bena@kainos.com>
2020-03-23 15:23:46 +0000

    Add reverse migration

--- cases/migrations/0010_auto_20200320_1255.py
+++ cases/migrations/0010_auto_20200320_1255.py
@@ -12,6 +12,22 @@ def convert_users(apps, schema_editor):
         assignment.delete()
 
 
+def reverse_users(apps, schema_editor):
+    Case = apps.get_model("cases", "Case")
+    Queue = apps.get_model("queues", "Queue")
+    CaseAssignment = apps.get_model("cases", "CaseAssignment")
+    cases = Case.objects.all()
+    queues = Queue.objects.all()
+
+    for case in cases:
+        for queue in queues:
+            assignments = CaseAssignment.objects.filter(case=case, queue=queue)
+            users = [assignment.user for assignment in assignments]
+            assignment = CaseAssignment.objects.create(case=case, queue=queue)
+            assignment.users.set(users)
+            assignments.delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -19,5 +35,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(convert_users),
+        migrations.RunPython(convert_users, reverse_code=reverse_users),
     ]
