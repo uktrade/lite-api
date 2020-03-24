928b294964e8711dd10fffe0814f11358a079cce
Jan Faracik <j.faracik@kainos.com>
2020-03-24 12:22:17 +0000

    Fix

--- organisations/providers.py
+++ organisations/providers.py
@@ -4,18 +4,18 @@ from faker.providers import BaseProvider
 
 
 class OrganisationProvider(BaseProvider):
-    # pylint: disable=B311
     def eori_number(self):
+        # pylint: disable=B311
         return str(random.randint(0, 99999999999999999))
 
-    # pylint: disable=B311
     def sic_number(self):
+        # pylint: disable=B311
         return str(random.randint(1110, 99999))
 
-    # pylint: disable=B311
     def vat_number(self):
+        # pylint: disable=B311
         return f"GB{random.randint(1000000, 9999999)}"
 
-    # pylint: disable=B311
     def registration_number(self):
+        # pylint: disable=B311
         return str(random.randint(10000000, 99999999))
