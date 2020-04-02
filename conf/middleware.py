9ff0dffb4766ee17936fda8863f20845fa353c82
Jan Faracik <j.faracik@kainos.com>
2020-04-02 16:32:37 +0100

    Update middleware.py

--- conf/middleware.py
+++ conf/middleware.py
@@ -1,19 +1,7 @@
 import logging
 import time
 import uuid
-from datetime import datetime
-
 from django.db import connection
-
-from conf.settings import env
-from test_helpers.colours import bold
-
-
-#
-#
-# Reset to master copy when done
-#
-#
 
 
 class LoggingMiddleware:
@@ -27,18 +15,17 @@ class LoggingMiddleware:
             correlation = request.META["HTTP_X_CORRELATION_ID"]
         request.correlation = correlation or uuid.uuid4().hex
         response = self.get_response(request)
-
-        # if not env("SHOW_VIEW_QUERIES"):
-        #     logging.info(
-        #         {
-        #             "message": "liteolog api",
-        #             "corrID": request.correlation,
-        #             "type": "http response",
-        #             "method": request.method,
-        #             "url": request.path,
-        #             "elapsed_time": time.time() - start,
-        #         }
-        #     )
+        logging.info(
+            {
+                "user": request.user.id,
+                "message": "liteolog api",
+                "corrID": request.correlation,
+                "type": "http response",
+                "method": request.method,
+                "url": request.path,
+                "elapsed_time": time.time() - start,
+            }
+        )
 
         return response
 
@@ -48,18 +35,23 @@ class DBLoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        start = datetime.now()
+        start = time.time()
         initial_queries = connection.queries
-
         response = self.get_response(request)
         final_queries = connection.queries
 
-        elapsed_time = datetime.now() - start
-
-        _type = request.method.upper()
-        duration = round(elapsed_time.microseconds / 100000, 2)  # noqa
-        queries = len(final_queries)
-
-        print(f"\n{_type} {bold(request.path)} - ⏱  {duration}s  🗂  {queries} queries\n")
+        elapsed_time = time.time() - start
+        logging.info(
+            {
+                "message": "liteolog db",
+                "corrID": request.correlation,
+                "type": "db details",
+                "elapsed_time": elapsed_time,
+                "initial query count": len(initial_queries),
+                "final query count": len(final_queries),
+                "query set": final_queries,
+                "method": "DB-QUERY-SET",
+            }
+        )
 
         return response
