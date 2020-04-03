9bc913c7bc0c2a3fca09016b333192edd62f6296
asmund <asmund.bekker@gmail.com>
2020-04-03 15:25:14 +0100

    updates

--- audit_trail/streams/service.py
+++ audit_trail/streams/service.py
@@ -3,15 +3,14 @@ MVP activity stream. To be extended appropriately as requirements are drawn up.
 """
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import Q
 
+from applications.models import CountryOnApplication
 from audit_trail.models import Audit
 from audit_trail.payload import AuditType
+from cases.models import Case
 from common.models import prefetch_generic_relations
-from static.countries.models import Country
 
 STREAMED_AUDITS = [
-    AuditType.CREATED.value,
     AuditType.ADD_CASE_OFFICER_TO_CASE.value,
     AuditType.REMOVE_CASE_OFFICER_FROM_CASE.value,
     AuditType.UPDATED_STATUS.value,
@@ -36,42 +35,30 @@ VERB_MAPPING = {
 }
 
 
-def get_stream_for_obj(obj):
-    obj_content_type = ContentType.objects.get_for_model(obj)
-
-    obj_as_action_filter = Q(action_object_object_id=obj.id, action_object_content_type=obj_content_type)
-    obj_as_target_filter = Q(target_object_id=obj.id, target_content_type=obj_content_type)
-
-    audit_trail_qs = Audit.objects.filter(obj_as_action_filter | obj_as_target_filter)
-
-    return audit_trail_qs
-
-
-def case_record_json(audit):
+def case_record_json(case_id, last_created_at, countries):
     """
-    Creates an activity stream compatible record for an application
+    Creates an activity stream compatible record for an application.
+    A record is only produced for a case with the last activity seen for a case.
     """
-    case = audit.action_object or audit.target
+    case = Case.objects.select_related("status", "case_officer").get(id=case_id)
     if case is None:
         # Some applications in draft status are being deleted
         return {}
-    countries = Country.objects.filter(countries_on_application__application=case.id).values_list("name", flat=True)
-    last_case_audit = get_stream_for_obj(case).values("created_at").first()
     return {
         "id": "dit:lite:case:application:{id}:{verb}".format(id=case.id, verb="create"),
-        "published": "{ts}".format(ts=last_case_audit["created_at"]),
+        "published": "{ts}".format(ts=last_created_at),
         "object": {
             "type": ["dit:lite:case", "dit:lite:record", "dit:lite:case:application",],
             "id": "dit:lite:case:application:{id}".format(id=case.id),
             "dit:submittedDate": "{ts}".format(ts=case.submitted_at or ""),
             "dit:status": "{status}".format(status=case.status.status),
             "dit:caseOfficer": case.case_officer.email if case.case_officer else "",
-            "dit:countries": list(countries),
+            "dit:countries": countries,
         },
     }
 
 
-def case_activity_json(audit):
+def case_activity_json(audit, case_type):
     """
     Creates an activity stream compatible record for an application activity
     """
@@ -87,7 +74,7 @@ def case_activity_json(audit):
             "dit:lite:activity",
             "dit:lite:case:change:{data_type}".format(data_type=data_type),
         ],
-        "attributedTo": {"id": "dit:lite:case:{case_type}:{id}".format(case_type=case.case_type.sub_type, id=case.id),},
+        "attributedTo": {"id": "dit:lite:case:{case_type}:{id}".format(case_type=case_type, id=case.id)},
     }
 
     # TODO: standardize audit payloads and clean
@@ -116,17 +103,35 @@ def get_stream(n):
     """
     Returns a paginated stream of activities.
     """
-    PAGE_SIZE = settings.STREAM_PAGE_SIZE
 
     qs = prefetch_generic_relations(
-        Audit.objects.filter(verb__in=STREAMED_AUDITS).order_by("created_at")[n * PAGE_SIZE : (n + 1) * PAGE_SIZE]
+        Audit.objects.filter(verb__in=STREAMED_AUDITS).order_by("created_at")[n * settings.STREAM_PAGE_SIZE : (n + 1) * settings.STREAM_PAGE_SIZE]
     )
 
+    case_ids = [value["target_object_id"] for value in qs.values("target_object_id")]
+
+    # Prefetch relevant information in bulk to be used for streams
+    countries_on_applications = CountryOnApplication.objects.filter(
+        application__id__in=case_ids
+    ).values("application_id", "country__name")
+
+    case_types = Case.objects.filter(id__in=case_ids).select_related("case_type").values("id", "case_type__sub_type")
+    case_types = {value["id"]: value["case_type__sub_type"] for value in case_types}
+
+    latest_case_audits = Audit.objects.filter(
+        target_object_id__in=case_ids,
+        target_content_type=ContentType.objects.get_for_model(Case),
+    ).order_by("target_object_id", "-created_at").distinct("target_object_id").values_list("id", flat=True)
+
+    # Create stream for each activity
     stream = []
 
     for audit in qs:
-        data = case_record_json(audit) if audit.verb == AuditType.CREATED.value else case_activity_json(audit)
+        data = case_activity_json(audit, case_types[audit.target_object_id])
         if data:
             stream.append(data)
-
+        if audit.id in latest_case_audits:
+            # Only create a case record for last seen activity for a given case.
+            countries = [d["country__name"] for d in countries_on_applications if d["application_id"] == audit.target_object_id]
+            stream.append(case_record_json(audit.target_object_id, audit.created_at, countries))
     return stream
--- audit_trail/tests/test_streams.py
+++ audit_trail/tests/test_streams.py
@@ -7,8 +7,7 @@ from static.statuses.enums import CaseStatusEnum
 from test_helpers.clients import DataTestClient
 
 
-class CasesAuditTrail(DataTestClient):
-    # TODO: test schema and creation
+class AuditTrailStreamTestCase(DataTestClient):
     def setUp(self):
         super().setUp()
         self.case = self.create_standard_application_case(self.organisation)
@@ -17,39 +16,24 @@ class CasesAuditTrail(DataTestClient):
         self.url = reverse("audit_trail:streams", kwargs={"n": 0})
         self.status_url = reverse("applications:manage_status", kwargs={"pk": self.case.id})
 
-    def test_case_created_in_stream(self):
+    def test_no_case_record_in_stream_with_no_audit(self):
         response = self.client.get(self.url, **self.exporter_headers)
         stream = response.json()
 
-        self.assertEqual(1, len(stream["orderedItems"]))
-        self.assertEqual(
-            stream["orderedItems"][0],
-            {
-                "id": "dit:lite:case:application:{id}:create".format(id=self.case.id),
-                "object": {
-                    "dit:caseOfficer": "",
-                    "dit:countries": [],
-                    "dit:status": self.case.status.status,
-                    "dit:submittedDate": str(self.case.submitted_at),
-                    "id": "dit:lite:case:application:{id}".format(id=self.case.id),
-                    "type": ["dit:lite:case", "dit:lite:record", "dit:lite:case:application"],
-                },
-                "published": str(self.case.created_at),
-            },
-        )
+        self.assertEqual(0, len(stream["orderedItems"]))
 
-    def test_gov_set_application_status_to_applicant_editing_failure(self):
+    def test_status_audit_created_and_record(self):
         data = {"status": CaseStatusEnum.APPLICANT_EDITING}
         old_status = self.case.status.status
         self.client.put(self.status_url, data=data, **self.exporter_headers)
-
+        self.case.refresh_from_db()
         response = self.client.get(self.url, **self.exporter_headers)
         stream = response.json()
 
         audit = Audit.objects.get(verb=AuditType.UPDATED_STATUS.value)
         self.assertEqual(2, len(stream["orderedItems"]))
         self.assertEqual(
-            stream["orderedItems"][1],
+            stream["orderedItems"][0],
             {
                 "id": "dit:lite:case:change:status:{case_id}:{audit_id}:update".format(
                     case_id=self.case.id, audit_id=audit.id
@@ -59,6 +43,21 @@ class CasesAuditTrail(DataTestClient):
                     "dit:to": {"dit:lite:case:status": CaseStatusEnum.get_text(data["status"])},
                     "dit:from": {"dit:lite:case:status": old_status},
                     "type": ["dit:lite:case:change", "dit:lite:activity", "dit:lite:case:change:status"],
+                },
+                "published": str(audit.created_at),
+            },
+        )
+        self.assertEqual(
+            stream["orderedItems"][1],
+            {
+                "id": "dit:lite:case:application:{id}:create".format(id=self.case.id),
+                "object": {
+                    "dit:caseOfficer": "",
+                    "dit:countries": [],
+                    "dit:status": self.case.status.status,
+                    "dit:submittedDate": str(self.case.submitted_at),
+                    "id": "dit:lite:case:application:{id}".format(id=self.case.id),
+                    "type": ["dit:lite:case", "dit:lite:record", "dit:lite:case:application"],
                 },
                 "published": str(audit.created_at),
             },
