d9bdbcaf5a9e6e73c52644cbd8f6bf977ee0b8db
Andy Taylor <a.taylor@kainos.com>
2020-04-03 15:44:08 +0100

    return an object

--- static/management/commands/seedorganisation.py
+++ static/management/commands/seedorganisation.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from django.db import transaction
 
 from conf.constants import Roles
 from organisations.enums import OrganisationType
-from organisations.models import Organisation
+from organisations.models import Organisation, Site
 from organisations.tests.factories import OrganisationFactory, SiteFactory
 from organisations.tests.providers import OrganisationProvider
 from static.management.SeedCommand import SeedCommand
@@ -14,6 +16,10 @@ from users.models import ExporterUser, UserOrganisationRelationship
 
 faker = Faker()
 faker.add_provider(OrganisationProvider)
+
+
+class Org(object):
+    pass
 
 
 class Command(SeedCommand):
@@ -33,7 +39,7 @@ class Command(SeedCommand):
         parser.add_argument("--sites", help="Total number of Sites to seed", type=int)
         parser.add_argument("--users", help="Total number of Exporter Users to seed", type=int)
         parser.add_argument(
-            "--super-user", help="Email of an Exporter User to set as a Super User on the Organisation", type=str
+            "--primary-user", help="Email of an Exporter User to set as the Primary User of the Organisation", type=str
         )
 
     @transaction.atomic
@@ -42,40 +48,42 @@ class Command(SeedCommand):
         org_type = options.get("type") or OrganisationType.COMMERCIAL
         no_of_sites = options.get("sites") or 1
         no_of_users = options.get("users") or 1
-        super_user = options.get("super_user")
+        primary_user = options.get("primary_user")
 
-        self.seed_organisation(org_name, org_type, no_of_sites, no_of_users, super_user)
+        self.seed_organisation(org_name, org_type, no_of_sites, no_of_users, primary_user)
 
     @classmethod
-    def seed_organisation(cls, org_name, org_type, no_of_sites, no_of_users, super_user):
+    def seed_organisation(
+        cls, org_name: str, org_type: str, no_of_sites: int, no_of_users: int, primary_user: str
+    ) -> Org:
         if Organisation.objects.filter(name__iexact=org_name).exists():
             raise Exception(f"An Organisation with name: '{org_name}' already exists")
 
         organisation = OrganisationFactory(name=org_name, type=org_type)
 
-        # Set the Organisation's Exporter Super User
-        super_user = cls._set_organisation_super_user(organisation, super_user)
+        # Sets a the Organisation's Primary Exporter User
+        primary_user = cls._set_organisation_primary_user(organisation, primary_user)
 
-        exporter_users = [super_user]
-        # Since a Super User has already been created and/or set, subtract 1 from total number of users to seed
+        exporter_users = [primary_user]
+        # Since a Primary User has already been created and/or set, subtract 1 from total number of users to seed
         exporter_users += create_exporter_users(organisation, no_of_users - 1)
 
         sites = [organisation.primary_site]
         # Since OrganisationFactory has already created a Site, subtract 1 from total number of sites to seed
         sites += [SiteFactory(organisation=organisation) for _ in range(no_of_sites - 1)]
 
-        cls._print_organisation_to_console(organisation, super_user)
-        return [("organisation", organisation), ("sites", sites), ("exporter_users", exporter_users)]
+        cls._print_organisation_to_console(organisation, primary_user)
+        return cls._create_org_obj(organisation, sites, exporter_users, primary_user)
 
     @classmethod
-    def _set_organisation_super_user(cls, organisation, super_user):
-        if not super_user:
+    def _set_organisation_primary_user(cls, organisation: Organisation, primary_user: str) -> ExporterUser:
+        if not primary_user:
             return create_exporter_users(organisation, 1, role_id=Roles.EXPORTER_SUPER_USER_ROLE_ID)
 
         try:
-            exporter_user = ExporterUser.objects.get(email__exact=super_user)
+            exporter_user = ExporterUser.objects.get(email__exact=primary_user)
         except ExporterUser.DoesNotExist:
-            raise Exception(f"An ExporterUser with email '{super_user}' does not exist")
+            raise Exception(f"An Exporter User with email '{primary_user}' does not exist")
 
         UserOrganisationRelationship.objects.create(
             organisation=organisation, user=exporter_user, role_id=Roles.EXPORTER_SUPER_USER_ROLE_ID
@@ -84,10 +92,27 @@ class Command(SeedCommand):
         return exporter_user
 
     @classmethod
-    def _print_organisation_to_console(cls, organisation, org_super_user):
+    def _print_organisation_to_console(cls, organisation: Organisation, primary_user: ExporterUser):
         organisation_representation = dict(
             id=str(organisation.id),
             primary_site_id=str(organisation.primary_site.id),
-            super_user_id=str(org_super_user.id),
+            primary_user_id=str(primary_user.id),
         )
         cls.print_created_or_updated(Organisation, organisation_representation, is_created=True)
+
+    @classmethod
+    def _create_org_obj(
+        cls, organisation: Organisation, sites: List[Site], users: List[ExporterUser], primary_user: ExporterUser
+    ) -> Org:
+        """
+        Returns an object containing an Organisation's properties, Sites and Users
+        """
+        org = Org()
+        org.primary_user = primary_user
+        org.sites = sites
+        org.users = users
+        for field in organisation._meta.fields:
+            key = field.name
+            org.key = getattr(organisation, key)
+
+        return org
