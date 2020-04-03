93a371ecbf1e6b61cf530e56e208d612bc1f63ec
asmund <asmund.bekker@gmail.com>
2020-04-03 16:40:57 +0100

    fix query

--- audit_trail/streams/service.py
+++ audit_trail/streams/service.py
@@ -124,7 +124,9 @@ def get_stream(n):
 
     latest_case_audits = (
         Audit.objects.filter(
-            target_object_id__in=case_ids, target_content_type=ContentType.objects.get_for_model(Case),
+            target_object_id__in=case_ids,
+            target_content_type=ContentType.objects.get_for_model(Case),
+            verb__in=STREAMED_AUDITS
         )
         .order_by("target_object_id", "-created_at")
         .distinct("target_object_id")
