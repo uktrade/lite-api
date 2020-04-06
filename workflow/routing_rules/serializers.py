1629e8aeaa58ac68d0b1c790d1dbcad7af5f4efd
Mark Spiers <marksp@kainos.com>
2020-04-06 09:09:57 +0100

    editing introduced

--- workflow/routing_rules/serializers.py
+++ workflow/routing_rules/serializers.py
@@ -21,21 +21,31 @@ class RoutingRuleSerializer(serializers.ModelSerializer):
     team = PrimaryKeyRelatedSerializerField(queryset=Team.objects.all(), serializer=TeamSerializer)
     queue = PrimaryKeyRelatedSerializerField(queryset=Queue.objects.all(), serializer=TinyQueueSerializer)
     status = PrimaryKeyRelatedSerializerField(queryset=CaseStatus.objects.all(), serializer=CaseStatusSerializer)
-    tier = serializers.IntegerField(min_value=0, max_value=32000)
+    tier = serializers.IntegerField(min_value=1, max_value=32000)
     additional_rules = serializers.MultipleChoiceField(
         choices=RoutingRulesAdditionalFields.choices, allow_empty=True, required=False
     )
 
     user = PrimaryKeyRelatedSerializerField(
-        queryset=GovUser.objects.all(), serializer=GovUserViewSerializer, required=False
+        queryset=GovUser.objects.all(), serializer=GovUserViewSerializer, required=False, allow_null=True,
     )
     case_types = PrimaryKeyRelatedSerializerField(
-        queryset=CaseType.objects.all(), serializer=CaseTypeSerializer, many=True, required=False
+        queryset=CaseType.objects.all(),
+        serializer=CaseTypeSerializer,
+        many=True,
+        required=False,
+        allow_null=True,
+        allow_empty=True,
     )
     flags = PrimaryKeyRelatedSerializerField(
-        queryset=Flag.objects.all(), serializer=FlagSerializer, many=True, required=False
+        queryset=Flag.objects.all(),
+        serializer=FlagSerializer,
+        many=True,
+        required=False,
+        allow_null=True,
+        allow_empty=True,
     )
-    country = CountrySerializerField(required=False)
+    country = CountrySerializerField(required=False, allow_null=True,)
 
     class Meta:
         model = RoutingRule
@@ -60,16 +70,55 @@ class RoutingRuleSerializer(serializers.ModelSerializer):
         if kwargs.get("data"):
             if not kwargs["data"].get("team"):
                 self.initial_data["team"] = kwargs["context"]["request"].user.team_id
-            additional_rules = kwargs["data"].get("additional_rules", [])
+
+            additional_rules = kwargs["data"].get("additional_rules")
+            if not isinstance(additional_rules, list):
+                additional_rules = [additional_rules]
 
             if RoutingRulesAdditionalFields.USERS in additional_rules:
                 self.fields["user"].required = True
+                self.fields["user"].allow_null = False
+            else:
+                self.initial_data["user"] = None
 
             if RoutingRulesAdditionalFields.CASE_TYPES in additional_rules:
                 self.fields["case_types"].required = True
+                self.fields["case_types"].allow_null = False
+                self.fields["case_types"].allow_empty = False
+            else:
+                self.initial_data["case_types"] = []
 
             if RoutingRulesAdditionalFields.COUNTRY in additional_rules:
                 self.fields["country"].required = True
+                self.fields["country"].allow_null = False
+            else:
+                self.initial_data["country"] = None
 
             if RoutingRulesAdditionalFields.FLAGS in additional_rules:
                 self.fields["flags"].required = True
+                self.fields["flags"].allow_null = False
+                self.fields["flags"].allow_empty = False
+            else:
+                self.initial_data["flags"] = []
+
+    def update(self, instance, validated_data):
+        instance.queue = validated_data.get("queue", instance.queue)
+        instance.tier = validated_data.get("tier", instance.tier)
+        instance.status = validated_data.get("status", instance.status)
+
+        instance.additional_rules = validated_data.get("additional_rules", instance.additional_rules)
+
+        instance.user = validated_data.get("user", instance.user)
+        instance.country = validated_data.get("country", instance.country)
+        instance.case_types.set(validated_data.get("case_types", instance.case_types.all()))
+        instance.flags.set(validated_data.get("flags", instance.flags.all()))
+
+        instance.save()
+        return instance
+
+
+# flattened serializer for editing purposes
+class EditRoutingRuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RoutingRule
+        fields = "__all__"
--- workflow/routing_rules/views.py
+++ workflow/routing_rules/views.py
@@ -1,11 +1,11 @@
 from django.http import Http404, JsonResponse
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.views import APIView
 
 from conf.authentication import GovAuthentication
 from workflow.routing_rules.models import RoutingRule
-from workflow.routing_rules.serializers import RoutingRuleSerializer
+from workflow.routing_rules.serializers import RoutingRuleSerializer, EditRoutingRuleSerializer
 
 
 class RoutingRulesList(ListCreateAPIView):
@@ -14,9 +14,23 @@ class RoutingRulesList(ListCreateAPIView):
     queryset = RoutingRule.objects.all()
 
 
-class RoutingRulesDetail(APIView):
+class RoutingRulesDetail(RetrieveUpdateAPIView):
     authentication_classes = (GovAuthentication,)
-    serializer_class = RoutingRuleSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            # light weight serializer for editing
+            return EditRoutingRuleSerializer
+        else:
+            # heavy serializer for validating, saving and getting list of objects
+            return RoutingRuleSerializer
+
+    def get_object(self):
+        return RoutingRule.objects.get(id=self.kwargs["pk"])
+
+    def perform_update(self, serializer):
+        if not self.request.data.get("validate_only", False):
+            serializer.save()
 
 
 class RoutingRulesActiveStatus(APIView):
