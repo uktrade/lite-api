fe32a1eaba3a5dee2458a7fa4c306c1504965d88
stephanieboyd <boydstep01@gmail.com>
2020-04-08 11:52:02 +0100

    add logic to prevent multiple sla tasks

--- cases/app.py
+++ cases/app.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from background_task.models import Task
 
 
 class CasesConfig(AppConfig):
@@ -10,7 +11,8 @@ class CasesConfig(AppConfig):
         from background_task.models import Task
         from cases.sla import update_cases_sla
 
-        update_cases_sla(repeat=Task.DAILY, repeat_until=None)  # noqa
+        if not Task.objects.filter(task_name="cases.sla.update_cases_sla").exists():
+            update_cases_sla(repeat=Task.DAILY, repeat_until=None)  # noqa
 
     def ready(self):
         post_migrate.connect(self.initialize_background_tasks, sender=self)
