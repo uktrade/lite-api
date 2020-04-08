3c3209affbc1d3713cca24bff4d712cb85ecf440
asmund <asmund.bekker@gmail.com>
2020-04-08 09:27:09 +0100

    Change sdtatus update order

--- cases/views/views.py
+++ cases/views/views.py
@@ -626,20 +626,6 @@ class FinaliseView(RetrieveUpdateAPIView):
 
         return_payload = {"case": pk}
 
-        # Finalise Licence if granting a licence
-        if Licence.objects.filter(application=case).exists():
-            licence = Licence.objects.get(application=case)
-            licence.is_complete = True
-            licence.decisions.set([Decision.objects.get(name=decision) for decision in required_decisions])
-            licence.save()
-            return_payload["licence"] = licence.id
-            audit_trail_service.create(
-                actor=request.user,
-                verb=AuditType.GRANTED_APPLICATION,
-                target=case,
-                payload={"licence_duration": licence.duration, "start_date": licence.start_date.strftime("%Y-%m-%d")},
-            )
-
         # Finalise Case
         old_status = case.status.status
         case.status = get_case_status_by_status(CaseStatusEnum.FINALISED)
@@ -651,6 +637,23 @@ class FinaliseView(RetrieveUpdateAPIView):
             target=case,
             payload={"status": {"new": case.status.status, "old": old_status}},
         )
+
+        try:
+            # If a licence object exists, finalise the licence.
+            licence = Licence.objects.get(application=case)
+        except Licence.DoesNotExist:
+            pass
+        else:
+            licence.is_complete = True
+            licence.decisions.set([Decision.objects.get(name=decision) for decision in required_decisions])
+            licence.save()
+            return_payload["licence"] = licence.id
+            audit_trail_service.create(
+                actor=request.user,
+                verb=AuditType.GRANTED_APPLICATION,
+                target=case,
+                payload={"licence_duration": licence.duration, "start_date": licence.start_date.strftime("%Y-%m-%d")},
+            )
 
         # Show documents to exporter & notify
         documents = GeneratedCaseDocument.objects.filter(advice_type__isnull=False, case=case)
