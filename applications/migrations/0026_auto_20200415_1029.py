f8718ebd1aee1558b5f2e68da73b26c7ccda67c0
Andy Taylor <a.taylor@kainos.com>
2020-04-15 17:17:04 +0100

    re-order migrations

--- applications/migrations/0026_auto_20200415_1029.py
+++ applications/migrations/0026_auto_20200415_1029.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("applications", "0024_auto_20200401_1542"),
+        ("applications", "0025_auto_20200414_1617"),
     ]
 
     operations = [
