218b4d9be1a6db7392d8576427236f49700431a2
Andy Taylor <a.taylor@kainos.com>
2020-04-16 12:05:56 +0100

    return array of enums

--- static/trade_control/views.py
+++ static/trade_control/views.py
@@ -7,11 +7,11 @@ from static.trade_control.enums import TradeControlProductCategory, TradeControl
 
 class Activities(APIView):
     def get(self, request):
-        activities = {key: value for key, value in TradeControlActivity.choices}
+        activities = [{"key": key, "value": value} for key, value in TradeControlActivity.choices]
         return JsonResponse(data={"activities": activities}, status=HTTP_200_OK)
 
 
 class ProductCategories(APIView):
     def get(self, request):
-        product_categories = {key: value for key, value in TradeControlProductCategory.choices}
+        product_categories = [{"key": key, "value": value} for key, value in TradeControlProductCategory.choices]
         return JsonResponse(data={"product_categories": product_categories}, status=HTTP_200_OK)
