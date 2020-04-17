7b466303b93ee3890ff8e450adcdad7c9c13c5db
cstobart <c.stobart@kainos.com>
2020-04-17 12:08:49 +0100

    Update test_automated_case_routing.py

--- workflow/tests/test_automated_case_routing.py
+++ workflow/tests/test_automated_case_routing.py
@@ -1,6 +1,14 @@
 from test_helpers.clients import DataTestClient
 
 
-class CaseRoutingAutomation(DataTestClient):
+class CaseRoutingAutomationTests(DataTestClient):
     def test_case_routed_to_new_queue_when_status_changed(self):
+        pass
+
+
+class ParameterSetModelMethodTests(DataTestClient):
+    def test_case_parameters_are_returned_in_a_set(self):
+        pass
+
+    def test_routing_rule_parameters_are_returned_in_a_set(self):
         pass
