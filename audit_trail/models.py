fca0304f5189019e63b678370b43168ab8e3ec81
BenAndrew-Kainos <bena@kainos.com>
2020-04-22 16:01:37 +0100

    Undo all changes

--- audit_trail/models.py
+++ audit_trail/models.py
@@ -1,6 +1,4 @@
-import logging
 import uuid
-from copy import deepcopy
 
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
@@ -10,7 +8,6 @@ from django.utils.translation import ugettext as _
 from jsonfield import JSONField
 
 from audit_trail.managers import AuditManager
-from audit_trail.payload import AuditType
 from common.models import TimestampableModel
 from users.models import GovNotification
 
@@ -74,33 +71,6 @@ class Audit(TimestampableModel):
         if self.action_object:
             return _("%(actor)s %(verb)s %(action_object)s %(age)s ago") % context
         return _("%(actor)s %(verb)s %(age)s ago") % context
-
-    def get_text(self):
-        try:
-            verb = self.get_verb()
-            payload = self.get_payload()
-            return verb.format(payload)
-        except Exception as e:  # noqa
-            logging.warning(e)
-
-    def get_verb(self):
-        return AuditType(self.verb)
-
-    def get_payload(self):
-        payload = deepcopy(self.payload)
-
-        for key, value in payload.items():
-            if value:
-                # If value is a list, join by comma.
-                if isinstance(value, list):
-                    payload[key] = ", ".join(value)
-
-                # TODO: standardise payloads across all audits and remove below
-                if key == "status" and value.get("new"):
-                    # Handle new payload format
-                    payload[key] = value.get("new")
-
-        return payload
 
     def age(self):
         return timesince.timesince(self.created_at).encode("utf8").replace(b"\xc2\xa0", b" ").decode("utf8")
