1540917d317d1e1170fc21ca388441204fab5d59
asmund <asmund.bekker@gmail.com>
2020-04-23 10:44:28 +0100

    fill in actor

---
+++ audit_trail/migrations/0005_null_actor_fix.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+
+from audit_trail.schema import AuditType
+
+
+def fill_in_missing_actor(apps, schema_editor):
+    if schema_editor.connection.alias != "default":
+        return
+    ContentType = apps.get_model("contenttypes", "ContentType")
+    Audit = apps.get_model("audit_trail", "Audit")
+    ExporterUser = apps.get_model("users", "ExporterUser")
+    content_type = ContentType.objects.get_for_model(ExporterUser)
+
+    for audit in Audit.objects.filter(
+        actor_content_type__isnull=True,
+        verb__in=[AuditType.UPDATED_STATUS.value, AuditType.CREATED.value]
+    ):
+        audit.actor_content_type = content_type
+        audit.actor_object_id = ExporterUser.objects.first().id
+        audit.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("applications", "0022_auto_20200331_1107"),
+        ("audit_trail", "0004_case_submitted_audits"),
+    ]
+    operations = [
+        migrations.RunPython(fill_in_missing_actor),
+    ]
