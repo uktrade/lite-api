6dbdae1517b0546f96d6a14297b74ff607661e2e
asmund <asmund.bekker@gmail.com>
2020-04-23 12:27:20 +0100

    update

--- audit_trail/migrations/0005_null_actor_fix.py
+++ audit_trail/migrations/0005_null_actor_fix.py
@@ -9,15 +9,18 @@ def fill_in_missing_actor(apps, schema_editor):
     ContentType = apps.get_model("contenttypes", "ContentType")
     Audit = apps.get_model("audit_trail", "Audit")
     ExporterUser = apps.get_model("users", "ExporterUser")
-    content_type = ContentType.objects.get_for_model(ExporterUser)
+    Role = apps.get_model("users", "Role")
     print("Filling in missing actors")
     for audit in Audit.objects.filter(
-        actor_content_type__isnull=True,
+        actor_content_type__isnull=ContentType.objects.get_for_model(Role),
         verb__in=[AuditType.UPDATED_STATUS.value, AuditType.CREATED.value]
     ):
         print("Updating audit for:", audit.id)
+        user = ExporterUser.objects.first()
+        print("USER OBJECT: ", user)
+        content_type = ContentType.objects.get_for_model(user)
         audit.actor_content_type = content_type
-        audit.actor_object_id = ExporterUser.objects.first().id
+        audit.actor_object_id = user.id
         audit.save()
 
 
@@ -25,6 +28,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("applications", "0022_auto_20200331_1107"),
         ("audit_trail", "0004_case_submitted_audits"),
+        ("users", "0005_auto_20200322_1547")
     ]
     operations = [
         migrations.RunPython(fill_in_missing_actor),
