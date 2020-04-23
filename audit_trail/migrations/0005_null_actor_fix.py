aa51527ca9897f08d2836c5d86f823702c88044f
asmund <asmund.bekker@gmail.com>
2020-04-23 11:09:34 +0100

    log

--- audit_trail/migrations/0005_null_actor_fix.py
+++ audit_trail/migrations/0005_null_actor_fix.py
@@ -10,11 +10,12 @@ def fill_in_missing_actor(apps, schema_editor):
     Audit = apps.get_model("audit_trail", "Audit")
     ExporterUser = apps.get_model("users", "ExporterUser")
     content_type = ContentType.objects.get_for_model(ExporterUser)
-
+    print("Filling in missing actors")
     for audit in Audit.objects.filter(
         actor_content_type__isnull=True,
         verb__in=[AuditType.UPDATED_STATUS.value, AuditType.CREATED.value]
     ):
+        print("Updating audit for:", audit.id)
         audit.actor_content_type = content_type
         audit.actor_object_id = ExporterUser.objects.first().id
         audit.save()
