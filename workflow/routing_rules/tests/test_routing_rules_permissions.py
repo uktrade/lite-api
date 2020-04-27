d3c32a802734e201a53490bbadbbc19e397625f6
Mark Spiers <marksp@kainos.com>
2020-04-27 16:23:47 +0100

    api tests tidy up

--- workflow/routing_rules/tests/test_routing_rules_permissions.py
+++ workflow/routing_rules/tests/test_routing_rules_permissions.py
@@ -8,11 +8,10 @@ from users.models import Role
 
 
 class RoutingRuleCreationTests(DataTestClient):
-    def test_all_permissions_works(self):
-        self.gov_user.role = self.super_user_role
-        self.gov_user.save()
+    def setUp(self):
+        super(RoutingRuleCreationTests, self).setUp()
 
-        routing_rule = self.create_routing_rule(
+        self.routing_rule = self.create_routing_rule(
             team_id=self.team.id,
             queue_id=self.queue.id,
             tier=5,
@@ -20,9 +19,14 @@ class RoutingRuleCreationTests(DataTestClient):
             additional_rules=[],
         )
 
-        url = reverse("routing_rules:detail", kwargs={"pk": routing_rule.id})
+        self.url = reverse("routing_rules:detail", kwargs={"pk": self.routing_rule.id})
 
-        response = self.client.get(url, **self.gov_headers)
+
+    def test_all_permissions_works(self):
+        self.gov_user.role = self.super_user_role
+        self.gov_user.save()
+
+        response = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -32,17 +36,7 @@ class RoutingRuleCreationTests(DataTestClient):
         self.gov_user.role = role
         self.gov_user.save()
 
-        routing_rule = self.create_routing_rule(
-            team_id=self.team.id,
-            queue_id=self.queue.id,
-            tier=5,
-            status_id=CaseStatus.objects.last().id,
-            additional_rules=[],
-        )
-
-        url = reverse("routing_rules:detail", kwargs={"pk": routing_rule.id})
-
-        response = self.client.get(url, **self.gov_headers)
+        response = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -52,61 +46,31 @@ class RoutingRuleCreationTests(DataTestClient):
         self.gov_user.role = role
         self.gov_user.save()
 
-        routing_rule = self.create_routing_rule(
-            team_id=self.team.id,
-            queue_id=self.queue.id,
-            tier=5,
-            status_id=CaseStatus.objects.last().id,
-            additional_rules=[],
-        )
-
-        url = reverse("routing_rules:detail", kwargs={"pk": routing_rule.id})
-
-        response = self.client.get(url, **self.gov_headers)
+        response = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_manage_all_permission_works_for_other_team_rules(self):
         role = Role.objects.create(name="test")
         role.permissions.set([constants.GovPermissions.MANAGE_ALL_ROUTING_RULES.name])
+        other_team = self.create_team("other")
         self.gov_user.role = role
+        self.gov_user.team = other_team
         self.gov_user.save()
 
-        other_team = self.create_team("other")
-
-        routing_rule = self.create_routing_rule(
-            team_id=other_team.id,
-            queue_id=self.queue.id,
-            tier=5,
-            status_id=CaseStatus.objects.last().id,
-            additional_rules=[],
-        )
-
-        url = reverse("routing_rules:detail", kwargs={"pk": routing_rule.id})
-
-        response = self.client.get(url, **self.gov_headers)
+        response = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_manage_team_permission_fails_for_other_team_rules(self):
         role = Role.objects.create(name="test")
         role.permissions.set([constants.GovPermissions.MANAGE_TEAM_ROUTING_RULES.name])
+        other_team = self.create_team("other")
         self.gov_user.role = role
+        self.gov_user.team = other_team
         self.gov_user.save()
 
-        other_team = self.create_team("other")
-
-        routing_rule = self.create_routing_rule(
-            team_id=other_team.id,
-            queue_id=self.queue.id,
-            tier=5,
-            status_id=CaseStatus.objects.last().id,
-            additional_rules=[],
-        )
-
-        url = reverse("routing_rules:detail", kwargs={"pk": routing_rule.id})
-
-        response = self.client.get(url, **self.gov_headers)
+        response = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -115,16 +79,6 @@ class RoutingRuleCreationTests(DataTestClient):
         self.gov_user.role = role
         self.gov_user.save()
 
-        routing_rule = self.create_routing_rule(
-            team_id=self.team.id,
-            queue_id=self.queue.id,
-            tier=5,
-            status_id=CaseStatus.objects.last().id,
-            additional_rules=[],
-        )
-
-        url = reverse("routing_rules:detail", kwargs={"pk": routing_rule.id})
-
-        response = self.client.get(url, **self.gov_headers)
+        response = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
