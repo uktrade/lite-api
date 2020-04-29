7a1e8411b76895288dc8a8348e30f5feb44ab649
BenAndrew-Kainos <bena@kainos.com>
2020-04-29 11:15:35 +0100

    Add case flags endpoint tests

---
+++ flags/tests/tests_case_flags.py
@@ -0,0 +1,33 @@
+from django.urls import reverse
+from rest_framework import status
+
+from test_helpers.clients import DataTestClient
+
+
+class CaseFlagsTests(DataTestClient):
+    def setUp(self):
+        super().setUp()
+        self.case = self.create_standard_application_case(self.organisation)
+        self.url = reverse("flags:case_flags", kwargs={"case_pk": self.case.pk})
+        self.flag_1 = self.create_flag("Case Flag 1", "Case", self.team)
+        self.flag_2 = self.create_flag("Case Flag 2", "Case", self.team)
+        self.flags = [self.flag_1, self.flag_2]
+        self.case.flags.set(self.flags)
+
+    def test_get_case_flags(self):
+        response = self.client.get(self.url, **self.gov_headers)
+        response_data = response.json()["flags"]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_data, [flag.name for flag in self.flags])
+
+    def test_get_case_flags_which_block_approval(self):
+        self.flag_1.blocks_approval = True
+        self.flag_1.save()
+
+        response = self.client.get(self.url + "?blocks_approval=True", **self.gov_headers)
+        response_data = response.json()["flags"]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response_data), 1)
+        self.assertEqual(response_data[0], self.flag_1.name)
