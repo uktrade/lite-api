b76ffe2d1cd634bd04e2d36818a0b3f98483aa9c
cstobart <c.stobart@kainos.com>
2020-04-30 12:25:57 +0100

    move method to helper

--- applications/serializers/open_application.py
+++ applications/serializers/open_application.py
@@ -1,10 +1,9 @@
-import csv
-
 from rest_framework import serializers
 from rest_framework.fields import CharField
 
 from applications.enums import ApplicationExportType, GoodsTypeCategory
-from applications.models import OpenApplication, CountryOnApplication
+from applications.libraries.goodstype_category_helpers import _set_goods_and_countries_for_open_media_application
+from applications.models import OpenApplication
 from applications.serializers.generic_application import (
     GenericApplicationCreateSerializer,
     GenericApplicationUpdateSerializer,
@@ -13,7 +12,7 @@ from applications.serializers.generic_application import (
 from applications.serializers.serializer_helper import validate_field
 from cases.enums import CaseTypeEnum
 from conf.serializers import KeyValueChoiceField
-from goodstype.serializers import GoodsTypeViewSerializer, GoodsTypeSerializer
+from goodstype.serializers import GoodsTypeViewSerializer
 from licences.models import Licence
 from licences.serializers.view_licence import CaseLicenceViewSerializer
 from lite_content.lite_api import strings
@@ -166,27 +165,7 @@ class OpenApplicationCreateSerializer(GenericApplicationCreateSerializer):
         application = super().create(validated_data)
 
         if validated_data.get("goodstype_category") == GoodsTypeCategory.MEDIA:
-            with open("lite_content/lite_api/OEIL_products.csv", newline="") as csvfile:
-                reader = csv.DictReader(csvfile)
-
-                for row in reader:
-                    if row["SUBTYPE"] == "MEDIA":
-                        data = {
-                            "application": application,
-                            "description": row["DESCRIPTION"],
-                            "is_good_controlled": "True",
-                            "is_good_incorporated": "False",
-                            "control_list_entries": row["CONTROL_ENTRY"].split(", "),
-                            "report_summary": row["ARS"],
-                        }
-                        serializer = GoodsTypeSerializer(data=data)
-                        if serializer.is_valid():
-                            serializer.save()
-                        else:
-                            print(serializer.errors)
-
-            for country in Country.objects.all():
-                CountryOnApplication(country=country, application=application).save()
+            _set_goods_and_countries_for_open_media_application(application)
 
         return application
 
