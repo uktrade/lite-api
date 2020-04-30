18a563ed5551971ece26bc42d2d2b2b6b16191d0
mh-hmcts <mh@hmcts.net>
2020-04-30 10:57:55 +0100

    Update seedstats.py

--- static/management/commands/seedstats.py
+++ static/management/commands/seedstats.py
@@ -1,5 +1,7 @@
 from applications.models import StandardApplication
 from organisations.models import Organisation
+from goods.models import Good
+from applications.models import GoodOnApplication
 from static.management.SeedCommand import SeedCommand
 from users.models import ExporterUser
 
@@ -16,5 +18,7 @@ class Command(SeedCommand):
     def operation(self, *args, **options):
 
         print(f"Organisations:{Organisation.objects.all().count()}"
-              f"\nSIELs:{StandardApplication.objects.all().count()}"
+              f"\nSIEL applications:{StandardApplication.objects.all().count()}"
+              f"\nOrganisation Products:{Good.objects.all().count()}"
+              f"\nProducts used in SEIL applications:{GoodOnApplication.objects.all().count()}"
               f"\nExport Users:{ExporterUser.objects.all().count()}")
