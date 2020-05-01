11668f0b0fde61227da78efc7826df7fd5daa4ab
BenAndrew-Kainos <bena@kainos.com>
2020-05-01 15:41:43 +0100

    hide notification bubble if user doesnt have permission

--- menu/views.py
+++ menu/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 
 from conf.authentication import GovAuthentication
+from conf.constants import GovPermissions
 from organisations.enums import OrganisationStatus
 from organisations.models import Organisation
 
@@ -10,7 +11,11 @@ class MenuNotifications(APIView):
     authentication_classes = (GovAuthentication,)
 
     def get(self, request):
-        notifications = {"organisations": Organisation.objects.filter(status=OrganisationStatus.IN_REVIEW).count()}
+        notifications = {
+            "organisations": Organisation.objects.filter(status=OrganisationStatus.IN_REVIEW).count()
+            if request.user.has_permission(GovPermissions.MANAGE_ORGANISATIONS)
+            else 0
+        }
         return JsonResponse(
             {"notifications": notifications, "has_notifications": any(value for value in notifications.values())}
         )
