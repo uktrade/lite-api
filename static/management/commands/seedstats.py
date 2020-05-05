beaaf6193d8f4d5e9863987e00fb4a04a7474262
mh-hmcts <mh@hmcts.net>
2020-05-05 14:04:08 +0100

    Update seedstats.py

--- static/management/commands/seedstats.py
+++ static/management/commands/seedstats.py
@@ -5,6 +5,7 @@ from applications.models import GoodOnApplication
 from static.management.SeedCommand import SeedCommand
 from users.models import ExporterUser
 
+
 class Command(SeedCommand):
     """
     pipenv run ./manage.py seeddata>
@@ -17,8 +18,10 @@ class Command(SeedCommand):
 
     def operation(self, *args, **options):
 
-        print(f"Organisations:{Organisation.objects.all().count()}"
-              f"\nSIEL applications:{StandardApplication.objects.all().count()}"
-              f"\nOrganisation Products:{Good.objects.all().count()}"
-              f"\nProducts used in SEIL applications:{GoodOnApplication.objects.all().count()}"
-              f"\nExport Users:{ExporterUser.objects.all().count()}")
+        print(
+            f"Organisations:{Organisation.objects.all().count()}"
+            f"\nSIEL applications:{StandardApplication.objects.all().count()}"
+            f"\nOrganisation Products:{Good.objects.all().count()}"
+            f"\nProducts used in SEIL applications:{GoodOnApplication.objects.all().count()}"
+            f"\nExport Users:{ExporterUser.objects.all().count()}"
+        )
