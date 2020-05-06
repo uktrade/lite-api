ab71ac0fff953d7e688bbdb2a1fd72c3ac99cc68
BenAndrew-Kainos <bena@kainos.com>
2020-05-06 12:44:56 +0100

    Fix test

--- users/tests/tests_exporter_user_notifications.py
+++ users/tests/tests_exporter_user_notifications.py
@@ -165,11 +165,10 @@ class ExporterUserNotificationTests(DataTestClient):
         self._create_clc_query_with_notifications()
 
         response = self.client.get(reverse_lazy("goods:goods"), **self.exporter_headers)
+        response_data = response.json()["results"][0]
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        good_response_data = response.json()["results"][0]["query"]
-        self.assertIn("exporter_user_notification_count", good_response_data)
-        self.assertEqual(good_response_data["exporter_user_notification_count"]["total"], 3)
+        self.assertEqual(response_data["exporter_user_notification_count"], 3)
 
     def test_get_good_with_notifications_success(self):
         case = self._create_clc_query_with_notifications()
