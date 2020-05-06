985062527b83a3e1f7eb0e10e188816e5860b8ad
BenAndrew-Kainos <bena@kainos.com>
2020-05-06 14:27:35 +0100

    Add helpers for getting org and org_id

--- organisations/libraries/get_organisation.py
+++ organisations/libraries/get_organisation.py
@@ -1,3 +1,4 @@
+from conf.authentication import ORGANISATION_ID
 from conf.exceptions import NotFoundError
 from organisations.models import Organisation
 
@@ -7,3 +8,13 @@ def get_organisation_by_pk(pk):
         return Organisation.objects.get(pk=pk)
     except Organisation.DoesNotExist:
         raise NotFoundError({"organisation": "Organisation not found - " + str(pk)})
+
+
+def get_request_user_organisation_id(request):
+    return request.META.get(ORGANISATION_ID)
+
+
+def get_request_user_organisation(request):
+    org_id = get_request_user_organisation_id(request)
+    return get_organisation_by_pk(org_id)
+
