67d995ae572570c0f0b8b9b9e4c5a3e7820a3bd5
BenAndrew-Kainos <bena@kainos.com>
2020-05-06 16:25:05 +0100

    Add ExporterOnlyAuthentication back in

--- conf/authentication.py
+++ conf/authentication.py
@@ -6,6 +6,7 @@ from gov_users.enums import GovUserStatuses
 from organisations.enums import OrganisationType, OrganisationStatus
 from organisations.models import Organisation
 from users.enums import UserStatuses
+from users.libraries.get_user import get_user_by_pk
 from users.libraries.token_to_user import token_to_user_pk
 from users.models import UserOrganisationRelationship, ExporterUser, GovUser
 
@@ -79,6 +80,15 @@ class HmrcExporterAuthentication(authentication.BaseAuthentication):
 
         return user, None
 
+
+class ExporterOnlyAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        """
+        When given a user token, validate that the user exists
+        """
+        exporter_user_token = request.META.get(EXPORTER_USER_TOKEN_HEADER)
+        exporter_user = get_user_by_pk(token_to_user_pk(exporter_user_token))
+        return exporter_user, None
 
 class GovAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
