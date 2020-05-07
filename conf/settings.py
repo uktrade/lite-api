6cf03d496e2fe04ec0ee4585f13b20b60daf4105
Andy Taylor <a.taylor@kainos.com>
2020-05-07 19:36:49 +0100

    rename key

--- conf/settings.py
+++ conf/settings.py
@@ -131,7 +131,7 @@ AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend"]
 HAWK_CREDENTIALS = {
     "exporter-frontend": {"id": "exporter-frontend", "key": env("LITE_EXPORTER_HAWK_KEY"), "algorithm": "sha256"},
     "internal-frontend": {"id": "internal-frontend", "key": env("LITE_INTERNAL_HAWK_KEY"), "algorithm": "sha256"},
-    "lite-e2e": {"id": "lite-e2e", "key": env("LITE_E2E_HAWK_KEY"), "algorithm": "sha256"},
+    "lite-api-client": {"id": "lite-api-client", "key": env("LITE_API_CLIENT_HAWK_KEY"), "algorithm": "sha256"},
 }
 
 HAWK_RECEIVER_NONCE_EXPIRY_SECONDS = 60 if not DEBUG else 3600  # When DEBUG is TRUE, allow more time to debug responses
