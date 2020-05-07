eefc1a7da6e07f2243c94da1e597a8b6624ae868
BenAndrew-Kainos <bena@kainos.com>
2020-05-07 14:09:40 +0100

    repeat for hmrc orgs

--- test_helpers/clients.py
+++ test_helpers/clients.py
@@ -131,7 +131,7 @@ class DataTestClient(APITestCase, URLPatternsTestCase):
 
         self.hmrc_exporter_headers = {
             "HTTP_EXPORTER_USER_TOKEN": user_to_token(self.hmrc_exporter_user),
-            "HTTP_ORGANISATION_ID": self.hmrc_organisation.id,
+            "HTTP_ORGANISATION_ID": str(self.hmrc_organisation.id),
         }
 
         self.queue = self.create_queue("Initial Queue", self.team)
