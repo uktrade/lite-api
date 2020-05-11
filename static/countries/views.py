49bc88342bf6e42ca00ae11f1f40ad4849e1310a
Andy Taylor <a.taylor@kainos.com>
2020-05-11 18:18:04 +0100

    countries need to be open to non authenticated users for registering

--- static/countries/views.py
+++ static/countries/views.py
@@ -2,13 +2,13 @@ from django.http import JsonResponse
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.views import APIView
 
-from conf.authentication import SharedAuthentication
+from conf.authentication import HawkOnlyAuthentication
 from static.countries.models import Country
 from static.countries.serializers import CountrySerializer
 
 
 class CountriesList(APIView):
-    authentication_classes = (SharedAuthentication,)
+    authentication_classes = (HawkOnlyAuthentication,)
 
     def get(self, request):
         countries = Country.objects.exclude(id__in=request.GET.getlist("exclude"))
@@ -17,7 +17,7 @@ class CountriesList(APIView):
 
 
 class CountryDetail(RetrieveAPIView):
-    authentication_classes = (SharedAuthentication,)
+    authentication_classes = (HawkOnlyAuthentication,)
 
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
