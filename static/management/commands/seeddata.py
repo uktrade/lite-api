d6220c6ae7d72066de3a4eda0abea6151a6fd283
mh-hmcts <mh@hmcts.net>
2020-05-11 10:00:34 +0100

    Update seeddata.py

--- static/management/commands/seeddata.py
+++ static/management/commands/seeddata.py
@@ -1,14 +1,17 @@
 import random
-from django.db import transaction
+from uuid import UUID
+
 from faker import Faker
 
 from applications.models import StandardApplication
+from conf.constants import Roles
 from organisations.enums import OrganisationType
 from organisations.models import Organisation
 from organisations.tests.providers import OrganisationProvider
 from static.management.SeedCommand import SeedCommand
 from static.management.commands.seedapplication import Command as AppCommand
 from static.management.commands.seedorganisation import Command as OrgCommand
+from users.models import UserOrganisationRelationship, ExporterUser
 
 faker = Faker()
 faker.add_provider(OrganisationProvider)
@@ -18,7 +21,23 @@ class Params(object):
     pass
 
 
-@transaction.atomic
+# class Command(SeedCommand):
+#     """
+#     pipenv run ./manage.py seeddata>
+#     """
+#
+#     help = "Seeds data"
+#     info = "Seeding data"
+#     success = "Successfully seeded data"
+#     seed_command = "data-org"
+#
+#     def add_arguments(self, parser):
+#         parser.add_argument("--count", help="the number of organisations", type=int)
+#
+#     def orgs_and_apps(self, options):
+#         self.stdout.write(self.style.SUCCESS(f"adding {options.get('org_count') or 1} orgs")
+
+
 class Command(SeedCommand):
     """
     pipenv run ./manage.py seeddata>
@@ -26,22 +45,167 @@ class Command(SeedCommand):
 
     help = "Seeds data"
     info = "Seeding data"
-    success = "Successfully seeded data"
+    success = "Successfully completed action"
     seed_command = "seeddata"
 
     def add_arguments(self, parser):
         # Named (optional) arguments
-        parser.add_argument("--org-count", help="the number of organisations", type=int)
-        parser.add_argument("--org-site-max", help="max number of sites", type=int)
-        parser.add_argument("--org-site-min", help="min number of sites", type=int)
-        parser.add_argument("--org-user-min", help="number of users", type=int)
-        parser.add_argument("--org-user-max", help="number of users", type=int)
-        parser.add_argument("--org-primary", help="primary email", type=str)
-        parser.add_argument("--org-goods", help="number of goods", type=int)
-        parser.add_argument("--org-applications", help="number of goods", type=int)
+        parser.add_argument(
+            "--action", help="command", choices=["org", "site", "remove-user", "add-user", "siel"], type=str
+        )
+        parser.add_argument("--organisation", help="application count", type=str)
+        parser.add_argument("--organisations", help="application count", type=int)
+        parser.add_argument("--site-max", help="max number of sites", type=int)
+        parser.add_argument("--site-min", help="min number of sites", type=int)
+        parser.add_argument("--user-min", help="number of users", type=int)
+        parser.add_argument("--user-max", help="number of users", type=int)
+        parser.add_argument("--user-email", help="user email", type=str)
+        parser.add_argument("--max-goods", help="max number of goods", type=int)
+        parser.add_argument("--applications", help="number of applications", type=int)
+
+    def operation(self, *args, **options):
+        action = options.get("action")
+
+        if action is None:
+            raise Exception("action not specified!")
+
+        self.stdout.write(self.style.SUCCESS(f"action = {action}"))
+
+        if action == "org":
+            org_count = options.get("organisations") or 1
+            site_max = options.get("site_max") or 1
+            site_min = options.get("site_min") or 1
+            user_min = options.get("user_min") or 1
+            user_max = options.get("user_max") or 1
+            user_email = options.get("user_email")
+            print(f"organisations={org_count}")
+            print(f"site_max={site_max}")
+            print(f"site_min={site_min}")
+            print(f"user_min={user_min}")
+            print(f"user_max={user_max}")
+            print(f"primary_email={user_email}")
+
+            # ensure the correct number of organisations
+            organisations = self.get_create_organisations(org_count, user_email, site_min, site_max, user_min, user_max)
+            print(f"Ensured that at least {len(organisations)} organisations exist")
+            return organisations
+
+        if action == "site":
+            return
+
+        if action == "add-user":
+            org, exporter_user, organisation = self.parse_user_options(options)
+            to_add = []
+
+            if org == "all":
+                organisations = Organisation.objects.all()
+                membership_ids = (
+                    UserOrganisationRelationship.objects.select_related("organisation")
+                    .filter(user=exporter_user)
+                    .values_list("organisation__id")
+                )
+                ids_to_add = set([org.id for org in organisations]) - set(membership_ids)
+                to_add = [org for id_to_add in ids_to_add for org in organisations if org.id == id_to_add]
+            else:
+                try:
+                    UserOrganisationRelationship.objects.get(user=exporter_user, organisation=organisation)
+                except Exception:
+                    to_add.append(organisation)
+
+            uors = [
+                UserOrganisationRelationship.objects.create(
+                    organisation=organisation, user=exporter_user, role_id=Roles.EXPORTER_SUPER_USER_ROLE_ID
+                )
+                for organisation in to_add
+            ]
+
+            self.stdout.write(self.style.SUCCESS(f"Added {exporter_user.email} to {len(uors)} organisations"))
+            return
+
+        if action == "remove-user":
+            org, exporter_user, organisation = self.parse_user_options(options)
+
+            if org == "all":
+                count, _ = (
+                    UserOrganisationRelationship.objects.select_related("organisation")
+                    .filter(user=exporter_user)
+                    .delete()
+                )
+            else:
+                try:
+                    count, _ = UserOrganisationRelationship.objects.filter(
+                        user=exporter_user, organisation=organisation
+                    ).delete()
+                except Exception:
+                    raise Exception(f"An Exporter User with email: '{exporter_user.email}' is not in organisation")
+
+            self.stdout.write(self.style.SUCCESS(f"Removed {exporter_user.email} from {count} organisations"))
+            return
+
+        if action == "siel":
+            org_count = options.get("organisations") or 1
+            app_count = options.get("applications") or 0
+            max_goods = options.get("max_goods") or 1
+
+            print("Add SIEL applications to organisations")
+            print(f"organisations={app_count}")
+            print(f"applications={app_count}")
+            print(f"max_goods={max_goods}")
+
+            # ensure the correct number of organisations
+            organisations = list(Organisation.objects.all())
+            if len(organisations) < org_count:
+                error_str = f"not enough organisations, found: {len(organisations)} of {org_count} required"
+                self.stdout.write(self.style.ERROR(error_str))
+                raise Exception(error_str)
+
+            # ensure the correct number of standard applications per org
+            org_app_counts = [
+                (org, StandardApplication.objects.filter(organisation_id=org.id).count()) for org in organisations
+            ]
+            applications_to_add = [(org, app_count - count) for org, count in org_app_counts if count < app_count]
+            apps = [
+                self.app_factory(org=org, max_goods_to_use=max_goods, applications_to_add=apps_to_add)
+                for org, apps_to_add in applications_to_add
+            ]
+            print(f"ensured {app_count} applications for the first {org_count} organisations")
+            print(f"added {sum(apps)} applications in total")
+            return apps
+
+    def parse_user_options(self, options):
+        org = options.get("organisation")
+        user_email = options.get("user_email")
+
+        if org is None:
+            raise Exception("organisation not specified!")
+
+        if user_email is None:
+            raise Exception("user email not specified!")
+        try:
+            exporter_user = ExporterUser.objects.get(email__exact=user_email)
+        except ExporterUser.DoesNotExist:
+            raise Exception(f"An Exporter User with email: '{user_email}' does not exist")
+
+        organisation = None
+        if org != "all":
+            try:
+                organisation = Organisation.objects.get(id=UUID(org))
+            except Organisation.DoesNotExist:
+                raise Exception(f"An Organisation with id: '{org}' does not exist")
+
+        return org, exporter_user, organisation
+
+    def get_create_organisations(self, org_count, org_primary, org_site_min, org_site_max, org_user_min, org_user_max):
+        organisations = list(Organisation.objects.all()[:org_count])
+        required = max(0, org_count - len(organisations))
+        organisations += [
+            self.org_factory(org_primary, org_site_min, org_site_max, org_user_min, org_user_max)
+            for _ in range(0, required)
+        ]
+        return organisations
 
     @staticmethod
-    def org_factory(params):
+    def org_factory(org_primary, org_site_min, org_site_max, org_user_min, org_user_max):
         org_name = faker.company()
         if Organisation.objects.filter(name__iexact=org_name).exists():
             idx = 1
@@ -51,57 +215,16 @@ class Command(SeedCommand):
                 new_name = f"{org_name} ({idx})"
             org_name = new_name
 
-        result = OrgCommand.seed_organisation(
+        organisation, _, _, _ = OrgCommand.seed_organisation(
             org_name,
             OrganisationType.COMMERCIAL,
-            random.randint(params.org_site_min, params.org_site_max),
-            random.randint(params.org_user_min, params.org_user_max),
-            params.org_primary,
+            random.randint(org_site_min, org_site_max),
+            random.randint(org_user_min, org_user_max),
+            org_primary,
         )
-        return result[0]
+        return organisation
 
     @staticmethod
     def app_factory(org, applications_to_add, max_goods_to_use):
         _, submitted_applications, _ = AppCommand.seed_siel_applications(org, applications_to_add, max_goods_to_use,)
         return len(submitted_applications)
-
-    def operation(self, *args, **options):
-        params = Params()
-        params.org_count = options.get("org_count") or 1
-        params.org_site_max = options.get("org_site_max") or 1
-        params.org_site_min = options.get("org_site_min") or 1
-        params.org_user_min = options.get("org_user_min") or 1
-        params.org_user_max = options.get("org_user_max") or 1
-        params.org_primary = options.get("org_primary")
-        params.org_goods = options.get("org_goods") or 1
-        params.org_applications = options.get("org_applications") or 0
-
-        print(f"org-count={params.org_count}")
-        print(f"org_site_max={params.org_site_max}")
-        print(f"org_site_min={params.org_site_min}")
-        print(f"org_user_min={params.org_user_min}")
-        print(f"org_user_max={params.org_user_max}")
-        print(f"org_site_primary={params.org_primary}")
-        print(f"org_goods={params.org_goods}")
-        print(f"org_applications={params.org_applications}")
-
-        # ensure the correct number of organisations
-        orgs = list(Organisation.objects.all()[: params.org_count])
-        required = max(0, params.org_count - len(orgs))
-        orgs += [self.org_factory(params) for _ in range(0, required)]
-        print(f"identified {params.org_count} organisations to use")
-
-        # ensure the correct number of standard applications per org
-        org_app_counts = [(org, StandardApplication.objects.filter(organisation_id=org.id).count()) for org in orgs]
-        applications_to_add = [
-            (org, params.org_applications - count) for org, count in org_app_counts if count < params.org_applications
-        ]
-        apps = [
-            self.app_factory(org=org, max_goods_to_use=params.org_goods, applications_to_add=apps_to_add)
-            for org, apps_to_add in applications_to_add
-        ]
-
-        print(
-            f"ensured  {params.org_applications} applications for the first {params.org_count} organisations"
-            f", adding {sum(apps)} applications"
-        )
