cc4f47601b4f9ab3d9f985fb8dcba91aa4e6abb3
mh-hmcts <mh@hmcts.net>
2020-05-12 13:51:42 +0100

    Update seeddata.py

--- static/management/commands/seeddata.py
+++ static/management/commands/seeddata.py
@@ -1,8 +1,7 @@
 import random
+
 from uuid import UUID
-
 from faker import Faker
-
 from applications.models import StandardApplication
 from conf.constants import Roles
 from organisations.enums import OrganisationType
@@ -17,149 +16,7 @@ faker = Faker()
 faker.add_provider(OrganisationProvider)
 
 
-class Command(SeedCommand):
-    """
-    pipenv run ./manage.py seeddata --action ...>
-    """
-
-    help = "Seeds data"
-    info = "Seeding data"
-    success = "Successfully completed action"
-    seed_command = "seeddata"
-
-    def add_arguments(self, parser):
-        # Named (optional) arguments
-        parser.add_argument(
-            "--action", help="command", choices=["org", "site", "remove-user", "add-user", "siel"], type=str
-        )
-        parser.add_argument("--organisation", help="application count", type=str)
-        parser.add_argument("--organisations", help="application count", type=int)
-        parser.add_argument("--sites-max", help="max number of sites", type=int)
-        parser.add_argument("--sites-min", help="min number of sites", type=int)
-        parser.add_argument("--users-min", help="number of users", type=int)
-        parser.add_argument("--users-max", help="number of users", type=int)
-        parser.add_argument("--user-email", help="user email", type=str)
-        parser.add_argument("--max-goods", help="max number of goods", type=int)
-        parser.add_argument("--applications", help="number of applications", type=int)
-
-    def operation(self, *args, **options):
-        action = options.get("action")
-        if action is None:
-            raise Exception("action not specified!")
-        self.stdout.write(self.style.SUCCESS(f"action = {action}"))
-
-        if action == "org":
-            org_count = self.get_arg(options, "organisations", 1)
-            site_max = self.get_arg(options, "site_max", 1)
-            site_min = self.get_arg(options, "site_min", 1)
-            user_min = self.get_arg(options, "user_min", 1)
-            user_max = self.get_arg(options, "user_max", 1)
-            user_email = self.get_arg(options, "user_email", 1)
-
-            # ensure the correct number of organisations
-            organisations, created_count = self.get_create_organisations(org_count, user_email, site_min, site_max)
-
-            # ensure the correct number of users
-            user_counts = [
-                len(self.organisation_get_create_users(organisation, user_max, user_min))
-                for organisation in organisations
-            ]
-
-            print(
-                f"Ensured that at least {len(organisations)} organisations exist"
-                f", added {created_count} new organisations"
-            )
-            print(f"each with between {user_min} and {user_max} users, added {sum(user_counts)} new users")
-
-            return organisations
-
-        if action == "users":
-            print("Add users to organisations")
-            org_count = self.get_arg(options, "organisations", 1)
-            user_min = self.get_arg(options, "users_min", 1)
-            user_max = self.get_arg(options, "users_max", 1)
-
-            # ensure the correct number of organisations
-            organisations = self.organisation_get_first_n(org_count)
-
-            # ensure the correct number of users
-            user_counts = [
-                len(self.organisation_get_create_users(organisation, user_max, user_min))
-                for organisation in organisations
-            ]
-
-            print(f"each with between {user_min} and {user_max} users, added {sum(user_counts)} new users")
-            return
-
-        if action == "site":
-            return
-
-        if action == "add-user":
-            org = self.get_arg(options, "organisation")
-            user_email = self.get_arg(options, "user_email")
-            exporter_user = ExporterUser.objects.get_or_create(first_name="first", last_name="last", email=user_email)
-
-            to_add = []
-            if org == "all":
-                membership = (
-                    UserOrganisationRelationship.objects.select_related("organisation")
-                        .filter(user=exporter_user)
-                        .values_list("organisation__id")
-                )
-                organisations = Organisation.objects.all()
-                ids_to_add = set([org.id for org in organisations]) - set(membership)
-                to_add = [org for id_to_add in ids_to_add for org in organisations if org.id == id_to_add]
-            else:
-                organisation = Organisation.objects.get(id=UUID(org))
-                user = self.organisation_get_user(organisation, exporter_user)
-                if user is None:
-                    to_add.append(organisation)
-
-            added_users = [
-                self.organisation_add_user(organisation=organisation, exporter_user=exporter_user)
-                for organisation in to_add
-            ]
-
-            self.stdout.write(self.style.SUCCESS(f"Added {exporter_user.email} to {len(added_users)} organisations"))
-            return
-
-        if action == "remove-user":
-            org = self.get_arg(options, "organisation")
-            user_email = self.get_arg(options, "user_email")
-            exporter_user = ExporterUser.objects.get_or_create(first_name="first", last_name="last", email=user_email)
-
-            if org == "all":
-                count, _ = self.organisation_delete_user_from_all(exporter_user=exporter_user)
-            else:
-                organisation = Organisation.objects.get(id=UUID(org))
-                count, _ = self.organisation_delete_user(organisation=organisation, exporter_user=exporter_user)
-
-            self.stdout.write(self.style.SUCCESS(f"Removed {exporter_user.email} from {count} organisations"))
-            return
-
-        if action == "siel":
-            print("Add SIEL applications to organisations")
-            org_count = self.get_arg(options, "organisations", 1)
-            app_count = self.get_arg(options, "applications", 1)
-            max_goods = self.get_arg(options, "max_goods", 1)
-
-            # ensure the correct number of organisations
-            organisations = self.organisation_get_first_n(org_count)
-
-            # ensure the correct number of standard applications per org
-            org_app_counts = [
-                (org, StandardApplication.objects.filter(organisation_id=org.id).count())
-                for org in organisations
-            ]
-            applications_to_add = [(org, app_count - count) for org, count in org_app_counts if count < app_count]
-            apps = [
-                self.app_factory(org=org, max_goods_to_use=max_goods, applications_to_add=apps_to_add)
-                for org, apps_to_add in applications_to_add
-            ]
-            print(f"ensured {app_count} applications for the first {org_count} organisations")
-            print(f"added {sum(apps)} applications in total")
-            return apps
-
+class SeedDataBase:
     def organisation_get_first_n(self, org_count):
         organisations = list(Organisation.objects.all())
         if len(organisations) < org_count:
@@ -248,5 +105,164 @@ class Command(SeedCommand):
 
     @staticmethod
     def app_factory(org, applications_to_add, max_goods_to_use):
-        _, submitted_applications, _ = AppCommand.seed_siel_applications(org, applications_to_add, max_goods_to_use, )
+        _, submitted_applications, _ = AppCommand.seed_siel_applications(org, applications_to_add, max_goods_to_use,)
         return len(submitted_applications)
+
+
+class ActionOrg(SeedDataBase):
+    def action(self, options):
+        org_count = self.get_arg(options, "count", 1)
+        site_max = self.get_arg(options, "site_max", 1)
+        site_min = self.get_arg(options, "site_min", 1)
+        user_min = self.get_arg(options, "users_min", 1)
+        user_max = self.get_arg(options, "users_max", 1)
+        user_email = self.get_arg(options, "user_email")
+
+        # ensure the correct number of organisations
+        organisations, created_count = self.get_create_organisations(org_count, user_email, site_min, site_max)
+
+        # ensure the correct number of users
+        user_counts = [
+            len(self.organisation_get_create_users(organisation, user_max, user_min)) for organisation in organisations
+        ]
+
+        print(
+            f"Ensured that at least {len(organisations)} organisations exist"
+            f", added {created_count} new organisations"
+        )
+        print(f"each with between {user_min} and {user_max} users, added {sum(user_counts)} new users")
+        return organisations
+
+
+class ActionAddFakeUsers(SeedDataBase):
+    def action(self, options):
+        print("Add fake users to organisations")
+        org_count = self.get_arg(options, "organisations", 1)
+        user_min = self.get_arg(options, "users_min", 1)
+        user_max = self.get_arg(options, "users_max", 1)
+
+        # ensure the correct number of organisations
+        organisations = self.organisation_get_first_n(org_count)
+
+        # ensure the correct number of users
+        user_counts = [
+            len(self.organisation_get_create_users(organisation, user_max, user_min)) for organisation in organisations
+        ]
+
+        print(
+            f"{len(organisations)} organisations"
+            f" each have between {user_min} and {user_max} users, added {sum(user_counts)} new users")
+        return
+
+
+class ActionUser(SeedDataBase):
+    def action(self, options):
+        return self.remove(options) if self.get_arg(options, "rm") else self.add(options)
+
+    def add(self, options):
+        org_uuid = UUID(self.get_arg(options, "uuid"))
+        user_email = self.get_arg(options, "email")
+        exporter_user = ExporterUser.objects.get_or_create(first_name="first", last_name="last", email=user_email)
+
+        to_add = []
+        if org == "all":
+            membership = (
+                UserOrganisationRelationship.objects.select_related("organisation")
+                .filter(user=exporter_user)
+                .values_list("organisation__id")
+            )
+            organisations = Organisation.objects.all()
+            ids_to_add = set([org.id for org in organisations]) - set(membership)
+            to_add = [org for id_to_add in ids_to_add for org in organisations if org.id == id_to_add]
+        else:
+            organisation = Organisation.objects.get(id=org_uuid)
+            user = self.organisation_get_user(organisation, exporter_user)
+            if user is None:
+                to_add.append(organisation)
+
+        added_users = [
+            self.organisation_add_user(organisation=organisation, exporter_user=exporter_user)
+            for organisation in to_add
+        ]
+
+        self.stdout.write(self.style.SUCCESS(f"Added {exporter_user.email} to {len(added_users)} organisations"))
+        return
+
+    def remove(self, options):
+        org = self.get_arg(options, "organisation")
+        user_email = self.get_arg(options, "user_email")
+        exporter_user = ExporterUser.objects.get_or_create(first_name="first", last_name="last", email=user_email)
+
+        if org == "all":
+            count, _ = self.organisation_delete_user_from_all(exporter_user=exporter_user)
+        else:
+            organisation = Organisation.objects.get(id=UUID(org))
+            count, _ = self.organisation_delete_user(organisation=organisation, exporter_user=exporter_user)
+
+        self.stdout.write(self.style.SUCCESS(f"Removed {exporter_user.email} from {count} organisations"))
+        return
+
+
+class ActionSiel(SeedDataBase):
+    def action(self, options):
+        print("Add SIEL applications to organisations")
+        org_count = self.get_arg(options, "organisations", 1)
+        app_count = self.get_arg(options, "applications", 1)
+        max_goods = self.get_arg(options, "max_goods", 1)
+
+        # ensure the correct number of organisations
+        organisations = self.organisation_get_first_n(org_count)
+
+        # ensure the correct number of standard applications per org
+        org_app_counts = [
+            (org, StandardApplication.objects.filter(organisation_id=org.id).count()) for org in organisations
+        ]
+        applications_to_add = [(org, app_count - count) for org, count in org_app_counts if count < app_count]
+        apps = [
+            self.app_factory(org=org, max_goods_to_use=max_goods, applications_to_add=apps_to_add)
+            for org, apps_to_add in applications_to_add
+        ]
+        print(f"ensured {app_count} applications for the first {org_count} organisations")
+        print(f"added {sum(apps)} applications in total")
+        return apps
+
+
+class ActionSites(SeedDataBase):
+    def action(self, options):
+        print("Add sites to organisations - NOT IMPLEMENTED")
+        pass
+
+
+class Command(SeedCommand):
+    help = "Seeds data"
+    info = "Seeding data"
+    success = "Successfully completed action"
+    seed_command = "seeddata"
+    action_map = {
+        "org": ActionOrg(),
+        "fakeuser": ActionAddFakeUsers(),
+        "user": ActionUser(),
+        "siel": ActionSiel(),
+        "site": ActionSites(),
+    }
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument("-a", "--action", help="action command", choices=list(self.action_map.keys()), type=str)
+        parser.add_argument("--uuid", help="a uuid", type=str)
+        parser.add_argument("-c", "--count", help="item count", type=int)
+        parser.add_argument("--sites-max", help="max number of sites", type=int)
+        parser.add_argument("--sites-min", help="min number of sites", type=int)
+        parser.add_argument("--users-min", help="number of users", type=int)
+        parser.add_argument("--users-max", help="number of users", type=int)
+        parser.add_argument("-e", "--email", help="user email", type=str)
+        parser.add_argument("--max-goods", help="max number of goods", type=int)
+        parser.add_argument("--applications", help="number of applications", type=int)
+        parser.add_argument("-r", "--rm", action="store_true", help="remove an item or items")
+
+    def operation(self, *args, **options):
+        action = options.get("action")
+        if action is None:
+            raise Exception("action not specified!")
+        self.stdout.write(self.style.SUCCESS(f"action = {action}"))
+        self.action_map[action].action(options)
