24c7f0b1189d01d3c27b33f9d18bc1c165b09eb2
Mark Spiers <marksp@kainos.com>
2020-05-13 15:09:26 +0100

    first fix

--- goods/serializers.py
+++ goods/serializers.py
@@ -272,6 +272,11 @@ class GoodSerializerInternal(serializers.Serializer):
     is_good_controlled = serializers.CharField()
     report_summary = serializers.CharField()
     flags = GoodsFlagSerializer(many=True)
+    documents = serializers.SerializerMethodField()
+
+    def get_documents(self, instance):
+        documents = GoodDocument.objects.filter(good=instance)
+        return SimpleGoodDocumentViewSerializer(documents, many=True).data
 
 
 class GoodSerializerExporter(serializers.Serializer):
