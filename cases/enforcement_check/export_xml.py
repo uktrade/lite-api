7329fb0407ec85aae23c863a8f04ae6c859127b0
BenAndrew-Kainos <bena@kainos.com>
2020-05-14 12:42:15 +0100

    refactor

--- cases/enforcement_check/export_xml.py
+++ cases/enforcement_check/export_xml.py
@@ -3,8 +3,6 @@ from xml.etree import ElementTree  # nosec
 from xml.sax.saxutils import escape  # nosec
 
 from applications.models import PartyOnApplication, SiteOnApplication
-from conf.exceptions import BadRequestError
-from lite_content.lite_api.strings import Cases
 from parties.enums import PartyRole, PartyType
 
 
@@ -15,38 +13,40 @@ def dict_to_xml(parent, data):
             element.text = escape(str(value))
 
 
-def get_party_sh_type(party_on_application):
-    if party_on_application["party__role"] == PartyRole.CONTACT:
-        return "CONTACT"
-    elif party_on_application["party__type"] == PartyType.THIRD_PARTY:
-        return "OTHER"
-    else:
-        return party_on_application["party__type"].upper()
-
-
-def export_party_on_application_xml(base, party_on_application):
+def entity_to_xml(
+    base, application_id, id, type, country, organisation, address_line_1, name=None, address_line_2=None
+):
     stakeholder = ElementTree.SubElement(base, "STAKEHOLDER")
     dict_to_xml(
         stakeholder,
         {
-            "ELA_ID": party_on_application["application_id"].int,
+            "ELA_ID": application_id.int,
             "ELA_DETAIL_ID": None,
-            "SH_ID": party_on_application["party_id"].int,
-            "SH_TYPE": get_party_sh_type(party_on_application),
-            "COUNTRY": party_on_application["party__country__name"],
-            "ORG_NAME": party_on_application["party__organisation__name"],
-            "PD_SURNAME": party_on_application["party__name"],
+            "SH_ID": id.int,
+            "SH_TYPE": type,
+            "COUNTRY": country,
+            "ORG_NAME": organisation,
+            "PD_SURNAME": name,
             "PD_FORENAME": None,
             "PD_MIDDLE_INITIALS": None,
-            "ADDRESS1": party_on_application["party__address"],
-            "ADDRESS2": None,
+            "ADDRESS1": address_line_1,
+            "ADDRESS2": address_line_2,
         },
     )
     return stakeholder
 
 
-def get_parties_on_applications(case_ids):
-    return (
+def get_party_sh_type(type, role):
+    if role == PartyRole.CONTACT:
+        return "CONTACT"
+    elif type == PartyType.THIRD_PARTY:
+        return "OTHER"
+    else:
+        return type.upper()
+
+
+def export_parties_on_application(case_ids, xml_base):
+    parties_on_applications = (
         PartyOnApplication.objects.filter(application_id__in=case_ids)
         .prefetch_related("party")
         .values(
@@ -60,32 +60,21 @@ def get_parties_on_applications(case_ids):
             "party__role",
         )
     )
+    for poa in parties_on_applications:
+        entity_to_xml(
+            xml_base,
+            application_id=poa["application_id"],
+            id=poa["party_id"],
+            type=get_party_sh_type(type=poa["party__type"], role=poa["party__role"]),
+            country=poa["party__country__name"],
+            organisation=poa["party__organisation__name"],
+            name=poa["party__name"],
+            address_line_1=poa["party__address"],
+        )
 
 
-def export_site_on_application_xml(base, site_on_application):
-    stakeholder = ElementTree.SubElement(base, "STAKEHOLDER")
-    dict_to_xml(
-        stakeholder,
-        {
-            "ELA_ID": site_on_application["application_id"].int,
-            "ELA_DETAIL_ID": None,
-            "SH_ID": site_on_application["site_id"].int,
-            "SH_TYPE": "SOURCE",
-            "COUNTRY": site_on_application["site__address__country__name"],
-            "ORG_NAME": site_on_application["site__organisation__name"],
-            "PD_SURNAME": None,
-            "PD_FORENAME": None,
-            "PD_MIDDLE_INITIALS": None,
-            "ADDRESS1": site_on_application["site__address__address_line_1"]
-            or site_on_application["site__address__address"],
-            "ADDRESS2": site_on_application["site__address__address_line_2"],
-        },
-    )
-    return stakeholder
-
-
-def get_sites_on_applications(case_ids):
-    return (
+def export_sites_on_applications(case_ids, xml_base):
+    sites_on_applications = (
         SiteOnApplication.objects.filter(application_id__in=case_ids)
         .prefetch_related("site", "site__address")
         .values(
@@ -98,32 +87,21 @@ def get_sites_on_applications(case_ids):
             "site__address__country__name",
         )
     )
+    for soa in sites_on_applications:
+        entity_to_xml(
+            xml_base,
+            application_id=soa["application_id"],
+            id=soa["site_id"],
+            type="SOURCE",
+            country=soa["site__address__country__name"],
+            organisation=soa["site__organisation__name"],
+            address_line_1=soa["site__address__address_line_1"] or soa["site__address__address"],
+            address_line_2=soa["site__address__address_line_2"],
+        )
 
 
-def export_organisation_on_application_xml(base, organisation_on_application):
-    stakeholder = ElementTree.SubElement(base, "STAKEHOLDER")
-    dict_to_xml(
-        stakeholder,
-        {
-            "ELA_ID": organisation_on_application["id"].int,
-            "ELA_DETAIL_ID": None,
-            "SH_ID": organisation_on_application["organisation_id"].int,
-            "SH_TYPE": "LICENSEE",
-            "COUNTRY": organisation_on_application["organisation__primary_site__address__country__name"],
-            "ORG_NAME": organisation_on_application["organisation__name"],
-            "PD_SURNAME": None,
-            "PD_FORENAME": None,
-            "PD_MIDDLE_INITIALS": None,
-            "ADDRESS1": organisation_on_application["organisation__primary_site__address__address_line_1"]
-            or organisation_on_application["organisation__primary_site__address__address"],
-            "ADDRESS2": organisation_on_application["organisation__primary_site__address__address_line_2"],
-        },
-    )
-    return stakeholder
-
-
-def get_organisations_on_applications(cases):
-    return cases.prefetch_related("organisation", "organisation__primary_site").values(
+def export_organisations_on_applications(cases, xml_base):
+    organisations_on_applications = cases.prefetch_related("organisation", "organisation__primary_site").values(
         "id",
         "organisation_id",
         "organisation__name",
@@ -132,22 +110,28 @@ def get_organisations_on_applications(cases):
         "organisation__primary_site__address__address_line_2",
         "organisation__primary_site__address__country__name",
     )
+    for org in organisations_on_applications:
+        entity_to_xml(
+            xml_base,
+            application_id=org["id"],
+            id=org["organisation_id"],
+            type="LICENSEE",
+            country=org["organisation__primary_site__address__country__name"],
+            organisation=org["organisation__name"],
+            address_line_1=org["organisation__primary_site__address__address_line_1"]
+            or org["organisation__primary_site__address__address"],
+            address_line_2=org["organisation__primary_site__address__address_line_2"]
+        )
 
 
 def export_cases_xml(cases):
     case_ids = cases.values_list("pk", flat=True)
-    parties_on_applications = get_parties_on_applications(case_ids)
-    sites_on_applications = get_sites_on_applications(case_ids)
-    organisations_on_applications = get_organisations_on_applications(cases)
 
     # Build XML structure
     base = ElementTree.Element("ENFORCEMENT_CHECK")
-    for poa in parties_on_applications:
-        export_party_on_application_xml(base, poa)
-    for soa in sites_on_applications:
-        export_site_on_application_xml(base, soa)
-    for org in organisations_on_applications:
-        export_organisation_on_application_xml(base, org)
+    export_parties_on_application(case_ids, base)
+    export_sites_on_applications(case_ids, base)
+    export_organisations_on_applications(cases, base)
 
     # Export XML
     xml = ElementTree.tostring(base, encoding="utf-8", method="xml")  # nosec
