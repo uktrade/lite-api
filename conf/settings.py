99c736ec4506ec6d31dd185693856d5dc1019979
Andy Taylor <a.taylor@kainos.com>
2020-05-15 12:47:39 +0100

    remove setting

--- conf/settings.py
+++ conf/settings.py
@@ -146,7 +146,6 @@ HAWK_CREDENTIALS = {
     },
     "lite-e2e": {"id": "lite-e2e", "key": env("LITE_E2E_HAWK_KEY"), "algorithm": SHA_ALGORITHM},
     "lite-performance": {"id": "lite-performance", "key": env("LITE_PERFORMANCE_HAWK_KEY"), "algorithm": SHA_ALGORITHM},
-    "lite-stream": {"id": "lite-performance", "key": env("LITE_PERFORMANCE_HAWK_KEY"), "algorithm": SHA_ALGORITHM},
 }
 
 HAWK_AUTHENTICATION_ENABLED = env("HAWK_AUTHENTICATION_ENABLED")
