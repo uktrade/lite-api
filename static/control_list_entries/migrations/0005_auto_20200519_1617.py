38cbae12181adb8ed9fd4b72ee84a874f4acdf8b
Mark Spiers <marksp@kainos.com>
2020-05-19 17:22:35 +0100

    rename

--- static/control_list_entries/migrations/0005_auto_20200519_1617.py
+++ static/control_list_entries/migrations/0005_auto_20200519_1617.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("control_list_entries", "0004_remove_is_decontrolled"),
+        ("control_list_entries", "0004_remove_decontrolled_entries"),
     ]
 
     operations = [
