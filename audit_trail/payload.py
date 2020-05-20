f1a8ff93c6f1385cd8019638c64d266326cd79ee
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-05-20 16:12:19 +0100

    Update enforcement audit

--- audit_trail/payload.py
+++ audit_trail/payload.py
@@ -97,6 +97,5 @@ audit_type_format = {
     AuditType.APPROVED_ORGANISATION: strings.Audit.APPROVED_ORGANISATION,
     AuditType.REMOVED_FLAG_ON_ORGANISATION: strings.Audit.REMOVED_FLAG_ON_ORGANISATION,
     AuditType.ADDED_FLAG_ON_ORGANISATION: strings.Audit.ADDED_FLAG_ON_ORGANISATION,
-    AuditType.ENFORCEMENT_CHECK: "exported the case for enforcement check",
-
+    AuditType.ENFORCEMENT_CHECK: strings.Audit.ENFORCEMENT_CHECK,
 }
