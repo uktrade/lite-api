de8899b202073d184ec3533b4fc0839359039c13
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-05-21 11:49:57 +0100

    Audit logic fixed

--- organisations/helpers.py
+++ organisations/helpers.py
@@ -4,6 +4,11 @@ from organisations.enums import OrganisationType, OrganisationStatus
 
 
 def add_edited_audit_entry(user, organisation, key, old_value, new_value):
+    if old_value == "" or old_value is None:
+        old_value = "N/A"
+    if new_value == "" or new_value is None:
+        new_value = "N/A"
+
     audit_trail_service.create(
         actor=user,
         verb=AuditType.UPDATED_ORGANISATION,
@@ -16,21 +21,39 @@ def audit_edited_organisation_fields(user, organisation, new_org, is_non_uk=None
     if new_org.get("name") and organisation.name != new_org.get("name"):
         add_edited_audit_entry(user, organisation, "name", organisation.name, new_org.get("name"))
 
-    if (is_non_uk or new_org.get("eori_number")) and organisation.eori_number != new_org.get("eori_number"):
+    if (
+        (organisation.type == OrganisationType.COMMERCIAL and new_org.get("eori_number"))
+        or organisation.type != OrganisationType.COMMERCIAL
+        or is_non_uk
+    ) and organisation.eori_number != new_org.get("eori_number"):
+
         add_edited_audit_entry(user, organisation, "EORI number", organisation.eori_number, new_org.get("eori_number"))
 
     if (
-        (is_non_uk or new_org.get("sic_number"))
+        (
+            (organisation.type == OrganisationType.COMMERCIAL and new_org.get("sic_number"))
+            or organisation.type != OrganisationType.COMMERCIAL
+            or is_non_uk
+        )
         and organisation.sic_number != new_org.get("sic_number")
         and organisation.type != OrganisationType.HMRC
     ):
         add_edited_audit_entry(user, organisation, "SIC number", organisation.sic_number, new_org.get("sic_number"))
 
-    if (is_non_uk or new_org.get("vat_number")) and organisation.vat_number != new_org.get("vat_number"):
+    if (
+        (organisation.type == OrganisationType.COMMERCIAL and new_org.get("vat_number"))
+        or organisation.type != OrganisationType.COMMERCIAL
+        or is_non_uk
+    ) and organisation.vat_number != new_org.get("vat_number"):
+
         add_edited_audit_entry(user, organisation, "VAT number", organisation.vat_number, new_org.get("vat_number"))
 
     if (
-        (is_non_uk or new_org.get("registration_number"))
+        (
+            (organisation.type == OrganisationType.COMMERCIAL and new_org.get("registration_number"))
+            or organisation.type != OrganisationType.COMMERCIAL
+            or is_non_uk
+        )
         and organisation.registration_number != new_org.get("registration_number")
         and organisation.type != OrganisationType.HMRC
     ):
