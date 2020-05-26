5b2386407bccd4b5bf6d44717d6754054e4c2ba4
BenAndrew-Kainos <53567470+BenAndrew-Kainos@users.noreply.github.com>
2020-05-26 14:07:45 +0100

    Performance: Applications performance (#445)

--- applications/views/documents.py
+++ applications/views/documents.py
@@ -12,15 +12,15 @@ from applications.libraries.document_helpers import (
     delete_goods_type_document,
     get_goods_type_document,
 )
+from applications.libraries.get_applications import get_application
 from applications.models import ApplicationDocument
 from applications.serializers.document import ApplicationDocumentSerializer
 from cases.enums import CaseTypeSubTypeEnum
 from conf.authentication import ExporterAuthentication
 from conf.decorators import (
-    authorised_users,
+    authorised_to_view_application,
     allowed_application_types,
-    application_in_major_editable_state,
-    application_in_editable_state,
+    application_in_state,
 )
 from goodstype.document.serializers import GoodsTypeDocumentSerializer
 from goodstype.helpers import get_goods_type
@@ -34,25 +34,25 @@ class ApplicationDocumentView(APIView):
 
     authentication_classes = (ExporterAuthentication,)
 
-    @authorised_users(ExporterUser)
-    def get(self, request, application):
+    @authorised_to_view_application(ExporterUser)
+    def get(self, request, pk):
         """
         View all additional documents on an application
         """
-        documents = ApplicationDocumentSerializer(
-            ApplicationDocument.objects.filter(application=application), many=True
-        ).data
+        application = get_application(pk)
+        documents = ApplicationDocumentSerializer(ApplicationDocument.objects.filter(application_id=pk), many=True).data
 
         return JsonResponse({"documents": documents, "editable": application.is_major_editable()})
 
     @swagger_auto_schema(request_body=ApplicationDocumentSerializer, responses={400: "JSON parse error"})
     @transaction.atomic
-    @authorised_users(ExporterUser)
-    @application_in_editable_state()
-    def post(self, request, application):
+    @authorised_to_view_application(ExporterUser)
+    @application_in_state(is_editable=True)
+    def post(self, request, pk):
         """
         Upload additional document onto an application
         """
+        application = get_application(pk)
         return upload_application_document(application, request.data, request.user)
 
 
@@ -63,8 +63,8 @@ class ApplicationDocumentDetailView(APIView):
 
     authentication_classes = (ExporterAuthentication,)
 
-    @authorised_users(ExporterUser)
-    def get(self, request, application, doc_pk):
+    @authorised_to_view_application(ExporterUser)
+    def get(self, request, pk, doc_pk):
         """
         View an additional document on an application
         """
@@ -72,12 +72,13 @@ class ApplicationDocumentDetailView(APIView):
 
     @swagger_auto_schema(request_body=ApplicationDocumentSerializer, responses={400: "JSON parse error"})
     @transaction.atomic
-    @authorised_users(ExporterUser)
-    @application_in_editable_state()
-    def delete(self, request, application, doc_pk):
+    @authorised_to_view_application(ExporterUser)
+    @application_in_state(is_editable=True)
+    def delete(self, request, pk, doc_pk):
         """
         Delete an additional document on an application
         """
+        application = get_application(pk)
         return delete_application_document(doc_pk, application, request.user)
 
 
@@ -89,25 +90,25 @@ class GoodsTypeDocumentView(APIView):
     authentication_classes = (ExporterAuthentication,)
 
     @allowed_application_types([CaseTypeSubTypeEnum.HMRC])
-    @authorised_users(ExporterUser)
-    def get(self, request, application, goods_type_pk):
+    @authorised_to_view_application(ExporterUser)
+    def get(self, request, pk, goods_type_pk):
         goods_type = get_goods_type(goods_type_pk)
         return get_goods_type_document(goods_type)
 
     @swagger_auto_schema(request_body=GoodsTypeDocumentSerializer, responses={400: "JSON parse error"})
     @transaction.atomic
     @allowed_application_types([CaseTypeSubTypeEnum.HMRC])
-    @application_in_major_editable_state()
-    @authorised_users(ExporterUser)
-    def post(self, request, application, goods_type_pk):
+    @application_in_state(is_major_editable=True)
+    @authorised_to_view_application(ExporterUser)
+    def post(self, request, pk, goods_type_pk):
         goods_type = get_goods_type(goods_type_pk)
         return upload_goods_type_document(goods_type, request.data)
 
     @swagger_auto_schema(request_body=GoodsTypeDocumentSerializer, responses={400: "JSON parse error"})
     @transaction.atomic
     @allowed_application_types([CaseTypeSubTypeEnum.HMRC])
-    @authorised_users(ExporterUser)
-    def delete(self, request, application, goods_type_pk):
+    @authorised_to_view_application(ExporterUser)
+    def delete(self, request, pk, goods_type_pk):
         goods_type = get_goods_type(goods_type_pk)
         if not goods_type:
             return JsonResponse(data={"error": "No such goods type"}, status=status.HTTP_400_BAD_REQUEST)
