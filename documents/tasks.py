96edc397b71b48141d0bd7d5a71e3c67ce8d8f5d
Andy Taylor <a.taylor@kainos.com>
2020-05-27 12:37:10 +0100

    use a filter so the task can be run synchronously

--- documents/tasks.py
+++ documents/tasks.py
@@ -23,8 +23,8 @@ def scan_document_for_viruses(document_id):
         doc = Document.objects.select_for_update(nowait=True).get(id=document_id)
 
         if doc.virus_scanned_at:
-            logging.info(f"Document '{document_id}' has already been scanned; is_safe={doc.is_safe}")
-            return doc.is_safe
+            logging.info(f"Document '{document_id}' has already been scanned; safe={doc.safe}")
+            return doc.safe
 
         try:
             return doc.scan_for_viruses()
@@ -35,9 +35,13 @@ def scan_document_for_viruses(document_id):
                 f"An unexpected error occurred when scanning document '{document_id}' -> {type(exc).__name__}: {exc}"
             )
 
-        # Get the task's attempt number by retrieving the previous attempt number and adding 1
-        current_task = Task.objects.get(queue=TASK_QUEUE, task_params__contains=document_id)
-        current_attempt = current_task.attempts + 1
+        # Get the task's current attempt number by retrieving the previous attempt number and adding 1
+        previous_attempt = (
+            Task.objects.filter(queue=TASK_QUEUE, task_params__contains=document_id)
+            .values_list("attempts", flat=True)
+            .first()
+        )
+        current_attempt = previous_attempt + 1
 
         if current_attempt >= settings.MAX_ATTEMPTS:
             logging.warning(f"MAX_ATTEMPTS {settings.MAX_ATTEMPTS} for document '{document_id}' has been reached")
