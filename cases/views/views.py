8ce0180719e4614d704a24d12fd0e93f0801d3ec
BenAndrew-Kainos <bena@kainos.com>
2020-06-02 15:00:28 +0100

    black

--- cases/views/views.py
+++ cases/views/views.py
@@ -708,10 +708,7 @@ class CaseApplicant(APIView):
     def get(self, request, pk):
         case = get_case(pk)
         applicant = case.submitted_by
-        return JsonResponse({
-            "name": applicant.first_name + " " + applicant.last_name,
-            "email": applicant.email
-        })
+        return JsonResponse({"name": applicant.first_name + " " + applicant.last_name, "email": applicant.email})
 
 
 class RerunRoutingRules(APIView):
