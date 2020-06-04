1a0f260f0f4ce77dc0f7bb4a5c9e5a13c908463e
asmund <asmund.bekker@gmail.com>
2020-06-04 10:26:32 +0100

    feat: LT-1335 - Gov notify (#498)

---
+++ gov_notify/service.py
@@ -0,0 +1,18 @@
+import logging
+from typing import Optional
+
+from notifications_python_client.errors import HTTPError
+
+from gov_notify.client import client
+from gov_notify.payloads import EmailData
+
+
+def send_email(email_address, template_type, data: Optional[EmailData] = None):
+    """
+    Send an email using the gov notify service.
+    """
+    data = data.as_dict() if data else None
+    try:
+        return client.send_email(email_address=email_address, template_id=template_type.template_id, data=data)
+    except HTTPError as e:
+        logging.exception(e)
