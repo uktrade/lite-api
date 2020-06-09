979b8f1cab1fe98d8d1a03919a919fbf9a1e5aa9
Jan Faracik <janf@kainos.com>
2020-06-09 17:09:19 +0100

    stuff

---
+++ applications/serializers/ogl_application.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from open_general_licences.models import OpenGeneralLicence
+from organisations.models import Site
+
+
+class OGLApplicationListSerializer(serializers.ModelSerializer):
+    open_general_licence = serializers.PrimaryKeyRelatedField(queryset=OpenGeneralLicence.objects.all())
+    site = serializers.PrimaryKeyRelatedField(queryset=Site.objects.all())
+
+
+class OGLApplicationCreateSerializer(serializers.ModelSerializer):
+    open_general_licence = serializers.PrimaryKeyRelatedField(queryset=OpenGeneralLicence.objects.all())
+
+
+class OGLApplicationDetailSerializer(serializers.ModelSerializer):
+    open_general_licence = serializers.PrimaryKeyRelatedField(queryset=OpenGeneralLicence.objects.all())
+    site = serializers.PrimaryKeyRelatedField(queryset=Site.objects.all())
+
+
+class OGLApplicationUpdateSerializer(serializers.ModelSerializer):
+    pass
