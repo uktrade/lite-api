a246d0ee0540adfffee699c2e1a71de4d3d5326c
BenAndrew-Kainos <bena@kainos.com>
2020-06-10 11:59:01 +0100

    Add organisation to OpenLicenceReturns

--- compliance/models.py
+++ compliance/models.py
@@ -1,12 +1,15 @@
 import uuid
 
 from django.db import models
+from django.db.models import deletion
 
 from licences.models import Licence
+from organisations.models import Organisation
 
 
 class OpenLicenceReturns(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    organisation = models.ForeignKey(Organisation, on_delete=deletion.CASCADE)
     file = models.TextField(blank=False, null=False)
     year = models.PositiveSmallIntegerField(blank=False, null=False)
     licences = models.ManyToManyField(Licence, related_name="open_licence_returns")
