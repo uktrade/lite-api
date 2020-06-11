d23bf65f79ebe773afad30bcbad1339adb0d22ab
Jan Faracik <janf@kainos.com>
2020-06-11 14:10:44 +0100

    Tests for days

--- licences/tests/test_register_open_general_licence.py
+++ licences/tests/test_register_open_general_licence.py
@@ -1,10 +1,12 @@
 from django.urls import reverse
+from parameterized import parameterized
 from rest_framework import status
 
 from cases.enums import CaseTypeEnum
 from cases.models import CaseType
 from open_general_licences.enums import OpenGeneralLicenceStatus
-from open_general_licences.tests.factories import OpenGeneralLicenceFactory
+from open_general_licences.models import OpenGeneralLicenceCase
+from open_general_licences.tests.factories import OpenGeneralLicenceFactory, OpenGeneralLicenceCaseFactory
 from test_helpers.clients import DataTestClient
 
 
@@ -26,9 +28,16 @@ class RegisterOpenGeneralLicenceTests(DataTestClient):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqualIgnoreType(response_data["open_general_licence"], self.open_general_licence.id)
+        self.assertEqual(OpenGeneralLicenceCase.objects.count(), 1)
 
-    def test_register_deactivated_open_general_licence_failure(self):
-        self.open_general_licence.status = OpenGeneralLicenceStatus.DEACTIVATED
+    @parameterized.expand(
+        [
+            ("status", OpenGeneralLicenceStatus.DEACTIVATED),  # Can't register deactivated OGLs
+            ("registration_required", False),  # Can't register OGLs that don't require registration
+        ]
+    )
+    def test_register_open_general_licence_failure(self, param, value):
+        setattr(self.open_general_licence, param, value)
         self.open_general_licence.save()
 
         data = {
@@ -38,15 +47,22 @@ class RegisterOpenGeneralLicenceTests(DataTestClient):
         response = self.client.post(self.url, data, **self.exporter_headers)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(OpenGeneralLicenceCase.objects.count(), 0)
 
-    def test_register_registration_not_required_open_general_licence_failure(self):
-        self.open_general_licence.registration_required = False
-        self.open_general_licence.save()
+    def test_register_existing_open_general_licence_does_nothing(self):
+        OpenGeneralLicenceCaseFactory(
+            open_general_licence=self.open_general_licence,
+            site=self.organisation.primary_site,
+            organisation=self.organisation,
+        )
 
         data = {
             "open_general_licence": str(self.open_general_licence.id),
         }
 
         response = self.client.post(self.url, data, **self.exporter_headers)
+        response_data = response.json()
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqualIgnoreType(response_data["open_general_licence"], self.open_general_licence.id)
+        self.assertEqual(OpenGeneralLicenceCase.objects.count(), 1)
--- open_general_licences/tests/factories.py
+++ open_general_licences/tests/factories.py
@@ -1,8 +1,12 @@
 import factory
 
+from cases.enums import CaseTypeEnum
+from cases.models import CaseType
 from open_general_licences import models
 from open_general_licences.enums import OpenGeneralLicenceStatus
 from static.control_list_entries.helpers import get_control_list_entry
+from static.statuses.enums import CaseStatusEnum
+from static.statuses.libraries.get_case_status import get_case_status_by_status
 
 
 class OpenGeneralLicenceFactory(factory.django.DjangoModelFactory):
@@ -39,3 +43,11 @@ class OpenGeneralLicenceFactory(factory.django.DjangoModelFactory):
 
         for country in extracted:
             self.countries.add(country)
+
+
+class OpenGeneralLicenceCaseFactory(factory.django.DjangoModelFactory):
+    status = get_case_status_by_status(CaseStatusEnum.REGISTERED)
+    case_type = CaseType.objects.get(id=CaseTypeEnum.OGTCL.id)
+
+    class Meta:
+        model = models.OpenGeneralLicenceCase
