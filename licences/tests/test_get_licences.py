f74a1d320ae764dc83fd08fe218570d945e6b0c7
BenAndrew-Kainos <bena@kainos.com>
2020-06-11 15:05:00 +0100

    black

--- licences/tests/test_get_licences.py
+++ licences/tests/test_get_licences.py
@@ -18,7 +18,9 @@ class GetLicencesTests(DataTestClient):
         self.standard_application = self.create_standard_application_case(self.organisation)
         self.f680_application = self.create_mod_clearance_application_case(self.organisation, CaseTypeEnum.F680)
         self.gifting_application = self.create_mod_clearance_application_case(self.organisation, CaseTypeEnum.GIFTING)
-        self.exhibition_application = self.create_mod_clearance_application_case(self.organisation, CaseTypeEnum.EXHIBITION)
+        self.exhibition_application = self.create_mod_clearance_application_case(
+            self.organisation, CaseTypeEnum.EXHIBITION
+        )
         self.open_application = self.create_open_application_case(self.organisation)
         self.applications = [
             self.standard_application,
