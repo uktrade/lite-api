934a4c4d79669f1fee35fe801da9fa37fae0b673
BenAndrew-Kainos <bena@kainos.com>
2020-06-12 13:21:54 +0100

    review comment

--- compliance/helpers.py
+++ compliance/helpers.py
@@ -29,12 +29,15 @@ def read_and_validate_csv(text):
 
 
 def fetch_and_validate_licences(references, organisation_id):
+    if len(references) == 0:
+        raise ValidationError({"file": [Compliance.OpenLicenceReturns.INVALID_LICENCES]})
+
     licence_ids = list(
         Licence.objects.filter(reference_code__in=references, application__organisation_id=organisation_id).values_list(
             "id", flat=True
         )
     )
-    if len(licence_ids) != len(references) or len(references) == 0:
+    if len(licence_ids) != len(references):
         raise ValidationError({"file": [Compliance.OpenLicenceReturns.INVALID_LICENCES]})
 
     return licence_ids
