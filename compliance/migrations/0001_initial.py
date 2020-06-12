1c95b95973768d6e77cb157f33747619842d2de8
BenAndrew-Kainos <bena@kainos.com>
2020-06-12 12:37:39 +0100

    black

--- compliance/migrations/0001_initial.py
+++ compliance/migrations/0001_initial.py
@@ -12,23 +12,29 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('licences', '0005_auto_20200610_1227'),
-        ('organisations', '0008_auto_20200601_0814'),
+        ("licences", "0005_auto_20200610_1227"),
+        ("organisations", "0008_auto_20200601_0814"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='OpenLicenceReturns',
+            name="OpenLicenceReturns",
             fields=[
-                ('created_at', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created_at')),
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('returns_data', models.TextField()),
-                ('year', models.PositiveSmallIntegerField()),
-                ('licences', models.ManyToManyField(related_name='open_licence_returns', to='licences.Licence')),
-                ('organisation', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='organisations.Organisation')),
+                (
+                    "created_at",
+                    model_utils.fields.AutoCreatedField(
+                        default=django.utils.timezone.now, editable=False, verbose_name="created_at"
+                    ),
+                ),
+                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ("returns_data", models.TextField()),
+                ("year", models.PositiveSmallIntegerField()),
+                ("licences", models.ManyToManyField(related_name="open_licence_returns", to="licences.Licence")),
+                (
+                    "organisation",
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="organisations.Organisation"),
+                ),
             ],
-            options={
-                'abstract': False,
-            },
+            options={"abstract": False,},
         ),
     ]
--- compliance/tests/test_add_open_licence_returns.py
+++ compliance/tests/test_add_open_licence_returns.py
@@ -33,7 +33,10 @@ class AddOpenLicenceReturnsTest(DataTestClient):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(
             OpenLicenceReturns.objects.filter(
-                organisation=self.organisation, returns_data=data["file"].strip(), year=data["year"], licences=self.licence
+                organisation=self.organisation,
+                returns_data=data["file"].strip(),
+                year=data["year"],
+                licences=self.licence,
             ).count(),
             1,
         )
