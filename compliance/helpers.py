6f2d2d21dbe331ef3ee2e182c324eeca09796e30
Mark Spiers <marksp@kainos.com>
2020-06-12 13:25:54 +0100

    audit

--- compliance/helpers.py
+++ compliance/helpers.py
@@ -1,6 +1,8 @@
 from django.db.models import When, Case as db_case, F
 from django.utils import timezone
 
+from audit_trail.enums import AuditType
+from audit_trail.models import Audit
 from cases.enums import CaseTypeEnum
 from cases.models import Case
 from compliance.models import ComplianceSiteCase
@@ -8,6 +10,8 @@ from goods.models import Good
 from organisations.models import Site
 from static.statuses.enums import CaseStatusEnum
 from static.statuses.libraries.get_case_status import get_case_status_by_status
+from users.enums import SystemUser
+from users.models import BaseUser
 
 
 def generate_compliance(case: Case):
@@ -40,17 +44,36 @@ def generate_compliance(case: Case):
 
     # Get list of record holding sites that do not relate to compliance case
     new_compliance_sites = set(Site.objects.filter(id__in=record_holding_sites_id, compliance__isnull=True))
+    existing_compliance_cases = set(Case.objects.filter(compliancesitecase__site_id__in=record_holding_sites_id))
+
+    audits = []
+    system_user = BaseUser.objects.get(id=SystemUser.id)
+    for comp_case in existing_compliance_cases:
+        audits.append(
+            Audit(
+                actor=system_user,
+                verb=AuditType.COMPLIANCE_SITE_CASE_NEW_LICENCE,
+                target=comp_case,
+                payload={"case_reference": case.reference_code},
+            )
+        )
 
     if not new_compliance_sites:
+        Audit.objects.bulk_create(audits)
         return None
 
     for site in new_compliance_sites:
-        ComplianceSiteCase(
+        comp_case = ComplianceSiteCase(
             site=site,
             status=get_case_status_by_status(CaseStatusEnum.OPEN),
             organisation_id=case.organisation_id,
             case_type_id=CaseTypeEnum.COMPLIANCE.id,
             submitted_at=timezone.now(),
         ).save()
+        audits.append(
+            Audit(
+                actor=system_user, verb=AuditType.COMPLIANCE_SITE_CASE_CREATE, target=comp_case.get_case(), payload={},
+            )
+        )
 
-    # TODO: bulk create audits for creating and new licence on compliance site.
+    Audit.objects.bulk_create(audits)
