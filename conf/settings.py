90118df2482d5d0cc135f698582667b435153510
BenAndrew-Kainos <bena@kainos.com>
2020-06-12 11:10:17 +0100

    Set sqlite3 for testing

--- conf/settings.py
+++ conf/settings.py
@@ -174,7 +174,17 @@ LETTER_TEMPLATES_DIRECTORY = os.path.join(BASE_DIR, "letter_templates", "layouts
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {"default": env.db()}
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'testdatabase'
+        }
+    }
+else:
+    DATABASES = {
+        'default': env.db()
+    }
 
 # AWS
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
