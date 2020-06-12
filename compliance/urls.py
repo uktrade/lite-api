3c6d13f978f9bc65467c5ff1bc24b45131bfe162
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-06-12 13:23:52 +0100

    add compliance manage status functionality

--- compliance/urls.py
+++ compliance/urls.py
@@ -1,6 +1,10 @@
-# from django.urls import path
+from django.urls import path
+
+from compliance import views
 
 app_name = "compliance"
 
 # urls will be required in future compliance stories, conf has already been set up
-urlpatterns = []
+urlpatterns = [
+    path("<uuid:pk>/status/", views.ComplianceManageStatus.as_view(), name="manage_status",),
+]
