d3259d6d0a2cff6d5782ce00f37811082ffcabd2
BenAndrew-Kainos <bena@kainos.com>
2020-06-16 09:55:27 +0100

    Ensure licence reference code is locked so that codes are unique

--- licences/helpers.py
+++ licences/helpers.py
@@ -1,9 +1,15 @@
+from django.db import transaction
+
 from licences.models import Licence
 from string import ascii_uppercase
 
 
+@transaction.atomic
 def get_reference_code(application_reference):
-    total_reference_codes = Licence.objects.filter(reference_code__icontains=application_reference).count()
+    # Needs to lock so that 2 Licences don't get the same reference code
+    total_reference_codes = (
+        Licence.objects.filter(reference_code__icontains=application_reference).select_for_update().count()
+    )
     return (
         f"{application_reference}/{ascii_uppercase[total_reference_codes-1]}"
         if total_reference_codes != 0
