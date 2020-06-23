249facf057630a403314c860b0039b3ae5dae99a
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-06-23 11:23:07 +0100

    asserting licence flags

--- compliance/tests/tests_get_compliance_licences.py
+++ compliance/tests/tests_get_compliance_licences.py
@@ -1,9 +1,22 @@
 from django.urls import reverse
 
 from compliance.tests.factories import ComplianceSiteCaseFactory
+from flags.enums import FlagLevels
 from static.statuses.enums import CaseStatusEnum
 from static.statuses.libraries.get_case_status import get_case_status_by_status
 from test_helpers.clients import DataTestClient
+
+
+def _assert_response_data(self, response_data, licence):
+    self.assertEqual(len(response_data), 1)
+    self.assertEqual(response_data[0]["id"], str(licence.application_id))
+    self.assertEqual(response_data[0]["reference_code"], licence.application.reference_code)
+    self.assertEqual(response_data[0]["status"]["key"], licence.application.status.status)
+    self.assertEqual(len(response_data[0]["flags"]), 2)
+    self.assertEqual(response_data[0]["flags"][0]["name"], "Item not verified")
+    self.assertEqual(response_data[0]["flags"][1]["name"], "Item not verified")
+    self.assertEqual(response_data[0]["flags"][0]["level"], FlagLevels.GOOD)
+    self.assertEqual(response_data[0]["flags"][1]["level"], FlagLevels.GOOD)
 
 
 class GetComplianceLicencesTests(DataTestClient):
@@ -20,7 +33,4 @@ class GetComplianceLicencesTests(DataTestClient):
         response = self.client.get(url, **self.gov_headers)
         response_data = response.json()["results"]
 
-        self.assertEqual(len(response_data), 1)
-        self.assertEqual(response_data[0]["id"], str(licence.application_id))
-        self.assertEqual(response_data[0]["reference_code"], licence.application.reference_code)
-        self.assertEqual(response_data[0]["status"]["key"], licence.application.status.status)
+        _assert_response_data(self, response_data, licence)
