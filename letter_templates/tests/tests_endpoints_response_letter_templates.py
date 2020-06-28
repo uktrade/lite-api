99b03c5ff16fb545a054e0f94a026b4eff55623e
asmund <asmund.bekker@gmail.com>
2020-06-28 23:53:54 +0100

    f

--- letter_templates/tests/tests_endpoints_response_letter_templates.py
+++ letter_templates/tests/tests_endpoints_response_letter_templates.py
@@ -1,11 +1,11 @@
-# from test_helpers.test_endpoints.test_endpoint_response_time import EndPointTests
-#
-#
-# class LetterTemplatesResponseTests(EndPointTests):
-#     url = "/letter-templates/"
-#
-#     def test_letter_templates_list(self):
-#         self.call_endpoint(self.get_gov_headers(), self.url)
-#
-#     def test_letter_templates_detail(self):
-#         self.call_endpoint(self.get_gov_headers(), self.url + self.get_letter_template_id())
+from test_helpers.test_endpoints.test_endpoint_response_time import EndPointTests
+
+
+class LetterTemplatesResponseTests(EndPointTests):
+    url = "/letter-templates/"
+
+    def test_letter_templates_list(self):
+        self.call_endpoint(self.get_gov_headers(), self.url)
+
+    def test_letter_templates_detail(self):
+        self.call_endpoint(self.get_gov_headers(), self.url + self.get_letter_template_id())
