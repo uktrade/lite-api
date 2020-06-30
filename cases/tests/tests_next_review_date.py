5db775bcb9b0334352b25c1945f5622f77df823a
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-06-30 13:35:43 +0100

    Adding assertions for next review date audits

--- cases/tests/tests_next_review_date.py
+++ cases/tests/tests_next_review_date.py
@@ -4,6 +4,7 @@ from rest_framework import status
 
 from audit_trail.enums import AuditType
 from audit_trail.models import Audit
+from conf.helpers import convert_date_to_string
 from lite_content.lite_api import strings
 from test_helpers.clients import DataTestClient
 
@@ -28,10 +29,13 @@ class NextReviewDateTests(DataTestClient):
         self.assertEqual(str(case_review_date.next_review_date), date)
         self.assertEqual(case_review_date.team_id, self.team.id)
         self.assertEqual(case_review_date.case_id, self.case.id)
+
         audit_qs = Audit.objects.all()
         self.assertEqual(audit_qs.count(), 2)
         self.assertEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
-        self.assertEqual(audit_qs.first().payload, {"old_ref_number": "no reference"})
+        self.assertEqual(
+            audit_qs.first().payload, {"next_review_date": convert_date_to_string(date), "team_name": self.team.name}
+        )
 
     def test_add_present_next_review_date_success(self):
         date = timezone.now().date()
@@ -46,6 +50,13 @@ class NextReviewDateTests(DataTestClient):
         self.assertEqual(case_review_date.team_id, self.team.id)
         self.assertEqual(case_review_date.case_id, self.case.id)
 
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 2)
+        self.assertEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
+        self.assertEqual(
+            audit_qs.first().payload, {"next_review_date": convert_date_to_string(date), "team_name": self.team.name}
+        )
+
     def test_add_blank_next_review_date_success(self):
         date = None
         request = self.client.put(self.url, data={"next_review_date": date}, **self.gov_headers)
@@ -59,8 +70,130 @@ class NextReviewDateTests(DataTestClient):
         self.assertEqual(case_review_date.team_id, self.team.id)
         self.assertEqual(case_review_date.case_id, self.case.id)
 
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 1)
+        self.assertNotEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
+
+    def test_clear_next_review_date_success(self):
+        date = "2025-01-01"
+        request = self.client.put(self.url, data={"next_review_date": date}, **self.gov_headers)
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.case.refresh_from_db()
+
+        case_review_date = self.case.case_review_date.all()[0]
+        self.assertEqual(len(self.case.case_review_date.all()), 1)
+        self.assertEqual(str(case_review_date.next_review_date), date)
+        self.assertEqual(case_review_date.team_id, self.team.id)
+        self.assertEqual(case_review_date.case_id, self.case.id)
+
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 2)
+        self.assertEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
+        self.assertEqual(
+            audit_qs.first().payload, {"next_review_date": convert_date_to_string(date), "team_name": self.team.name}
+        )
+
+        date = None
+        request = self.client.put(self.url, data={"next_review_date": date}, **self.gov_headers)
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.case.refresh_from_db()
+
+        case_review_date = self.case.case_review_date.all()[0]
+        self.assertEqual(len(self.case.case_review_date.all()), 1)
+        self.assertEqual(case_review_date.next_review_date, date)
+        self.assertEqual(case_review_date.team_id, self.team.id)
+        self.assertEqual(case_review_date.case_id, self.case.id)
+
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 3)
+        self.assertEqual(AuditType(audit_qs.first().verb), AuditType.REMOVED_NEXT_REVIEW_DATE)
+        self.assertEqual(audit_qs.first().payload, {"team_name": self.team.name})
+
+    def test_change_next_review_date_success(self):
+        old_date = "2025-01-01"
+        request = self.client.put(self.url, data={"next_review_date": old_date}, **self.gov_headers)
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.case.refresh_from_db()
+
+        case_review_date = self.case.case_review_date.all()[0]
+        self.assertEqual(len(self.case.case_review_date.all()), 1)
+        self.assertEqual(str(case_review_date.next_review_date), old_date)
+        self.assertEqual(case_review_date.team_id, self.team.id)
+        self.assertEqual(case_review_date.case_id, self.case.id)
+
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 2)
+        self.assertEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
+        self.assertEqual(
+            audit_qs.first().payload,
+            {"next_review_date": convert_date_to_string(old_date), "team_name": self.team.name},
+        )
+
+        new_date = "2026-05-06"
+        request = self.client.put(self.url, data={"next_review_date": new_date}, **self.gov_headers)
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.case.refresh_from_db()
+
+        case_review_date = self.case.case_review_date.all()[0]
+        self.assertEqual(len(self.case.case_review_date.all()), 1)
+        self.assertEqual(str(case_review_date.next_review_date), new_date)
+        self.assertEqual(case_review_date.team_id, self.team.id)
+        self.assertEqual(case_review_date.case_id, self.case.id)
+
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 3)
+        self.assertEqual(AuditType(audit_qs.first().verb), AuditType.EDITED_NEXT_REVIEW_DATE)
+        self.assertEqual(
+            audit_qs.first().payload,
+            {
+                "old_date": convert_date_to_string(old_date),
+                "new_date": convert_date_to_string(new_date),
+                "team_name": self.team.name,
+            },
+        )
+
+    def test_resubmit_same_next_review_date_success(self):
+        date = "2025-01-01"
+        request = self.client.put(self.url, data={"next_review_date": date}, **self.gov_headers)
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.case.refresh_from_db()
+
+        case_review_date = self.case.case_review_date.all()[0]
+        self.assertEqual(len(self.case.case_review_date.all()), 1)
+        self.assertEqual(str(case_review_date.next_review_date), date)
+        self.assertEqual(case_review_date.team_id, self.team.id)
+        self.assertEqual(case_review_date.case_id, self.case.id)
+
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 2)
+        self.assertEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
+        self.assertEqual(
+            audit_qs.first().payload, {"next_review_date": convert_date_to_string(date), "team_name": self.team.name}
+        )
+
+        request = self.client.put(self.url, data={"next_review_date": date}, **self.gov_headers)
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.case.refresh_from_db()
+
+        case_review_date = self.case.case_review_date.all()[0]
+        self.assertEqual(len(self.case.case_review_date.all()), 1)
+        self.assertEqual(str(case_review_date.next_review_date), date)
+        self.assertEqual(case_review_date.team_id, self.team.id)
+        self.assertEqual(case_review_date.case_id, self.case.id)
+
+        audit_qs = Audit.objects.all()
+        # Resubmitting the same date shouldn't created an new edited audit
+        self.assertEqual(audit_qs.count(), 2)
+        self.assertNotEqual(AuditType(audit_qs.first().verb), AuditType.EDITED_NEXT_REVIEW_DATE)
+
     def test_add_invalid_type_next_review_date_failure(self):
-        date = "Invalid date format"
+        date = "YYYY-mm-dd"
         request = self.client.put(self.url, data={"next_review_date": date}, **self.gov_headers)
 
         self.assertEqual(request.status_code, status.HTTP_400_BAD_REQUEST)
@@ -68,6 +201,10 @@ class NextReviewDateTests(DataTestClient):
             str(request.data["errors"]["next_review_date"][0]), strings.Cases.NextReviewDate.Errors.INVALID_DATE_FORMAT
         )
         self.assertEqual(len(self.case.case_review_date.all()), 0)
+
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 1)
+        self.assertNotEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
 
     def test_add_invalid_format_next_review_date_failure(self):
         date = "2025-13-40"
@@ -79,6 +216,10 @@ class NextReviewDateTests(DataTestClient):
         )
         self.assertEqual(len(self.case.case_review_date.all()), 0)
 
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 1)
+        self.assertNotEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
+
     def test_add_past_next_review_date_failure(self):
         date = "2000-01-01"
         request = self.client.put(self.url, data={"next_review_date": date}, **self.gov_headers)
@@ -88,3 +229,7 @@ class NextReviewDateTests(DataTestClient):
             str(request.data["errors"]["next_review_date"][0]), strings.Cases.NextReviewDate.Errors.DATE_IN_PAST
         )
         self.assertEqual(len(self.case.case_review_date.all()), 0)
+
+        audit_qs = Audit.objects.all()
+        self.assertEqual(audit_qs.count(), 1)
+        self.assertNotEqual(AuditType(audit_qs.first().verb), AuditType.ADDED_NEXT_REVIEW_DATE)
