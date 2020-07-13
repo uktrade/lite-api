93bdbcbb9471efbb7184b05b64c43e3ca280f029
Mark Spiers <marksp@kainos.com>
2020-07-13 13:59:21 +0100

    minor performance imrpovement on to_str functions for enums

--- applications/enums.py
+++ applications/enums.py
@@ -101,7 +101,7 @@ class MTCRAnswers:
 
     @classmethod
     def to_str(cls, obj):
-        return [grading[1] for grading in cls.choices if grading[0] == obj][0]
+        return next(grading[1] for grading in cls.choices if grading[0] == obj)
 
 
 class ServiceEquipmentType:
@@ -117,7 +117,7 @@ class ServiceEquipmentType:
 
     @classmethod
     def to_str(cls, obj):
-        return [grading[1] for grading in cls.choices if grading[0] == obj][0]
+        return next(grading[1] for grading in cls.choices if grading[0] == obj)
 
     def to_representation(self):
         return {
