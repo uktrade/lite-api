e084bfbf649fb262259bec50854b131bd2b9ee58
BenAndrew-Kainos <bena@kainos.com>
2020-07-16 12:28:07 +0100

    Revert "Rename refuse to reject"
    This reverts commit e9a85c64c08b9474d42763bc9efb0d50c2b43bb0.

--- cases/enums.py
+++ cases/enums.py
@@ -303,7 +303,7 @@ class AdviceType:
     choices = [
         (APPROVE, "Approve"),
         (PROVISO, "Proviso"),
-        (REFUSE, "Reject"),
+        (REFUSE, "Refuse"),
         (NO_LICENCE_REQUIRED, "No Licence Required"),
         (NOT_APPLICABLE, "Not Applicable"),
         (CONFLICTING, "Conflicting"),
