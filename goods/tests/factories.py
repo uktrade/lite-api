d03bb489d8401831f5d07e7394d754cb99422dbe
Mark-Spiers-kainos <53567485+Mark-Spiers-kainos@users.noreply.github.com>
2020-07-20 15:22:07 +0100

    Bugs: LT - 1379/2760/2911

--- goods/tests/factories.py
+++ goods/tests/factories.py
@@ -2,7 +2,7 @@ import factory
 from django.utils import timezone
 
 from goods import models
-from goods.enums import GoodControlled, ItemCategory, Component, MilitaryUse, FirearmGoodType
+from goods.enums import GoodControlled, ItemCategory, Component, MilitaryUse, FirearmGoodType, GoodPvGraded
 from static.control_list_entries.helpers import get_control_list_entry
 
 
@@ -14,6 +14,7 @@ class GoodFactory(factory.django.DjangoModelFactory):
     item_category = ItemCategory.GROUP1_COMPONENTS
     is_military_use = MilitaryUse.NO
     is_component = Component.NO
+    is_pv_graded = GoodPvGraded.NO
     uses_information_security = False
     information_security_details = None
     modified_military_use_details = None
--- test_helpers/clients.py
+++ test_helpers/clients.py
@@ -525,9 +525,7 @@ class DataTestClient(APITestCase, URLPatternsTestCase):
         return good
 
     def create_goods_query(self, description, organisation, clc_reason, pv_reason) -> GoodsQuery:
-        good = DataTestClient.create_good(
-            description=description, organisation=organisation, is_pv_graded=GoodPvGraded.NO
-        )
+        good = DataTestClient.create_good(description=description, organisation=organisation)
 
         goods_query = GoodsQuery.objects.create(
             clc_raised_reasons=clc_reason,
@@ -545,9 +543,7 @@ class DataTestClient(APITestCase, URLPatternsTestCase):
         return goods_query
 
     def create_clc_query(self, description, organisation) -> GoodsQuery:
-        good = DataTestClient.create_good(
-            description=description, organisation=organisation, is_pv_graded=GoodPvGraded.NO
-        )
+        good = DataTestClient.create_good(description=description, organisation=organisation)
 
         clc_query = GoodsQuery.objects.create(
             clc_raised_reasons="this is a test text",
