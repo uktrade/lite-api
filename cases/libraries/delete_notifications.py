3289035b1b57254b699adcc93b6cab8857e4b47e
Jan Faracik <j.faracik@kainos.com>
2020-07-21 12:46:49 +0100

    Update delete_notifications.py

--- cases/libraries/delete_notifications.py
+++ cases/libraries/delete_notifications.py
@@ -6,6 +6,5 @@ def delete_exporter_notifications(user: ExporterUser, organisation_id, objects: 
     ExporterNotification.objects.filter(user=user, organisation_id=organisation_id, object_id__in=id_list).delete()
 
 
-def delete_gov_user_notifications(user: GovUser, objects: list):
-    id_list = [obj.id for obj in objects]
-    GovNotification.objects.filter(user=user, object_id=id_list).delete()
+def delete_gov_user_notifications(user: GovUser, id_list: list):
+    GovNotification.objects.filter(user=user, object_id__in=id_list).delete()
