270b6556b6e3eda0a9c2dc207bc3ecb317542e53
Jan Faracik <j.faracik@kainos.com>
2020-07-22 15:28:12 +0100

    Update serializers.py

--- parties/serializers.py
+++ parties/serializers.py
@@ -22,9 +22,9 @@ class PartySerializer(serializers.ModelSerializer):
     organisation = relations.PrimaryKeyRelatedField(queryset=Organisation.objects.all())
     document = serializers.SerializerMethodField()
     role = KeyValueChoiceField(choices=PartyRole.choices, error_messages=PartyErrors.ROLE, required=False)
-    role_other = serializers.CharField(max_length=75)
+    role_other = serializers.CharField(max_length=75, allow_null=True, allow_blank=True, required=False)
     sub_type = KeyValueChoiceField(choices=SubType.choices, error_messages=PartyErrors.SUB_TYPE)
-    sub_type_other = serializers.CharField(max_length=75)
+    sub_type_other = serializers.CharField(max_length=75, allow_null=True, allow_blank=True, required=False)
     flags = FlagSerializer(many=True, required=False)
     clearance_level = KeyValueChoiceField(choices=PvGrading.choices, allow_null=True, required=False, allow_blank=True)
     descriptors = serializers.CharField(allow_null=True, required=False, allow_blank=True)
@@ -43,7 +43,9 @@ class PartySerializer(serializers.ModelSerializer):
             "organisation",
             "document",
             "sub_type",
+            "sub_type_other",
             "role",
+            "role_other",
             "flags",
             "copy_of",
             "deleted_at",
@@ -53,22 +55,38 @@ class PartySerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         application_type = kwargs.pop("application_type", None)
-
         super(PartySerializer, self).__init__(*args, **kwargs)
-        party_type = kwargs.get("data", {}).get("type")
-        if party_type == PartyType.THIRD_PARTY:
-            for field, serializer_instance in self.fields.items():
-                if field == "role":
-                    serializer_instance.required = True
-                if self.initial_data.get("sub_type") == "other":
-                    if field == "descriptors" and application_type != CaseTypeSubTypeEnum.F680:
-                        serializer_instance.required = True
-                        serializer_instance.allow_blank = False
 
-        if application_type == CaseTypeSubTypeEnum.F680:
-            for field, serializer_instance in self.fields.items():
-                if field == "clearance_level":
-                    serializer_instance.required = True
+        if hasattr(self, "initial_data"):
+            party_type = kwargs.get("data", {}).get("type")
+            role = self.initial_data.get("role")
+            sub_type = self.initial_data.get("sub_type")
+
+            if sub_type == SubType.OTHER:
+                self.fields["sub_type_other"].required = True
+                self.fields["sub_type_other"].allow_blank = False
+                self.fields["sub_type_other"].allow_null = False
+            else:
+                self.fields.pop("sub_type_other")
+
+            if party_type == PartyType.THIRD_PARTY:
+                self.fields["role"].required = True
+
+                if role == PartyRole.OTHER:
+                    self.fields["role_other"].required = True
+                    self.fields["role_other"].allow_blank = False
+                    self.fields["role_other"].allow_null = False
+
+                    if application_type != CaseTypeSubTypeEnum.F680:
+                        self.fields["descriptors"].required = True
+                        self.fields["descriptors"].allow_blank = False
+                else:
+                    self.fields.pop("role_other")
+
+            if application_type == CaseTypeSubTypeEnum.F680:
+                for field, serializer_instance in self.fields.items():
+                    if field == "clearance_level":
+                        serializer_instance.required = True
 
     @staticmethod
     def validate_website(value):
