aa8e2d61b5c1f9a0b84e35038bfcb561c3c7dadf
richtier <contact@richardtier.co.uk>
2020-08-19 17:36:05 +0100

    Increase worker count

--- conf/gconfig.py
+++ conf/gconfig.py
@@ -2,9 +2,9 @@ import os
 
 PORT = os.environ.get("PORT", 8080)
 bind = "0.0.0.0:{PORT}".format(PORT=PORT)
-workers = 1
+workers = 2
 worker_connections = 1000
-keep_alive = 5
+keep_alive = 2
 # timeout affect document uploads
 # if timeout is set to be too low, documents that would successfully upload will fail.
 # creating a 502 bad gateway error
