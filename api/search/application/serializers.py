22af2862fe37b0f3dc84404858876f31e59dd713
Richard Tier <contact@richardtier.co.uk>
2020-09-08 11:57:04 +0100

    Expose score (#613)

--- api/search/application/serializers.py
+++ api/search/application/serializers.py
@@ -6,6 +6,7 @@ from api.search.application import documents
 
 class ApplicationDocumentSerializer(DocumentSerializer):
     highlight = serializers.SerializerMethodField()
+    score = serializers.SerializerMethodField()
 
     class Meta:
         document = documents.ApplicationDocumentType
@@ -27,3 +28,6 @@ class ApplicationDocumentSerializer(DocumentSerializer):
         if hasattr(obj.meta, "highlight"):
             return {key.replace(".", "_"): value for key, value in obj.meta.highlight.to_dict().items()}
         return {}
+
+    def get_score(self, obj):
+        return obj.meta.score
