7ab5bac545b72db8111aeabb0a0e5fa3323e76cf
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-09-11 17:07:00 +0100

    Fix signals to update ES index
    Previous changes to update the ES index on data updates are not working.
    The registry expects model corresponding to the index which is BaseApplication
    in our case so the updates for related models were not happening. Update
    signals to get the relevant BaseApplication instance.

--- api/search/signals.py
+++ api/search/signals.py
@@ -1,6 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_elasticsearch_dsl.registries import registry
+
+from api.applications.models import BaseApplication
 
 
 @receiver(post_save)
@@ -9,33 +12,48 @@ def update_application_document(sender, **kwargs):
     model_name = sender._meta.model_name
     instance = kwargs["instance"]
 
+    """
+    To keep the index fresh we need to update them whenever the underlying data is updated.
+    The immediate attributes of the index are handled by django_elasticsearch_dsl but
+    additional changes are required to update the related models.
+
+    The registry update code requires model corresponding to the index which is BaseApplication
+    in our case. We intercept the signals and update the index.
+    """
     if app_label == "applications":
-        if model_name == "standardapplication" or model_name == "baseapplication":
-            instances = instance.goods.all()
-            for _instance in instances:
-                registry.update(_instance)
+        if model_name in [
+            "standardapplication",
+            "baseapplication",
+            "openapplication",
+            "exhibitionclearanceapplication",
+        ]:
+            application = BaseApplication.objects.get(id=instance.id)
+            registry.update(application)
 
-        if model_name == "goodonapplication":
-            registry.update(instance)
+    if app_label == "cases":
+        if model_name == "case":
+            base_application_id = BaseApplication.objects.get(id=instance.id)
+            registry.update(base_application_id)
 
-        if model_name == "goodonapplication":
-            registry.update(instance)
+        if model_name == "caseassignment":
+            base_application_id = BaseApplication.objects.get(id=instance.case.id)
+            registry.update(base_application_id)
 
+    if app_label == "goods":
         if model_name == "good":
-            instances = instance.goods_on_application.all()
-            for _instance in instances:
-                registry.update(instance.application)
-
-        if model_name == "partyonapplication":
-            registry.update(instance.application)
+            for goa in instance.goods_on_application.all():
+                registry.update(goa.application)
 
     if app_label == "parties":
         if model_name == "party":
-            for _instance in instance.parties_on_application:
-                registry.update(_instance.application)
+            for poa in instance.parties_on_application.all():
+                registry.update(poa.application)
 
     if app_label == "organisations":
         if model_name == "organisation":
-            instances = instance.cases.all()
-            for _instance in instances:
-                registry.update(_instance)
+            for case in instance.cases.all():
+                try:
+                    base_application_id = BaseApplication.objects.get(id=case.id)
+                    registry.update(base_application_id)
+                except ObjectDoesNotExist:
+                    pass
