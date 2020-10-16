3213f43d30a0f62c22bd60fda6d57d58d232f730
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-10-16 17:54:15 +0100

    Add limitoffsetpagination to DW licences endpoint

--- api/data_workspace/views.py
+++ api/data_workspace/views.py
@@ -1,9 +1,9 @@
 from django.db.models import Q
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 
 from api.cases.enums import CaseTypeEnum
 from api.core.authentication import DataWorkspaceOnlyAuthentication
-from api.licences import models
+from api.licences.models import Licence
 from api.licences.enums import LicenceStatus
 from api.licences.serializers.view_licence import LicenceListSerializer
 from api.staticdata.statuses.enums import CaseStatusEnum
@@ -13,15 +13,15 @@ from api.staticdata.statuses.models import CaseStatus
 class LicencesListDW(viewsets.ReadOnlyModelViewSet):
     authentication_classes = (DataWorkspaceOnlyAuthentication,)
     serializer_class = LicenceListSerializer
+    pagination_class = pagination.LimitOffsetPagination
     non_active_states = CaseStatus.objects.filter(status=CaseStatusEnum.SURRENDERED)
 
-    def get_queryset(self):
+    queryset = (
         # OGL's are always hidden as we don't treat them as a licence
         # and they shouldn't be viewed from this endpoint
-        licences = models.Licence.objects.exclude(
-            Q(case__status__in=self.non_active_states)
+        Licence.objects.exclude(
+            Q(case__status__in=non_active_states)
             | Q(case__case_type__id__in=CaseTypeEnum.OPEN_GENERAL_LICENCE_IDS)
             | Q(status=LicenceStatus.DRAFT)
-        )
-
-        return licences.order_by("created_at").reverse()
+        ).order_by("created_at").reverse()
+    )
