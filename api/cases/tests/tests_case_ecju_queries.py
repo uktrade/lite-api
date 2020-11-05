f87ca997e9acb78a1fe0f30d27c21ec4cef722cc
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2020-11-05 15:45:20 +0000

    Add unit tests for ECJU query response

--- api/cases/tests/tests_case_ecju_queries.py
+++ api/cases/tests/tests_case_ecju_queries.py
@@ -1,6 +1,8 @@
 from unittest import mock
 
 from django.urls import reverse
+from django.utils import timezone
+from faker import Faker
 from parameterized import parameterized
 from rest_framework import status
 
@@ -14,6 +16,8 @@ from api.staticdata.statuses.enums import CaseStatusEnum
 from api.staticdata.statuses.libraries.get_case_status import get_case_status_by_status
 from test_helpers.clients import DataTestClient
 from api.users.tests.factories import ExporterUserFactory
+
+faker = Faker()
 
 
 class ECJUQueriesViewTests(DataTestClient):
@@ -281,3 +285,155 @@ class ECJUQueriesComplianceCreateTest(DataTestClient):
         self.assertEqual(response_data["ecju_query_id"], str(ecju_query.id))
         self.assertEqual(ecju_query.question, self.data["question"])
         self.assertEqual(mock_client.send_email.call_count, 1)
+
+
+class ECJUQueriesResponseTests(DataTestClient):
+    @mock.patch("api.documents.tasks.scan_document_for_viruses.now")
+    def add_query_document(self, case_id, query_id, expected_status, doc_virus_scan_task):
+        doc_virus_scan_task.return_value = None
+        url = reverse("cases:case_ecju_query_add_document", kwargs={"pk": case_id, "query_pk": query_id})
+        file_name = faker.file_name()
+        data = {
+            "name": file_name,
+            "s3_key": f"{file_name}_{faker.uuid4()}",
+            "description": faker.text(),
+            "size": 1 << 10,
+            "virus_scanned_at": timezone.now(),
+        }
+        response = self.client.post(url, data, **self.exporter_headers)
+        self.assertEqual(response.status_code, expected_status)
+        return response.json()
+
+    def _test_exporter_responds_to_query(self, add_documents, query_type):
+        case = self.create_standard_application_case(self.organisation)
+        url = reverse("cases:case_ecju_queries", kwargs={"pk": case.id})
+        data = {"question": "Please provide required documents", "query_type": query_type}
+
+        with mock.patch("gov_notify.service.client") as mock_notify_client:
+            response = self.client.post(url, data, **self.gov_headers)
+        response_data = response.json()
+        ecju_query = EcjuQuery.objects.get(case=case)
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(response_data["ecju_query_id"], str(ecju_query.id))
+        self.assertEqual("Please provide required documents", ecju_query.question)
+        self.assertIsNone(ecju_query.response)
+        mock_notify_client.send_email.assert_called()
+
+        documents_to_be_added = []
+        if add_documents:
+            documents_to_be_added = [
+                self.add_query_document(case.id, ecju_query.id, status.HTTP_201_CREATED) for _ in range(5)
+            ]
+
+        query_response_url = reverse("cases:case_ecju_query", kwargs={"pk": case.id, "ecju_pk": ecju_query.id})
+        data = {"response": "Attached the requested documents"}
+        response = self.client.put(query_response_url, data, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = response.json()["ecju_query"]
+        self.assertEqual(response["response"], data["response"])
+
+        if add_documents:
+            self.assertEqual(len(response["documents"]), len(documents_to_be_added))
+
+    @parameterized.expand([ECJUQueryType.ECJU, ECJUQueryType.PRE_VISIT_QUESTIONNAIRE, ECJUQueryType.COMPLIANCE_ACTIONS])
+    def test_exporter_responds_to_query(self, query_type):
+        """Respond to queries without and with adding documents"""
+        self._test_exporter_responds_to_query(False, query_type)
+        self._test_exporter_responds_to_query(True, query_type)
+
+    def test_exporter_cannot_respond_to_same_ecju_query_twice(self):
+        """Once a query is responded it is closed so ensure we cannot respond to closed queries"""
+        case = self.create_standard_application_case(self.organisation)
+        url = reverse("cases:case_ecju_queries", kwargs={"pk": case.id})
+        data = {"question": "Please provide more details", "query_type": ECJUQueryType.ECJU}
+
+        response = self.client.post(url, data, **self.gov_headers)
+        response_data = response.json()
+        ecju_query = EcjuQuery.objects.get(case=case)
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(response_data["ecju_query_id"], str(ecju_query.id))
+        self.assertEqual("Please provide more details", ecju_query.question)
+        self.assertIsNone(ecju_query.response)
+
+        url = reverse("cases:case_ecju_query", kwargs={"pk": case.id, "ecju_pk": ecju_query.id})
+        data = {"response": "Additional details included"}
+        response = self.client.put(url, data, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = response.json()["ecju_query"]
+        self.assertEqual(response["response"], data["response"])
+
+        new_data = {"response": "Overwriting previous response"}
+        response = self.client.put(url, new_data, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.get(url, data, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = response.json()["ecju_query"]
+        self.assertEqual(response["response"], data["response"])
+
+    def test_exporter_cannot_add_documents_to_closed_query(self):
+        """Once a query is responded it is closed so ensure we cannot add documents to closed queries"""
+        case = self.create_standard_application_case(self.organisation)
+        url = reverse("cases:case_ecju_queries", kwargs={"pk": case.id})
+        data = {"question": "Please provide required documents", "query_type": ECJUQueryType.ECJU}
+
+        response = self.client.post(url, data, **self.gov_headers)
+        response_data = response.json()
+        ecju_query = EcjuQuery.objects.get(case=case)
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(response_data["ecju_query_id"], str(ecju_query.id))
+        self.assertEqual("Please provide required documents", ecju_query.question)
+        self.assertIsNone(ecju_query.response)
+
+        self.add_query_document(case.id, ecju_query.id, status.HTTP_201_CREATED)
+
+        query_response_url = reverse("cases:case_ecju_query", kwargs={"pk": case.id, "ecju_pk": ecju_query.id})
+        data = {"response": "Attached the requested documents"}
+        response = self.client.put(query_response_url, data, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = response.json()["ecju_query"]
+        self.assertEqual(response["response"], data["response"])
+        self.assertEqual(len(response["documents"]), 1)
+
+        # try to add document again
+        self.add_query_document(case.id, ecju_query.id, status.HTTP_400_BAD_REQUEST)
+
+    def test_exporter_cannot_delete_documents_of_closed_query(self):
+        """Once a query is responded it is closed so ensure we cannot delete documents to closed queries"""
+        case = self.create_standard_application_case(self.organisation)
+        url = reverse("cases:case_ecju_queries", kwargs={"pk": case.id})
+        data = {"question": "Please provide more details", "query_type": ECJUQueryType.ECJU}
+
+        response = self.client.post(url, data, **self.gov_headers)
+        response_data = response.json()
+        ecju_query = EcjuQuery.objects.get(case=case)
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(response_data["ecju_query_id"], str(ecju_query.id))
+        self.assertEqual("Please provide more details", ecju_query.question)
+        self.assertIsNone(ecju_query.response)
+
+        self.add_query_document(case.id, ecju_query.id, status.HTTP_201_CREATED)
+
+        url = reverse("cases:case_ecju_query", kwargs={"pk": case.id, "ecju_pk": ecju_query.id})
+        data = {"response": "Additional details included"}
+        response = self.client.put(url, data, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = response.json()["ecju_query"]
+        self.assertEqual(response["response"], data["response"])
+        self.assertEqual(len(response["documents"]), 1)
+
+        url = reverse(
+            "cases:case_ecju_query_document_detail",
+            kwargs={"pk": case.id, "query_pk": ecju_query.id, "doc_pk": response["documents"][0]["id"]},
+        )
+        response = self.client.delete(url, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.get(url, data, **self.exporter_headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = response.json()
+        self.assertIsNotNone(response["document"]["id"])
