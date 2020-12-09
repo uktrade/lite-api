c7adea55f2eff81e2076dd71c0549bbdbdbdd3d3
richtier <contact@richardtier.co.uk>
2020-12-09 10:51:12 +0000

    Add product comments

--- api/search/product/urls.py
+++ api/search/product/urls.py
@@ -12,6 +12,8 @@ router.register(r"search", views.ProductDocumentView, basename="product_search")
 urlpatterns = [
     path("", include(router.urls)),
     path("suggest/", views.ProductSuggestDocumentView.as_view(), name="product_suggest"),
+    path("spire/<str:pk>/comment/", views.CommentView.as_view(), name="retrieve_spire_product_comment"),
+    path("lite/<uuid:pk>/comment/", views.CommentView.as_view(), name="retrieve_lite_product_comment"),
     path("spire/<str:pk>/", views.RetrieveSpireProductView.as_view(), name="retrieve_spire_product"),
     path("lite/<uuid:pk>/", views.RetrieveLiteProductView.as_view(), name="retrieve_lite_product"),
 ]
