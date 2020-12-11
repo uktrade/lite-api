01f7877b5eb272a681d5e0bc729fee62c29e5153
Kerin Cosford <cosford@gmail.com>
2020-12-11 16:49:18 +0000

    Add RoutingRule natural key

--- api/workflow/routing_rules/models.py
+++ api/workflow/routing_rules/models.py
@@ -15,6 +15,20 @@ from api.users.models import GovUser
 from api.workflow.routing_rules.enum import RoutingRulesAdditionalFields
 
 
+class RoutingRuleManager(models.Manager):
+    def get_by_natural_key(self, team, queue, status, tier, additional_rules, active, user, country):
+        return self.get(
+            team=team,
+            queue=queue,
+            status=status,
+            tier=tier,
+            additional_rules=additional_rules,
+            active=active,
+            user=user,
+            country=country,
+        )
+
+
 class RoutingRule(TimestampableModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     team = models.ForeignKey(Team, related_name="routing_rules", on_delete=models.CASCADE)
@@ -34,9 +48,26 @@ class RoutingRule(TimestampableModel):
         Country, related_name="routing_rules", on_delete=models.DO_NOTHING, blank=True, null=True
     )
 
+    objects = RoutingRuleManager()
+
     class Meta:
         indexes = [models.Index(fields=["created_at", "tier"])]
         ordering = ["team__name", "tier", "-created_at"]
+        unique_together = ["team", "queue", "status", "tier", "additional_rules", "active", "user", "country"]
+
+    def natural_key(self):
+        return (
+            self.team,
+            self.queue,
+            self.status,
+            self.tier,
+            self.additional_rules,
+            self.active,
+            self.user,
+            self.country,
+        )
+
+    natural_key.dependencies = ["teams.Team", "queues.Queue", "users.GovUser", "countries.Country"]
 
     def parameter_sets(self):
         """
