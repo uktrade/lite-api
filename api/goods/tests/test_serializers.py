b1cbefeb9b26f28f4ba720d335d70acf36930699
Richard Tier <contact@richardtier.co.uk>
2020-12-16 17:28:58 +0000

    Update test_serializers.py

--- api/goods/tests/test_serializers.py
+++ api/goods/tests/test_serializers.py
@@ -7,10 +7,10 @@ class TestPvGradingDetailsSerializer(unittest.TestCase):
     def test_pv_grading_details_serializer(self):
         serializer_one = serializers.PvGradingDetailsSerializer(data={})
 
-        assert serializer_one.is_valid() is False
-        assert "custom_grading" in serializer_one.errors
+        self.assertEqual(serializer_one.is_valid(), False)
+        self.assertEqual("custom_grading" in serializer_one.errors, True)
 
         serializer_two = serializers.PvGradingDetailsSerializer(data={"custom_grading": "foo"})
 
-        assert serializer_two.is_valid() is False
-        assert "custom_grading" not in serializer_two.errors
+        self.assertEqual(serializer_two.is_valid(), False)
+        self.assertEqual("custom_grading" in serializer_two.errors, False)
