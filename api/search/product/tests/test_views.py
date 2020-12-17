e9bdd6a689b55f03ed29c5af5951f37dd9093850
richtier <contact@richardtier.co.uk>
2020-12-17 17:35:52 +0000

    Add ES to circle

--- api/search/product/tests/test_views.py
+++ api/search/product/tests/test_views.py
@@ -1,10 +1,8 @@
 from test_helpers.clients import DataTestClient
 
 from django.urls import reverse
-from django.test import tag
 
 
-@tag("elasticsearch")
 class MoreLikeThisViewTests(DataTestClient):
     def test_more_like_this_404(self):
         url = reverse("more_like_this", kwargs={"pk": "a1e4d94f-8519-4ef3-8863-e8fa17bdd685"})
