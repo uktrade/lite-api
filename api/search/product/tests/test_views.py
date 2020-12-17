381538f51369768adde48bb9c02d95cbf506bb26
richtier <contact@richardtier.co.uk>
2020-12-17 16:15:21 +0000

    more-like-this Handle produts on OIELs

---
+++ api/search/product/tests/test_views.py
@@ -0,0 +1,13 @@
+from test_helpers.clients import DataTestClient
+
+from django.urls import reverse
+
+
+class MoreLikeThisViewTests(DataTestClient):
+    def test_more_like_this_404(self):
+        url = reverse('more_like_this', kwargs={'pk': 'a1e4d94f-8519-4ef3-8863-e8fa17bdd685'})
+
+        response = self.client.get(url, **self.gov_headers)
+
+        assert response.status_code == 200
+        assert response.json() == []
