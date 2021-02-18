02f3cd63bca21110617768b4b30f5998e5dab060
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2021-02-18 13:22:15 +0000

    Populate goods list irrespective of whether a document available or not

--- api/applications/views/goods.py
+++ api/applications/views/goods.py
@@ -92,8 +92,6 @@ class ApplicationGoodsOnApplication(APIView):
 
             good = get_good_with_organisation(data.get("good"), get_request_user_organisation_id(request))
 
-            if good.is_document_available and not GoodDocument.objects.filter(good=good).exists():
-                return JsonResponse(data={"error": strings.Goods.DOCUMENT_ERROR}, status=status.HTTP_400_BAD_REQUEST)
             serializer = GoodOnApplicationCreateSerializer(data=data)
             if serializer.is_valid():
                 serializer.save()
