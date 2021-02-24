9535587536e76611b1517e820a7ddc9e101052c1
Richard Tier <contact@richardtier.co.uk>
2021-02-24 16:35:51 +0000

    Upgrade to Django3 (#718)
    * Upgrade to django 3
    
    * Suport dumpdata by natural keys (#719)
    
    * Suport dumpdata
    
    * Update api/staticdata/statuses/models.py
    
    Co-authored-by: django-doctor[bot] <72320148+django-doctor[bot]@users.noreply.github.com>
    
    Co-authored-by: django-doctor[bot] <72320148+django-doctor[bot]@users.noreply.github.com>
    
    * Update models.py
    
    Co-authored-by: django-doctor[bot] <72320148+django-doctor[bot]@users.noreply.github.com>

--- api/queues/models.py
+++ api/queues/models.py
@@ -5,13 +5,25 @@ from django.db import models
 from api.teams.models import Team
 
 
+class QueueManager(models.Manager):
+    def get_by_natural_key(self, name, team_name):
+        return self.get(name=name, team__name=team_name)
+
+
 class Queue(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField(default="Untitled Queue")
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     countersigning_queue = models.ForeignKey("self", on_delete=models.DO_NOTHING, null=True)
 
+    objects = QueueManager()
+
     class Meta:
         ordering = ["name"]
         db_table = "queue"
         unique_together = ["name", "team"]
+
+    def natural_key(self):
+        return (self.name, self.team.name)
+
+    natural_key.dependencies = ["teams.Team"]
