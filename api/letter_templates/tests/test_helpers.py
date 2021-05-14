fe3d7c6f3a3845cd057df51da662e682dba61ed7
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2021-05-14 16:22:13 +0100

    Add test

--- api/letter_templates/tests/test_helpers.py
+++ api/letter_templates/tests/test_helpers.py
@@ -1,7 +1,26 @@
 from django.test import TestCase
+from test_helpers.clients import DataTestClient
 from api.letter_templates.helpers import generate_preview
 
 
 class PreviewTestCase(TestCase):
     def test_generate_preview(self):
         generate_preview(layout="case_context_test", text="Hello World!")
+
+
+class DocumentGenerationTestCase(DataTestClient):
+    def test_document_layouts(self):
+        case = self.create_standard_application_case(self.organisation, user=self.exporter_user)
+        layouts = [
+            "application_form",
+            "case_context_test",
+            "ecju_queries_and_notes",
+            "ecju",
+            "nlr",
+            "refusal",
+            "siel",
+        ]
+        for layout in layouts:
+            html = generate_preview(layout=layout, case=case, text="")
+            # check there are no missing variables in the rendered template
+            assert "{{" not in html and "}}" not in html
