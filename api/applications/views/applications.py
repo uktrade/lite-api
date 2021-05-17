5615466fada4ca54346f5534dd368e4dc682ffcb
DIT User <dituser@DITs-MacBook-Pro.local>
2021-05-17 11:13:34 +0100

    more linting

--- api/applications/views/applications.py
+++ api/applications/views/applications.py
@@ -74,7 +74,7 @@ from api.flags.models import Flag
 from api.goods.serializers import GoodCreateSerializer
 from api.goodstype.models import GoodsType
 from gov_notify import service as gov_notify_service
-from gov_notify.enums import TemplateType
+# from gov_notify.enums import TemplateType
 # from gov_notify.payloads import ApplicationStatusEmailData
 from api.licences.enums import LicenceStatus
 from api.licences.helpers import get_licence_reference_code, update_licence_status
