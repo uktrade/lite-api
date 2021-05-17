c344f0f111d73ae8331c7020d07152a6f9617bf0
Ali Zaidi <alixedi@gmail.com>
2021-05-17 10:45:12 +0100

    Remove address from the ES query & update tests.

--- api/applications/tests/test_helpers.py
+++ api/applications/tests/test_helpers.py
@@ -66,37 +66,17 @@ class AbstractAutoMatchTests:
         self.assertEquals(str(party_on_application.flags.first().pk), SystemFlags.SANCTION_UK_MATCH)
 
     @pytest.mark.elasticsearch
-    def test_auto_match_sanctions_match_address(self):
-        prepare_index()
-
-        application = self.create_application()
-        party = self.get_party(application)
-        party.address = "123 Fake Street"
-        party.save()
-
-        document = SanctionDocumentType(
-            name="Johnson Woodlington",
-            address="123 fake street",
-            flag_uuid=SystemFlags.SANCTION_UK_MATCH,
-            reference="123",
-        )
-        document.save()
-        SanctionDocumentType._index.refresh()
-
-        helpers.auto_match_sanctions(application)
-
-        party_on_application = application.parties.get(party=party)
-
-        self.assertEquals(party_on_application.sanction_matches.count(), 1)
-        self.assertEquals(party_on_application.sanction_matches.first().elasticsearch_reference, "123")
-        self.assertEquals(str(party_on_application.flags.first().pk), SystemFlags.SANCTION_UK_MATCH)
-
-    @pytest.mark.elasticsearch
-    def test_auto_match_sanctions_match_avoid_false_positive_similar_name_different_address(self):
+    def test_auto_match_sanctions_avoid_false_positives(self):
         names = [
             "Jeremy Thompson",
             "Fred Jackson",
             "Jack Jeremyson",
+            "Jeremy Jacks",
+            "JeremyJackson",
+            "J Jackson",
+            "J. Jackson",
+            "Mr. J Jackson",
+            "Mr. Jackson",
         ]
         for name_variant in names:
             prepare_index()
@@ -109,7 +89,7 @@ class AbstractAutoMatchTests:
 
             document = SanctionDocumentType(
                 name=name_variant,
-                address="456 Fake Street, Berlin",
+                address="123 Plank Street, London, E19 8NX",
                 flag_uuid=SystemFlags.SANCTION_UK_MATCH,
                 reference="123",
             )
@@ -124,51 +104,15 @@ class AbstractAutoMatchTests:
             self.assertEquals(party_on_application.flags.count(), 0)
 
     @pytest.mark.elasticsearch
-    def test_auto_match_sanctions_match_address_similar(self):
-        addresses = [
-            "123 Fake Street, London, E14 9IX",
-            "123 Fake Street, London, E149IX",
-            "123 Fake Street, London, E14 9IX",
-            "123 Fake Street, London, e14 9ix",
-            "123 Fake Street\nLondon\nE14 9IX",
-            "123 Fake Street\nLondon\nUK\nE14 9IX",
-        ]
-
-        for address_variant in addresses:
-            prepare_index()
-
-            application = self.create_application()
-            party = self.get_party(application)
-            party.signatory_name_euu = "Jeremy Jackson"
-            party.address = "123 Fake Street, London, E14 9IX"
-            party.save()
-
-            document = SanctionDocumentType(
-                name="Jeremy Jackson",
-                address=address_variant,
-                flag_uuid=SystemFlags.SANCTION_UK_MATCH,
-                reference="123",
-            )
-            document.save()
-            SanctionDocumentType._index.refresh()
-
-            helpers.auto_match_sanctions(application)
-
-            party_on_application = application.parties.get(party=party)
-
-            self.assertEquals(party_on_application.sanction_matches.count(), 1, msg=f'tried "{address_variant}"')
-            self.assertEquals(str(party_on_application.flags.first().pk), SystemFlags.SANCTION_UK_MATCH)
-
-    @pytest.mark.elasticsearch
     def test_auto_match_sanctions_match_name_similar(self):
+        """ This test exposes the fact that ES tokenization etc. would mean
+        that the following names will be matched to "Jeremy Jackson". There is
+        no simple way around this.
+        """
         names = [
-            "Jeremy Jackson",
             "Jackson, Jeremy",
-            "JeremyJackson",
-            "J Jackson",
-            "J. Jackson",
-            "Mr. J Jackson",
-            "Mr. Jackson",
+            "Jackson Jeremy",
+            "Mr. Jeremy Jackson",
         ]
 
         for name_variant in names:
@@ -195,60 +139,6 @@ class AbstractAutoMatchTests:
             party_on_application = application.parties.get(party=party)
 
             self.assertEquals(party_on_application.sanction_matches.count(), 1, msg=f'tried "{name_variant}"')
-            self.assertEquals(str(party_on_application.flags.first().pk), SystemFlags.SANCTION_UK_MATCH)
-
-    @pytest.mark.elasticsearch
-    def test_auto_match_sanctions_match_name_address_similar(self):
-        names = [
-            "Jeremy Jackson",
-            "Jackson, Jeremy",
-            "JeremyJackson",
-            "J Jackson",
-            "J. Jackson",
-            "Mr. J Jackson",
-            "Mr. Jackson",
-        ]
-
-        addresses = [
-            "123 Fake Street, London",
-            "123 Fake Street, London",
-            "123 Fake Street, London",
-            "123 Fake Street, London",
-            "123 Fake Street\nLondon\n",
-            "123 Fake Street\nLondon\nUK",
-        ]
-
-        postcodes = [
-            "E14 9IX",
-            "e14 9ix",
-            "E149IX",
-        ]
-
-        for name, address, postcode in itertools.product(names, addresses, postcodes):
-            prepare_index()
-
-            application = self.create_application()
-            party = self.get_party(application)
-            party.signatory_name_euu = "Jeremy Jackson"
-            party.address = "123 Fake Street, London, E14 9IX"
-            party.save()
-
-            document = SanctionDocumentType(
-                name=name,
-                address=f"{address} {postcode}",
-                postcode=postcode,
-                flag_uuid=SystemFlags.SANCTION_UK_MATCH,
-                reference="123",
-            )
-            document.save()
-            SanctionDocumentType._index.refresh()
-            helpers.auto_match_sanctions(application)
-
-            party_on_application = application.parties.get(party=party)
-
-            self.assertEquals(
-                party_on_application.sanction_matches.count(), 1, msg=f'tried "{name} + "{address}" "{postcode}"'
-            )
             self.assertEquals(str(party_on_application.flags.first().pk), SystemFlags.SANCTION_UK_MATCH)
 
 
