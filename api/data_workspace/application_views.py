e67a95077ea7461c8d3e8dfa70dfa65946f85001
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2021-08-06 13:54:07 +0100

    Expose Application specific endpoints to data workspace

---
+++ api/data_workspace/application_views.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from api.applications import models
+from api.applications.serializers import standard_application, good, party
+from api.core.authentication import DataWorkspaceOnlyAuthentication
+
+
+class StandardApplicationListView(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (DataWorkspaceOnlyAuthentication,)
+    serializer_class = standard_application.StandardApplicationViewSerializer
+    pagination_class = LimitOffsetPagination
+    queryset = models.StandardApplication.objects.all()
+
+
+class GoodOnApplicationListView(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (DataWorkspaceOnlyAuthentication,)
+    serializer_class = good.GoodOnApplicationViewSerializer
+    pagination_class = LimitOffsetPagination
+    queryset = models.GoodOnApplication.objects.all()
+
+
+class PartyOnApplicationListView(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = (DataWorkspaceOnlyAuthentication,)
+    serializer_class = party.PartyOnApplicationViewSerializer
+    pagination_class = LimitOffsetPagination
+    queryset = models.PartyOnApplication.objects.all()
--- api/data_workspace/urls.py
+++ api/data_workspace/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from api.data_workspace import application_views
 from api.data_workspace import license_views
 
 app_name = "data_workspace"
 
 router = DefaultRouter()
+router.register(
+    "v1/standard-applications", application_views.StandardApplicationListView, basename="dw-standard-applications"
+)
+router.register(
+    "v1/good-on-applications", application_views.GoodOnApplicationListView, basename="dw-good-on-applications"
+)
+router.register(
+    "v1/party-on-applications", application_views.PartyOnApplicationListView, basename="dw-party-on-applications"
+)
 router.register("v1/licences", license_views.LicencesListDW, basename="dw-licences")
 router.register("v1/ogl", license_views.OpenGeneralLicenceListDW, basename="dw-ogl")
 
