fd96b16a61b5caa5f3c170bf38d828d945f75afd
Ali Zaidi <alixedi@gmail.com>
2021-08-06 10:59:04 +0100

    Rename existing views.

--- api/data_workspace/urls.py
+++ api/data_workspace/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.data_workspace import views
+from api.data_workspace import license_views
 
 app_name = "data_workspace"
 
 router = DefaultRouter()
-router.register("v1/licences", views.LicencesListDW, basename="dw-licences")
-router.register("v1/ogl", views.OpenGeneralLicenceListDW, basename="dw-ogl")
+router.register("v1/licences", license_views.LicencesListDW, basename="dw-licences")
+router.register("v1/ogl", license_views.OpenGeneralLicenceListDW, basename="dw-ogl")
 
 urlpatterns = [
     path("", include(router.urls)),
