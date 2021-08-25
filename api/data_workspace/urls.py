d2339572672c9c31f57b627bd61013b237db60bd
Daniel Ball <mail@danielball.com>
2021-08-25 17:43:30 +0100

    test_users_views.py

--- api/data_workspace/urls.py
+++ api/data_workspace/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.data_workspace import application_views, case_views, good_views, license_views, views, staticdata_views
+from api.data_workspace import (
+    application_views,
+    case_views,
+    good_views,
+    license_views,
+    views,
+    staticdata_views,
+    users_views,
+)
 
 app_name = "data_workspace"
 
@@ -48,6 +56,8 @@ router_v1.register("case-assignment", case_views.CaseAssignmentList, basename="d
 router_v1.register("case-assignment-slas", case_views.CaseAssignmentSlaList, basename="dw-case-assignment-sla")
 router_v1.register("case-types", case_views.CaseTypeList, basename="dw-case-type")
 router_v1.register("case-queues", case_views.CaseQueueList, basename="dw-case-queue")
+router_v1.register("users-base-users", users_views.BaseUserListView, basename="dw-users-base-users")
+router_v1.register("users-gov-users", users_views.GovUserListView, basename="dw-users-gov-users")
 
 urlpatterns = [
     path("v0/", include(router_v0.urls)),
