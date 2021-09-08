07fbab2c21fb554b611112a498a9fd7e8a3806c2
Mano <manossef@gmail.com>
2021-09-08 16:20:41 +0100

    Add test for old serializer

--- api/data_workspace/tests/test_serializers.py
+++ api/data_workspace/tests/test_serializers.py
@@ -1,6 +1,7 @@
 from api.data_workspace.serializers import EcjuQuerySerializer
+from api.cases.serializers import CaseAssignmentSerializer
 
-from api.cases.tests.factories import EcjuQueryFactory
+from api.cases.tests.factories import EcjuQueryFactory, CaseAssignmentFactory
 
 
 def test_EcjuQuerySerializer(db):
@@ -9,3 +10,10 @@ def test_EcjuQuerySerializer(db):
     assert serialized.data
     assert "question" in serialized.data
     assert "response" in serialized.data
+
+
+def test_CaseAssignmentSerializer(db):
+    case_assignment = CaseAssignmentFactory()
+    serialized = CaseAssignmentSerializer(case_assignment)
+    expected_fields = {"case", "user", "id", "queue"}
+    assert set(serialized.data.keys()) == expected_fields
