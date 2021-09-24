06a385415960c449aa1296d2e1f83befa5ad7fba
Will Keeling <will@zifferent.com>
2021-09-24 14:05:47 +0100

    Introduce is_ogd flag

--- api/teams/tests/test_edit.py
+++ api/teams/tests/test_edit.py
@@ -15,7 +15,7 @@ class TeamEditTests(DataTestClient):
         team = Team(name="Team", part_of_ecju=True)
         team.save()
 
-        data = {"name": "Renamed Team", "part_of_ecju": False}
+        data = {"name": "Renamed Team", "part_of_ecju": False, "is_ogd": False}
 
         url = reverse("teams:team", kwargs={"pk": team.id})
         response = self.client.put(url, data, **self.gov_headers)
@@ -24,12 +24,13 @@ class TeamEditTests(DataTestClient):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(team.name, data["name"])
         self.assertEqual(team.part_of_ecju, data["part_of_ecju"])
+        self.assertEqual(team.part_of_ecju, data["is_ogd"])
 
-    def test_edit_team_failure(self):
+    def test_edit_team_missing_part_of_ecju(self):
         team = Team(name="Team")
         team.save()
 
-        data = {"name": "Renamed Team"}
+        data = {"name": "Renamed Team", "is_ogd": False}
 
         url = reverse("teams:team", kwargs={"pk": team.id})
         response = self.client.put(url, data, **self.gov_headers)
@@ -38,12 +39,25 @@ class TeamEditTests(DataTestClient):
         response = response.json()["errors"]
         self.assertEqual(response["part_of_ecju"][0], "Select yes if the team is part of ECJU")
 
+    def test_edit_team_missing_is_ogd(self):
+        team = Team(name="Team")
+        team.save()
+
+        data = {"name": "Renamed Team", "part_of_ecju": False}
+
+        url = reverse("teams:team", kwargs={"pk": team.id})
+        response = self.client.put(url, data, **self.gov_headers)
+        team.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        response = response.json()["errors"]
+        self.assertEqual(response["is_ogd"][0], "Select yes if the team is an OGD")
+
     def test_cannot_rename_to_an_already_used_name_case_insensitive(self):
         """
         Test that a valid gov user cannot edit their team name
         to the same as another teams
         """
-        team = Team(name="name", part_of_ecju=False)
+        team = Team(name="name", part_of_ecju=False, is_ogd=False)
         team.save()
 
         Team(name="test").save()
@@ -59,7 +73,7 @@ class TeamEditTests(DataTestClient):
     def test_cannot_edit_admin_team(self):
         team = get_team_by_pk(pk=Teams.ADMIN_TEAM_ID)
 
-        data = {"name": "Renamed Team", "part_of_ecju": True}
+        data = {"name": "Renamed Team", "part_of_ecju": True, "is_ogd": False}
 
         url = reverse("teams:team", kwargs={"pk": team.id})
         response = self.client.put(url, data, **self.gov_headers)
