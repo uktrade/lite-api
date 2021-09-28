0eaa15406ea4f0aa38cece66bf1beaa1f60e99b6
Mano <manossef@gmail.com>
2021-09-28 14:51:45 +0100

    Fix factory to add department to team automatically

--- api/teams/tests/factories.py
+++ api/teams/tests/factories.py
@@ -2,15 +2,21 @@ import factory
 from api.teams.models import Team, Department
 
 
-class TeamFactory(factory.django.DjangoModelFactory):
-    name = factory.Faker("word")
-
-    class Meta:
-        model = Team
-
-
 class DepartmentFactory(factory.django.DjangoModelFactory):
-    name = factory.Faker("word")
+    name = factory.Iterator(["HMRC", "MOD", "BEIS", "DIT", "NCSC"])
+    # Commented out since probably we want to manually add this.
+    # team = factory.RelatedFactory(
+    #     TeamFactory,
+    #     factory_related_name='department',
+    # )
 
     class Meta:
         model = Department
+
+
+class TeamFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker("word")
+    department = factory.SubFactory(DepartmentFactory)
+
+    class Meta:
+        model = Team
