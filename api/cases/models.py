37f0225b777dd50a37cc72ce41a98a24f33b98c2
Mano <manossef@gmail.com>
2021-09-29 11:34:59 +0100

    Add docstring to SLA models

--- api/cases/models.py
+++ api/cases/models.py
@@ -244,12 +244,18 @@ class CaseQueue(TimestampableModel):
 
 
 class CaseAssignmentSla(models.Model):
+    """
+    Keeps track of days passed since case assigned to a team
+    """
     sla_days = models.IntegerField()
     queue = models.ForeignKey(Queue, related_name="slas", on_delete=models.CASCADE)
     case = models.ForeignKey(Case, related_name="slas", on_delete=models.CASCADE)
 
 
 class DepartmentSla(models.Model):
+    """
+    Keeps track of days passed since application received in department
+    """
     sla_days = models.IntegerField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="department_slas")
     case = models.ForeignKey(Case, on_delete=models.CASCADE, related_name="department_slas")
