5b15ed217069db409b32e825138a6cc587bd00b3
Daniel Ball <mail@danielball.com>
2021-10-11 12:43:42 +0100

    Add advice test

---
+++ api/data_workspace/tests/test_advice_views.py
@@ -0,0 +1,28 @@
+from django.urls import reverse
+
+from api.cases.enums import AdviceType, AdviceLevel
+from test_helpers.clients import DataTestClient
+
+
+class DataWorkspaceTests(DataTestClient):
+    def setUp(self):
+        super().setUp()
+        self.standard_application = self.create_standard_application_case(self.organisation)
+        self.create_advice(
+            self.gov_user,
+            self.standard_application,
+            "good",
+            AdviceType.APPROVE,
+            AdviceLevel.FINAL,
+            advice_text="advice_text",
+        )
+
+    def test_advice(self):
+        url = reverse("data_workspace:dw-advice")
+        response = self.client.get(url)
+        payload = response.json()
+        last_result = payload["results"][-1]
+
+        # Ensure we get some expected fields
+        expected_fields = {"id", "case", "user", "team"}
+        assert set(last_result.keys()) == expected_fields
