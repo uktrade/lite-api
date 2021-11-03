9b05caeda5df59ff0b53355495fac23e469c9bfb
Mano <manossef@gmail.com>
2021-11-03 16:17:45 +0000

    Add (failing) test to cover no_document_comments being saved

--- api/goods/tests/test_goods_documents.py
+++ api/goods/tests/test_goods_documents.py
@@ -4,6 +4,10 @@ from rest_framework import status
 from api.applications.models import GoodOnApplication
 from test_helpers.clients import DataTestClient
 
+from api.applications.tests.factories import StandardApplicationFactory
+from api.goods.tests.factories import GoodFactory
+from api.organisations.tests.factories import OrganisationFactory
+
 
 # from nose.tools import assert_true
 # import requests
@@ -17,10 +21,10 @@ class GoodDocumentsTests(DataTestClient):
 
     def test_can_view_all_documents_on_a_good(self):
         self.create_good_document(
-            good=self.good, user=self.exporter_user, organisation=self.organisation, s3_key="doc1key", name="doc1.pdf",
+            good=self.good, user=self.exporter_user, organisation=self.organisation, s3_key="doc1key", name="doc1.pdf"
         )
         self.create_good_document(
-            good=self.good, user=self.exporter_user, organisation=self.organisation, s3_key="doc2key", name="doc2.pdf",
+            good=self.good, user=self.exporter_user, organisation=self.organisation, s3_key="doc2key", name="doc2.pdf"
         )
 
         response = self.client.get(self.url, **self.exporter_headers)
@@ -46,6 +50,25 @@ class GoodDocumentsTests(DataTestClient):
     #     self.assertEqual(response.status_code, status.HTTP_200_OK)
     #     self.assertEqual(len(response_data['documents']), 1)
 
+    def test_no_document_comments_saved(self):
+        """
+        Ensure no-doc comments by applicant are stored in the database
+        """
+        app = StandardApplicationFactory()
+        org = OrganisationFactory()
+        good = GoodFactory(organisation=OrganisationFactory())
+
+        url = reverse("goods:good_document_availability", kwargs={"pk": good.id})
+        payload = {"is_document_available": "no", "no_document_comments": "tezt"}
+        response = self.client.post(url, payload, **self.exporter_headers)
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        # Ensure fields updated
+        good.refresh_from_db()
+        assert good.is_document_available == False
+        assert good.no_document_comments == "tezt"
+
     def test_submitted_good_cannot_have_docs_added(self):
         """
         Tests that the good cannot be edited after submission
@@ -67,7 +90,7 @@ class GoodDocumentsTests(DataTestClient):
         draft = self.create_draft_standard_application(self.organisation)
         good = GoodOnApplication.objects.get(application=draft).good
         document_1 = self.create_good_document(
-            good=good, user=self.exporter_user, organisation=self.organisation, s3_key="doc1key", name="doc1.pdf",
+            good=good, user=self.exporter_user, organisation=self.organisation, s3_key="doc1key", name="doc1.pdf"
         )
         self.submit_application(draft)
 
