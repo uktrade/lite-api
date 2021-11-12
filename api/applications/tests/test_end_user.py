8f582978495aac7c1fc116607d007040bdedcedb
Mano <manossef@gmail.com>
2021-11-12 15:14:26 +0000

    Make translation fields required for end_user

--- api/applications/tests/test_end_user.py
+++ api/applications/tests/test_end_user.py
@@ -13,6 +13,34 @@ from api.staticdata.countries.helpers import get_country
 from test_helpers.clients import DataTestClient
 
 
+class TestEndUserDocument:
+    def test_requires_extra_fields_for_end_user(self, end_user, exporter_client_with_standard_application):
+        """
+        For end-user we require to know if the content is in English and
+        if company letterhead is there (LTD-1368)
+        """
+        client, app = exporter_client_with_standard_application
+        app.add_party(end_user.party)
+        url = reverse("applications:party_document", kwargs={"pk": app.id, "party_pk": end_user.party.id})
+
+        # Missing required fields for end-user
+        response = client.post(url, {"name": "document_name.pdf", "s3_key": "s3_keykey.pdf", "size": 123456})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+        # With required fields for end-user
+        response = client.post(
+            url,
+            {
+                "name": "document_name.pdf",
+                "s3_key": "s3_keykey.pdf",
+                "size": 123456,
+                "is_content_english": True,
+                "includes_company_letterhead": False,
+            },
+        )
+        assert response.status_code == status.HTTP_201_CREATED
+
+
 class EndUserOnDraftTests(DataTestClient):
     def setUp(self):
         super().setUp()
@@ -32,11 +60,7 @@ class EndUserOnDraftTests(DataTestClient):
             "applications:party_document", kwargs={"pk": self.draft.id, "party_pk": self.draft.end_user.party.id}
         )
 
-        self.new_document_data = {
-            "name": "document_name.pdf",
-            "s3_key": "s3_keykey.pdf",
-            "size": 123456,
-        }
+        self.new_document_data = {"name": "document_name.pdf", "s3_key": "s3_keykey.pdf", "size": 123456}
 
     @parameterized.expand([SubType.GOVERNMENT, SubType.COMMERCIAL, SubType.OTHER])
     def test_set_end_user_on_draft_standard_application_successful(self, data_type):
@@ -56,7 +80,7 @@ class EndUserOnDraftTests(DataTestClient):
         response = self.client.post(self.url, data, **self.exporter_headers)
 
         party_on_application = PartyOnApplication.objects.get(
-            application=self.draft, party__type=PartyType.END_USER, deleted_at__isnull=True,
+            application=self.draft, party__type=PartyType.END_USER, deleted_at__isnull=True
         )
 
         self.draft.refresh_from_db()
@@ -118,7 +142,7 @@ class EndUserOnDraftTests(DataTestClient):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
             PartyOnApplication.objects.filter(
-                party__type=PartyType.END_USER, application=self.draft, deleted_at__isnull=True,
+                party__type=PartyType.END_USER, application=self.draft, deleted_at__isnull=True
             ).count(),
             1,
         )
