7ebb8aa1438f7e7d129bb0d84e094064c652c4e4
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-02-08 14:50:47 +0000

    Add test to check previous reference code increments correctly on licences creation

--- api/licences/tests/test_get_licence_reference_code.py
+++ api/licences/tests/test_get_licence_reference_code.py
@@ -1,4 +1,4 @@
-from string import ascii_lowercase
+from string import ascii_lowercase, ascii_uppercase
 
 from api.licences.enums import LicenceStatus
 from api.licences.helpers import get_licence_reference_code
@@ -17,6 +17,18 @@ class GetLicenceReferenceCodeTests(DataTestClient):
         reference_code = get_licence_reference_code(self.application.reference_code)
 
         self.assertEqual(reference_code, self.application.reference_code)
+
+    def test_get_amended_licence_old_reference_code_format(self):
+        """
+        Check all amended licences get suffix '/A' -> '/Z' in the old format
+        """
+        self.application.reference_code = "GBSIEL/2021/0000001/P"
+        self.application.save()
+
+        for letter in ascii_uppercase:
+            self.create_licence(self.application, status=LicenceStatus.ISSUED)
+            reference_code = get_licence_reference_code(self.application.reference_code)
+            self.assertEqual(reference_code, f"{self.application.reference_code}/{letter}")
 
     def test_get_amended_licence_reference_code(self):
         """
