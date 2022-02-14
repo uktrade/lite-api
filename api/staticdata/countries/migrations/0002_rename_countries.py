218672d405c13f6cf0b7e4965aad2bd9d0c109e5
Le H Ngo <hgle.ngo@gmail.com>
2022-02-14 14:16:50 +0000

    -Adds migration to rename countries.

---
+++ api/staticdata/countries/migrations/0002_rename_countries.py
@@ -0,0 +1,42 @@
+import logging
+
+from django.db import migrations
+
+from api.staticdata.countries.models import Country
+
+logger = logging.getLogger(__name__)
+
+
+def rename_countries(apps, schema_editor):
+    change_needed = {"United Kingdom": "Great Britain", "St Vincent": "St Vincent and the Grenadines"}
+
+    for key in change_needed:
+      country_query = Country.objects.filter(name__iexact=key)
+
+      if country_query.exists():
+        country = country_query.first()
+        country.name = change_needed[key]
+        country.save()
+
+def rename_reverse_countries(apps, schema_editor):
+    change_needed = {"Great Britain": "United Kingdom", "St Vincent and the Grenadines": "St Vincent"}
+
+    for key in change_needed:
+      country_query = Country.objects.filter(name__iexact=key)
+
+      if country_query.exists():
+        country = country_query.first()
+        country.name = change_needed[key]
+        country.save()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('countries', '0001_squashed_0003_auto_20210105_1058'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            rename_countries,
+            rename_reverse_countries
+        ),
+    ]
