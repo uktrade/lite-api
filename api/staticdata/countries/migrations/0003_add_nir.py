62cb6e8e526e73541363309b51472d9016123a8a
Le H Ngo <hgle.ngo@gmail.com>
2022-02-24 11:30:26 +0000

    -Fixes GB being in EU flag to False.

--- api/staticdata/countries/migrations/0003_add_nir.py
+++ api/staticdata/countries/migrations/0003_add_nir.py
@@ -3,17 +3,29 @@ from django.db import migrations
 
 def add_nir(apps, schema_editor):
     Country = apps.get_model("countries", "Country")
-    country = Country.objects.filter(name__iexact="Northern Ireland")
+    gb_nier = Country.objects.filter(name__iexact="Northern Ireland")
 
-    if country.exists():
+    if gb_nier.exists():
         return
 
-    Country.objects.create(id="GB-NIR", name="Northern Ireland", type="gov.uk Country", is_eu=True)
+    Country.objects.create(id="GB-NIR", name="Northern Ireland", type="gov.uk Country", is_eu=False)
+
+    gb_query = Country.objects.filter(name__iexact="Great Britain")
+    if gb_query.exists():
+        gb = gb_query.first()
+        gb.is_eu = False
+        gb.save()
 
 
 def remove_nir(apps, schema_editor):
     Country = apps.get_model("countries", "Country")
     Country.objects.filter(name__iexact="Northern Ireland").delete()
+
+    gb_query = Country.objects.filter(name__iexact="Great Britain")
+    if gb_query.exists():
+        gb = gb_query.first()
+        gb.is_eu = True
+        gb.save()
 
 
 class Migration(migrations.Migration):
