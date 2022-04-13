8ba3a356febd06d86454fafd416fdd64b80fd1b2
Pippo Raimondi <filipporai@gmail.com>
2022-04-13 10:05:16 +0100

    Update start command

--- start-e2e.sh
+++ start-e2e.sh
@@ -8,12 +8,5 @@ python /app/manage.py seedall
 INTERNAL_USERS='[{"email"=>"foo@bar.gov.uk"}]' /app/manage.py seedinternalusers
 EXPORTER_USERS='[{"email"=>"foo@bar.com"}]' /app/manage.py seedexporterusers
 
-# Run background tasks
-python /app/manage.py process_tasks
-
-# Run runserver in a while loop as the whole docker container will otherwise die
-# when there is bad syntax
-while true; do
-    python /app/manage.py runserver_plus 0.0.0.0:8100
-    sleep 1
-done
+# Run app
+python /app/manage.py runserver 0.0.0.0:8100
