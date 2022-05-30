3133c9460986378a6104cdfeb2e862f36f0e16b6
Sekhar Panja <sekhar.panja@digital.trade.gov.uk>
2022-05-30 10:02:21 +0100

    fixup! prefill ES in order for tests based on it will work during CI

--- start-e2e.sh
+++ start-e2e.sh
@@ -5,11 +5,11 @@ python /app/manage.py migrate --noinput
 INTERNAL_USERS='[{"email"=>"foo@bar.gov.uk"}]' /app/manage.py seedinternalusers
 EXPORTER_USERS='[{"email"=>"foo@bar.com"}]' /app/manage.py seedexporterusers
 
-# Rebuild ES index
-LITE_API_ENABLE_ES=true /app/manage.py search_index --rebuild -f
-
 # Fill sanctions in ES
 LITE_API_ENABLE_ES=true /app/manage.py ingest_sanctions
+
+# Rebuild ES index
+LITE_API_ENABLE_ES=true /app/manage.py search_index --rebuild -f
 
 # Run app
 python /app/manage.py runserver 0.0.0.0:8100
