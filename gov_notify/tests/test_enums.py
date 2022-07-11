9b998e80ac08c4eb5061396be1718bbe8f4d9ddd
Brendan Smith <brendan.smith@bsmitty.co.uk>
2022-07-11 15:48:04 +0100

    Remove unused ECJU_CREATED notify template

--- gov_notify/tests/test_enums.py
+++ gov_notify/tests/test_enums.py
@@ -9,7 +9,6 @@ from gov_notify.enums import TemplateType
 class TemplateTypeTests(APITestCase):
     @parameterized.expand(
         [
-            (TemplateType.ECJU_CREATED, "bcf052e0-54d9-4ed2-b77e-2f5a77589466"),
             (TemplateType.ECJU_COMPLIANCE_CREATED, "b23f4c55-fef0-4d8f-a10b-1ad7f8e7c672"),
             (TemplateType.APPLICATION_STATUS, "b9c3403a-8d09-416e-acd3-99baabf5b043"),
             (TemplateType.ORGANISATION_STATUS, "c57ef67e-14fd-4af9-a9b2-5015040fa408"),
