dc26c08cf2430d3ab825449cfb832b790a56e13d
Brendan Smith <brendan.smith@bsmitty.co.uk>
2022-07-18 16:39:18 +0100

    Add celery to Procfile

--- api/core/celery_tasks.py
+++ api/core/celery_tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from api.cases.models import Case
 
 
 @shared_task
@@ -7,3 +8,11 @@ def debug_add(x, y):
     Simple debug celery task to add two numbers.
     """
     return x + y
+
+
+@shared_task
+def debug_count_cases():
+    """
+    Simple debug celery task to count the number of cases in the app.
+    """
+    return Case.objects.count()
