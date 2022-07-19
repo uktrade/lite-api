c699716c470b9ec3f1b812db6581a8ec7c08f164
Brendan Smith <brendan.smith@bsmitty.co.uk>
2022-07-19 11:37:41 +0100

    Fix flaky good serializer testcase

--- gov_notify/tests/test_enums.py
+++ gov_notify/tests/test_enums.py
@@ -12,12 +12,9 @@ class TemplateTypeTests(APITestCase):
             (TemplateType.APPLICATION_STATUS, "b9c3403a-8d09-416e-acd3-99baabf5b043"),
             (TemplateType.EXPORTER_REGISTERED_NEW_ORG, "6096c45e-0cbb-4ecd-a7a9-0ad674e1d2c0"),
             (TemplateType.EXPORTER_USER_ADDED, "c9b67dca-0916-453a-99c0-70ba563e1bdd"),
-<<<<<<< HEAD
             (TemplateType.EXPORTER_ORGANISATION_APPROVED, "d5e94717-ae78-4d18-8064-ecfcd99143f1"),
             (TemplateType.EXPORTER_ORGANISATION_REJECTED, "1dec3acd-94b0-47bb-832a-384ba5c6f51a"),
-=======
             (TemplateType.EXPORTER_ECJU_QUERY, "84431173-72a9-43a1-8926-b43dec7871f9"),
->>>>>>> Add email notification to exporter for new ECJU query
         ]
     )
     def test_template_id(self, template_type, expected_template_id):
