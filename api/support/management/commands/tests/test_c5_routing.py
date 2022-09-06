1bad83854c7ab3afa32bba0bdabb49c3228a5957
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2022-09-06 15:12:32 +0100

    Update mocking function path

--- api/support/management/commands/tests/test_c5_routing.py
+++ api/support/management/commands/tests/test_c5_routing.py
@@ -6,12 +6,12 @@ from test_helpers.clients import DataTestClient
 
 
 class C5RoutingMgmtCommandTests(DataTestClient):
-    @mock.patch("lite_routing.routing_rules_internal.tests.helpers.activate_c5_routing")
+    @mock.patch("api.support.management.commands.c5_routing.activate_c5_routing")
     def test_activating_rules(self, mock_activate_c5_routing):
         call_command("c5_routing", "activate")
         assert mock_activate_c5_routing.called
 
-    @mock.patch("lite_routing.routing_rules_internal.tests.helpers.deactivate_c5_routing")
+    @mock.patch("api.support.management.commands.c5_routing.deactivate_c5_routing")
     def test_deactivating_rules(self, mock_deactivate_c5_routing):
         call_command("c5_routing", "deactivate")
         assert mock_deactivate_c5_routing.called
