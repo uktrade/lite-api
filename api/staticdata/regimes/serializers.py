5b112731fead5acf3e7694bb0a5d3d93ae25c21f
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-09-26 15:21:09 +0100

    Add nested regime representation to regime serializers

--- api/staticdata/regimes/serializers.py
+++ api/staticdata/regimes/serializers.py
@@ -1,10 +1,41 @@
 from rest_framework import serializers
 
 
-from .models import RegimeEntry
+from .models import (
+    Regime,
+    RegimeEntry,
+    RegimeSubsection,
+)
+
+
+class RegimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Regime
+        fields = [
+            "pk",
+            "name",
+        ]
+
+
+class RegimeSubsectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RegimeSubsection
+        fields = [
+            "pk",
+            "name",
+            "regime",
+        ]
+
+    regime = RegimeSerializer()
 
 
 class RegimeEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = RegimeEntry
-        fields = ["pk", "name"]
+        fields = [
+            "pk",
+            "name",
+            "subsection",
+        ]
+
+    subsection = RegimeSubsectionSerializer()
