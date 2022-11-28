5e32d38ad3b5ed9e16858cf30b4ef2228b0d2b17
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-11-28 13:46:49 +0000

    Consolidate regime entries into a single view
    This still keeps the previous URLs intact but redirects to the new URL for backwards compatibility with our frontend until we explicitly make the same change on the frontend

--- api/staticdata/regimes/views.py
+++ api/staticdata/regimes/views.py
@@ -2,23 +2,56 @@ from rest_framework import generics
 
 from api.core.authentication import HawkOnlyAuthentication
 
-from .enums import RegimesEnum
+from django.db.models.functions import Lower
+from django.http import Http404
+from django.urls import reverse
+from django.views.generic.base import RedirectView
+
 from .models import RegimeEntry
 from .serializers import RegimeEntrySerializer
 
 
-class BaseEntriesView(generics.ListAPIView):
+class EntriesView(generics.ListAPIView):
     authentication_classes = (HawkOnlyAuthentication,)
     pagination_class = None
     serializer_class = RegimeEntrySerializer
 
     def get_queryset(self):
-        return RegimeEntry.objects.filter(subsection__regime=self.regime_type).order_by("name")
+        regime_type = self.kwargs["regime_type"]
+
+        regime_entries = (
+            RegimeEntry.objects.annotate(regime_slug=Lower("subsection__regime__name"))
+            .filter(
+                regime_slug=regime_type,
+            )
+            .order_by("name")
+        )
+
+        if not regime_entries.exists():
+            raise Http404
+
+        return regime_entries
 
 
-class MTCREntriesView(BaseEntriesView):
-    regime_type = RegimesEnum.MTCR
+class MTCREntriesView(RedirectView):
+    permanent = True
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse(
+            "staticdata:regimes:entries",
+            kwargs={
+                "regime_type": "mtcr",
+            },
+        )
 
 
-class WassenaarEntriesView(BaseEntriesView):
-    regime_type = RegimesEnum.WASSENAAR
+class WassenaarEntriesView(RedirectView):
+    permanent = True
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse(
+            "staticdata:regimes:entries",
+            kwargs={
+                "regime_type": "wassenaar",
+            },
+        )
