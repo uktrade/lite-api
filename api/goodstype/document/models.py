a22044b9402158e7fdb8d7efdfbc27f906eb91ce
Mark Thomas <mathomas@deloitte.co.uk>
2022-11-28 10:54:37 +0000

    undo goods desc. length restriction removal

--- api/goodstype/document/models.py
+++ api/goodstype/document/models.py
@@ -5,4 +5,4 @@ from api.documents.models import Document
 
 class GoodsTypeDocument(Document):
     goods_type = models.ForeignKey("goodstype.GoodsType", on_delete=models.CASCADE)
-    description = models.TextField(default=None, blank=True, null=True)
+    description = models.TextField(default=None, blank=True, null=True, max_length=280)
