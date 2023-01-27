a133282543cd7929cbd9ce7dee590fa954941642
depsiatwal <depsi.a@gmail.com>
2023-01-27 09:37:11 +0000

    add invalid serilizer test

--- api/cases/tests/test_cases_case_officer.py
+++ api/cases/tests/test_cases_case_officer.py
@@ -1,7 +1,9 @@
+import pytest
 from django.urls import reverse
 from rest_framework import status
 
 from test_helpers.clients import DataTestClient
+from django.core.exceptions import ValidationError
 
 
 class CasesUpdateCaseOfficerTests(DataTestClient):
@@ -30,3 +32,7 @@ class CasesUpdateCaseOfficerTests(DataTestClient):
         request = self.client.put(self.url, data={"gov_user_pk": self.user.pk}, **self.gov_headers)
 
         self.assertEqual(request.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_assign_gov_user_to_multiple_cases_invalid(self):
+        with pytest.raises(ValidationError):
+            self.client.put(self.url, data={"gov_user_pk": self.user.pk, "case_ids": ["ambmsb@"]}, **self.gov_headers)
