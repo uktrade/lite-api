0a78fa6f97b2d877e85493aa95a863df2e5cccd1
Anthony Roy <anthony.roy@madetech.com>
2023-02-07 13:50:48 +0000

    Refactored out the queryset from the prefix and subject views

--- api/staticdata/report_summaries/views.py
+++ api/staticdata/report_summaries/views.py
@@ -7,6 +7,28 @@ from api.staticdata.report_summaries.models import ReportSummaryPrefix, ReportSu
 from api.staticdata.report_summaries.serializers import ReportSummaryPrefixSerializer, ReportSummarySubjectSerializer
 
 
+def filtered_ordered_queryset(model_class, part_of_name):
+    """
+    Creates a queryset to get rows for the model where the
+    'name' column contains the part_of_name parameter. The results
+    are sorted so that entries where the name is prefixed by
+    the part_of_name appear higher in the rankings and
+    alphabetically after that.
+    """
+    queryset = model_class.objects.all()
+    if part_of_name:
+        queryset = queryset.filter(name__icontains=part_of_name)
+        queryset = queryset.annotate(
+            is_prefixed=Case(
+                When(name__istartswith=part_of_name.lower(), then=True),
+                default=False,
+                output_field=BinaryField(),
+            ),
+        )
+        queryset = queryset.order_by("-is_prefixed", "name")
+    return queryset
+
+
 class ReportSummaryPrefixView(APIView):
     authentication_classes = (GovAuthentication,)
 
@@ -14,20 +36,9 @@ class ReportSummaryPrefixView(APIView):
         """
         Returns report summary prefixes
         """
-        name = self.request.GET.get("name")
-        prefixes = ReportSummaryPrefix.objects.all()
-
-        if name:
-            prefixes = prefixes.filter(name__icontains=name)
-            prefixes = prefixes.annotate(
-                is_prefixed=Case(
-                    When(name__istartswith=name.lower(), then=True),
-                    default=False,
-                    output_field=BinaryField(),
-                ),
-            )
-            prefixes = prefixes.order_by("-is_prefixed", "name")
-
+        part_of_name = self.request.GET.get("name")
+        model_class = ReportSummaryPrefix
+        prefixes = filtered_ordered_queryset(model_class, part_of_name)
         prefix_serializer = ReportSummaryPrefixSerializer(prefixes, many=True)
         return JsonResponse(data={"report_summary_prefixes": prefix_serializer.data})
 
@@ -39,19 +50,8 @@ class ReportSummarySubjectView(APIView):
         """
         Returns report summary subjects
         """
-        name = self.request.GET.get("name")
-        subjects = ReportSummarySubject.objects.all()
-
-        if name:
-            subjects = subjects.filter(name__icontains=name)
-            subjects = subjects.annotate(
-                is_prefixed=Case(
-                    When(name__istartswith=name.lower(), then=True),
-                    default=False,
-                    output_field=BinaryField(),
-                ),
-            )
-            subjects = subjects.order_by("-is_prefixed", "name")
-
+        part_of_name = self.request.GET.get("name")
+        model_class = ReportSummarySubject
+        subjects = filtered_ordered_queryset(model_class, part_of_name)
         subject_serializer = ReportSummarySubjectSerializer(subjects, many=True)
         return JsonResponse(data={"report_summary_subjects": subject_serializer.data})
