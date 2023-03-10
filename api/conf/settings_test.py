e50cd4378d531925d415293585e7158e71790964
Kevin Carrogan <kevin@carrogan.com>
2023-03-10 13:01:09 +0000

    Suppress output from seeding commands by default during tests

--- api/conf/settings_test.py
+++ api/conf/settings_test.py
@@ -4,3 +4,5 @@ from api.conf.settings import *
 ELASTICSEARCH_SANCTION_INDEX_ALIAS = "sanctions-alias-test"
 
 LOGGING = {"version": 1, "disable_existing_loggers": True}
+
+SUPPRESS_TEST_OUTPUT = True
