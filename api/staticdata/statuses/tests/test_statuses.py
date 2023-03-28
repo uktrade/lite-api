7adc3796019680ba7b613debb4d169312aecaf7f
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2023-03-28 12:45:29 +0100

    Update CaseStatusEnum to evaluate status choices at runtime
    The new statuses introduced for LU Countersigning are behind a
    feature flag so we need to evaluate these at runtime (when running tests)
    otherwise they don't get picked up and tests fail.

--- api/staticdata/statuses/tests/test_statuses.py
+++ api/staticdata/statuses/tests/test_statuses.py
@@ -1,6 +1,8 @@
 import pytest
+from django.test import override_settings
 from parameterized import parameterized
 
+from api.staticdata.statuses.enums import CaseStatusEnum
 from api.staticdata.statuses.libraries.get_case_status import get_case_status_by_status
 from api.applications.libraries.case_status_helpers import get_case_statuses
 
@@ -16,3 +18,15 @@ class TestCaseStatus:
     def test_editable_case_statuses(self, status):
         editable_case_status = get_case_status_by_status(status)
         assert editable_case_status.is_read_only is False
+
+    @override_settings(FEATURE_COUNTERSIGN_ROUTING_ENABLED=True)
+    def test_read_only_status_contains_lu_countersign_statuses(self):
+        statuses = get_case_statuses(read_only=True)
+        assert CaseStatusEnum.FINAL_REVIEW_COUNTERSIGN in statuses
+        assert CaseStatusEnum.FINAL_REVIEW_SECOND_COUNTERSIGN in statuses
+
+    @override_settings(FEATURE_COUNTERSIGN_ROUTING_ENABLED=True)
+    def test_all_status_contains_lu_countersign_statuses(self):
+        statuses = CaseStatusEnum.all()
+        assert CaseStatusEnum.FINAL_REVIEW_COUNTERSIGN in statuses
+        assert CaseStatusEnum.FINAL_REVIEW_SECOND_COUNTERSIGN in statuses
