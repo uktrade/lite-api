38eb0fc258616b6ae60a817f890e906ea711b63b
Mark Thomas <mark.thomas@digital.trade.gov.uk>
2023-04-05 12:53:45 +0100

    lu refusal notification emails sent from case post save signal

--- api/applications/notify.py
+++ api/applications/notify.py
@@ -1,6 +1,11 @@
 from django.urls import reverse
 
+from api.cases.enums import AdviceLevel, AdviceType, CountersignOrder
+from api.cases.models import CountersignAdvice
 from api.core.helpers import get_caseworker_frontend_url, get_exporter_frontend_url
+from api.staticdata.statuses.enums import CaseStatusEnum
+from api.teams.enums import TeamIdEnum
+from api.teams.models import Team
 from gov_notify.enums import TemplateType
 from gov_notify.payloads import CaseWorkerCountersignCaseReturn, ExporterCaseOpenedForEditing
 from gov_notify.service import send_email
@@ -27,16 +32,31 @@ def notify_exporter_case_opened_for_editing(application):
     )
 
 
-def notify_caseworker_countersign_return(user_email, application, countersign_advice):
-    relative_url = reverse("cases:countersign_decision_advice", kwargs={"pk": application.id})
+# def notify_caseworker_countersign_return(user_email, application, countersign_advice):
+def notify_caseworker_countersign_return(case):
+    countersign_order = (
+        CountersignOrder.FIRST_COUNTERSIGN
+        if case._previous_status.status == CaseStatusEnum.FINAL_REVIEW_COUNTERSIGN
+        else CountersignOrder.SECOND_COUNTERSIGN
+    )
+    countersign_advice = CountersignAdvice.objects.get(
+        case=case,
+        valid=True,
+        order=countersign_order,
+        outcome_accepted=False,
+        advice__user__team=Team.objects.get(id=TeamIdEnum.LICENSING_UNIT),
+        advice__level=AdviceLevel.FINAL,
+        advice__type=AdviceType.REFUSE,
+    )
+    relative_url = reverse("cases:countersign_decision_advice", kwargs={"pk": case.id})
     countersigner = countersign_advice.countersigned_user
     data = {
-        "case_reference": application.reference_code,
+        "case_reference": case.reference_code,
         "countersigned_user_name": f"{countersigner.first_name} {countersigner.last_name}",
         "countersign_reasons": countersign_advice.reasons,
         "recommendation_section_url": get_caseworker_frontend_url(relative_url),
     }
-    _notify_caseworker_countersign_return(user_email, data)
+    _notify_caseworker_countersign_return(case.case_officer.email, data)
 
 
 def _notify_caseworker_countersign_return(email, data):
--- api/cases/signals.py
+++ api/cases/signals.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from api.applications.notify import notify_caseworker_countersign_return
 from api.cases.models import Case
 from api.staticdata.statuses.enums import CaseStatusEnum
 from api.staticdata.statuses.libraries.get_case_status import get_case_status_by_status
@@ -20,8 +21,20 @@ def case_post_save_handler(sender, instance, raw=False, **kwargs):
         return
 
     status_changed = instance._previous_status != instance.status
-    instance._previous_status = instance.status
     status_draft = instance.status == get_case_status_by_status(CaseStatusEnum.DRAFT)
     new_status_terminal = instance.status.is_terminal
     if status_changed and not status_draft and not new_status_terminal:
         apply_flagging_rules_to_case(instance)
+        _check_for_countersign_rejection(instance)
+    instance._previous_status = instance.status
+
+
+def _check_for_countersign_rejection(case):
+    if (
+        settings.FEATURE_COUNTERSIGN_ROUTING_ENABLED
+        and case.status.status == CaseStatusEnum.UNDER_FINAL_REVIEW
+        and case._previous_status.status
+        in [CaseStatusEnum.FINAL_REVIEW_COUNTERSIGN, CaseStatusEnum.FINAL_REVIEW_SECOND_COUNTERSIGN]
+    ):
+        # send notification as advice has been rejected by countersigner
+        notify_caseworker_countersign_return(case)
