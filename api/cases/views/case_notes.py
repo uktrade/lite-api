2c18dc31a1b9a8008d0ab930543212028ea9c140
depsiatwal <depsi.a@gmail.com>
2023-06-01 09:40:11 +0100

    fix count query

--- api/cases/views/case_notes.py
+++ api/cases/views/case_notes.py
@@ -160,16 +160,6 @@ class UserCaseNoteMentionsNewCount(APIView):
 
     def get(self, request):
         """Gets count of all new mentions for user."""
-        qs = (
-            CaseNoteMentions.objects.select_related(
-                "case_note",
-                "user",
-                "case_note__user",
-                "case_note__user__govuser__team",
-                "case_note__case",
-            )
-            .filter(user_id=request.user.pk)
-            .filter(is_accessed=False)
-        )
+        qs = CaseNoteMentions.objects.filter(user_id=request.user.pk, is_accessed=False)
 
         return JsonResponse(data={"count": qs.count()})
