f98a55dcec7977425d9aeb8f17504b9028af2204
Kevin Carrogan <kevin@carrogan.com>
2023-07-28 19:16:20 +0100

    Check output of generated preview

--- api/letter_templates/tests/test_helpers.py
+++ api/letter_templates/tests/test_helpers.py
@@ -1,11 +1,23 @@
+from pathlib import Path
+
 from django.test import TestCase
 from test_helpers.clients import DataTestClient
 from api.letter_templates.helpers import generate_preview
 
 
+TEST_DATA_PATH = Path(__file__).resolve().parent / "data"
+
+
 class PreviewTestCase(TestCase):
     def test_generate_preview(self):
-        generate_preview(layout="case_context_test", text="Hello World!")
+        generated_preview = generate_preview(
+            layout="case_context_test",
+            text="Hello World!",
+        )
+        with open(TEST_DATA_PATH / "generated-preview.html") as expected_output_file:
+            expected_output = expected_output_file.read()
+
+        assert generated_preview == expected_output
 
 
 class DocumentGenerationTestCase(DataTestClient):
