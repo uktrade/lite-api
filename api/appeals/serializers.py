91e472418b2ec42114a766d3d15dfac7f6b3bbee
Kevin Carrogan <kevin@carrogan.com>
2023-08-18 12:31:49 +0100

    Add endpoint for creating appeal document

--- api/appeals/serializers.py
+++ api/appeals/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from .models import Appeal
+from api.documents.libraries.process_document import process_document
+
+from .models import Appeal, AppealDocument
 
 
 class AppealSerializer(serializers.ModelSerializer):
@@ -10,3 +12,26 @@ class AppealSerializer(serializers.ModelSerializer):
             "id",
             "grounds_for_appeal",
         )
+
+
+class AppealDocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppealDocument
+        fields = (
+            "id",
+            "name",
+            "size",
+            "s3_key",
+            "safe",
+        )
+
+    def __init__(self, *args, appeal, **kwargs):
+        self.appeal = appeal
+        super().__init__(*args, **kwargs)
+
+    def create(self, validated_data):
+        validated_data["appeal"] = self.appeal
+        document = super().create(validated_data)
+        process_document(document)
+        document.refresh_from_db()
+        return document
