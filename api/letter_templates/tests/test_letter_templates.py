72c13bcd4668842f44b7ebd6dce959e29d32b77d
Le H Ngo <hgle.ngo@gmail.com>
2023-09-05 17:39:39 +0100

    Fixes and refactor unit-test

--- api/letter_templates/tests/test_letter_templates.py
+++ api/letter_templates/tests/test_letter_templates.py
@@ -15,6 +15,10 @@ class TemplatesTestCase(TestCase):
                 },
                 {
                     "good": {"name": "Test Good 2"},
+                    "denial_reasons": [
+                        {"display_value": "1", "description": "Test Description 1"},
+                        {"display_value": "2", "description": "Test Description 2"},
+                    ],
                 },
             ]
         }
@@ -27,9 +31,6 @@ class TemplatesTestCase(TestCase):
         context = Context({"goods": goods_data})
         rendered_template = template.render(context)
 
-        with open(
-            "api/letter_templates/tests/templates/letter_templates/test-refusal.html", "r", encoding="utf_8"
-        ) as output_file:
-            file_content = output_file.read()
-
-        self.assertEqual(rendered_template, file_content)
+        self.assertIn("Test Good 1", rendered_template)
+        self.assertIn("Criterion 1", rendered_template)
+        self.assertIn("Test Description 1", rendered_template)
