6a49a3e420b48c5133f654dc916e3ce44a7162fc
Le H Ngo <hgle.ngo@gmail.com>
2023-09-06 13:20:23 +0100

    Updates to migration test

--- api/letter_templates/migrations/0008_refusal_letter_update.py
+++ api/letter_templates/migrations/0008_refusal_letter_update.py
@@ -4,7 +4,7 @@ from api.picklists.enums import PicklistType
 
 def update_refusal_letter_template_paragraphs(apps, schema_editor):
     PicklistItem = apps.get_model("picklists", "PicklistItem")
-    Team = Team = apps.get_model("teams", "Team")
+    Team = apps.get_model("teams", "Team")
 
     pick_list_item = PicklistItem.objects.filter(name="Refusal letter content").first()
     admin = Team.objects.get(name="Admin")
@@ -12,13 +12,9 @@ def update_refusal_letter_template_paragraphs(apps, schema_editor):
     with open("lite_content/lite_api/letter_paragraphs/refusal_letter.txt", "r", encoding="utf_8") as f:
         text = f.read()
 
-        pick_list_item, created = PicklistItem.objects.get_or_create(
+        PicklistItem.objects.update_or_create(
             team=admin, name="Refusal letter content", text=text, type=PicklistType.LETTER_PARAGRAPH
         )
-
-        if not created:
-            pick_list_item.text = text
-            pick_list_item.save()
 
 
 class Migration(migrations.Migration):
