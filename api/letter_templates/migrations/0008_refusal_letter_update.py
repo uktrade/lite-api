8e38fad75eb0bae3edb0f3e48cb2ce49b9bfdb83
Le H Ngo <hgle.ngo@gmail.com>
2023-09-08 23:09:45 +0100

    Adds defaults to migration missed that

--- api/letter_templates/migrations/0008_refusal_letter_update.py
+++ api/letter_templates/migrations/0008_refusal_letter_update.py
@@ -6,14 +6,13 @@ def update_refusal_letter_template_paragraphs(apps, schema_editor):
     PicklistItem = apps.get_model("picklists", "PicklistItem")
     Team = apps.get_model("teams", "Team")
 
-    pick_list_item = PicklistItem.objects.filter(name="Refusal letter content").first()
     admin = Team.objects.get(name="Admin")
 
     with open("lite_content/lite_api/letter_paragraphs/refusal_letter.txt", "r", encoding="utf_8") as f:
         text = f.read()
 
         PicklistItem.objects.update_or_create(
-            team=admin, name="Refusal letter content", text=text, type=PicklistType.LETTER_PARAGRAPH
+            team=admin, name="Refusal letter content", type=PicklistType.LETTER_PARAGRAPH, defaults={"text": text}
         )
 
 
