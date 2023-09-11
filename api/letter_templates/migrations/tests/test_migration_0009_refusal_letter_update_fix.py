eef0c32e0af70a60790fdd2a5f959bdcd6c8ee7a
Le H Ngo <hgle.ngo@gmail.com>
2023-09-11 14:32:05 +0100

    Addresses comments

--- api/letter_templates/migrations/tests/test_migration_0009_refusal_letter_update_fix.py
+++ api/letter_templates/migrations/tests/test_migration_0009_refusal_letter_update_fix.py
@@ -17,7 +17,7 @@ class TestChangeInformLetterAdviceType(MigratorTestCase):
             team=admin,
             name="Refusal letter content",
             type=PicklistType.LETTER_PARAGRAPH,
-            text="Dear,\r\n\r\nHaving carefully considered your application, an export licence has been refused for the goods listed in the attached schedule. If you would like further information about the decision, please contact Jeanette Rosenberg at jeanette.rosenberg@trade.gov.uk, (tel: 07917 751 668).\r\n\r\nYou may appeal against this decision, but you must do so in writing within 28 calendar days of the date of this refusal letter. In doing so you must provide argument or information that was not available to us at the time of refusal, and which could materially affect the decision to refuse.\r\n\r\nAppeal letters should be sent by email to Colin Baker at colin.baker@trade.gov.uk. If you have any questions about the progress of your appeal, please contact Colin (tel: 07391 864 815).\r\n\r\nFor licence applications refused under the Strategic Export Licensing Criteria more information is available at:\r\nhttps://questions-statements.parliament.uk/written-statements/detail/2021-12-08/hcws449.\r\n\r\nYours sincerely,\r\n\r\nExport Control Joint Unit",
+            text="Test 1",
         )
 
         PicklistItem.objects.create(
