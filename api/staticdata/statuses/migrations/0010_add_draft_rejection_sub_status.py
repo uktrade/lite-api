fdac415ab35333d1ab207c711f6ef96011853f03
Gurdeep Atwal <depsi.a@gmail.com>
2023-09-27 11:17:18 +0100

    remove migration dependency

--- api/staticdata/statuses/migrations/0010_add_draft_rejection_sub_status.py
+++ api/staticdata/statuses/migrations/0010_add_draft_rejection_sub_status.py
@@ -18,7 +18,6 @@ def add_case_sub_status_draft_rejection(apps, schema_editor):
 class Migration(migrations.Migration):
     dependencies = [
         ("statuses", "0009_update_request_received_name"),
-        ("lite_routing", "0001_initial"),
     ]
 
     operations = [
