551032e8cd97a5eaaaf6369b92fd604859020c4e
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2023-11-10 11:35:13 +0000

    Assessing officer field is no longer a nested field

--- api/search/product/views.py
+++ api/search/product/views.py
@@ -59,7 +59,6 @@ class ProductDocumentView(DocumentViewSet):
     search_nested_fields = {
         # explicitly defined to make highlighting work
         "clc": {"path": "control_list_entries", "fields": ["rating", "text", "parent"]},
-        "assessed_by": {"path": "assessed_by", "fields": ["first_name", "last_name", "email"]},
         "regime_entries": {"path": "regime_entries", "fields": ["shortened_name", "name"]},
     }
 
