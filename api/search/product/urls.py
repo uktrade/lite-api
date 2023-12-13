c112a6d6f0c9f33a51d7d4ce5291d2ea790b8ecc
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2023-12-13 14:55:09 +0000

    Remove unused product search urls

--- api/search/product/urls.py
+++ api/search/product/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
@@ -13,14 +12,4 @@ router.register(r"search", views.ProductDocumentView, basename="product_search")
 urlpatterns = [
     path("", include(router.urls)),
     path("suggest/", views.ProductSuggestDocumentView.as_view(), name="product_suggest"),
-    path("spire/<str:pk>/comment/", views.CommentView.as_view(), name="retrieve_spire_product_comment"),
-    path("lite/<uuid:pk>/comment/", views.CommentView.as_view(), name="retrieve_lite_product_comment"),
-    path("lite/<uuid:pk>/", views.RetrieveLiteProductView.as_view(), name="retrieve_lite_product"),
-    path("more-like-this/<str:pk>/", views.MoreLikeThisView.as_view(), name="more_like_this"),
-    path("more-like-this/<uuid:pk>/", views.MoreLikeThisView.as_view(), name="more_like_this"),
 ]
-
-if settings.ENABLE_SPIRE_SEARCH:
-    urlpatterns += [
-        path("spire/<str:pk>/", views.RetrieveSpireProductView.as_view(), name="retrieve_spire_product"),
-    ]
