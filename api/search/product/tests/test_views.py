9c83c2296872a57fddd1356fdf056513cb18dfb8
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2023-12-13 15:00:11 +0000

    Remove unused product search view test

--- api/search/product/tests/test_views.py
+++ api/search/product/tests/test_views.py
@@ -375,14 +375,3 @@ class ProductSearchSuggestionsTests(ProductSearchTests):
         self.assertEqual(response.status_code, 200)
 
         self.assertEqual(response.json(), expected_suggestions)
-
-
-class MoreLikeThisViewTests(DataTestClient):
-    @pytest.mark.elasticsearch
-    def test_more_like_this_404(self):
-        url = reverse("more_like_this", kwargs={"pk": "a1e4d94f-8519-4ef3-8863-e8fa17bdd685"})
-
-        response = self.client.get(url, **self.gov_headers)
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), [])
