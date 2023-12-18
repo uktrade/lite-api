de92f13592b982bdc4b6b3d9c8636841a02d3247
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2023-12-18 15:58:21 +0000

    Fix invalid query 400 response

--- api/search/product/views.py
+++ api/search/product/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.exceptions import ValidationError
 
 from django.conf import settings
 from django.db.models import Case, When
@@ -116,19 +117,10 @@ class ProductDocumentView(DocumentViewSet):
         response = self.document._index.validate_query(body=query)
         return response["valid"]
 
-    def dispatch(self, request, *args, **kwargs):
-        self.search._index = self.get_search_indexes()
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
         if not self.validate_search_terms():
-            return JsonResponse(
-                data={
-                    "error": "Invalid search string",
-                    "results": [],
-                    "count": 0,
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        return super().dispatch(request, *args, **kwargs)
+            raise ValidationError({"error": "Invalid search string"})
 
     def get_queryset(self):
         self.search._index = self.get_search_indexes()
