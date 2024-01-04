03dd14b341f8bcbcfe02bba91b5d50b8ee1a7ff3
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2024-01-04 16:47:52 +0000

    Add celery beat DB file to git ignore
    Fixup: Fix lint complaint

--- api/conf/celery.py
+++ api/conf/celery.py
@@ -3,8 +3,6 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-from celery.schedules import crontab
-
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.conf.settings")
 
