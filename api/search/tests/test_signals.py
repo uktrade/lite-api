4ea3776255abf5cea22ad5ef5082dcb64ea0c33f
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-01-17 15:29:35 +0000

    Remove background_task and related env variables
    We migrated all tasks to use celery so remove background_task app
    and related env variables

--- api/search/tests/test_signals.py
+++ api/search/tests/test_signals.py
@@ -9,13 +9,13 @@ from test_helpers.clients import DataTestClient
 
 
 class UpdateApplicationDocumentTest(DataTestClient):
-    @override_settings(BACKGROUND_TASK_ENABLED=True, LITE_API_ENABLE_ES=True)
+    @override_settings(LITE_API_ENABLE_ES=True)
     @patch("api.search.celery_tasks.registry")
     def test_standard_application(self, mock_registry):
         application = self.create_standard_application_case(self.organisation)
         mock_registry.update.assert_any_call(application.baseapplication)
 
-    @override_settings(BACKGROUND_TASK_ENABLED=True, LITE_API_ENABLE_ES=True)
+    @override_settings(LITE_API_ENABLE_ES=True)
     @patch("api.search.celery_tasks.registry")
     def test_case_assignment(self, mock_registry):
         assignment = self.create_case_assignment(
@@ -23,14 +23,14 @@ class UpdateApplicationDocumentTest(DataTestClient):
         )
         mock_registry.update.assert_any_call(assignment.case.baseapplication)
 
-    @override_settings(BACKGROUND_TASK_ENABLED=True, LITE_API_ENABLE_ES=True)
+    @override_settings(LITE_API_ENABLE_ES=True)
     @patch("api.search.celery_tasks.registry")
     def test_case(self, mock_registry):
         case = self.create_standard_application_case(self.organisation).get_case()
 
         mock_registry.update.assert_any_call(case.baseapplication)
 
-    @override_settings(BACKGROUND_TASK_ENABLED=True, LITE_API_ENABLE_ES=True)
+    @override_settings(LITE_API_ENABLE_ES=True)
     @patch("api.search.celery_tasks.registry")
     def test_good(self, mock_registry):
         application = self.create_standard_application_case(self.organisation)
@@ -41,7 +41,7 @@ class UpdateApplicationDocumentTest(DataTestClient):
 
         mock_registry.update.assert_any_call(good_on_app)
 
-    @override_settings(BACKGROUND_TASK_ENABLED=True, LITE_API_ENABLE_ES=True)
+    @override_settings(LITE_API_ENABLE_ES=True)
     @patch("api.search.celery_tasks.registry")
     def test_party(self, mock_registry):
         application = self.create_standard_application_case(self.organisation)
@@ -50,17 +50,23 @@ class UpdateApplicationDocumentTest(DataTestClient):
 
         mock_registry.update.assert_any_call(application.baseapplication)
 
-    @override_settings(BACKGROUND_TASK_ENABLED=True, LITE_API_ENABLE_ES=True)
+    @override_settings(LITE_API_ENABLE_ES=True)
     @patch("api.search.celery_tasks.registry")
     def test_organisation(self, mock_registry):
         application_organisation = self.create_standard_application_case(self.organisation)
 
         mock_registry.update.assert_any_call(application_organisation.baseapplication)
 
-    @override_settings(BACKGROUND_TASK_ENABLED=True, LITE_API_ENABLE_ES=True)
+    @override_settings(LITE_API_ENABLE_ES=True)
     @patch("api.search.celery_tasks.registry")
     def test_good_on_application_update_in_index(self, mock_registry):
         application = self.create_standard_application_case(self.organisation)
 
         for good_on_application in application.goods.all():
             mock_registry.update.assert_any_call(good_on_application)
+
+    @override_settings(LITE_API_ENABLE_ES=False)
+    @patch("api.search.celery_tasks.registry")
+    def test_standard_application_with_elasticsearch_disabled(self, mock_registry):
+        self.create_standard_application_case(self.organisation)
+        mock_registry.update.assert_not_called()
