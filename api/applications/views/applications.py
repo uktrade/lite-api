8aa25269db9d7b65a8cb2b3360f2bc02c920b7e1
Kevin Carrogan <kevin@carrogan.com>
2024-01-25 14:02:43 +0000

    safekeeping

--- api/applications/views/applications.py
+++ api/applications/views/applications.py
@@ -22,7 +22,7 @@ from rest_framework.views import APIView
 from api.appeals.models import Appeal
 from api.appeals.serializers import AppealSerializer
 from api.applications import constants
-from api.applications.creators import validate_application_ready_for_submission, _validate_agree_to_declaration
+from api.applications.creators import validate_application_ready_for_submission, validate_agree_to_declaration
 from api.applications.enums import ContractType
 from api.applications.helpers import (
     get_application_create_serializer,
@@ -407,7 +407,7 @@ class ApplicationSubmission(APIView):
             CaseTypeSubTypeEnum.EXHIBITION,
         ]:
             if request.data.get("submit_declaration"):
-                errors = _validate_agree_to_declaration(request, errors)
+                errors = validate_agree_to_declaration(request, errors)
                 if errors:
                     return JsonResponse(data={"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
