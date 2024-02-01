b0ced7df6201276811ae2bb3094df6ae322b629a
Kevin Carrogan <kevin@carrogan.com>
2024-02-01 10:45:56 +0000

    Add an endpoint to stream document data

--- api/documents/tests/test_document_stream.py
+++ api/documents/tests/test_document_stream.py
@@ -1,53 +1,91 @@
+import boto3
+
+from moto import mock_aws
+
+from django.http import StreamingHttpResponse
 from django.urls import reverse
 
-from api.documents import permissions
 from test_helpers.clients import DataTestClient
 
+from api.conf.settings import (
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION,
+    AWS_STORAGE_BUCKET_NAME,
+)
+from api.documents.libraries.s3_operations import init_s3_client
 
-class CertificateDownload(DataTestClient):
-    def test_document_detail_as_caseworker(self):
+
+@mock_aws
+class DocumentStream(DataTestClient):
+    def setUp(self):
+        super().setUp()
+        init_s3_client()
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=AWS_REGION,
+        )
+        s3.create_bucket(
+            Bucket=AWS_STORAGE_BUCKET_NAME,
+            CreateBucketConfiguration={
+                "LocationConstraint": AWS_REGION,
+            },
+        )
+        s3.put_object(
+            Bucket=AWS_STORAGE_BUCKET_NAME,
+            Key="thisisakey",
+            Body=b"test",
+        )
+
+    def test_document_stream_as_caseworker(self):
         # given there is a case document
         case = self.create_standard_application_case(self.organisation)
         document = self.create_case_document(case=case, user=self.gov_user, name="jimmy")
 
         # when a caseworker tries to access it
-        url = reverse("documents:document", kwargs={"pk": document.pk})
+        url = reverse("documents:document_stream", kwargs={"pk": document.pk})
         response = self.client.get(url, **self.gov_headers)
 
         # then they can
         self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, StreamingHttpResponse)
+        self.assertEqual(b"".join(response.streaming_content), b"test")
 
-    def test_document_detail_as_exporter(self):
+    def test_document_stream_as_exporter(self):
         # given there is a case document that is visible to the exporter
         case = self.create_standard_application_case(self.organisation)
         document = self.create_case_document(case=case, user=self.gov_user, name="jimmy")
 
         # when the exporter tries to access it
-        url = reverse("documents:document", kwargs={"pk": document.pk})
+        url = reverse("documents:document_stream", kwargs={"pk": document.pk})
         response = self.client.get(url, **self.exporter_headers)
 
         # then they can
         self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, StreamingHttpResponse)
+        self.assertEqual(b"".join(response.streaming_content), b"test")
 
-    def test_document_detail_as_exporter_on_invisible_document(self):
+    def test_document_stream_as_exporter_on_invisible_document(self):
         # givem there is a document that's invisible to the exporter
         case = self.create_standard_application_case(self.organisation)
         document = self.create_case_document(case=case, user=self.gov_user, name="jimmy", visible_to_exporter=False)
 
         # when the exporter tries to access it
-        url = reverse("documents:document", kwargs={"pk": document.pk})
+        url = reverse("documents:document_stream", kwargs={"pk": document.pk})
         response = self.client.get(url, **self.exporter_headers)
 
         # then they cannot
         self.assertEqual(response.status_code, 403)
 
-    def test_document_detail_as_illegal_exporter(self):
+    def test_document_stream_as_illegal_exporter(self):
         # given there is a case document in organisation a
         other_organisation, _ = self.create_organisation_with_exporter_user()
         case = self.create_standard_application_case(other_organisation)
         document = self.create_case_document(case=case, user=self.gov_user, name="jimmy", visible_to_exporter=False)
 
-        url = reverse("documents:document", kwargs={"pk": document.pk})
+        url = reverse("documents:document_stream", kwargs={"pk": document.pk})
 
         # when user from organisation b tries to access it
         response = self.client.get(url, **self.exporter_headers)
