0b38d2475068ff91d9a5960ac750cab86297942b
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2024-02-22 16:19:53 +0000

    Exclude DocumentData table data from pg_dump

--- bin/dump_and_anonymise.sh
+++ bin/dump_and_anonymise.sh
@@ -7,4 +7,4 @@ ln -s /home/vcap/deps/0/apt/usr/lib/postgresql/12/bin/pg_dump /home/vcap/deps/0/
 rm /home/vcap/deps/0/bin/psql
 ln -s /home/vcap/deps/0/apt/usr/lib/postgresql/12/bin/psql /home/vcap/deps/0/bin/psql
 
-python manage.py dump_and_anoymise
+python manage.py dump_and_anonymise
