b5a4a1ad1ac5fb180e8fcb06414b03b3a2613fc6
Kevin Carrogan <kevin@carrogan.com>
2024-02-28 11:28:16 +0000

    Setup test exporter user

--- start-e2e.sh
+++ start-e2e.sh
@@ -4,7 +4,7 @@ pipenv run ./manage.py migrate --noinput
 
 # Create initial users
 INTERNAL_USERS='[{"email"=>"foo@bar.gov.uk"}]' pipenv run ./manage.py seedinternalusers
-EXPORTER_USERS='[{"email"=>"foo@bar.com"}]' pipenv run ./manage.py seedexporterusers
+EXPORTER_USERS='[{"email"=>"test-exporter@example.com"}]' pipenv run ./manage.py seedexporterusers
 
 # Fill sanctions in ES
 LITE_API_ENABLE_ES=true pipenv run ./manage.py ingest_ui_test_sanctions
