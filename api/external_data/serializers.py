861383f9f1222afe3d7e7cb86431bdff912281da
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2024-04-18 11:16:31 +0100

    Update DenialEntitySerializer validation

--- api/external_data/serializers.py
+++ api/external_data/serializers.py
@@ -46,6 +46,11 @@ class DenialEntitySerializer(serializers.ModelSerializer):
         validated_data = super().validate(data)
         if validated_data.get("is_revoked") and not validated_data.get("is_revoked_comment"):
             raise serializers.ValidationError({"is_revoked_comment": "This field is required"})
+        # TODO: this uses DenialFromCSVFileOldSerializer instead of
+        # DenialFromCSVFileSerializer which is for backwards compatibility,
+        # this should be updated once lite-routing test data has been updated
+        if not (set(DenialFromCSVFileOldSerializer.required_headers)).issubset(set(validated_data.keys())):
+            raise serializers.ValidationError("Missing required fields in data")
         return validated_data
 
     def get_entity_type(self, obj):
