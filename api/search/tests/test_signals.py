44f0cd762f348f2100cc5b84ce9cb047bf90646b
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2024-04-24 14:40:14 +0100

    Make search signal testing de-coupled from custom signal implementation.

--- api/search/tests/test_signals.py
+++ api/search/tests/test_signals.py
@@ -13,66 +13,60 @@ from test_helpers.clients import DataTestClient
 
 class UpdateApplicationDocumentTest(DataTestClient):
     @override_settings(LITE_API_ENABLE_ES=True)
-    @patch("api.search.celery_tasks.registry")
-    def test_standard_application(self, mock_registry):
+    @patch("django_elasticsearch_dsl.registries.registry.update")
+    def test_standard_application(self, mock_registry_update):
         application = self.create_standard_application_case(self.organisation)
-        mock_registry.update.assert_any_call(application.baseapplication)
+        mock_registry_update.assert_any_call(application.baseapplication)
 
     @override_settings(LITE_API_ENABLE_ES=True)
-    @patch("api.search.celery_tasks.registry")
-    def test_case_assignment(self, mock_registry):
+    @patch("django_elasticsearch_dsl.registries.registry.update")
+    def test_case_assignment(self, mock_registry_update):
         assignment = self.create_case_assignment(
             self.queue, self.create_standard_application_case(self.organisation), self.gov_user
         )
-        mock_registry.update.assert_any_call(assignment.case.baseapplication)
+        mock_registry_update.assert_any_call(assignment.case.baseapplication)
 
     @override_settings(LITE_API_ENABLE_ES=True)
-    @patch("api.search.celery_tasks.registry")
-    def test_case(self, mock_registry):
+    @patch("django_elasticsearch_dsl.registries.registry.update")
+    def test_case(self, mock_registry_update):
         case = self.create_standard_application_case(self.organisation).get_case()
 
-        mock_registry.update.assert_any_call(case.baseapplication)
+        mock_registry_update.assert_any_call(case.baseapplication)
 
     @override_settings(LITE_API_ENABLE_ES=True)
-    @patch("api.search.celery_tasks.registry")
-    def test_good(self, mock_registry):
+    @patch("django_elasticsearch_dsl.registries.registry.update")
+    def test_good(self, mock_registry_update):
         application = self.create_standard_application_case(self.organisation)
         good = GoodFactory(organisation=self.organisation)
         good_on_app = self.create_good_on_application(application, good)
         application.goods.add(good_on_app)
         application.save()
 
-        mock_registry.update.assert_any_call(good_on_app)
+        mock_registry_update.assert_any_call(good_on_app)
 
     @override_settings(LITE_API_ENABLE_ES=True)
-    @patch("api.search.celery_tasks.registry")
-    def test_party(self, mock_registry):
+    @patch("django_elasticsearch_dsl.registries.registry.update")
+    def test_party(self, mock_registry_update):
         application = self.create_standard_application_case(self.organisation)
         party = self.create_party("test party", self.organisation, PartyType.END_USER, application=application)
         party.save()
 
-        mock_registry.update.assert_any_call(application.baseapplication)
+        mock_registry_update.assert_any_call(application.baseapplication)
 
     @override_settings(LITE_API_ENABLE_ES=True)
-    @patch("api.search.celery_tasks.registry")
-    def test_organisation(self, mock_registry):
+    @patch("django_elasticsearch_dsl.registries.registry.update")
+    def test_organisation(self, mock_registry_update):
         application_organisation = self.create_standard_application_case(self.organisation)
 
-        mock_registry.update.assert_any_call(application_organisation.baseapplication)
+        mock_registry_update.assert_any_call(application_organisation.baseapplication)
 
     @override_settings(LITE_API_ENABLE_ES=True)
-    @patch("api.search.celery_tasks.registry")
-    def test_good_on_application_update_in_index(self, mock_registry):
+    @patch("django_elasticsearch_dsl.registries.registry.update")
+    def test_good_on_application_update_in_index(self, mock_registry_update):
         application = self.create_standard_application_case(self.organisation)
 
         for good_on_application in application.goods.all():
-            mock_registry.update.assert_any_call(good_on_application)
-
-    @override_settings(LITE_API_ENABLE_ES=False)
-    @patch("api.search.celery_tasks.registry")
-    def test_standard_application_with_es_disabled(self, mock_registry):
-        self.create_standard_application_case(self.organisation)
-        mock_registry.update.assert_not_called()
+            mock_registry_update.assert_any_call(good_on_application)
 
 
 class ESDSLSignalProcessorTest(DataTestClient):
