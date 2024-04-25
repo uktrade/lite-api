7de64b46a5e9013ddb5fc550194139204b032c8f
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-04-25 16:05:47 +0100

    stripping out entity_type_value model

--- api/external_data/serializers.py
+++ api/external_data/serializers.py
@@ -9,29 +9,6 @@ from rest_framework import serializers
 from api.external_data import documents, models
 from api.external_data.helpers import get_denial_entity_type
 from api.flags.enums import SystemFlags
-from api.external_data.enums import DenialEntityType
-
-
-def get_denial_entity_type_value(data):
-
-    if isinstance(data, dict):
-        entity_type = ""
-        normalised_entity_type_dict = {keys.lower(): values.lower() for keys, values in data.items()}
-
-        is_end_user_flag = normalised_entity_type_dict.get("end_user_flag", "false") == "true"
-        is_consignee_flag = normalised_entity_type_dict.get("consignee_flag", "false") == "true"
-        is_other_role = len(normalised_entity_type_dict.get("other_role", "")) > 0
-
-        if is_end_user_flag and is_consignee_flag:
-            entity_type = DenialEntityType.END_USER
-        elif not is_end_user_flag and is_consignee_flag:
-            entity_type = DenialEntityType.CONSIGNEE
-        elif is_end_user_flag and not is_consignee_flag:
-            entity_type = DenialEntityType.END_USER
-        elif not is_end_user_flag and not is_consignee_flag and is_other_role:
-            entity_type = DenialEntityType.THIRD_PARTY
-
-        return entity_type
 
 
 class EntityTypeSerializerField(serializers.Field):
