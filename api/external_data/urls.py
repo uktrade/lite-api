fb2f7282d74566a7b16b122446670c64b9c69675
Tomos Williams <tllewwilliams@gmail.com>
2024-05-23 15:10:17 +0100

    temp commit to show potential direction

--- api/external_data/urls.py
+++ api/external_data/urls.py
@@ -8,10 +8,10 @@ app_name = "external_data"
 router = DefaultRouter()
 router.register("denial", views.DenialViewSet, basename="denial")
 router.register("sanction", views.SanctionViewSet, basename="sanction")
-router.register("denial-search", views.DenialSearchView, basename="denial_search")
 
 
 urlpatterns = [
     path("", include(router.urls)),
     path("sanction-search", views.SanctionSearchView.as_view(), name="sanction-search"),
+    path("denial-search/", views.DenialSearchView.as_view(), name="denial_search"),
 ]
--- api/external_data/views.py
+++ api/external_data/views.py
@@ -1,4 +1,4 @@
-from elasticsearch_dsl import Search
+from elasticsearch_dsl import Search, Q
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -35,53 +35,68 @@ class SanctionViewSet(viewsets.ModelViewSet):
         pass
 
 
-class DenialSearchView(DocumentViewSet):
-    document = documents.DenialEntityDocument
-    serializer_class = serializers.DenialSearchSerializer
+class ElasticApiView(APIView):
     authentication_classes = (GovAuthentication,)
-    pagination_class = MaxPageNumberPagination
-    lookup_field = "id"
-    filter_backends = [
-        filter_backends.SearchFilterBackend,
-        filter_backends.SourceBackend,
-        filter_backends.FilteringFilterBackend,
-        filter_backends.HighlightBackend,
-    ]
-    search_fields = ["name", "address", "item_list_codes"]
-    filter_fields = {
-        "country": {
-            "enabled": True,
-            "field": "country.raw",
-        }
-    }
-    ordering = "_score"
-    highlight_fields = {
-        "name": {
-            "enabled": True,
-            "options": {
-                "pre_tags": ["<mark>"],
-                "post_tags": ["</mark>"],
-            },
-        },
-        "address": {
-            "enabled": True,
-            "options": {
-                "pre_tags": ["<mark>"],
-                "post_tags": ["</mark>"],
-            },
-        },
-        "item_list_codes": {
-            "enabled": True,
-            "options": {
-                "pre_tags": ["<mark>"],
-                "post_tags": ["</mark>"],
-            },
-        },
-    }
+    elastic_index = settings.ELASTICSEARCH_DENIALS_INDEX_ALIAS
+    search_keys = []
 
-    def filter_queryset(self, queryset):
-        queryset = queryset.filter("term", is_revoked=False).exclude("term", notifying_government="United Kingdom")
-        return super().filter_queryset(queryset)
+    def parse_search(self, search_parameters):
+        search_dict = {"country": search_parameters.get("country", ""), "page": search_parameters.get("page", "")}
+        search_params = search_parameters.getlist("search")
+        for item in search_params:
+            key, value = item.split(":")
+            if search_dict.get(key):
+                search_dict[key].append(value)
+            else:
+                self.search_keys.append(key)
+                search_dict[key] = [value]
+        return search_dict
+
+    def get_pagination(self, page):
+        results_per_page = settings.STREAM_PAGE_SIZE
+        end = results_per_page * int(page)
+        start = end - results_per_page
+        return start, end
+
+    def get_query(self, params, search_keys):
+        search_value = []
+        for key in search_keys:
+            if params.get(key):
+                for values in params[key]:
+                    search_value.append({"match": {key: {"query": values}}})
+
+        return Q(
+            "bool",
+            filter=[
+                Q("term", is_revoked=False),
+                Q("bool", must_not=Q("term", notifying_government="United Kingdom")),
+                {"terms": {"country.raw": [params["country"]]}},
+            ],
+            should=search_value,
+            minimum_should_match=1,
+        )
+
+    def results_json_parseable(self, results):
+        return [result.to_dict() for result in results]
+
+    def get(self, request):
+        search_params = self.parse_search(request.GET)
+        start, end = self.get_pagination(search_params["page"])
+        search = Search(index=self.elastic_index)
+        query = self.get_query(search_params, self.search_keys)
+        results = search.query(query).execute()
+        final_results = self.results_json_parseable(results[start:end])
+
+        response = {
+            "results": final_results,
+            "total_pages": len(results) / settings.STREAM_PAGE_SIZE,
+            "count": len(final_results),
+        }
+        return Response(response)
+
+
+class DenialSearchView(ElasticApiView):
+    elastic_index = settings.ELASTICSEARCH_DENIALS_INDEX_ALIAS
 
 
 class SanctionSearchView(APIView):
