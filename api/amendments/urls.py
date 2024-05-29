59d8f713744c57fb7ee9eef2454b90a654252a07
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-05-29 16:43:27 +0100

    Add endpoint stub to create a copy of given application

--- api/amendments/urls.py
+++ api/amendments/urls.py
@@ -5,5 +5,5 @@ from api.amendments import views
 app_name = "amendments"
 
 urlpatterns = [
-
+    path("create-application-copy/<uuid:case_pk>/", views.CreateApplicationCopyView.as_view(), name="create_application_copy"),
 ]
--- api/amendments/views.py
+++ api/amendments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import generics
+from rest_framework import status
 
-# Create your views here.
+from api.applications.models import StandardApplication
+from api.core.authentication import ExporterAuthentication
+
+
+class CreateApplicationCopyView(generics.CreateAPIView):
+    authentication_classes = (ExporterAuthentication,)
+
+    def post(self, request, pk):
+        try:
+            application = StandardApplication.objects.get(id=pk)
+        except StandardApplication.DoesNotExist:
+            return JsonResponse(
+                data={"errors": {"application": "Invalid Standard application"}},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return JsonResponse(data={}, status=status.HTTP_201_CREATED)
