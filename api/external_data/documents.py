3d4511d38257de424b2ba6c8712aa5b0c39179dc
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-06-07 09:57:44 +0100

    change to standard tokenizer to break up search terms based on standard algorithm

--- api/external_data/documents.py
+++ api/external_data/documents.py
@@ -55,13 +55,13 @@ address_stop_words_filter = analysis.token_filter(
 
 address_analyzer = analysis.analyzer(
     "address_analyzer",
-    tokenizer="whitespace",
+    tokenizer="standard",
     filter=["lowercase", "asciifolding", "trim", address_stop_words_filter, ngram_filter],
 )
 
 address_analyzer_no_ngram = analysis.analyzer(
     "address_analyzer",
-    tokenizer="whitespace",
+    tokenizer="standard",
     filter=["lowercase", "asciifolding", "trim", address_stop_words_filter],
 )
 
