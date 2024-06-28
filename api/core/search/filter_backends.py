b53987ea940cb2f9d2bd97700eda1b97022c7695
Gurdeep Atwal <depsi.a@gmail.com>
2024-06-28 15:27:48 +0100

    fix forward slash searching

--- api/core/search/filter_backends.py
+++ api/core/search/filter_backends.py
@@ -13,6 +13,20 @@ class QueryStringSearchFilterBackend(BaseSearchFilterBackend):
 
     search_param = "search"
 
+    def get_search_query_params(self, request):
+        """Get search query params.
+
+        :param request: Django REST framework request.
+        :type request: rest_framework.request.Request
+        :return: List of search query params.
+        :rtype: list
+        """
+        query_params = request.query_params.copy()
+        # This is required as query_string is unable to handle a single /
+        query_string = query_params.get(self.search_param, "").replace("/", "//")
+        query_params[self.search_param] = query_string
+        return query_params.getlist(self.search_param, [])
+
     query_backends = [
         QueryStringQueryBackend,
     ]
