7b71d4c504bf690b83ea8b16d2b2bb766f205be8
Kevin Carrogan <kevin@carrogan.com>
2024-07-01 16:20:20 +0100

    fixup

--- api/applications/tests/test_audit_trail.py
+++ api/applications/tests/test_audit_trail.py
@@ -1,15 +1,22 @@
+from api.audit_trail.models import Audit
 from api.audit_trail.tests.factories import AuditFactory
+
 from test_helpers.clients import DataTestClient
 
 
 class GoodOnApplicationAuditTrailTests(DataTestClient):
     def test_removing_object_keeps_audit_trail(self):
         application = self.create_draft_standard_application(self.organisation)
+        Audit.objects.all().delete()
+
         good_on_application = application.goods.first()
         audit = AuditFactory(
             action_object=good_on_application,
         )
+        self.assertEqual(Audit.objects.count(), 1)
 
         good_on_application.delete()
-        audit.refresh_from_db()
+
+        self.assertEqual(Audit.objects.count(), 1)
+        audit = Audit.objects.get(pk=audit.pk)
         self.assertIsNone(audit.action_object)
