c50b69e869aa2b78294850ccf2ef84b87ad0c8b4
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-01 15:04:07 +0100

    Update application for DBT Platform

---
+++ .copilot/phases/pre_build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+git_clone_base_url="https://codestar-connections.eu-west-2.amazonaws.com/git-http/730335529260/eu-west-2/192881c6-e3f2-41a9-9dcb-fcc87d8b90be/uktrade"
+
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+
+cat <<EOF > ./.gitmodules
+[submodule "lite-content"]
+	path = lite_content
+	url = $git_clone_base_url/lite-content.git
+	branch = master
+[submodule "lite_routing"]
+	path = lite_routing
+	url = $git_clone_base_url/lite-routing.git
+	branch = main
+[submodule "django_db_anonymiser"]
+	path = django_db_anonymiser
+	url = $git_clone_base_url/django-db-anonymiser.git
+EOF
+
+git submodule update --init --remote --recursive
