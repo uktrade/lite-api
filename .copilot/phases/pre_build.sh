8e81e1e67b79037a6ee299b0eb40a6134607d7f0
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-02 16:40:48 +0100

    Overwrite Github URLs to go through CodeStar

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -3,4 +3,13 @@
 # Exit early if something goes wrong
 set -e
 
-# Add commands below to run as part of the pre_build phase
+echo -e "\nGet CodeStar connection details"
+codestar_connections=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']")
+aws_account=$(echo "$codestar_connections" | jq -r ".[0].OwnerAccountId")
+codestar_arn=$(echo "$codestar_connections" | jq -r ".[0].ConnectionArn")
+codestar_connection_id=$(echo "${codestar_arn##*/}")
+
+export GIT_CLONE_BASE_URL="https://codestar-connections.eu-west-2.amazonaws.com/git-http/$aws_account/eu-west-2/$codestar_connection_id/uktrade"
+
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
