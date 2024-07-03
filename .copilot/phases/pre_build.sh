8e42fa1caea1f54e6bfd2ac0b7be0d6108e482ab
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-07-03 10:43:16 +0100

    Hardcode CodeStar connection ID
    The pipeline isn't authorised to list the codestar connections.
    Hardcode this until a more permanent solution is in place

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -3,13 +3,7 @@
 # Exit early if something goes wrong
 set -e
 
-echo -e "\nGet CodeStar connection details"
-codestar_connections=$(aws codestar-connections list-connections --provider-type GitHub --query "Connections[? ConnectionStatus == 'AVAILABLE']")
-aws_account=$(echo "$codestar_connections" | jq -r ".[0].OwnerAccountId")
-codestar_arn=$(echo "$codestar_connections" | jq -r ".[0].ConnectionArn")
-codestar_connection_id=$(echo "${codestar_arn##*/}")
-
-export GIT_CLONE_BASE_URL="https://codestar-connections.eu-west-2.amazonaws.com/git-http/$aws_account/eu-west-2/$codestar_connection_id/uktrade"
+export GIT_CLONE_BASE_URL="https://codestar-connections.eu-west-2.amazonaws.com/git-http/730335529260/eu-west-2/192881c6-e3f2-41a9-9dcb-fcc87d8b90be/uktrade"
 
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
