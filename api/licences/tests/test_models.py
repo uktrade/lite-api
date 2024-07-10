bed28277f2dd96dc032303b3d3a313d6c73b4df2
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-07-10 15:23:59 +0100

    updated to use StandardLicenceFactory

--- api/licences/tests/test_models.py
+++ api/licences/tests/test_models.py
@@ -1,6 +1,5 @@
-from django.utils import timezone
 from test_helpers.clients import DataTestClient
-from api.licences.models import Licence
+from api.licences.tests.factories import StandardLicenceFactory
 from api.licences.enums import LicenceStatus, HMRCIntegrationActionEnum, licence_status_to_hmrc_integration_action
 
 
@@ -10,15 +9,7 @@ class LicenceStatusTests(DataTestClient):
         self.standard_application = self.create_standard_application_case(self.organisation)
 
     def test_suspend_licence_cancel_message_not_sent_to_hmrc(self):
-
-        licence = Licence.objects.create(
-            case=self.standard_application,
-            status=LicenceStatus.ISSUED,
-            reference_code="GBSIEL/2024/0000001/P",
-            start_date=timezone.now().date(),
-            duration=10,
-        )
-
+        licence = StandardLicenceFactory(case=self.standard_application, status=LicenceStatus.ISSUED)
         licence.suspend()
 
         assert licence.status == LicenceStatus.SUSPENDED
