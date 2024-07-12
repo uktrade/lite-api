cb5bc7947a36b1b805907018cf05d61418a4b748
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-07-12 09:14:55 +0100

    remove validation from serializer and add protection in send_licence_details_to_lite_hmrc function

--- api/licences/celery_tasks.py
+++ api/licences/celery_tasks.py
@@ -45,6 +45,9 @@ def send_licence_details_to_lite_hmrc(licence_id, action):
 
 def schedule_licence_details_to_lite_hmrc(licence_id, action):
     licence = Licence.objects.get(id=licence_id)
+    if licence.status == LicenceStatus.SUSPENDED and action == HMRCIntegrationActionEnum.CANCEL:
+        logger.info("Licence %s is suspended, skipping sending details to lite-hmrc", licence.reference_code)
+        return
     if (
         licence.status == LicenceStatus.ISSUED
         and action == HMRCIntegrationActionEnum.INSERT
