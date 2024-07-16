7b7f5887dfcc80a1d11cacb689e7f2a0d1ef748b
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-07-16 16:35:28 +0100

    new exporter email for suspended status structure put in place

--- api/cases/notify.py
+++ api/cases/notify.py
@@ -12,6 +12,7 @@ from gov_notify.payloads import (
     ExporterLicenceRevoked,
     ExporterInformLetter,
     ExporterAppealAcknowledgement,
+    ExporterLicenceSuspended,
 )
 from gov_notify.service import send_email
 
@@ -73,6 +74,28 @@ def notify_exporter_licence_revoked(licence):
     exporter = licence.case.submitted_by
     case = licence.case.get_case()
     _notify_exporter_licence_revoked(
+        exporter.email,
+        {
+            "user_first_name": exporter.first_name,
+            "application_reference": case.reference_code,
+            "exporter_frontend_url": get_exporter_frontend_url("/"),
+        },
+    )
+
+
+def _notify_exporter_licence_suspended(email, data):
+    payload = ExporterLicenceSuspended(**data)
+    send_email(
+        email,
+        TemplateType.EXPORTER_LICENCE_SUSPENDED,
+        payload,
+    )
+
+
+def notify_exporter_licence_suspended(licence):
+    exporter = licence.case.submitted_by
+    case = licence.case.get_case()
+    _notify_exporter_licence_suspended(
         exporter.email,
         {
             "user_first_name": exporter.first_name,
