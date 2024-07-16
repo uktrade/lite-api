796f2de0643e99687ff05dab5addd90d86058ea7
Gareth Pitt-Nash <gareth.pitt-nash@digital.trade.gov.uk>
2024-07-16 10:36:21 +0100

    chore: remove commented out healthcheck

--- api/conf/settings.py
+++ api/conf/settings.py
@@ -124,7 +124,6 @@ INSTALLED_APPS = [
     "api.external_data",
     "api.support",
     "health_check",
-    #"health_check.cache",
     "health_check.contrib.celery",
     "health_check.contrib.celery_ping",
     "django_audit_log_middleware",
