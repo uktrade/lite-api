a158b3408728c7d571e3c1571065ee7f2863de81
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2024-07-25 15:08:25 +0100

    Add CaseInCaseworkerOperableStatus permission class

--- api/staticdata/statuses/tests/test_enums.py
+++ api/staticdata/statuses/tests/test_enums.py
@@ -3,16 +3,12 @@ from parameterized import parameterized
 from api.staticdata.statuses.enums import CaseStatusEnum
 
 
-CASEWORKER_OPERABLE_STATUSES = CaseStatusEnum.caseworker_operable_statuses()
-CASEWORKER_INOPERABLE_STATUSES = list(set(CaseStatusEnum.all()) - set(CaseStatusEnum.caseworker_operable_statuses()))
-
-
 class TestCaseStatusEnum:
 
-    @parameterized.expand(CASEWORKER_OPERABLE_STATUSES)
+    @parameterized.expand(CaseStatusEnum.caseworker_operable_statuses())
     def test_is_caseworker_operable_operable_status_status_operable(self, status):
         assert CaseStatusEnum.is_caseworker_operable(status) is True
 
-    @parameterized.expand(CASEWORKER_INOPERABLE_STATUSES)
+    @parameterized.expand(CaseStatusEnum.caseworker_inoperable_statuses())
     def test_is_caseworker_operable_operable_status_status_inoperable(self, status):
         assert CaseStatusEnum.is_caseworker_operable(status) is False
--- api/staticdata/statuses/tests/test_models.py
+++ api/staticdata/statuses/tests/test_models.py
@@ -6,18 +6,14 @@ from api.staticdata.statuses.models import CaseStatus
 from test_helpers.clients import DataTestClient
 
 
-CASEWORKER_OPERABLE_STATUSES = CaseStatusEnum.caseworker_operable_statuses()
-CASEWORKER_INOPERABLE_STATUSES = list(set(CaseStatusEnum.all()) - set(CaseStatusEnum.caseworker_operable_statuses()))
-
-
 class TestCaseStatus(DataTestClient):
 
-    @parameterized.expand(CASEWORKER_OPERABLE_STATUSES)
+    @parameterized.expand(CaseStatusEnum.caseworker_operable_statuses())
     def test_is_caseworker_operable_operable_status_status_operable(self, status):
         status_record = CaseStatus.objects.get(status=status)
         assert status_record.is_caseworker_operable is True
 
-    @parameterized.expand(CASEWORKER_INOPERABLE_STATUSES)
+    @parameterized.expand(CaseStatusEnum.caseworker_inoperable_statuses())
     def test_is_caseworker_operable_operable_status_status_inoperable(self, status):
         status_record = CaseStatus.objects.get(status=status)
         assert status_record.is_caseworker_operable is False
