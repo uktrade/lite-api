44600ac8e4a7de052a7e5a20cc67101415a2aa55
Kevin Carrogan <kevin@carrogan.com>
2024-07-29 14:52:14 +0100

    Move around v2 views

--- api/data_workspace/v2/urls.py
+++ api/data_workspace/v2/urls.py
@@ -1,16 +1,16 @@
 from rest_framework.routers import DefaultRouter
 
-from api.data_workspace.v2.views import application_views
+from api.data_workspace.v2 import views
 
 
 router_v2 = DefaultRouter()
 router_v2.register(
     "applications",
-    application_views.ApplicationListView,
+    views.ApplicationListView,
     basename="dw-applications",
 )
 router_v2.register(
     "rfis",
-    application_views.RFIListView,
+    views.RFIListView,
     basename="dw-rfis",
 )
