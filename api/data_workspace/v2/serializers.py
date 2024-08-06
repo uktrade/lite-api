65d0cc7eb0690eceda1aaf70dc3a13ee1cc267a2
Kevin Carrogan <kevin@carrogan.com>
2024-08-06 07:45:07 +0100

    Update application serializer to match v2 data model

--- api/data_workspace/v2/serializers.py
+++ api/data_workspace/v2/serializers.py
@@ -18,40 +18,14 @@ def get_last_application(obj):
 
 class ApplicationSerializer(serializers.ModelSerializer):
     id = serializers.SerializerMethodField(required=False)
-    created_at = serializers.SerializerMethodField(required=False)
-    submitted_at = serializers.SerializerMethodField(required=False)
-    closed_at = serializers.SerializerMethodField(required=False)
-    closed_status = serializers.SerializerMethodField(required=False)
 
     class Meta:
         model = StandardApplication
-        fields = (
-            "id",
-            "created_at",
-            "submitted_at",
-            "closed_at",
-            "closed_status",
-        )
+        fields = ("id",)
 
     def get_id(self, application):
-        application = get_last_application(application)
+        application = get_original_application(application)
         return application.pk
-
-    def get_created_at(self, application):
-        application = get_original_application(application)
-        return application.created_at
-
-    def get_submitted_at(self, application):
-        application = get_original_application(application)
-        return application.first_submitted_at
-
-    def get_closed_at(self, application):
-        application = get_last_application(application)
-        return application.closed_at
-
-    def get_closed_status(self, application):
-        application = get_last_application(application)
-        return application.closed_status
 
 
 class RFISerializer(serializers.ModelSerializer):
@@ -68,7 +42,7 @@ class RFISerializer(serializers.ModelSerializer):
         )
 
     def get_application_id(self, rfi):
-        return get_last_application(rfi.case).pk
+        return get_original_application(rfi.case).pk
 
     def get_closed_at(self, rfi):
         return rfi.responded_at
