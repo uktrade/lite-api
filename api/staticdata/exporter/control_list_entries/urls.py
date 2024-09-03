849c9a414c09707a1704edda5e82d94ccf89b3ef
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2024-09-03 16:49:07 +0100

    Reformat list

--- api/staticdata/exporter/control_list_entries/urls.py
+++ api/staticdata/exporter/control_list_entries/urls.py
@@ -4,4 +4,6 @@ from api.staticdata.exporter.control_list_entries import views
 
 app_name = "control_list_entries"
 
-urlpatterns = [path("", views.ControlListEntriesList.as_view(), name="control_list_entries")]
+urlpatterns = [
+    path("", views.ControlListEntriesList.as_view(), name="control_list_entries"),
+]
