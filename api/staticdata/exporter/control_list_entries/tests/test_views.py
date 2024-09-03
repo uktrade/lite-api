f4eafa126c6c3a38de85d6674bc4d3f810f131a3
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2024-09-03 16:25:38 +0100

    Add test for exact response

--- api/staticdata/exporter/control_list_entries/tests/test_views.py
+++ api/staticdata/exporter/control_list_entries/tests/test_views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.reverse import reverse
 
+from api.staticdata.control_list_entries.models import ControlListEntry
+from api.staticdata.control_list_entries.factories import ControlListEntriesFactory
 from test_helpers.clients import DataTestClient
 
 
@@ -14,6 +16,8 @@ class ControlListEntriesListTests(DataTestClient):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+        self.assertTrue(len(response.json()) > 0)
+
         for cle in response.json():
             self.assertEqual(list(cle.keys()), ["rating", "text"])
 
@@ -21,3 +25,24 @@ class ControlListEntriesListTests(DataTestClient):
         response = self.client.get(self.url, **self.gov_headers)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_view_success_exact_response(self):
+        # Set up empty CLE db table for this test only
+        ControlListEntry.objects.all().delete()
+
+        cle_1 = ControlListEntriesFactory(rating="ABC123", controlled=True)
+        cle_2 = ControlListEntriesFactory(rating="1Z101", controlled=True)
+        cle_3 = ControlListEntriesFactory(rating="ZXYW", controlled=True)
+
+        response = self.client.get(self.url, **self.exporter_headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(
+            response.json(),
+            [
+                {"rating": cle_1.rating, "text": cle_1.text},
+                {"rating": cle_2.rating, "text": cle_2.text},
+                {"rating": cle_3.rating, "text": cle_3.text},
+            ],
+        )
