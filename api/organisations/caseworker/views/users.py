d3f5a544929b274b4fec848db91023941d402f46
Gurdeep Atwal <depsi.a@gmail.com>
2024-09-17 16:15:42 +0100

    tmp

--- api/organisations/caseworker/views/users.py
+++ api/organisations/caseworker/views/users.py
@@ -1,13 +1,11 @@
-from uuid import UUID
-from django.http import JsonResponse
 from api.core.authentication import GovAuthentication
 from api.core.permissions import CanCaseworkersManageOrgainsation
 from api.organisations.models import Organisation
 
 from api.organisations.caseworker.serializers.serializers import ExporterUserCreateSerializer
 
-from rest_framework import status
 from rest_framework.generics import CreateAPIView
+from django.http import Http404
 
 
 class CreateExporterUser(CreateAPIView):
@@ -16,24 +14,17 @@ class CreateExporterUser(CreateAPIView):
     serializer_class = ExporterUserCreateSerializer
     permission_classes = [CanCaseworkersManageOrgainsation]
 
-    def post(self, request, org_pk):
-        """
-        Caseworker View to Create Exporter
-        """
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
 
-        data = request.data
+        try:
+            self.organisation = Organisation.objects.get(pk=self.kwargs["org_pk"])
+        except Organisation.DoesNotExist:
+            raise Http404()
 
-        data["organisation"] = org_pk
-        data["role"] = UUID(data["role"])
-        serializer = self.serializer_class(data=data)
-
-        if not serializer.is_valid():
-            return JsonResponse(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        serializer.save()
-        response_data = serializer.data
-        orgainsation = Organisation.objects.get(id=org_pk)
-        orgainsation.notify_exporter_user_added(serializer.validated_data["email"])
-        orgainsation.add_case_note_add_export_user(request.user, data["sites"], serializer.validated_data["email"])
-
-        return JsonResponse(data=response_data, status=status.HTTP_201_CREATED)
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        self.orgainsation.notify_exporter_user_added(serializer.validated_data["email"])
+        self.orgainsation.add_case_note_add_export_user(
+            self.request.user, serializer.data["sites"], serializer.validated_data["email"]
+        )
