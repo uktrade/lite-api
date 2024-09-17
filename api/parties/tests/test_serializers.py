9e6be5756bf6d0d568f83883b3659f675b8fa8af
Tomos Williams <tllewwilliams@gmail.com>
2024-09-17 14:38:45 +0100

    new serializer and tests for party.name

--- api/parties/tests/test_serializers.py
+++ api/parties/tests/test_serializers.py
@@ -92,3 +92,55 @@ class TestPartySerializer(DataTestClient):
             str(address_errors[0]),
             error_message,
         )
+
+    @parameterized.expand(
+        [
+            "random name",
+            "party-name",
+            "party!name",
+            "party-!.<>/%&*;+'(),.name",
+        ]
+    )
+    def test_validate_party_name_valid(self, name):
+        serializer = PartySerializer(
+            data={"name": name},
+            partial=True,
+        )
+        self.assertTrue(serializer.is_valid())
+
+    @parameterized.expand(
+        [
+            (
+                "party\aname",
+                "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes",
+            ),
+            (
+                "party-\wname",
+                "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes",
+            ),
+            (
+                "party_name",
+                "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes",
+            ),
+            (
+                "party$name",
+                "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes",
+            ),
+            (
+                "party@name",
+                "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes",
+            ),
+        ]
+    )
+    def test_party_name_invalid(self, name, error_message):
+        serializer = PartySerializer(
+            data={"name": name},
+            partial=True,
+        )
+        self.assertFalse(serializer.is_valid())
+        serializer_error = serializer.errors["name"]
+        self.assertEqual(len(serializer_error), 1)
+        self.assertEqual(
+            str(serializer_error[0]),
+            error_message,
+        )
