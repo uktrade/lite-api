d5fc14e4dfa0c2c3fcad95fec3f1fee9c15da0a7
Tomos Williams <tllewwilliams@gmail.com>
2024-09-19 15:43:47 +0100

    testing view

--- api/applications/views/tests/test_parties.py
+++ api/applications/views/tests/test_parties.py
@@ -1,3 +1,4 @@
+from parameterized import parameterized
 from django.urls import reverse
 from rest_framework import status
 
@@ -72,3 +73,117 @@ class TestApplicationPartyView(DataTestClient):
                     ]
                 },
             )
+
+
+class TestApplicationPartyViewValues(DataTestClient):
+
+    def setUp(self):
+        super().setUp()
+        self.application = StandardApplicationFactory(organisation=self.organisation)
+        self.case_statuses = [
+            case_status.status
+            for case_status in CaseStatus.objects.all()
+            if case_status.status not in ["draft", "applicant_editing"]
+        ]
+        self.party_on_application = PartyOnApplicationFactory(application=self.application)
+
+    @parameterized.expand(
+        [
+            (
+                {"name": "end user", "address": "1 Example Street"},
+                False,
+                {},
+            ),
+            (
+                {"name": "end/!user", "address": "1 Example Street"},
+                False,
+                {},
+            ),
+            (
+                {"name": "end-user", "address": "1 Example Street"},
+                False,
+                {},
+            ),
+            (
+                {"name": "end-user", "address": "1 \r\nExample Street"},
+                False,
+                {},
+            ),
+            (
+                {"name": "end_user", "address": "1 Example Street"},
+                True,
+                {
+                    "name": [
+                        "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes"
+                    ]
+                },
+            ),
+            (
+                {"name": "end\auser", "address": "1 Example Street"},
+                True,
+                {
+                    "name": [
+                        "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes"
+                    ]
+                },
+            ),
+            (
+                {"name": "end£user", "address": "1 Example Street"},
+                True,
+                {
+                    "name": [
+                        "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes"
+                    ]
+                },
+            ),
+            (
+                {"name": "end user", "address": "1_Example Street"},
+                True,
+                {
+                    "address": [
+                        "Address must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes"
+                    ]
+                },
+            ),
+            (
+                {"name": "end user", "address": "1\aExample Street"},
+                True,
+                {
+                    "address": [
+                        "Address must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes"
+                    ]
+                },
+            ),
+            (
+                {"name": "end_user", "address": "1\aExample Street"},
+                True,
+                {
+                    "name": [
+                        "Party name must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes"
+                    ],
+                    "address": [
+                        "Address must only include letters, numbers, and common special characters such as hyphens, brackets and apostrophes"
+                    ],
+                },
+            ),
+        ]
+    )
+    def test_party_post(self, data, error, error_message):
+        data["country"] = {"id": "FR", "name": "France"}
+        self.url = reverse(
+            "applications:party",
+            kwargs={"pk": str(self.application.pk), "party_pk": str(self.party_on_application.party.pk)},
+        )
+        party = Party.objects.get(id=self.party_on_application.party.pk)
+        versions = Version.objects.get_for_object(party)
+        self.assertEqual(versions.count(), 0)
+
+        self.application.status = CaseStatus.objects.get(status="draft")
+        self.application.save()
+
+        response = self.client.put(self.url, **self.exporter_headers, data=data)
+        if error:
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.assertEqual(response.json()["errors"], error_message)
+        else:
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
