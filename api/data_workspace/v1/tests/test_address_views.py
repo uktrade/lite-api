b4bfe6dd12b1fd207c935997a971a466b69c5ee6
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2024-10-07 14:06:26 +0100

    test update

--- api/data_workspace/v1/tests/test_address_views.py
+++ api/data_workspace/v1/tests/test_address_views.py
@@ -6,7 +6,16 @@ from test_helpers.clients import DataTestClient
 class AddressDataWorkspaceTests(DataTestClient):
     def test_addresses(self):
         url = reverse("data_workspace:v1:dw-address-list")
-        expected_fields = {"id", "address_line_1", "address_line_2", "city", "region", "postcode", "country"}
+        expected_fields = {
+            "id",
+            "address_line_1",
+            "address_line_2",
+            "address_line_3",
+            "city",
+            "region",
+            "postcode",
+            "country",
+        }
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
