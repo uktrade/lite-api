ab31298ef09a9f401f0e2b1bfea08c6c8dc9aa24
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-10-15 17:12:50 +0100

    Address review comments
     - remove licence statuses endpoint for now
     - make tests work

--- api/data_workspace/v2/views.py
+++ api/data_workspace/v2/views.py
@@ -1,31 +1,20 @@
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.settings import api_settings
 
 from rest_framework_csv.renderers import PaginatedCSVRenderer
 
 from api.core.authentication import DataWorkspaceOnlyAuthentication
-from api.data_workspace.v2.serializers import LicenceStatusSerializer, SIELLicenceSerializer
+from api.data_workspace.v2.serializers import LicenceSerializer
 from api.licences.enums import LicenceStatus
 from api.licences.models import Licence
 
 
-class LicenceStatusesListView(viewsets.GenericViewSet, ListAPIView):
+class LicencesListView(viewsets.ReadOnlyModelViewSet):
     authentication_classes = (DataWorkspaceOnlyAuthentication,)
     pagination_class = LimitOffsetPagination
     renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (PaginatedCSVRenderer,)
-    serializer_class = LicenceStatusSerializer
-
-    def get_queryset(self):
-        return LicenceStatus.all()
-
-
-class SIELLicencesListView(viewsets.ReadOnlyModelViewSet):
-    authentication_classes = (DataWorkspaceOnlyAuthentication,)
-    pagination_class = LimitOffsetPagination
-    renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (PaginatedCSVRenderer,)
-    serializer_class = SIELLicenceSerializer
+    serializer_class = LicenceSerializer
 
     def get_queryset(self):
         return Licence.objects.exclude(status=LicenceStatus.DRAFT)
