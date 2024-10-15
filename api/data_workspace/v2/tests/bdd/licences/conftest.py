50ee2502dd46567658c921e82677013a7cb2e945
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-10-15 15:29:22 +0100

    Add endpoint for retrieving SIEL licences list

---
+++ api/data_workspace/v2/tests/bdd/licences/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+
+from api.applications.tests.factories import GoodOnApplicationFactory, StandardApplicationFactory
+from api.goods.tests.factories import GoodFactory
+from api.licences.enums import LicenceStatus
+from api.licences.tests.factories import GoodOnLicenceFactory, StandardLicenceFactory
+from api.staticdata.statuses.enums import CaseStatusEnum
+from api.staticdata.statuses.models import CaseStatus
+from api.staticdata.units.enums import Units
+
+
+@pytest.fixture()
+def standard_licence():
+    application = StandardApplicationFactory(
+        status=CaseStatus.objects.get(status=CaseStatusEnum.FINALISED),
+    )
+    good = GoodFactory(organisation=application.organisation)
+    good_on_application = GoodOnApplicationFactory(
+        application=application, good=good, quantity=100.0, value=1500, unit=Units.NAR
+    )
+    licence = StandardLicenceFactory(case=application, status=LicenceStatus.DRAFT)
+    GoodOnLicenceFactory(
+        good=good_on_application,
+        quantity=good_on_application.quantity,
+        usage=0.0,
+        value=good_on_application.value,
+        licence=licence,
+    )
+    licence.status = LicenceStatus.ISSUED
+    licence.save()
+    return licence
