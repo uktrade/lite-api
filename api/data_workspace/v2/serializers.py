545f1cdc898face2db1f53669e95fbc5652d6b34
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-10-18 11:21:51 +0100

    Add licence issue date to licences list
    Licence document generation date is taken for this purpose.

--- api/data_workspace/v2/serializers.py
+++ api/data_workspace/v2/serializers.py
@@ -1,13 +1,40 @@
 from rest_framework import serializers
 
+from api.cases.generated_documents.models import GeneratedCaseDocument
+from api.licences.enums import LicenceStatus
 from api.licences.models import Licence
+
+SIEL_TEMPLATE_ID = "d159b195-9256-4a00-9bc8-1eb2cebfa1d2"
 
 
 class LicenceSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+    issued_date = serializers.SerializerMethodField()
 
     class Meta:
         model = Licence
         fields = (
             "id",
             "reference_code",
+            "status",
+            "issued_date",
         )
+
+    def get_status(self, licence):
+        return LicenceStatus.ISSUED
+
+    def get_issued_date(self, licence):
+        licence_document = GeneratedCaseDocument.objects.filter(
+            licence=licence,
+            template_id=SIEL_TEMPLATE_ID,
+            safe=True,
+            visible_to_exporter=True,
+        )
+
+        if not licence_document.exists():
+            return "Invalid licence"
+
+        if licence_document.count() > 1:
+            return "Multiple licence documents"
+
+        return licence_document.first().updated_at
