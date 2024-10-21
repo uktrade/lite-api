d7198bb6e3667db429fd92fc8f7f54ea86e3c0e1
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-10-21 15:07:06 +0100

    Update licence test to include steps for generating documents

--- api/data_workspace/v2/tests/bdd/licences/conftest.py
+++ api/data_workspace/v2/tests/bdd/licences/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from api.applications.tests.factories import GoodOnApplicationFactory, StandardApplicationFactory
+from api.cases.tests.factories import FinalAdviceFactory
 from api.goods.tests.factories import GoodFactory
 from api.licences.enums import LicenceStatus
 from api.licences.tests.factories import GoodOnLicenceFactory, StandardLicenceFactory
@@ -49,3 +50,16 @@ def standard_licence():
     licence.status = LicenceStatus.ISSUED
     licence.save()
     return licence
+
+
+@pytest.fixture()
+def standard_case_with_final_advice(lu_case_officer):
+    case = StandardApplicationFactory(
+        status=CaseStatus.objects.get(status=CaseStatusEnum.UNDER_FINAL_REVIEW),
+    )
+    good = GoodFactory(organisation=case.organisation)
+    good_on_application = GoodOnApplicationFactory(
+        application=case, good=good, quantity=100.0, value=1500, unit=Units.NAR
+    )
+    FinalAdviceFactory(user=lu_case_officer, case=case, good=good_on_application.good)
+    return case
--- api/data_workspace/v2/tests/bdd/scenarios/licences.feature
+++ api/data_workspace/v2/tests/bdd/scenarios/licences.feature
@@ -5,6 +5,13 @@ Scenario: Check that draft licences are not included in the extract
     Given a standard draft licence is created
     Then the draft licence is not included in the extract
 
-Scenario: Issued licence is included in the extract
-    Given a standard licence is issued
+Scenario: Check that cancelled licences are not included in the extract
+    Given a standard licence is cancelled
+    Then the cancelled licence is not included in the extract
+
+Scenario: Licence document is generated when licence is issued
+    Given a case is ready to be finalised
+    When the licence for the case is approved
+    And case officer generates licence documents
+    And case officer issues licence for this case
     Then the issued licence is included in the extract
