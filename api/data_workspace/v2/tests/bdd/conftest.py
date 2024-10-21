673b60c4d9935c7166972eb4a58a271c612f2956
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-10-21 06:15:25 +0100

    Add more common fixtures

--- api/data_workspace/v2/tests/bdd/conftest.py
+++ api/data_workspace/v2/tests/bdd/conftest.py
@@ -2,9 +2,11 @@ import pytest
 
 from rest_framework import status
 
-from api.users.enums import SystemUser
-from api.users.models import BaseUser
-from api.users.tests.factories import BaseUserFactory
+from api.core.constants import GovPermissions
+from api.users.libraries.user_to_token import user_to_token
+from api.users.enums import SystemUser, UserType
+from api.users.models import BaseUser, Permission
+from api.users.tests.factories import BaseUserFactory, GovUserFactory, RoleFactory
 
 
 @pytest.fixture(autouse=True)
@@ -13,6 +15,32 @@ def system_user(db):
         return BaseUser.objects.get(id=SystemUser.id)
     else:
         return BaseUserFactory(id=SystemUser.id)
+
+
+@pytest.fixture()
+def gov_user():
+    return GovUserFactory()
+
+
+@pytest.fixture()
+def gov_user_permissions():
+    for permission in GovPermissions:
+        Permission.objects.get_or_create(id=permission.name, name=permission.name, type=UserType.INTERNAL)
+
+
+@pytest.fixture()
+def lu_case_officer(gov_user, gov_user_permissions):
+    gov_user.role = RoleFactory(name="Case officer", type=UserType.INTERNAL)
+    gov_user.role.permissions.set(
+        [GovPermissions.MANAGE_LICENCE_FINAL_ADVICE.name, GovPermissions.MANAGE_LICENCE_DURATION.name]
+    )
+    gov_user.save()
+    return gov_user
+
+
+@pytest.fixture()
+def gov_headers(gov_user):
+    return {"HTTP_GOV_USER_TOKEN": user_to_token(gov_user.baseuser_ptr)}
 
 
 @pytest.fixture()
