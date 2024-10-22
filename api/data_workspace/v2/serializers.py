d49fc4a93bc2d5c525a84505c5b7bc904921003d
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-10-22 16:58:01 +0100

    Use Case as base object the get licences list
    Use Case as base object and use it to filter on the licence documents.
    This avoids filtering of various case statuses, NLR products etc

--- api/data_workspace/v2/serializers.py
+++ api/data_workspace/v2/serializers.py
@@ -1,18 +1,16 @@
+from django.db.models import F
 from rest_framework import serializers
 
 from api.cases.generated_documents.models import GeneratedCaseDocument
-from api.licences.enums import LicenceStatus
-from api.licences.models import Licence
-
-SIEL_TEMPLATE_ID = "d159b195-9256-4a00-9bc8-1eb2cebfa1d2"
+from api.cases.models import Case
 
 
-class LicenceSerializer(serializers.ModelSerializer):
+class CaseLicenceSerializer(serializers.ModelSerializer):
     decision = serializers.SerializerMethodField()
     issued_date = serializers.SerializerMethodField()
 
     class Meta:
-        model = Licence
+        model = Case
         fields = (
             "id",
             "reference_code",
@@ -20,18 +18,13 @@ class LicenceSerializer(serializers.ModelSerializer):
             "issued_date",
         )
 
-    def get_decision(self, licence):
-        return LicenceStatus.ISSUED
+    def get_decision(self, case):
+        return "issued"
 
-    def get_issued_date(self, licence):
-        licence_document = GeneratedCaseDocument.objects.filter(
-            licence=licence,
-            template_id=SIEL_TEMPLATE_ID,
-            safe=True,
-            visible_to_exporter=True,
+    def get_issued_date(self, case):
+        return (
+            case.licences.all()
+            .annotate(issued_at=F("generatedcasedocument__created_at"))
+            .earliest("issued_at")
+            .issued_at
         )
-
-        if licence_document.count() > 1:
-            return "Multiple licence documents"
-
-        return licence_document.first().updated_at
