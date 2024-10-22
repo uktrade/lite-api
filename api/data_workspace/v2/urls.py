060f962c0f1544944c6f8fec8bdec2d7d5388306
Kevin Carrogan <kevin@carrogan.com>
2024-10-22 19:01:34 +0100

    Update licence list API view to be called licence decision

--- api/data_workspace/v2/urls.py
+++ api/data_workspace/v2/urls.py
@@ -6,7 +6,7 @@ from api.data_workspace.v2 import views
 router_v2 = DefaultRouter()
 
 router_v2.register(
-    "licences",
-    views.LicencesListView,
-    basename="dw-licences",
+    "licence-decisions",
+    views.LicenceDecisionListView,
+    basename="dw-licence-decisions",
 )
--- api/data_workspace/v2/views.py
+++ api/data_workspace/v2/views.py
@@ -5,27 +5,24 @@ from rest_framework.settings import api_settings
 from rest_framework_csv.renderers import PaginatedCSVRenderer
 
 from api.cases.models import Case
-from api.cases.generated_documents.models import GeneratedCaseDocument
 from api.core.authentication import DataWorkspaceOnlyAuthentication
-from api.data_workspace.v2.serializers import CaseLicenceSerializer
+from api.data_workspace.v2.serializers import LicenceDecisionSerializer
+
 
 SIEL_TEMPLATE_ID = "d159b195-9256-4a00-9bc8-1eb2cebfa1d2"
 
 
-class LicencesListView(viewsets.ReadOnlyModelViewSet):
+class LicenceDecisionListView(viewsets.ReadOnlyModelViewSet):
     authentication_classes = (DataWorkspaceOnlyAuthentication,)
     pagination_class = LimitOffsetPagination
+    queryset = (
+        Case.objects.filter(
+            licences__generatedcasedocument__template_id=SIEL_TEMPLATE_ID,
+            licences__generatedcasedocument__visible_to_exporter=True,
+            licences__generatedcasedocument__safe=True,
+        )
+        .distinct()
+        .order_by("-reference_code")
+    )
     renderer_classes = tuple(api_settings.DEFAULT_RENDERER_CLASSES) + (PaginatedCSVRenderer,)
-    serializer_class = CaseLicenceSerializer
-
-    def get_queryset(self):
-        licence_documents = GeneratedCaseDocument.objects.filter(
-            template_id=SIEL_TEMPLATE_ID, visible_to_exporter=True, safe=True
-        )
-        return (
-            Case.objects.filter(
-                licences__generatedcasedocument__in=licence_documents,
-            )
-            .distinct()
-            .order_by("-reference_code")
-        )
+    serializer_class = LicenceDecisionSerializer
