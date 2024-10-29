df81a440b97cc41864ab158585e488f0a76d3102
Kevin Carrogan <kevin@carrogan.com>
2024-10-29 14:04:06 +0000

    Add revoked as a licence decision

--- api/data_workspace/v2/serializers.py
+++ api/data_workspace/v2/serializers.py
@@ -2,7 +2,10 @@ from enum import Enum
 
 from rest_framework import serializers
 
+from api.audit_trail.enums import AuditType
+from api.audit_trail.models import Audit
 from api.cases.models import Case
+from api.licences.enums import LicenceStatus
 
 SIEL_TEMPLATE_ID = "d159b195-9256-4a00-9bc8-1eb2cebfa1d2"
 SIEL_REFUSAL_TEMPLATE_ID = "074d8a54-ee10-4dca-82ba-650460650342"
@@ -41,10 +44,21 @@ class LicenceDecisionSerializer(serializers.ModelSerializer):
         return case.decision
 
     def get_decision_made_at(self, case):
-        documents = case.casedocument_set.filter(
-            generatedcasedocument__template_id=LicenceDecisionType.get_template(case.decision),
-            safe=True,
-            visible_to_exporter=True,
-        )
+        if case.decision in list(LicenceDecisionType):
+            documents = case.casedocument_set.filter(
+                generatedcasedocument__template_id=LicenceDecisionType.get_template(case.decision),
+                safe=True,
+                visible_to_exporter=True,
+            )
+            return documents.earliest("created_at").created_at
 
-        return documents.earliest("created_at").created_at
+        if case.decision == "revoked":
+            audits = Audit.objects.filter(
+                target_object_id=case.pk,
+                payload__status=LicenceStatus.REVOKED,
+                verb=AuditType.LICENCE_UPDATED_STATUS,
+            )
+
+            return audits.earliest("created_at").created_at
+
+        raise ValueError(f"Unknown decision type `{case.decision}`")
