bd525fb8c8e53c80f2f7378a194c685c4d54d287
Kevin Carrogan <kevin@carrogan.com>
2024-10-29 14:04:06 +0000

    Add revoked as a licence decision

--- api/data_workspace/v2/views.py
+++ api/data_workspace/v2/views.py
@@ -8,6 +8,7 @@ from rest_framework.settings import api_settings
 from rest_framework_csv.renderers import PaginatedCSVRenderer
 
 from django.contrib.postgres.aggregates import ArrayAgg
+from django.contrib.postgres.fields import ArrayField
 from django.db.models import (
     Case as DBCase,
     Q,
@@ -17,6 +18,8 @@ from django.db.models import (
 )
 from django.db.models.functions import Cast
 
+from api.audit_trail.enums import AuditType
+from api.audit_trail.models import Audit
 from api.cases.models import Case
 from api.core.authentication import DataWorkspaceOnlyAuthentication
 from api.core.helpers import str_to_bool
@@ -24,6 +27,7 @@ from api.data_workspace.v2.serializers import (
     LicenceDecisionSerializer,
     LicenceDecisionType,
 )
+from api.licences.enums import LicenceStatus
 
 
 class DisableableLimitOffsetPagination(LimitOffsetPagination):
@@ -39,31 +43,47 @@ class LicenceDecisionViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = DisableableLimitOffsetPagination
 
     queryset = (
-        Case.objects.filter(
-            casedocument__generatedcasedocument__template_id__in=LicenceDecisionType.templates().values(),
-            casedocument__visible_to_exporter=True,
-            casedocument__safe=True,
-        )
-        .annotate(
-            template_ids=ArrayAgg(
-                Cast("casedocument__generatedcasedocument__template_id", output_field=TextField()), distinct=True
+        (
+            Case.objects.filter(
+                casedocument__generatedcasedocument__template_id__in=LicenceDecisionType.templates().values(),
+                casedocument__visible_to_exporter=True,
+                casedocument__safe=True,
             )
-        )
-        .filter(
-            functools.reduce(
-                operator.or_,
-                [Q(template_ids=[template_id]) for template_id in LicenceDecisionType.templates().values()],
+            .annotate(
+                template_ids=ArrayAgg(
+                    Cast("casedocument__generatedcasedocument__template_id", output_field=TextField()), distinct=True
+                )
             )
-        )
-        .annotate(
-            decision=DBCase(
-                *[
-                    When(template_ids=[template_id], then=Value(decision.value))
-                    for decision, template_id in LicenceDecisionType.templates().items()
-                ]
+            .filter(
+                functools.reduce(
+                    operator.or_,
+                    [Q(template_ids=[template_id]) for template_id in LicenceDecisionType.templates().values()],
+                )
             )
+            .annotate(
+                decision=DBCase(
+                    *[
+                        When(template_ids=[template_id], then=Value(decision.value))
+                        for decision, template_id in LicenceDecisionType.templates().items()
+                    ]
+                )
+            )
+            .distinct()
         )
-        .distinct()
+        .union(
+            Case.objects.filter(
+                pk__in=list(
+                    Audit.objects.filter(
+                        payload__status=LicenceStatus.REVOKED,
+                        verb=AuditType.LICENCE_UPDATED_STATUS,
+                    ).values_list("target_object_id", flat=True)
+                )
+            ).annotate(
+                template_ids=Value([], output_field=ArrayField(TextField())),
+                decision=Value("revoked", output_field=TextField()),
+            ),
+            all=True,
+        )
         .order_by("-reference_code")
     )
 
