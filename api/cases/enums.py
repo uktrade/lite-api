3ad9f1a3d64ce2d1e0fe9014f84e7a39ed314563
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-10-31 12:47:11 +0000

    Add migration to back populate LicenceDecision instances
    These are created for issued, refused and revoked outcomes.

--- api/cases/enums.py
+++ api/cases/enums.py
@@ -440,3 +440,7 @@ class LicenceDecisionType:
     @classmethod
     def advice_type_to_decision(cls, advice_type):
         return cls.decision_map[advice_type]
+
+    @classmethod
+    def get_template(cls, decision):
+        return cls.templates()[decision]
