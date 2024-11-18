65a8da9892b220da2698bd6fdb97263bbba63aec
Henry Cooksley <henry.cooksley@digital.trade.gov.uk>
2024-11-18 15:33:40 +0000

    Add BDD tests for adding or removing countries

--- api/data_workspace/v2/tests/bdd/scenarios/countries.feature
+++ api/data_workspace/v2/tests/bdd/scenarios/countries.feature
@@ -8,3 +8,16 @@ Scenario: Check that the correct country code and name is included in the extrac
 Scenario: Check that the correct territory code and name is included in the extract
     When I fetch the list of countries
     Then the correct territory code and name is included in the extract
+
+Scenario: A new country appears in the extract when added to countries list
+    Given I add a new country to the countries list
+    When I fetch the list of countries
+    Then the new country appears in the extract
+
+Scenario: A new country disappears from the extract when removed from countries list
+    Given I add a new country to the countries list
+    When I fetch the list of countries
+    Then the new country appears in the extract
+    Given I remove the new country from the countries list
+    When I fetch the list of countries
+    Then the new country does not appear in the extract
--- api/data_workspace/v2/tests/bdd/test_countries.py
+++ api/data_workspace/v2/tests/bdd/test_countries.py
@@ -1,10 +1,13 @@
 from django.urls import reverse
 import pytest
 from pytest_bdd import (
+    given,
     then,
     when,
     scenarios,
 )
+
+from api.staticdata.countries.models import Country
 
 scenarios("./scenarios/countries.feature")
 
@@ -12,6 +15,11 @@ scenarios("./scenarios/countries.feature")
 @pytest.fixture()
 def countries_list_url():
     return reverse("data_workspace:v2:dw-countries-list")
+
+
+@pytest.fixture()
+def example_country():
+    return {"id": "EX", "name": "Example", "is_eu": False}
 
 
 @when("I fetch the list of countries", target_fixture="countries")
@@ -29,3 +37,26 @@ def correct_country_code_included_in_extract(countries):
 def correct_territory_code_included_in_extract(countries):
     example_territory = {"code": "AE-DU", "name": "Dubai"}
     assert example_territory in countries
+
+
+@given("I add a new country to the countries list")
+def add_new_country_to_countries_list(example_country):
+    Country.objects.create(**example_country)
+
+
+@then("the new country appears in the extract")
+def new_country_appears_in_extract(countries, example_country):
+    new_country = {"code": example_country["id"], "name": example_country["name"]}
+    assert new_country in countries
+
+
+@given("I remove the new country from the countries list")
+def remove_new_country_from_countries_list(example_country):
+    new_country = Country.objects.get(id=example_country["id"])
+    new_country.delete()
+
+
+@then("the new country does not appear in the extract")
+def new_country_does_not_appear_in_extract(countries, example_country):
+    new_country = {"code": example_country["id"], "name": example_country["name"]}
+    assert new_country not in countries
