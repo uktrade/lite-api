1674458ad771b959ca71171be4dcfd673b893633
Kevin Carrogan <kevin@carrogan.com>
2024-11-20 14:39:05 +0000

    Add metadata router

---
+++ api/data_workspace/metadata/routers.py
@@ -0,0 +1,65 @@
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.routers import DefaultRouter
+from rest_framework.views import APIView
+
+from django.urls import (
+    NoReverseMatch,
+    path,
+)
+
+
+class TableMetadataView(APIView):
+    _ignore_model_permissions = True
+    schema = None  # exclude from schema
+    metadata = None
+
+    def get(self, request, *args, **kwargs):
+        tables = []
+        namespace = request.resolver_match.namespace
+        for table_metadata in self.metadata:
+            url_name = table_metadata["endpoint"]
+            if namespace:
+                url_name = f"{namespace}:{url_name}"
+            try:
+                url = reverse(
+                    url_name,
+                    args=args,
+                    kwargs=kwargs,
+                    request=request,
+                )
+            except NoReverseMatch:
+                # Don't bail out if eg. no list routes exist, only detail routes.
+                continue
+
+            tables.append(
+                {
+                    "table_name": table_metadata["table_name"],
+                    "endpoint": url,
+                }
+            )
+        return Response({"tables": tables})
+
+
+class TableMetadataRouter(DefaultRouter):
+    def get_metadata_view(self, urls):
+        metadata = []
+        list_name = self.routes[0].name
+        for prefix, viewset, basename in self.registry:
+            metadata.append(
+                {
+                    "table_name": viewset.DataWorkspace.table_name,
+                    "endpoint": list_name.format(basename=basename),
+                }
+            )
+
+        return TableMetadataView.as_view(metadata=metadata)
+
+    def get_urls(self):
+        urls = super().get_urls()
+
+        view = self.get_metadata_view(urls)
+        metadata_url = path("table-metadata/", view, name="table-metadata")
+        urls.append(metadata_url)
+
+        return urls
