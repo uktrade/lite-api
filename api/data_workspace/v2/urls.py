0bf2a34c0b8634be709b13e02e71bf49e085e807
Kevin Carrogan <kevin@carrogan.com>
2024-11-20 14:41:17 +0000

    Switch to using metadata router on v2 endpoints

--- api/data_workspace/v2/urls.py
+++ api/data_workspace/v2/urls.py
@@ -1,9 +1,9 @@
-from rest_framework.routers import DefaultRouter
-
 from api.data_workspace.v2 import views
 
+from api.data_workspace.metadata.routers import TableMetadataRouter
 
-router_v2 = DefaultRouter()
+
+router_v2 = TableMetadataRouter()
 
 router_v2.register(
     "licence-decisions",
