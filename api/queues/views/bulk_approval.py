bf23f2191391c0db054a50bf4888418ce6368653
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2024-12-24 13:49:24 +0000

    Move the cases forward as per the process after bulk approval

--- api/queues/views/bulk_approval.py
+++ api/queues/views/bulk_approval.py
@@ -5,9 +5,13 @@ from rest_framework.generics import CreateAPIView
 
 from api.applications.models import StandardApplication
 from api.applications.serializers.advice import BulkApprovalAdviceSerializer
+from api.audit_trail import service as audit_trail_service
+from api.audit_trail.enums import AuditType
 from api.cases.enums import AdviceLevel, AdviceType
-from api.cases.models import Advice, Case
+from api.cases.models import Case, CaseAssignment
 from api.core.authentication import GovAuthentication
+from api.queues.models import Queue
+from api.workflow.user_queue_assignment import user_queue_assignment_workflow
 
 
 class BulkApprovalCreateView(CreateAPIView):
@@ -18,15 +22,44 @@ class BulkApprovalCreateView(CreateAPIView):
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.case_ids = []
+        self.queue = Queue.objects.get(id=kwargs["pk"])
+
+    def move_case_forward(self, request, case_id):
+        assignments = (
+            CaseAssignment.objects.select_related("queue")
+            .filter(case_id=case_id, queue=self.queue)
+            .order_by("queue__name")
+        )
+        case = Case.objects.get(id=case_id)
+
+        # Unassign existing case advisors to be able to move forward
+        if assignments:
+            assignments.delete()
+
+        # Run routing rules and move the case forward
+        user_queue_assignment_workflow([self.queue], case)
+
+        audit_trail_service.create(
+            actor=request.user,
+            verb=AuditType.UNASSIGNED_QUEUES,
+            target=case,
+            payload={"queues": [self.queue.name], "additional_text": ""},
+        )
+
+    def move_cases_forward(self, request, case_ids):
+        for case_id in case_ids:
+            self.move_case_forward(request, case_id)
 
     def get_advice_data(self, request, application):
         subjects = [("good", good_on_application.good.id) for good_on_application in application.goods.all()] + [
             (poa.party.type, poa.party.id) for poa in application.parties.all()
         ]
+        proviso = self.advice.get("proviso", "")
+        advice_type = AdviceType.PROVISO if proviso else AdviceType.APPROVE
         return [
             {
                 "level": AdviceLevel.USER,
-                "type": AdviceType.APPROVE,
+                "type": advice_type,
                 "case": str(application.id),
                 "user": request.user,
                 subject_name: str(subject_id),
@@ -55,6 +88,9 @@ class BulkApprovalCreateView(CreateAPIView):
         serializer.is_valid(raise_exception=True)
 
         super().perform_create(serializer)
+
+        self.move_cases_forward(request, self.case_ids)
+
         return JsonResponse(
             {},
             status=status.HTTP_201_CREATED,
