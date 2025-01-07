c982ab0c7b5e811750eb7b700a70bd6b35e798da
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2025-01-07 15:46:59 +0000

    Refactor Case activity view to use filter backends

--- api/cases/views/search/activity.py
+++ api/cases/views/search/activity.py
@@ -2,26 +2,37 @@ from django.contrib.contenttypes.models import ContentType
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 
+from api.audit_trail.models import Audit
 from api.audit_trail import service as audit_trail_service
 from api.audit_trail.serializers import AuditSerializer
+from api.cases.views.search.activity_filters import (
+    AuditEventCaseFilter,
+    AuditEventExporterUserFilter,
+    AuditEventMentionsFilter,
+    AuditEventTeamFilter,
+)
 from api.cases.models import Case
 from api.core.authentication import GovAuthentication
 
 
-class CaseActivityView(APIView):
+class CaseActivityView(ListAPIView):
     authentication_classes = (GovAuthentication,)
+    filter_backends = [
+        AuditEventCaseFilter,
+        AuditEventExporterUserFilter,
+        AuditEventMentionsFilter,
+        AuditEventTeamFilter,
+    ]
+    queryset = Audit.objects.all()
+    serializer_class = AuditSerializer
 
-    def get(self, request, pk):
-        filter_data = audit_trail_service.get_filters(request.GET)
-        content_type = ContentType.objects.get_for_model(Case)
-        audit_trail_qs = audit_trail_service.filter_object_activity(
-            object_id=pk, object_content_type=content_type, **filter_data
-        )
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
 
-        data = AuditSerializer(audit_trail_qs, many=True).data
-
-        return JsonResponse(data={"activity": data}, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(queryset, many=True)
+        return JsonResponse(data={"activity": serializer.data}, status=status.HTTP_200_OK)
 
 
 class CaseActivityFiltersView(APIView):
