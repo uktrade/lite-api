f6a17d490b3e2db3eb862a6bed8a0238640003fd
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2025-01-10 13:30:52 +0000

    Add unit test for the move case forward function
    The test takes a case that is assigned to an OGD queue and moves it forward.
    To be able to verify that the current queue is unassigned and next queue
    as per routing rules is assigned correctly the case is initially processed
    through product assessments and pre-circ. This is to prevent triggering of
    fallback routing rules which otherwise doesn't unassign the current queue
    and in addition routes to this fallback queue as this rules relies on
    presence of specific audit events.
    
    Though this verifies next queue as per routing rules this is not be
    considered a test for routing rules.

--- api/queues/caseworker/tests/test_bulk_approval.py
+++ api/queues/caseworker/tests/test_bulk_approval.py
@@ -7,10 +7,15 @@ from api.applications.tests.factories import DraftStandardApplicationFactory
 from api.audit_trail.enums import AuditType
 from api.audit_trail.models import Audit
 from api.cases.enums import AdviceLevel, AdviceType
+from api.cases.models import CaseAssignment
+from api.cases.tests.factories import CaseAssignmentFactory
 from api.parties.tests.factories import PartyDocumentFactory
 from api.queues.models import Queue
+from api.staticdata.control_list_entries.models import ControlListEntry
 from api.staticdata.statuses.enums import CaseStatusEnum
 from api.staticdata.statuses.models import CaseStatus
+from api.teams.models import Team
+from api.users.tests.factories import GovUserFactory
 
 from lite_routing.routing_rules_internal.enums import QueuesEnum, TeamIdEnum
 
@@ -28,28 +33,49 @@ def mod_bulk_approval_url():
 
 
 @pytest.fixture
-def multiple_cases_ogd_queue(organisation, submit_application):
+def get_cases_with_ogd_queue_assigned(organisation, submit_application):
 
-    def _multiple_cases_ogd_queue(queue_id, count=5):
-        ogd_advice = CaseStatus.objects.get(status=CaseStatusEnum.OGD_ADVICE)
-        draft_applications = [DraftStandardApplicationFactory(organisation=organisation) for i in range(count)]
+    def _get_cases_with_ogd_queue_assigned(queue_id, count=5):
+        applications = [DraftStandardApplicationFactory(organisation=organisation) for i in range(count)]
         _ = [
             PartyDocumentFactory(
                 party=application.end_user.party,
                 s3_key="party-document",
                 safe=True,
             )
-            for application in draft_applications
+            for application in applications
         ]
-        cases = [submit_application(application) for application in draft_applications]
+        cases = [submit_application(application) for application in applications]
+
+        cle = ControlListEntry.objects.get(rating="PL9002b")
+        gov_user = GovUserFactory()
+        gov_user.team = Team.objects.get(id=TeamIdEnum.LICENSING_UNIT)
+        gov_user.save()
+        queue = Queue.objects.get(id=QueuesEnum.LU_PRE_CIRC)
+
+        for application in applications:
+            for good_on_application in application.goods.all():
+                good_on_application.is_good_controlled = True
+                good_on_application.control_list_entries.add(*[cle])
+                good_on_application.good.control_list_entries.add(*[cle])
+
         for case in cases:
-            case.status = ogd_advice
+            case.status = CaseStatus.objects.get(status=CaseStatusEnum.UNDER_REVIEW)
             case.save()
-            case.queues.add(*[queue_id])
+            case.queues.set([QueuesEnum.LU_PRE_CIRC])
+            case.refresh_from_db()
+
+            # Circulate to OGDs
+            case.move_case_forward(queue, gov_user)
+
+            # Reset queue with the specified OGD queue only
+            queue = Queue.objects.get(id=queue_id)
+            case.refresh_from_db()
+            case.queues.set([queue])
 
         return cases
 
-    return _multiple_cases_ogd_queue
+    return _get_cases_with_ogd_queue_assigned
 
 
 def case_subjects(case):
@@ -57,8 +83,10 @@ def case_subjects(case):
     return list(application.goods.all()) + list(application.parties.all())
 
 
-def test_user_bulk_approves_cases(api_client, mod_officer_headers, mod_bulk_approval_url, multiple_cases_ogd_queue):
-    cases = multiple_cases_ogd_queue(QueuesEnum.MOD_CAPPROT, count=25)
+def test_user_bulk_approves_cases(
+    api_client, mod_officer_headers, mod_bulk_approval_url, get_cases_with_ogd_queue_assigned
+):
+    cases = get_cases_with_ogd_queue_assigned(QueuesEnum.MOD_CAPPROT, count=25)
     data = {
         "cases": [str(case.id) for case in cases],
         "advice": {
@@ -94,9 +122,9 @@ def test_user_bulk_approves_cases(api_client, mod_officer_headers, mod_bulk_appr
 
 
 def test_user_bulk_approves_fails_for_unsupported_users(
-    api_client, fcdo_officer_headers, fcdo_bulk_approval_url, multiple_cases_ogd_queue
+    api_client, fcdo_officer_headers, fcdo_bulk_approval_url, get_cases_with_ogd_queue_assigned
 ):
-    cases = multiple_cases_ogd_queue(QueuesEnum.FCDO)
+    cases = get_cases_with_ogd_queue_assigned(QueuesEnum.FCDO)
     data = {
         "case_ids": [str(case.id) for case in cases],
         "advice": {
@@ -126,3 +154,44 @@ def test_user_bulk_approves_fails_for_unsupported_users(
             Audit.objects.filter(target_object_id=case.id, verb=AuditType.CREATE_BULK_APPROVAL_RECOMMENDATION).exists()
             is False
         )
+
+
+@pytest.mark.parametrize(
+    "team_id, queue_id,next_queue_id",
+    (
+        (TeamIdEnum.DESNZ_CHEMICAL, QueuesEnum.DESNZ_CHEMICAL, QueuesEnum.LU_POST_CIRC),
+        (TeamIdEnum.DESNZ_NUCLEAR, QueuesEnum.DESNZ_NUCLEAR, QueuesEnum.DESNZ_NUCLEAR_COUNTERSIGNING),
+        (TeamIdEnum.FCDO, QueuesEnum.FCDO, QueuesEnum.FCDO_COUNTER_SIGNING),
+        (TeamIdEnum.FCDO, QueuesEnum.FCDO_COUNTER_SIGNING, QueuesEnum.LU_POST_CIRC),
+        (TeamIdEnum.MOD_CAPPROT, QueuesEnum.MOD_CAPPROT, QueuesEnum.MOD_ECJU_REVIEW_AND_COMBINE),
+        (TeamIdEnum.MOD_DI, QueuesEnum.MOD_DI_DIRECT, QueuesEnum.LU_POST_CIRC),
+        (TeamIdEnum.MOD_DI, QueuesEnum.MOD_DI_INDIRECT, QueuesEnum.MOD_ECJU_REVIEW_AND_COMBINE),
+        (TeamIdEnum.MOD_DSR, QueuesEnum.MOD_DSR, QueuesEnum.MOD_ECJU_REVIEW_AND_COMBINE),
+        (TeamIdEnum.MOD_DSTL, QueuesEnum.MOD_DSTL, QueuesEnum.MOD_ECJU_REVIEW_AND_COMBINE),
+        (TeamIdEnum.NCSC, QueuesEnum.NCSC, QueuesEnum.LU_POST_CIRC),
+    ),
+)
+def test_case_move_forward(
+    get_cases_with_ogd_queue_assigned,
+    team_id,
+    queue_id,
+    next_queue_id,
+):
+
+    case = get_cases_with_ogd_queue_assigned(queue_id, count=1)[0]
+    gov_user = GovUserFactory()
+    gov_user.team = Team.objects.get(id=team_id)
+    gov_user.save()
+
+    queue = Queue.objects.get(id=queue_id)
+    CaseAssignmentFactory(case=case, queue=queue, user=gov_user)
+
+    case.move_case_forward(queue, gov_user)
+
+    assert CaseAssignment.objects.filter(case=case, queue=queue, user=gov_user).exists() is False
+    assert queue_id not in [str(queue.id) for queue in case.queues.all()]
+    assert next_queue_id in [str(queue.id) for queue in case.queues.all()]
+    audit_event = Audit.objects.filter(target_object_id=case.id, verb=AuditType.UNASSIGNED_QUEUES).first()
+    assert audit_event.payload == {
+        "queues": [queue.name],
+    }
