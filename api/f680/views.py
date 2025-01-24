cb06cac236f4c9071ef976f1a3be43848ae897e8
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-01-24 15:41:07 +0000

    add put to api

--- api/f680/views.py
+++ api/f680/views.py
@@ -7,7 +7,7 @@ from api.core.decorators import authorised_to_view_application
 from api.organisations.libraries.get_organisation import get_request_user_organisation, get_request_user_organisation_id
 from api.users.models import ExporterUser
 
-from .serializers import F680Serializer, F680ApplicationViewSerializer
+from .serializers import F680Serializer, F680ApplicationViewSerializer, F680ApplicationUpdateSerializer
 from .models import F680Application
 from api.applications.libraries.get_applications import get_f680_application
 
@@ -25,6 +25,7 @@ class F680CreateView(CreateAPIView):
 
 class F680Detail(RetrieveUpdateDestroyAPIView):
     authentication_classes = (ExporterAuthentication,)
+    queryset = F680Application.objects.all()  # /PS-IGNORE
     serializer_class = F680ApplicationViewSerializer  # /PS-IGNORE
 
     @authorised_to_view_application(ExporterUser)
@@ -43,4 +44,20 @@ class F680Detail(RetrieveUpdateDestroyAPIView):
                 "organisation_id": get_request_user_organisation_id(request),
             },
         ).data
+        return JsonResponse(data=data, status=status.HTTP_200_OK)
+
+    @authorised_to_view_application(ExporterUser)
+    def put(self, request, pk):
+        application = get_f680_application(pk)
+        update_serializer = F680ApplicationUpdateSerializer
+        data = request.data
+        serializer = update_serializer(
+            application, data=data, context=get_request_user_organisation(request), partial=True
+        )
+        if not serializer.is_valid():
+            return JsonResponse(data={"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        data.update({"pk": application.id})
         return JsonResponse(data=data, status=status.HTTP_200_OK)
