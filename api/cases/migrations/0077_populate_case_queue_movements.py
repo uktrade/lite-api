d022789c1247f7198dd9bb7d7ae3ca6dd34f4d56
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2025-02-05 11:18:09 +0000

    Create instances for all queue_ids
    Some events have multiple queue_ids in the payload

--- api/cases/migrations/0077_populate_case_queue_movements.py
+++ api/cases/migrations/0077_populate_case_queue_movements.py
@@ -22,15 +22,15 @@ def populate_case_queue_movements(apps, schema_editor):
     )
     for event in move_case_qs:
         case_id = event.action_object_object_id
-        queue_id = event.payload["queue_ids"][0]
 
-        case_queue_movements.append(
-            CaseQueueMovement(
-                case_id=case_id,
-                queue_id=queue_id,
-                created_at=event.created_at,
+        for queue_id in event.payload["queue_ids"]:
+            case_queue_movements.append(
+                CaseQueueMovement(
+                    case_id=case_id,
+                    queue_id=queue_id,
+                    created_at=event.created_at,
+                )
             )
-        )
 
     # Some of the early events only have queue name in the payload so they are processed separately
     move_case_qs = Audit.objects.filter(
