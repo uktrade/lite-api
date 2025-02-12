4d9faad3d1f5f0ef9ff435d7c3a5294ef65f99c8
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2025-02-12 12:49:10 +0000

    Update migration that populates user in CaseQueueMovement
    Previously in the migration we were iterating through each object
    and updating the 'user' field which was timing out.
    
    Looking at all MOVE_CASE events in UAT, all of the users are SystemUser
    so we can just update user for all instances of CaseQueueMovement
    as SystemUser without iterating through MOVE_CASE events.

--- api/cases/migrations/0081_populate_user_case_queue_movements.py
+++ api/cases/migrations/0081_populate_user_case_queue_movements.py
@@ -2,56 +2,17 @@
 
 from django.db import migrations, transaction
 
-from api.audit_trail.enums import AuditType
+from api.users.enums import SystemUser
 
 
 @transaction.atomic
 def populate_user_case_queue_movements(apps, schema_editor):
-    Audit = apps.get_model("audit_trail", "Audit")
     CaseQueueMovement = apps.get_model("cases", "CaseQueueMovement")
-    Queue = apps.get_model("queues", "Queue")
 
-    case_queue_movements_to_update = []
-
-    move_case_qs = Audit.objects.filter(
-        verb=AuditType.MOVE_CASE,
-        action_object_object_id__isnull=False,
-        payload__queue_ids__isnull=False,
-        payload__queues__isnull=False,
-    )
-    for event in move_case_qs:
-        case_id = event.action_object_object_id
-
-        for queue_id in event.payload["queue_ids"]:
-            obj = CaseQueueMovement.objects.get(
-                case_id=case_id,
-                queue_id=queue_id,
-                created_at=event.created_at,
-            )
-            obj.user = event.actor
-            case_queue_movements_to_update.append(obj)
-
-    # Some of the early events only have queue name in the payload so they are processed separately
-    move_case_qs = Audit.objects.filter(
-        verb=AuditType.MOVE_CASE,
-        action_object_object_id__isnull=False,
-        payload__queue_ids__isnull=True,
-        payload__queues__isnull=False,
-    )
-    for event in move_case_qs:
-        case_id = event.action_object_object_id
-        queue_name = event.payload["queues"]
-        if Queue.objects.filter(name=queue_name).exists():
-            queue = Queue.objects.get(name=queue_name)
-            obj = CaseQueueMovement.objects.get(
-                case_id=case_id,
-                queue_id=queue.id,
-                created_at=event.created_at,
-            )
-            obj.user = event.actor
-            case_queue_movements_to_update.append(obj)
-
-    CaseQueueMovement.objects.bulk_update(case_queue_movements_to_update, ["user"])
+    # looking at all MOVE_CASE events in UAT, all of the users are SystemUser
+    # so we can just update user for all instances of CaseQueueMovement
+    # as SystemUser without iterating through MOVE_CASE events.
+    CaseQueueMovement.objects.all().update(user_id=SystemUser.id)
 
 
 class Migration(migrations.Migration):
--- api/cases/migrations/tests/test_0081_populate_user_case_queue_movements.py
+++ api/cases/migrations/tests/test_0081_populate_user_case_queue_movements.py
@@ -1,36 +1,23 @@
 import pytest
 
-
-from api.audit_trail.enums import AuditType
+from api.cases.tests.factories import CaseFactory
+from api.queues.tests.factories import QueueFactory
+from api.users.tests.factories import SystemUserFactory
 
 
 @pytest.mark.django_db()
 def test_populate_user_case_queue_movements(migrator):
     old_state = migrator.apply_initial_migration(("cases", "0080_casequeuemovement_user"))
 
-    Audit = old_state.apps.get_model("audit_trail", "Audit")
     CaseQueueMovement = old_state.apps.get_model("cases", "CaseQueueMovement")
-    Queue = old_state.apps.get_model("queues", "Queue")
+
+    system_user = SystemUserFactory()
+
+    for _ in range(25):
+        case = CaseFactory()
+        queue = QueueFactory()
+        CaseQueueMovement.objects.create(case_id=case.id, queue_id=queue.id, user=None)
 
     migrator.apply_tested_migration(("cases", "0081_populate_user_case_queue_movements"))
 
-    move_case_qs = Audit.objects.filter(
-        verb=AuditType.MOVE_CASE,
-        action_object_object_id__isnull=False,
-        payload__queues__isnull=False,
-    )
-
-    assert move_case_qs.count() == CaseQueueMovement.objects.count()
-
-    for event in move_case_qs:
-        case_id = event.action_object_object_id
-        queue_name = event.payload["queues"]
-        if Queue.objects.filter(name=queue_name).exists():
-            queue = Queue.objects.get(name=queue_name)
-            obj = CaseQueueMovement.objects.get(
-                case_id=case_id,
-                queue_id=queue.id,
-                created_at=event.created_at,
-            )
-
-            assert event.actor == obj.actor
+    assert set(CaseQueueMovement.objects.all().values_list("user_id", flat=True)) == {system_user.id}
