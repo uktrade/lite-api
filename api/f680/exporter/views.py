d66c292fb956ac4aa804760078c7ba813c70b853
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2025-02-17 16:33:40 +0000

    Add F680 application submit endpoint

--- api/f680/exporter/views.py
+++ api/f680/exporter/views.py
@@ -1,11 +1,19 @@
+from django.utils import timezone
+
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.cases.enums import CaseTypeEnum
+from api.cases.celery_tasks import get_application_target_sla
+from api.cases.models import CaseQueueMovement
 from api.core.authentication import ExporterAuthentication
 from api.organisations.libraries.get_organisation import get_request_user_organisation
 from api.organisations.filters import CurrentExporterUserOrganisationFilter
 from api.staticdata.statuses.enums import CaseStatusEnum
 from api.staticdata.statuses.libraries.get_case_status import get_case_status_by_status
+from lite_routing.routing_rules_internal.flagging_engine import apply_flagging_rules_to_case
+from lite_routing.routing_rules_internal.routing_engine import run_routing_rules
 
 from api.f680.models import F680Application
 from api.f680.exporter.serializers import F680ApplicationSerializer
@@ -24,3 +32,30 @@ class F680ApplicationViewSet(viewsets.ModelViewSet):
         serializer_context["default_status"] = get_case_status_by_status(CaseStatusEnum.DRAFT)
         serializer_context["case_type_id"] = CaseTypeEnum.F680.id
         return serializer_context
+
+    @action(detail=True)
+    def submit(self, request, **kwargs):
+        # TODO: What follows is a lean slice of applications.views.applications.ApplicationSubmission.
+        #   We should review exactly how best to structure application submission; it could be better
+        #   to depend on a model method, a library utility, or something else
+        application = self.get_object()
+
+        # TODO: some sort of validation that we have everything we need on the application -
+        #   this may duplicate frontend validation in some way so needs some consideration.
+
+        application.status = get_case_status_by_status(CaseStatusEnum.SUBMITTED)
+        application.submitted_at = timezone.now()
+        application.sla_remaining_days = get_application_target_sla(application.case_type.sub_type)
+        application.sla_days = 0
+        application.submitted_by = request.user.exporteruser
+        application.save()
+
+        apply_flagging_rules_to_case(application)
+        queues_assigned = run_routing_rules(application)
+        for queue in queues_assigned:
+            CaseQueueMovement.objects.create(
+                case=application.case_ptr, queue_id=queue, created_at=application.submitted_at
+            )
+
+        serializer = self.get_serializer(application)
+        return Response(serializer.data)
