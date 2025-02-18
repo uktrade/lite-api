9fc9573b5d66b96eb25e2bb29a0b9b125cbc5abd
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2025-02-18 13:50:29 +0000

    Add unit tests for F680Application model

--- api/f680/models.py
+++ api/f680/models.py
@@ -5,30 +5,18 @@ from api.applications.models import BaseApplication
 from api.f680.managers import F680ApplicationQuerySet
 
 
-class Hasher(dict):
-    # https://stackoverflow.com/a/3405143/190597
-    def __missing__(self, key):
-        value = self[key] = type(self)()
-        return value
-
-
 class F680Application(BaseApplication):  # /PS-IGNORE
     objects = F680ApplicationQuerySet.as_manager()
 
     application = models.JSONField()
 
-    @property
-    def application_dict(self):
-        if not isinstance(self.application, dict):
-            return Hasher({})
-        return Hasher(self.application)
-
     def get_application_field_value(self, section, field_key):
         # TODO: investigate wrapping up accessing fields on application JSON with some OOP
-        section_fields = self.application_dict["sections"][section]["fields"]
+        #   we should be able to solve all the chained .gets() with a decent interface
+        section_fields = self.application.get("sections", {}).get(section, {}).get("fields", [])
         for field in section_fields:
-            if field["key"] == field_key:
-                return field["raw_answer"]
+            if field.get("key") == field_key:
+                return field.get("raw_answer")
         return None
 
     def on_submit(self):
