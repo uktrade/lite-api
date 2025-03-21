6595f6aa0f410724c72be0a3ac9f0c425aee430f
Arun Siluvery <arun.siluvery@digital.trade.gov.uk>
2025-03-21 13:20:59 +0000

    Keep security release requests details to a minimum in Recommendation serializer
    We only need id here as rest of them can be taken from case details.

--- api/f680/caseworker/read_only_serializers.py
+++ api/f680/caseworker/read_only_serializers.py
@@ -1,13 +1,8 @@
 from rest_framework import serializers
 
 from api.core.serializers import KeyValueChoiceField
-from api.f680.enums import RecipientType, RecommendationType, SecurityGrading
+from api.f680.enums import RecommendationType, SecurityGrading
 from api.f680.models import Recommendation
-
-
-class CountryViewSerializer(serializers.Serializer):
-    id = serializers.UUIDField(read_only=True)
-    name = serializers.CharField(read_only=True)
 
 
 class TeamViewSerializer(serializers.Serializer):
@@ -23,36 +18,12 @@ class GovUserViewSerializer(serializers.Serializer):
     team = TeamViewSerializer()
 
 
-class F680ProductViewSerializer(serializers.Serializer):
-    name = serializers.CharField(read_only=True)
-    description = serializers.CharField(read_only=True)
-    security_grading = KeyValueChoiceField(choices=SecurityGrading.product_choices)
-    security_grading_other = serializers.CharField(read_only=True)
-
-
-class F680RecipientViewSerializer(serializers.Serializer):
-    name = serializers.CharField(read_only=True)
-    address = serializers.CharField(read_only=True)
-    country = CountryViewSerializer()
-    type = KeyValueChoiceField(choices=RecipientType.choices)
-
-
-class F680SecurityReleaseRequestSerializer(serializers.Serializer):
-    recipient = F680RecipientViewSerializer()
-    product = F680ProductViewSerializer()
-    security_grading = KeyValueChoiceField(choices=SecurityGrading.security_release_choices)
-    security_grading_other = serializers.CharField(read_only=True)
-    approval_types = serializers.ListField(child=serializers.CharField(read_only=True))
-    intended_use = serializers.CharField(read_only=True)
-
-
 class F680RecommendationViewSerializer(serializers.ModelSerializer):
     case = serializers.ReadOnlyField(source="case_id")
     user = GovUserViewSerializer()
     team = TeamViewSerializer()
     type = KeyValueChoiceField(choices=RecommendationType.choices)
     security_grading = KeyValueChoiceField(choices=SecurityGrading.security_release_choices)
-    security_release_request = F680SecurityReleaseRequestSerializer()
 
     class Meta:
         model = Recommendation
