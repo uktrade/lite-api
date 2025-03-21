f2a48f4fead0317c937f8c791fd59cb8c1baa10c
Kevin Carrogan <kevin@carrogan.com>
2025-03-21 11:38:25 +0000

    Change application manifest to work at the lowest case type level

--- api/f680/application_manifest.py
+++ api/f680/application_manifest.py
@@ -2,11 +2,14 @@ from api.f680.models import F680Application
 from api.f680.caseworker.serializers import F680ApplicationSerializer
 from api.application_manifests.base import BaseManifest
 from api.application_manifests.registry import application_manifest_registry
-from api.cases.enums import ApplicationFeatures, CaseTypeSubTypeEnum
+from api.cases.enums import (
+    ApplicationFeatures,
+    CaseTypeReferenceEnum,
+)
 from gov_notify.enums import TemplateType
 
 
-@application_manifest_registry.register(CaseTypeSubTypeEnum.F680)
+@application_manifest_registry.register(CaseTypeReferenceEnum.F680)
 class F680ApplicationManifest(BaseManifest):
     model_class = F680Application
     caseworker_serializers = {"view": F680ApplicationSerializer}
