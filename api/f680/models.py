b5bf875dbf6eb83f0b5a34089a88d7acd9545f57
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-04-07 12:06:20 +0100

    add FoiDeclarationSerializer first pass

--- api/f680/models.py
+++ api/f680/models.py
@@ -28,6 +28,7 @@ class F680Application(BaseApplication):  # /PS-IGNORE
     def on_submit(self, application_data):
         self.name = application_data["sections"]["general_application_details"]["fields"]["name"]["raw_answer"]
         self.agreed_to_foi = application_data["agreed_to_foi"]
+        self.foi_reason = application_data["foi_reason"]
         self.save()
 
         product_information_fields = application_data["sections"]["product_information"]["fields"]
