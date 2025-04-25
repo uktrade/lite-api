bcb1357398f5af9bd3988043f4da82167b836414
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-04-25 14:37:49 +0100

    refactor

--- api/f680/caseworker/views.py
+++ api/f680/caseworker/views.py
@@ -4,6 +4,7 @@ from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from api.applications.libraries.get_applications import get_application
+from api.audit_trail import service as audit_trail_service
 from api.audit_trail.enums import AuditType
 from api.core.authentication import GovAuthentication
 from api.core.exceptions import NotFoundError
@@ -48,19 +49,6 @@ class F680RecommendationViewSet(viewsets.ModelViewSet):
             for item in request_data
         ]
 
-    def create_audit(self, audit_type, recommendation_type=None, payload=None):
-        from api.audit_trail import service as audit_trail_service
-
-        if recommendation_type:
-            payload.update({"additional_text": f"With advice type of {recommendation_type}"})
-
-        audit_trail_service.create(
-            actor=self.request.user,
-            verb=audit_type,
-            target=self.application.get_case(),
-            payload=payload,
-        )
-
     def create(self, request, *args, **kwargs):
         data = self.prepare_data(request.data.copy())
         serializer = self.get_serializer(data=data, many=True)
@@ -68,14 +56,18 @@ class F680RecommendationViewSet(viewsets.ModelViewSet):
 
         self.perform_create(serializer)
 
-        # A record of the security release objects impacted by the recommendation for the audit table
-        security_release_request_object_ids = [str(item["security_release_request"]) for item in serializer.data]
-        # Recommendation  type records will all be the same type as serializer does not allowed mixed advice
         recommendation_type = serializer.data[0]["type"]["value"]
-        self.create_audit(
-            audit_type=AuditType.CREATE_OGD_F680_RECOMMENDATION,
-            advice_type=recommendation_type,
-            payload={"security_release_request_object_ids": security_release_request_object_ids},
+        qs = self.get_queryset().filter(user_id=request.user.id, team=request.user.govuser.team)
+        security_release_request_ids = [str(item.security_release_request.id) for item in qs]
+
+        audit_trail_service.create(
+            actor=self.request.user,
+            verb=AuditType.CREATE_OGD_F680_RECOMMENDATION,
+            target=self.application.get_case(),
+            payload={
+                "security_release_request_ids": security_release_request_ids,
+                "additional_text": f"With advice type of {recommendation_type}",
+            },
         )
 
         headers = self.get_success_headers(serializer.data)
@@ -87,19 +79,19 @@ class F680RecommendationViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
-        user = request.user
-        qs = self.get_queryset().filter(user_id=user.id, team=user.govuser.team)
-
-        # A record for the audit table of the recommendation_object_ids which have been removed
-        recommendation_ids = [str(recommendation.id) for recommendation in qs]
+        qs = self.get_queryset().filter(user_id=request.user.id, team=request.user.govuser.team)
+        security_release_request_ids = [str(item.security_release_request.id) for item in qs]
 
         if qs.exists:
             qs.delete()
 
-        self.create_audit(
-            audit_type=AuditType.CLEAR_OGD_F680_RECOMMENDATION,
-            payload={"recommendation_object_ids": recommendation_ids},
+        audit_trail_service.create(
+            actor=self.request.user,
+            verb=AuditType.CLEAR_OGD_F680_RECOMMENDATION,
+            target=self.application.get_case(),
+            payload={"security_release_request_ids": security_release_request_ids},
         )
+
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
