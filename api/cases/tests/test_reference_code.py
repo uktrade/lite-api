effa15d177ef02c45c2766203a88edf8e3e8f2ab
Kevin Carrogan <kevin@carrogan.com>
2025-04-29 15:28:56 +0100

    Switch to using factories for reference code tests

--- api/cases/tests/test_reference_code.py
+++ api/cases/tests/test_reference_code.py
@@ -3,23 +3,24 @@ from freezegun import freeze_time
 from test_helpers.clients import DataTestClient
 
 from api.applications.enums import ApplicationExportType
+from api.applications.tests.factories import (
+    DraftStandardApplicationFactory,
+    StandardApplicationFactory,
+)
 
 
 @freeze_time("2023-11-03 12:00:00")
 class ReferenceCode(DataTestClient):
-    def test_standard_application_reference_code(self):
-        standard_application = self.create_draft_standard_application(self.organisation)
-        standard_application = self.submit_application(standard_application)
+    def test_permanent_standard_application_reference_code(self):
+        standard_application = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
 
         self.assertEqual(
             standard_application.reference_code,
             "GBSIEL/2023/0000001/P",
         )
 
-    def test_temporary_application_reference_code(self):
-        standard_application = self.create_draft_standard_application(self.organisation)
-        standard_application.export_type = ApplicationExportType.TEMPORARY
-        self.submit_application(standard_application)
+    def test_temporary_standard_application_reference_code(self):
+        standard_application = StandardApplicationFactory(export_type=ApplicationExportType.TEMPORARY)
 
         self.assertEqual(
             standard_application.reference_code,
@@ -27,14 +28,13 @@ class ReferenceCode(DataTestClient):
         )
 
     def test_draft_applications_dont_have_reference_codes(self):
-        draft = self.create_draft_standard_application(self.organisation)
+        draft = DraftStandardApplicationFactory()
+
         self.assertIsNone(draft.reference_code)
 
     def test_reference_code_increment(self):
-        standard_application_1 = self.create_draft_standard_application(self.organisation)
-        standard_application_1 = self.submit_application(standard_application_1)
-        standard_application_2 = self.create_draft_standard_application(self.organisation)
-        standard_application_2 = self.submit_application(standard_application_2)
+        standard_application_1 = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
+        standard_application_2 = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
 
         self.assertEqual(standard_application_1.reference_code, "GBSIEL/2023/0000001/P")
         self.assertEqual(standard_application_2.reference_code, "GBSIEL/2023/0000002/P")
