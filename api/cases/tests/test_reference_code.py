c4fc8e16357d01b051f326e7a4039b1645fbfb7d
Kevin Carrogan <kevin@carrogan.com>
2025-04-29 15:35:44 +0100

    Switch to pytest style tests for reference code

--- api/cases/tests/test_reference_code.py
+++ api/cases/tests/test_reference_code.py
@@ -1,6 +1,6 @@
-from freezegun import freeze_time
+import pytest
 
-from rest_framework.test import APITestCase
+from freezegun import freeze_time
 
 from api.applications.enums import ApplicationExportType
 from api.applications.tests.factories import (
@@ -9,32 +9,33 @@ from api.applications.tests.factories import (
 )
 
 
-@freeze_time("2023-11-03 12:00:00")
-class ReferenceCode(APITestCase):
-    def test_permanent_standard_application_reference_code(self):
-        standard_application = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
+pytestmark = pytest.mark.django_db
 
-        self.assertEqual(
-            standard_application.reference_code,
-            "GBSIEL/2023/0000001/P",
-        )
 
-    def test_temporary_standard_application_reference_code(self):
-        standard_application = StandardApplicationFactory(export_type=ApplicationExportType.TEMPORARY)
+@pytest.fixture(autouse=True)
+def set_time():
+    with freeze_time("2023-11-03 12:00:00"):
+        yield
 
-        self.assertEqual(
-            standard_application.reference_code,
-            "GBSIEL/2023/0000001/T",
-        )
 
-    def test_draft_applications_dont_have_reference_codes(self):
-        draft = DraftStandardApplicationFactory()
+def test_permanent_standard_application_reference_code():
+    standard_application = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
+    assert standard_application.reference_code == "GBSIEL/2023/0000001/P"
 
-        self.assertIsNone(draft.reference_code)
 
-    def test_reference_code_increment(self):
-        standard_application_1 = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
-        standard_application_2 = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
+def test_temporary_standard_application_reference_code():
+    standard_application = StandardApplicationFactory(export_type=ApplicationExportType.TEMPORARY)
+    assert standard_application.reference_code == "GBSIEL/2023/0000001/T"
 
-        self.assertEqual(standard_application_1.reference_code, "GBSIEL/2023/0000001/P")
-        self.assertEqual(standard_application_2.reference_code, "GBSIEL/2023/0000002/P")
+
+def test_draft_applications_dont_have_reference_codes():
+    draft = DraftStandardApplicationFactory()
+    assert draft.reference_code is None
+
+
+def test_reference_code_increment():
+    standard_application_1 = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
+    assert standard_application_1.reference_code == "GBSIEL/2023/0000001/P"
+
+    standard_application_2 = StandardApplicationFactory(export_type=ApplicationExportType.PERMANENT)
+    assert standard_application_2.reference_code == "GBSIEL/2023/0000002/P"
