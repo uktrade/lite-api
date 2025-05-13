a32d9f27597be0eab4807f5c3b0958488987fd64
mark.j0hnst0n <mark.johnston@digital.trade.gov.uk>
2025-05-13 09:11:48 +0100

    remove unnecessary

--- api/f680/caseworker/filters.py
+++ api/f680/caseworker/filters.py
@@ -1,10 +1,4 @@
 from rest_framework import filters
-from api.cases.enums import CaseTypeSubTypeEnum
-
-
-class F680CaseFilter(filters.BaseFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        return queryset.filter(case__case_type__sub_type=CaseTypeSubTypeEnum.F680)
 
 
 class CurrentCaseFilter(filters.BaseFilterBackend):
