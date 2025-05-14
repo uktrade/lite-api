1fbb8f96108f0b1971efd5c537daa8f40bd1fddd
alexslingsby <alex.slingsby@digital.trade.gov.uk>
2025-05-14 14:23:25 +0100

    Removing unused commit.

--- api/application_manifests/models.py
+++ api/application_manifests/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from api.cases.enums import ApplicationFeatures, CaseTypeReferenceEnum
+from api.cases.enums import CaseTypeReferenceEnum
 
 class ApplicationManifestFeatures(models.Model):
     case_type = models.CharField(
